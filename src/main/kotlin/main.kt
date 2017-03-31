5c9dbfc1cbf07fef083b2c84abbada07d74c7f46
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-03-31 16:39:29 +0200

    Add some basic command line parsing
    Also add package manager skeletons to activate.
    
    Change-Id: I4c983398f06d9c28657456cb0feb8fa821c66456

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,158 @@
-data class Person(val name: String, val age: Int? = null)
+import com.beust.jcommander.IStringConverter
+import com.beust.jcommander.JCommander
+import com.beust.jcommander.Parameter
 
-fun main(args: Array<String>) {
-    val persons = listOf(Person("Alice"), Person("Bob", age = 42))
-    val oldest = persons.maxBy { it.age ?: 0 }
-    println("The oldest is: $oldest")
+import java.util.ArrayList
+
+import kotlin.system.exitProcess
+
+interface PackageManager {
+    // Returns the URL to the package manager's homepage.
+    fun homepageUrl(): String
+
+    // Returns the name of the programming language this package manager is primarily used with.
+    fun primaryLanguage(): String
+
+    // Returns a prioritized list of glob patterns of definition files supported by this package manager.
+    fun pathsToDefinitionFiles(): List<String>
+}
+
+object Bower: PackageManager {
+    override fun homepageUrl(): String = "https://bower.io/"
+
+    override fun primaryLanguage(): String = "JavaScript"
+
+    override fun pathsToDefinitionFiles(): List<String> {
+        return arrayListOf("bower.json")
+    }
+}
+
+object Bundler: PackageManager {
+    override fun homepageUrl(): String = "http://bundler.io/"
+
+    override fun primaryLanguage(): String = "Ruby"
+
+    override fun pathsToDefinitionFiles(): List<String> {
+        return arrayListOf("Gemfile.lock", "Gemfile")
+    }
+}
+
+object CocoaPods: PackageManager {
+    override fun homepageUrl(): String = "https://cocoapods.org/"
+
+    override fun primaryLanguage(): String = "Objective-C"
+
+    override fun pathsToDefinitionFiles(): List<String> {
+        return arrayListOf("Podfile.lock", "Podfile")
+    }
+}
+
+object Godep: PackageManager {
+    override fun homepageUrl(): String = "https://godoc.org/github.com/tools/godep"
+
+    override fun primaryLanguage(): String = "Go"
+
+    override fun pathsToDefinitionFiles(): List<String> {
+        return arrayListOf("Godeps/Godeps.json")
+    }
+}
+
+object Gradle: PackageManager {
+    override fun homepageUrl(): String = "https://gradle.org/"
+
+    override fun primaryLanguage(): String = "Java"
+
+    override fun pathsToDefinitionFiles(): List<String> {
+        return arrayListOf("build.gradle")
+    }
+}
+
+object Maven: PackageManager {
+    override fun homepageUrl(): String = "https://maven.apache.org/"
+
+    override fun primaryLanguage(): String = "Java"
+
+    override fun pathsToDefinitionFiles(): List<String> {
+        return arrayListOf("pom.xml", "build.sbt", "build.scala")
+    }
+}
+
+object NPM: PackageManager {
+    override fun homepageUrl(): String = "https://www.npmjs.com/"
+
+    override fun primaryLanguage(): String = "JavaScript"
+
+    override fun pathsToDefinitionFiles(): List<String> {
+        return arrayListOf("package.json")
+    }
+}
+
+object PIP: PackageManager {
+    override fun homepageUrl(): String = "https://pip.pypa.io/"
+
+    override fun primaryLanguage(): String = "Python"
+
+    override fun pathsToDefinitionFiles(): List<String> {
+        return arrayListOf("setup.py", "requirements*.txt")
+    }
+}
+
+object ProvenanceAnalzyer {
+    // Prioritized list of package managers.
+    val PACKAGE_MANAGERS = arrayListOf(
+        Gradle,
+        Maven,
+        NPM,
+        CocoaPods,
+        Godep,
+        Bower,
+        PIP,
+        Bundler
+    )
+
+    class PackageManagerListConverter: IStringConverter<List<PackageManager>> {
+        override fun convert(managers: String): List<PackageManager> {
+            val activePackageManagers = ArrayList<PackageManager>()
+
+            // Map lower-cased package manager class names to their instances.
+            val packageManagerNames = packageManagers.associateBy { it.javaClass.name.toLowerCase() }
+
+            val names = managers.toLowerCase().split(",")
+            for (name in names) {
+                val manager = packageManagerNames.get(name)
+                if (manager != null) {
+                    activePackageManagers.add(manager)
+                }
+            }
+
+            return activePackageManagers
+        }
+    }
+
+    @Parameter(names = arrayOf("--package-managers", "-m"), description = "A list of package managers to activate.", listConverter = PackageManagerListConverter::class)
+    var packageManagers: List<PackageManager> = PACKAGE_MANAGERS
+
+    @Parameter(names = arrayOf("--help", "-h"), help = true)
+    var help = false
+
+    @Parameter
+    var projectPath: String? = null
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val jc = JCommander(this, *args)
+
+        if (help) {
+            jc.usage()
+            exitProcess(1)
+        }
+
+        if (projectPath == null) {
+            println("Please specify a project path.")
+            exitProcess(2)
+        }
+
+        println("The following package managers are activated:")
+        println(packageManagers.map { it.javaClass.name }.joinToString(", "))
+    }
 }
