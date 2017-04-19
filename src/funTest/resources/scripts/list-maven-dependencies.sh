70e14562e003718b15937e72110c0945602b2fad
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-04-19 11:23:47 +0200

    list-maven-dependencies: Add a comment why outputFile is not used
    Change-Id: I85f21c8d49d6107c091eb3cba8bbac94bc84366d

--- src/funTest/resources/scripts/list-maven-dependencies.sh
+++ src/funTest/resources/scripts/list-maven-dependencies.sh
@@ -7,4 +7,8 @@ fi
 
 # Make the dependency plugin's goal reactor-aware by specifying "compile" as a dummy phase in the same run, see
 # http://stackoverflow.com/a/1905927/1127485
+#
+# Note that specifying an outputFile [1] did not include all expected dependencies despite appendOutput also being specified.
+# [1] https://maven.apache.org/plugins/maven-dependency-plugin/list-mojo.html#outputFile
+# [2] https://maven.apache.org/plugins/maven-dependency-plugin/list-mojo.html#appendOutput
 mvn -B -f $1/pom.xml compile dependency:list 2>/dev/null | sed -nr "s/^\[INFO\]    (.+:.+:.+)$/\1/p" | sort -u
