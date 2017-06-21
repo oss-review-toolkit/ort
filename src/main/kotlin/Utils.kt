5bff9839dfed0790ec3da0c260364e0720992063
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-06-21 14:11:02 +0200

    Utils: Make isWindows a property variable that is only evaluated once
    As the OS does not change at runtime, it is safe to be evaluated once.
    
    Change-Id: I1063f085832080a79753a3c515456c0e2870833f

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -3,5 +3,5 @@ package com.here.provenanceanalyzer
 object OS {
     private val OS_NAME = System.getProperty("os.name").toLowerCase()
 
-    val isWindows get() = OS_NAME.contains("windows")
+    val isWindows = OS_NAME.contains("windows")
 }
