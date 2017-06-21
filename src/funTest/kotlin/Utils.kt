9262a8c2ffcf61c11e26bb43fb8f3d4b00f617cb
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-06-21 16:20:01 +0200

    readResource: Suppress the EmptyClassBlock rule
    Change-Id: I7e2164bb548f621a6f2817f3c18ee0d3d859baa9

--- src/funTest/kotlin/Utils.kt
+++ src/funTest/kotlin/Utils.kt
@@ -3,6 +3,7 @@ package com.here.provenanceanalyzer.functionaltest
 import java.io.File
 
 fun readResource(path: String): List<String> {
-    // Create anonymous class to be able to get a class reference, see: https://stackoverflow.com/a/38230890/639421
+    // Create an anonymous class to be able to get a class reference, see https://stackoverflow.com/a/38230890/639421.
+    @Suppress("EmptyClassBlock")
     return File(object {}.javaClass.getResource(path).toURI()).readLines()
 }
