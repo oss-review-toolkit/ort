839bcdb31b000e996694b69fa455a56ababa6713
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-06-26 12:56:31 +0200

    Fix parsing long JSON lines as output by yarn
    This now uses temporary files instead of pipes, like npm-deps-srcs.groovy
    already does, to work around the 64k pipe buffer limit on Linux [1].
    
    [1] https://unix.stackexchange.com/a/11954/53328
    
    Change-Id: Ifbf2f72dcdd710d3f322de378d383cc2339cb0b4

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -9,13 +9,8 @@ import com.google.gson.JsonObject
 
 import com.here.provenanceanalyzer.managers.NPM
 
-import java.io.BufferedReader
-import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.InputStream
-import java.io.InputStreamReader
 import java.io.IOException
-import java.io.OutputStream
 
 object OS {
     private val OS_NAME = System.getProperty("os.name").toLowerCase()
@@ -23,42 +18,28 @@ object OS {
     val isWindows = OS_NAME.contains("windows")
 }
 
-class ProcessCapture(directory: File, vararg command: String) {
-    private class StreamGobbler(private val stream: InputStream, private val writer: OutputStream) : Thread() {
-        override fun run() {
-            BufferedReader(InputStreamReader(stream)).use { reader ->
-                do {
-                    val value = reader.read()
-                    if (value == -1) {
-                        break
-                    }
-                    writer.write(value)
-                } while (true)
-            }
-        }
-    }
+class ProcessCapture(workingDir: File, vararg command: String) {
+    val stdoutFile = File.createTempFile(command.first(), ".stdout")!!
+    val stderrFile = File.createTempFile(command.first(), ".stderr")!!
 
-    private val builder = ProcessBuilder(*command).directory(directory)
+    private val builder = ProcessBuilder(*command)
+            .directory(workingDir)
+            .redirectOutput(stdoutFile)
+            .redirectError(stderrFile)
+
     private val process = builder.start()
-    private val stdoutStream = ByteArrayOutputStream()
-    private val stderrStream = ByteArrayOutputStream()
-    private val stdoutGobbler = StreamGobbler(process.inputStream, stdoutStream)
-    private var stderrGobbler = StreamGobbler(process.errorStream, stderrStream)
 
     init {
-        stdoutGobbler.start()
-        stderrGobbler.start()
+        stdoutFile.deleteOnExit()
+        stderrFile.deleteOnExit()
 
         process.waitFor()
-
-        stdoutGobbler.join()
-        stderrGobbler.join()
     }
 
     fun exitValue() = process.exitValue()
 
-    fun stdout() = stdoutStream.toString()
-    fun stderr() = stderrStream.toString()
+    fun stdout() = stdoutFile.readText()
+    fun stderr() = stderrFile.readText()
 }
 
 /**
@@ -74,7 +55,7 @@ fun parseJsonProcessOutput(workingDir: File, vararg command: String): JsonElemen
 
     // Wrap yarn's output, which is one JSON object per line, so the output as a whole can be parsed as a JSON array.
     if (command.first() == NPM.yarn && command.contains("--json")) {
-        return jsonArray(process.stdout().lines().map { gson.fromJson<JsonObject>(it) })
+        return jsonArray(process.stdoutFile.readLines().map { gson.fromJson<JsonObject>(it) })
     }
 
     return gson.fromJson<JsonElement>(process.stdout())
