6ca155a041f1001ffa21cea42872af146cf589e4
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-06-26 11:07:20 +0200

    NpmTest: Compare list sizes after their contents
    If both size and content mismatch, it is more obvious to fail on
    mismatched content first. Also rename variables for slightly better
    fitting names.
    
    Change-Id: Ia0c1bc7471221766db478e3a9c22dcc405122355

--- src/funTest/kotlin/synthetic/NpmTest.kt
+++ src/funTest/kotlin/synthetic/NpmTest.kt
@@ -10,27 +10,35 @@ import java.io.File
 class NpmTest : StringSpec() {
     init {
         "yarn dependencies are resolved correctly" {
-            val expectedDependencies = readResource("/projects/synthetic/project-npm-expected-yarn-dependencies.txt")
+            val expectedDependenciesList = readResource("/projects/synthetic/project-npm-expected-yarn-dependencies.txt")
+
             val resolvedDependencies = NPM.resolveYarnDependencies(
                     File("src/funTest/resources/projects/synthetic/project-npm"))
-
             val resolvedDependencyList = resolvedDependencies.toString().lines().filter { it.isNotEmpty() }
-            resolvedDependencyList.size shouldBe expectedDependencies.size
-            resolvedDependencyList.zip(expectedDependencies).forEach { (resolved, expected) ->
+
+            resolvedDependencyList.zip(expectedDependenciesList).forEach { (resolved, expected) ->
                 resolved shouldBe expected
             }
+
+            // As zip() returns a list of pairs whose length is that of the shortest collection we need to compare the
+            // sizes, too.
+            resolvedDependencyList.size shouldBe expectedDependenciesList.size
         }
 
         "NPM dependencies are resolved correctly" {
-            val expectedDependencies = readResource("/projects/synthetic/project-npm-expected-npm-dependencies.txt")
+            val expectedDependenciesList = readResource("/projects/synthetic/project-npm-expected-npm-dependencies.txt")
+
             val resolvedDependencies = NPM.resolveNpmDependencies(
                     File("src/funTest/resources/projects/synthetic/project-npm"))
-
             val resolvedDependencyList = resolvedDependencies.toString().lines().filter { it.isNotEmpty() }
-            resolvedDependencyList.size shouldBe expectedDependencies.size
-            resolvedDependencyList.zip(expectedDependencies).forEach { (resolved, expected) ->
+
+            resolvedDependencyList.zip(expectedDependenciesList).forEach { (resolved, expected) ->
                 resolved shouldBe expected
             }
+
+            // As zip() returns a list of pairs whose length is that of the shortest collection we need to compare the
+            // sizes, too.
+            resolvedDependencyList.size shouldBe expectedDependenciesList.size
         }
     }
 }
