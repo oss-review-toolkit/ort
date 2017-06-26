427898176aa7077727275e6572758334ffed7599
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2017-06-26 14:10:10 +0200

    NpmTest: Make sure node_modules is deleted after failing tests
    Change-Id: I2712147579213589778506ce5f05f498eb0b56b7

--- src/funTest/kotlin/synthetic/NpmTest.kt
+++ src/funTest/kotlin/synthetic/NpmTest.kt
@@ -2,18 +2,36 @@ package com.here.provenanceanalyzer.functionaltest
 
 import com.here.provenanceanalyzer.managers.NPM
 
+import io.kotlintest.TestCaseContext
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
 import java.io.File
 
 class NpmTest : StringSpec() {
+
+    val projectDir = File("src/funTest/resources/projects/synthetic/project-npm")
+
+    @Suppress("CatchException")
+    override fun interceptTestCase(context: TestCaseContext, test: () -> Unit) {
+        try {
+            test()
+        } catch (exception: Exception) {
+            // Make sure the node_modules directory is always deleted to prevent side-effects from failing tests.
+            val nodeModulesDir = File(projectDir, "node_modules")
+            if (nodeModulesDir.isDirectory) {
+                nodeModulesDir.deleteRecursively()
+            }
+
+            throw exception
+        }
+    }
+
     init {
         "yarn dependencies are resolved correctly" {
             val expectedDependenciesList = readResource("/projects/synthetic/project-npm-expected-yarn-dependencies.txt")
 
-            val resolvedDependencies = NPM.resolveYarnDependencies(
-                    File("src/funTest/resources/projects/synthetic/project-npm"))
+            val resolvedDependencies = NPM.resolveYarnDependencies(projectDir)
             val resolvedDependencyList = resolvedDependencies.toString().lines().filter { it.isNotEmpty() }
 
             resolvedDependencyList.zip(expectedDependenciesList).forEach { (resolved, expected) ->
@@ -28,8 +46,7 @@ class NpmTest : StringSpec() {
         "NPM dependencies are resolved correctly" {
             val expectedDependenciesList = readResource("/projects/synthetic/project-npm-expected-npm-dependencies.txt")
 
-            val resolvedDependencies = NPM.resolveNpmDependencies(
-                    File("src/funTest/resources/projects/synthetic/project-npm"))
+            val resolvedDependencies = NPM.resolveNpmDependencies(projectDir)
             val resolvedDependencyList = resolvedDependencies.toString().lines().filter { it.isNotEmpty() }
 
             resolvedDependencyList.zip(expectedDependenciesList).forEach { (resolved, expected) ->
