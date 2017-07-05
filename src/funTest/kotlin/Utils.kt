ea1a033897d9458c6f6980a2c61b5f5319d0364a
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-07-05 13:10:40 +0200

    Utils: Use an internal object instead of an anonymous one
    This does not trigger detekt's "EmptyClassBlock", and in fact this is
    the approach that detekt itself uses in its source code.
    
    Change-Id: Ie671c71d13364c728ed4afd87832769c200e0e11

--- src/funTest/kotlin/Utils.kt
+++ src/funTest/kotlin/Utils.kt
@@ -2,6 +2,8 @@ package com.here.provenanceanalyzer.functionaltest
 
 import java.io.File
 
+internal object Resources
+
 /**
  * A helper function to return the contents of a resource text file as a list of strings.
  *
@@ -9,7 +11,5 @@ import java.io.File
  * @return The text as a list of strings.
  */
 fun readResource(path: String): List<String> {
-    // Create an anonymous class to be able to get a class reference, see https://stackoverflow.com/a/38230890/639421.
-    @Suppress("EmptyClassBlock")
-    return File(object {}.javaClass.getResource(path).toURI()).readLines()
+    return File(Resources.javaClass.getResource(path).toURI()).readLines()
 }
