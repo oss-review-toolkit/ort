82890bced5ef0991a1e3832cc062e9f2bb03caf4
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-07-14 15:34:55 +0200

    Normalize dependency repository URLs
    Change-Id: Ie9026fccedd4c0dacfe0fb9297349b07e8722e81

--- src/main/kotlin/model/Dependency.kt
+++ src/main/kotlin/model/Dependency.kt
@@ -1,6 +1,9 @@
 package com.here.provenanceanalyzer.model
 
 import com.vdurmont.semver4j.Semver
+import com.vdurmont.semver4j.Semver.SemverType
+
+import java.net.URI
 
 data class Dependency(
         val group: String? = null,
@@ -8,15 +11,54 @@ data class Dependency(
         val version: Semver,
         val scope: String,
         val dependencies: List<Dependency> = listOf(),
-        val scm: String? = null
+        private val scm: String? = null
 ) {
+    val normalizedScm = scm?.let { normalizeRepositoryUrl(it) }
+
+    private fun normalizeRepositoryUrl(url: String): String {
+        // A hierarchical URI looks like
+        //     [scheme:][//authority][path][?query][#fragment]
+        // where a server-based "authority" has the syntax
+        //     [user-info@]host[:port]
+        val uri = URI(url)
+
+        if (version.type == SemverType.NPM) {
+            // https://docs.npmjs.com/files/package.json#repository
+            val path = uri.schemeSpecificPart
+            if (path != null) {
+                if (uri.authority == null && uri.query == null && uri.fragment == null) {
+                    // Handle shortcut URLs.
+                    when (uri.scheme) {
+                        null -> return "https://github.com/$path.git"
+                        "gist" -> return "https://gist.github.com/$path"
+                        "bitbucket" -> return "https://bitbucket.org/$path.git"
+                        "gitlab" -> return "https://gitlab.com/$path.git"
+                    }
+                }
+            }
+        }
+
+        if (uri.host.endsWith("github.com")) {
+            // Ensure the path ends in ".git".
+            val path = if (uri.path.endsWith(".git")) uri.path else uri.path + ".git"
+
+            // Remove any user name and "www" prefix.
+            val host = uri.authority.substringAfter("@").removePrefix("www.")
+
+            return "https://" + host + path
+        }
+
+        // Return the URL unmodified.
+        return url
+    }
+
     override fun toString(): String {
         return toString("")
     }
 
     private fun toString(indent: String): String {
         return buildString {
-            append("$indent$group:$artifact:${version.value}:$scope:$scm")
+            append("$indent$group:$artifact:${version.value}:$scope:$normalizedScm")
             append(System.lineSeparator())
             dependencies.forEach { dependency ->
                 append(dependency.toString("$indent  "))
