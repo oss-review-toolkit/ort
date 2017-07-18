59863d876cd6c85eeb82509321ff018b053d664c
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-07-18 17:32:54 +0200

    Add a simple test that shows how to test command line arguments
    Change-Id: I0c41b966da6900d794722ab28ced1e03a895a107

---
+++ src/funTest/kotlin/MainTest.kt
@@ -0,0 +1,32 @@
+package com.here.provenanceanalyzer.functionaltest
+
+import com.here.provenanceanalyzer.Main
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+/**
+ * A test for the main entry point of the application.
+ */
+class MainTest : StringSpec() {
+    init {
+        "Activating only NPM works" {
+            // Redirect standard output to a stream.
+            val standardOut = System.out
+            val streamOut = ByteArrayOutputStream()
+            System.setOut(PrintStream(streamOut))
+
+            Main.main(arrayOf("-m", "NPM", "src/funTest/assets/projects/synthetic/project-npm"))
+
+            // Restore standard output.
+            System.setOut(standardOut)
+            val lines = streamOut.toString().lines()
+
+            lines.component1() shouldBe "The following package managers are activated:"
+            lines.component2().trim() shouldBe "NPM"
+        }
+    }
+}
