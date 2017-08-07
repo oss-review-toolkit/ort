2fd639c9a473a927b914ccc3c1e4bd5379fc75a9
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2017-08-07 12:01:00 +0200

    Add a simple tool to visualize dependency graphs
    The tool can visualize the dependency tree from the output file generated
    by provenance-analyzer using the GraphStream [1] library.
    
    [1] http://graphstream-project.org/
    
    Change-Id: Ia1b827a8471fe40585fd88aec0e65bcea577d088

---
+++ graph/src/main/kotlin/Main.kt
@@ -0,0 +1,113 @@
+package com.here.provenanceanalyzer.graph
+
+import com.beust.jcommander.JCommander
+import com.beust.jcommander.Parameter
+import com.here.provenanceanalyzer.model.Dependency
+
+import com.here.provenanceanalyzer.model.OutputFormat
+import com.here.provenanceanalyzer.model.ScanResult
+import com.here.provenanceanalyzer.model.jsonMapper
+import com.here.provenanceanalyzer.model.yamlMapper
+import org.graphstream.graph.Edge
+import org.graphstream.graph.implementations.SingleGraph
+import org.graphstream.graph.implementations.SingleNode
+import org.graphstream.ui.layout.Eades84Layout
+import org.graphstream.ui.layout.HierarchicalLayout
+import org.graphstream.ui.layout.springbox.implementations.LinLog
+import org.graphstream.ui.layout.springbox.implementations.SpringBox
+
+import java.io.File
+import java.util.*
+
+import kotlin.system.exitProcess
+
+/**
+ * The main entry point of the application.
+ */
+object Main {
+
+    @Parameter(description = "provenance data file path")
+    private var provenanceFilePath: String? = null
+
+    @Parameter(names = arrayOf("--help", "-h"),
+            description = "Display the command line help.",
+            help = true,
+            order = 100)
+    private var help = false
+
+    /**
+     * The entry point for the application.
+     *
+     * @param args The list of application arguments.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val jc = JCommander(this)
+        jc.parse(*args)
+        jc.programName = "pran"
+
+        if (help || provenanceFilePath == null) {
+            jc.usage()
+            exitProcess(1)
+        }
+
+        var provenanceFile = File(provenanceFilePath)
+        require(provenanceFile.isFile) {
+            "Provided path is not a file: ${provenanceFile.absolutePath}"
+        }
+
+        val mapper = when {
+            provenanceFile.name.endsWith(OutputFormat.JSON.fileEnding) -> jsonMapper
+            provenanceFile.name.endsWith(OutputFormat.YAML.fileEnding) -> yamlMapper
+            else -> throw IllegalArgumentException("Provided input file is neither JSON or YAML.")
+        }
+
+        val scanResult = mapper.readValue(provenanceFile, ScanResult::class.java)
+
+        showGraph(scanResult)
+    }
+
+    private fun showGraph(scanResult: ScanResult) {
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer")
+
+        val project = scanResult.project
+
+        val graph = SingleGraph(project.name)
+
+        graph.setStrict(true)
+        graph.setAutoCreate(true)
+        graph.addAttribute("ui.antialias")
+        graph.addAttribute("ui.quality")
+
+        val rootNode = graph.addNode<SingleNode>(project.name)
+        rootNode.addAttribute("ui.label", project.name)
+
+        project.scopes.forEach { scope ->
+            println("Adding scope ${scope.name}.")
+            val scopeNode = graph.addNode<SingleNode>(scope.name)
+            scopeNode.addAttribute("ui.label", scope.name)
+            graph.addEdge<Edge>(scope.name, rootNode, scopeNode)
+
+            scope.dependencies.forEach { dependency ->
+                addDependency(scopeNode, dependency)
+            }
+        }
+
+        val viewer = graph.display(false)
+        val layout = SpringBox()
+        viewer.enableAutoLayout(layout)
+
+    }
+
+    private fun addDependency(parent: SingleNode, dependency: Dependency) {
+        val identifier = "${dependency.namespace}:${dependency.name}:${dependency.version}"
+        val dependencyNode = parent.graph.addNode<SingleNode>(UUID.randomUUID().toString())
+        dependencyNode.addAttribute("ui.label", identifier)
+        parent.graph.addEdge<Edge>(UUID.randomUUID().toString(), parent, dependencyNode)
+
+        dependency.dependencies.forEach {
+            addDependency(dependencyNode, it)
+        }
+    }
+
+}
