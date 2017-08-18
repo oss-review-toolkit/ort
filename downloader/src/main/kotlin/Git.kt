436ece6192e17c8c936f414acb2aab6d634db5bf
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2017-08-18 12:32:16 +0200

    Downloader: Add very basic implementation of Git
    The implementation clones the vcsRevision if available or master.
    
    Change-Id: I01634fefc8d24d2edb16886c9cbf83e79bb1802d

--- downloader/src/main/kotlin/Git.kt
+++ downloader/src/main/kotlin/Git.kt
@@ -5,9 +5,24 @@ import java.io.File
 object Git : VersionControlSystem() {
 
     override fun download(vcsUrl: String, vcsRevision: String?, targetDir: File): Boolean {
-        return false
+        runGitCommand(targetDir, "init")
+        runGitCommand(targetDir, "remote", "add", "origin", vcsUrl)
+        val committish = if (vcsRevision.isNullOrEmpty()) "master" else vcsRevision!!
+        runGitCommand(targetDir, "fetch", "origin", committish)
+        runGitCommand(targetDir, "reset", "--hard", "FETCH_HEAD")
+        return true
     }
 
     override fun isApplicable(vcsUrl: String) = vcsUrl.endsWith(".git")
+
+    private fun runGitCommand(targetDir: File, vararg args: String) {
+        val builder = ProcessBuilder("git", *args)
+                .directory(targetDir)
+        val process = builder.start()
+        process.waitFor()
+        require(process.exitValue() == 0) {
+            "Git command ${args.joinToString(" ")} failed: ${process.errorStream.bufferedReader().use { it.readText() }}"
+        }
+    }
 
 }
