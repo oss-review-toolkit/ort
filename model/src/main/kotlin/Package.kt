cfcc91cd373b29dfda60c007225ef31055068ce9
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2017-09-11 15:22:01 +0200

    Model: Document the Package class
    Change-Id: I253fda597b694823071754216e1476a2452565d6

--- model/src/main/kotlin/Package.kt
+++ model/src/main/kotlin/Package.kt
@@ -5,32 +5,90 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 import com.vdurmont.semver4j.Semver
 
+/**
+ * A descriptor for a software package. It contains information about naming, version, and how to retrieve the package
+ * and its source code.
+ */
 @JsonIgnoreProperties("identifier", "normalizedVcsUrl", "semverType")
 data class Package(
+        /**
+         * Name of the package manager that was used to discover this dependency, for example Maven or NPM.
+         */
         @JsonProperty("package_manager")
-        val packageManager: String, // e.g. "Maven", "NPM", ...
-        val namespace: String, // e.g. group in Maven ("org.junit"), scope in NPM ("@types")
-        val name: String, // e.g. "junit"
+        val packageManager: String,
+
+        /**
+         * The namespace of the package, for example the group id in Maven or the scope in NPM.
+         */
+        val namespace: String,
+
+        /**
+         * The name of the package.
+         */
+        val name: String,
+
+        /**
+         * The description of the package, as provided by the package manager.
+         */
         val description: String,
-        val version: String, // concrete version, e.g. 1.2.3
+
+        /**
+         * The version of the package.
+         */
+        val version: String,
+
+        /**
+         * The homepage of the package.
+         */
         @JsonProperty("homepage_url")
         val homepageUrl: String?,
+
+        /**
+         * The optional URL to the binary artifact of the package.
+         */
         @JsonProperty("download_url")
         val downloadUrl: String?,
-        val hash: String, // e.g. sha1, sha512, ... of download
+
+        /**
+         * The optional hash value of the binary artifact of the package.
+         */
+        val hash: String,
+
+        /**
+         * The optional name of the VCS provider, for example Git or SVN.
+         */
         @JsonProperty("vcs_provider")
         val vcsProvider: String?,
+
+        /**
+         * The optional URL to the VCS repository.
+         */
         @JsonProperty("vcs_url")
         val vcsUrl: String?,
+
+        /**
+         * The optional revision of the VCS that this [version] of the package was built from.
+         */
         @JsonProperty("vcs_revision")
         val vcsRevision: String?
 ) {
+    /**
+     * The unique identifier for this package, created from [packageManager], [namespace], [name], and [version].
+     */
     val identifier = "$packageManager:$namespace:$name:$version"
 
+    /**
+     * The [Semver.SemverType] used for the [version] of this package.
+     */
     val semverType = when (packageManager) {
         "NPM" -> Semver.SemverType.NPM
         else -> Semver.SemverType.STRICT
     }
 
+    /**
+     * The normalized VCS URL.
+     *
+     * @see normalizeVcsUrl
+     */
     val normalizedVcsUrl = if (vcsUrl == null) null else normalizeVcsUrl(vcsUrl, semverType)
 }
