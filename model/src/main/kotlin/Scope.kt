23a39dbd54eda158dc71ab7ff734ed5be6306c73
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-09-25 16:45:53 +0200

    model: Document the Scope class
    Change-Id: Id35aecc4907b8ce686a0fbec648e2a686694430b

--- model/src/main/kotlin/Scope.kt
+++ model/src/main/kotlin/Scope.kt
@@ -1,7 +1,28 @@
 package com.here.provenanceanalyzer.model
 
+/**
+ * The scope class puts package dependencies into context.
+ */
 data class Scope(
+        /**
+         * The respective package manager's native name for the scope, e.g. "compile", " provided" etc. for Maven, or
+         * "dependencies", "devDependencies" etc. for NPM.
+         */
         val name: String,
+
+        /**
+         * A flag to indicate whether this scope is delivered along with the product, i.e. distributed to external
+         * parties.
+         */
         val delivered: Boolean,
+
+        /**
+         * The list of references to packages in this scope. Note that only the first-order packages in this list
+         * actually belong to the scope of [name]. Transitive dependency packages usually belong to the scope that
+         * describes the packages required to compile the product. As an example, if this was the Maven "test" scope,
+         * all first-order items in [dependencies] would be packages required for testing the product. But transitive
+         * dependencies would not be test dependencies of the test dependencies, but compile dependencies of test
+         * dependencies.
+         */
         val dependencies: List<PackageReference>
 )
