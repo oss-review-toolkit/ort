46bfb82eaf3b09ae35467a4921b7c3f4f0ff6e21
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-09-28 15:39:44 +0200

    OutputFormat: Add all missing docs
    Change-Id: I0f304179f24a0c0dc109c78dbfbc8a33e45fb6d9

--- model/src/main/kotlin/OutputFormat.kt
+++ model/src/main/kotlin/OutputFormat.kt
@@ -1,6 +1,16 @@
 package com.here.provenanceanalyzer.model
 
+/**
+ * An enumeration of supported output file formats.
+ */
 enum class OutputFormat(val fileEnding: String) {
+    /**
+     * Specifies the [JSON](http://www.json.org/) format.
+     */
     JSON("json"),
+
+    /**
+     * Specifies the [YAML](http://yaml.org/) format.
+     */
     YAML("yml")
 }
