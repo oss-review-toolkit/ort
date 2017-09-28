2943e9d8b13dd7aa5ba7f0fb1b689d14efcd945a
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-09-28 15:25:06 +0200

    ScanResult: Add all missing docs
    Change-Id: I65e002367c5c3b9c98db26ca5592ac4a89e8c508

--- model/src/main/kotlin/ScanResult.kt
+++ model/src/main/kotlin/ScanResult.kt
@@ -1,6 +1,17 @@
 package com.here.provenanceanalyzer.model
 
+/**
+ * A class that bundles all information generated during a scan.
+ */
 data class ScanResult(
+        /**
+         * The project that was scanned. The tree of dependencies is implicitly contained in the scopes in the form
+         * of package references.
+         */
         val project: Project,
+
+        /**
+         * The list of identified packages used by the project.
+         */
         val packages: List<Package>
 )
