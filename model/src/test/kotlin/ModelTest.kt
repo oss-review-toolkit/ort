87ce074a188371bb554680104b274cf9acf0d22d
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2017-09-29 11:52:00 +0200

    Move tests for normalizeVcsUrl to model module
    Keep some basic tests to verify that the normalizedVcsUrl does acutally
    normalize the vcsUrl, but move the thorough testing of the algorithm to a
    separate test class as it saves a lot of boilerplate.
    
    Also move the PackageTest class to the model module.
    
    Change-Id: I6d6b7e9eb7c0cb183c81c1d5fe9ac7d90d051552

---
+++ model/src/test/kotlin/ModelTest.kt
@@ -0,0 +1,56 @@
+package com.here.provenanceanalyzer.model
+
+import com.vdurmont.semver4j.Semver
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.WordSpec
+
+class ModelTest : WordSpec({
+    "normalizeVcsUrl" should {
+        "properly handle NPM shortcut URLs" {
+            val packages = mapOf(
+                    "npm/npm"
+                            to "https://github.com/npm/npm.git",
+                    "gist:11081aaa281"
+                            to "https://gist.github.com/11081aaa281",
+                    "bitbucket:example/repo"
+                            to "https://bitbucket.org/example/repo.git",
+                    "gitlab:another/repo"
+                            to "https://gitlab.com/another/repo.git"
+            )
+
+            packages.forEach { actualUrl, expectedUrl ->
+                normalizeVcsUrl(actualUrl, Semver.SemverType.NPM) shouldBe expectedUrl
+            }
+        }
+
+        "convert non-https to anonymous https for GitHub URLs" {
+            val packages = mapOf(
+                    "git://github.com/cheeriojs/cheerio.git"
+                            to "https://github.com/cheeriojs/cheerio.git",
+                    "git+https://github.com/fb55/boolbase.git"
+                            to "https://github.com/fb55/boolbase.git",
+                    "git+ssh://git@github.com/logicalparadox/idris.git"
+                            to "https://github.com/logicalparadox/idris.git",
+                    "https://www.github.com/DefinitelyTyped/DefinitelyTyped.git"
+                            to "https://github.com/DefinitelyTyped/DefinitelyTyped.git"
+            )
+
+            packages.forEach { actualUrl, expectedUrl ->
+                normalizeVcsUrl(actualUrl, Semver.SemverType.STRICT) shouldBe expectedUrl
+            }
+        }
+
+        "add missing .git for GitHub URLs" {
+            val packages = mapOf(
+                    "https://github.com/fb55/nth-check"
+                            to "https://github.com/fb55/nth-check.git",
+                    "git://github.com/isaacs/inherits"
+                            to "https://github.com/isaacs/inherits.git"
+            )
+
+            packages.forEach { actualUrl, expectedUrl ->
+                normalizeVcsUrl(actualUrl, Semver.SemverType.STRICT) shouldBe expectedUrl
+            }
+        }
+    }
+})
---
+++ model/src/test/kotlin/PackageTest.kt
@@ -0,0 +1,25 @@
+package com.here.provenanceanalyzer.model
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.WordSpec
+
+/**
+ * Test cases for [Package].
+ */
+class PackageTest : WordSpec({
+    "normalizedVcsUrl" should {
+        "actually be normalized" {
+            val pkg = Package("", "", "", "", "1.0.0", "", "", "", "", "", "", "https://github.com/fb55/nth-check", "")
+            val expectedUrl = "https://github.com/fb55/nth-check.git"
+
+            pkg.normalizedVcsUrl shouldBe expectedUrl
+        }
+
+        "should understand NPM shortcuts for NPM packages" {
+            val pkg = Package("NPM", "", "", "", "1.0.0", "", "", "", "", "", "", "npm/npm", "")
+            val expectedUrl = "https://github.com/npm/npm.git"
+
+            pkg.normalizedVcsUrl shouldBe expectedUrl
+        }
+    }
+})
