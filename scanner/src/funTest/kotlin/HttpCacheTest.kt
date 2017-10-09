4db7246c116c19faf57b98fe6be8de513bc9dab5
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-10-09 16:28:47 +0200

    scanner: Add a functional test for caching HTTP responses
    This is a very basic test to uncover issues like the one fixed by
    8fc8878.
    
    Change-Id: Ibdb0f4280c4410b9e00ca9c135138903ea023540

---
+++ scanner/src/funTest/kotlin/HttpCacheTest.kt
@@ -0,0 +1,95 @@
+package com.here.provenanceanalyzer.scanner.functionaltest
+
+import com.here.provenanceanalyzer.model.Package
+import com.here.provenanceanalyzer.scanner.ArtifactoryCache
+
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpServer
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldEqual
+import io.kotlintest.specs.StringSpec
+import io.kotlintest.TestCaseContext
+
+import java.net.HttpURLConnection
+import java.net.InetAddress
+import java.net.InetSocketAddress
+
+class HttpCacheTest : StringSpec() {
+    private val loopback = InetAddress.getLoopbackAddress()
+    private val port = 8888
+
+    private class MyHttpHandler : HttpHandler {
+        val requests = mutableMapOf<String, String>()
+
+        override fun handle(exchange: HttpExchange) {
+            when (exchange.requestMethod) {
+                "PUT" -> {
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_CREATED, 0)
+                    requests[exchange.requestURI.toString()] = exchange.requestBody.reader().use { it.readText() }
+                }
+                "GET" -> {
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0)
+                    exchange.responseBody.writer().use { it.write(requests[exchange.requestURI.toString()]) }
+                }
+            }
+        }
+    }
+
+    override fun interceptTestCase(context: TestCaseContext, test: () -> Unit) {
+        val server = HttpServer.create(InetSocketAddress(loopback, port), 0)
+
+        try {
+            // Start the local HTTP server.
+            server.createContext("/", MyHttpHandler())
+            server.start()
+
+            test()
+        } finally {
+            // Ensure the server is properly stopped even in case of exceptions.
+            server.stop(0)
+        }
+    }
+
+    init {
+        "HTTP GET returns what was PUT" {
+            val cache = ArtifactoryCache("http://${loopback.hostAddress}:$port", "apiToken")
+
+            val pkg = Package(
+                    "packageManager",
+                    "namespace",
+                    "name",
+                    "version",
+                    "description",
+                    "homepageUrl",
+                    "downloadUrl",
+                    "hash",
+                    "hashAlgorithm",
+                    "vcsPath",
+                    "vcsProvider",
+                    "vcsUrl",
+                    "vcsRevision"
+            )
+
+            val resultFile = createTempFile()
+            val resultContent = "magic"
+
+            // Put the file contents into the cache.
+            resultFile.writeText(resultContent)
+            cache.write(pkg, resultFile) shouldBe true
+
+            // Delete the original result file to ensure it gets re-created.
+            resultFile.delete() shouldBe true
+
+            // Get the file contents from the cache.
+            cache.read(pkg, resultFile) shouldBe true
+            resultFile.readText() shouldEqual resultContent
+
+            // Clean up.
+            // TODO: Change this after https://github.com/kittinunf/Fuel/pull/255 has been merged.
+            resultFile.deleteOnExit()
+            //resultFile.delete() shouldBe true
+        }
+    }
+}
