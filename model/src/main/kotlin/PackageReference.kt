cc79098e14689efe6bbcaa2e17eb50af14755fba
Sebastian Schuberth <sebastian.schuberth@here.com>
2017-10-09 14:44:25 +0200

    PackageReference: Further clean-ups after the removal of the packageHash
    This is a fixup for d90c5df.
    
    Change-Id: I910a703dcb8b9d2763333b24659d695925fee5b2

--- model/src/main/kotlin/PackageReference.kt
+++ model/src/main/kotlin/PackageReference.kt
@@ -1,15 +1,12 @@
 package com.here.provenanceanalyzer.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
 
 import java.util.SortedSet
 
 /**
- * A human-readable reference to a software package. Strictly speaking, a [packageHash] would be enough to uniquely
- * refer to a package in a machine readable form. However, to make the serialized reference more readable for human
- * beings, also add the name, namespace and version properties of the referred package. A package reference can itself
- * refer to other packages that happen to be dependencies of this package.
+ * A human-readable reference to a software [Package]. Each package reference itself refers to other package
+ * references that are dependencies of the package.
  */
 @JsonIgnoreProperties("identifier")
 data class PackageReference(
