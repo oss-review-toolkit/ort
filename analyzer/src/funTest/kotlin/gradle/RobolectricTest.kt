6738ba35d8c2faadbb0aa9ce29e4668efd1faf3c
Piotr Nowakowski <piotr.nowakowski@meelogic.com>
2017-11-15 10:08:32 +0100

    Add analyzer functional tests for external projects
    Add analyzer tests for external projects: Robolectric, K9Mail.
    Verify analyzer results with expected results for all analyzer outputs.
    Mark new tests with kotlintest tag: Expensive.

---
+++ analyzer/src/funTest/kotlin/gradle/RobolectricTest.kt
@@ -0,0 +1,27 @@
+package com.here.ort.analyzer.integration
+
+import com.here.ort.model.Package
+
+class RobolectricTest : BaseGradleSpec() {
+
+    override val pkg = Package(
+            packageManager = "Gradle",
+            namespace = "org.robolectric",
+            name = "robolectric",
+            version = "3.3.2",
+            declaredLicenses = sortedSetOf(),
+            description = "",
+            homepageUrl = "",
+            downloadUrl = "",
+            hash = "",
+            hashAlgorithm = "",
+            vcsProvider = "Git",
+            vcsUrl = "git@github.com:robolectric/robolectric.git",
+            vcsRevision = "757dfd56499a415376ea04bfa520b317bf2e3b58",
+            vcsPath = ""
+    )
+
+
+    override val expectedResultsDir = "src/funTest/assets/projects/synthetic/gradle-expected-results/robolectric/"
+
+}
