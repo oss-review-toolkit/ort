65a157e1ef3daf4ce7f66f0e992de63a8bd8c626
nowakow.pio <piotr.nowakowski@meelogic.com>
2017-11-15 15:15:44 +0100

    RobolectricTest: robolectric int tests file
    Test if every gradle file in project has corresponding ABCD analyzer
    result file.

---
+++ analyzer/src/intTest/kotlin/RobolectricTest.kt
@@ -0,0 +1,54 @@
+package com.here.ort.analyzer
+
+import com.here.ort.analyzer.managers.Gradle
+import com.here.ort.model.Package
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+import java.io.File
+import com.here.ort.downloader.Main as DownloaderMain
+import com.here.ort.analyzer.Main as AnalyzerMain
+
+class RobolectricTest : StringSpec() {
+
+    private val outputDir = createTempDir()
+
+    fun after() {
+        outputDir.deleteRecursively()
+    }
+
+    val robolectricPackage = Package(
+            packageManager = "Gradle",
+            namespace = "org.robolectric",
+            name = "robolectric",
+            version = "3.3.2",
+            declaredLicenses = sortedSetOf(),
+            description = "",
+            homepageUrl = "",
+            downloadUrl = "",
+            vcsPath = "",
+            vcsProvider = "Git",
+            vcsUrl = "git@github.com:robolectric/robolectric.git",
+            hashAlgorithm = "",
+            hash = "",
+            vcsRevision = "")
+
+    init {
+        "analyzer produces ABCD files for all robolectric build.gradle files" {
+            val downloadedDir = DownloaderMain.download(robolectricPackage, outputDir)
+            val analyzerResultsDir = File(outputDir, "analyzer_results");
+            AnalyzerMain.main(arrayOf(
+                    "-i", downloadedDir.absolutePath,
+                    "-o", analyzerResultsDir.absolutePath
+            ))
+
+            val sourceGradleProjectFiles = downloadedDir.walkTopDown().asIterable().filter { file: File ->
+                Gradle.matchersForDefinitionFiles.any() { glob ->
+                    glob.matches(file.toPath())
+                }
+            }
+
+            analyzerResultsDir.walkTopDown().asIterable().filter { it.extension == "yml" }.count() shouldBe sourceGradleProjectFiles.size
+            after()
+        }
+    }
+}
