1b4d8c04cee4dc12adc178de43b11584c5579458
piotr.nowakowski <piotr.nowakowski@meelogic.com>
2017-11-27 13:09:58 +0100

    Gradle tests: change expected results files locations
    Store expected analyzer results files location in shallower directory
    hierarchy, than corresponding build files are stored in source (due to
    Windows system limitations).

--- analyzer/src/funTest/kotlin/gradle/GradleWrapperSubprojectTest.kt
+++ analyzer/src/funTest/kotlin/gradle/GradleWrapperSubprojectTest.kt
@@ -1,6 +1,7 @@
 package com.here.ort.analyzer.integration
 
 import com.here.ort.model.Package
+import java.io.File
 
 class GradleWrapperSubprojectTest : BaseGradleSpec() {
     override val pkg = Package(
@@ -20,5 +21,10 @@ class GradleWrapperSubprojectTest : BaseGradleSpec() {
             vcsPath = "subprojects/wrapper/"
             )
 
-    override val expectedResultsDir = "src/funTest/assets/projects/synthetic/gradle-expected-results/gradle/"
+    override val expectedResultsDir = "src/funTest/assets/projects/external/gradle-submodules"
+
+    override val expectedResultsDirsMap = mapOf(
+            "${expectedResultsDir}/subprojects/wrapper/wrapper-gradle-dependencies.yml"
+                    to File("src/funTest/assets/projects/external/gradle-submodules/wrapper/expected-wrapper-gradle-dependencies.yml")
+    )
 }
