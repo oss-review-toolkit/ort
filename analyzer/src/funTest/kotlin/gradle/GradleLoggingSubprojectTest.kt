e8a7b046b6687c5e679930046dc83d5333625ad8
piotr.nowakowski <piotr.nowakowski@meelogic.com>
2017-11-27 13:09:58 +0100

    Gradle tests: change expected results files locations
    Store expected analyzer results files location in shallower directory
    hierarchy, than corresponding build files are stored in source (due to
    Windows system limitations).

--- analyzer/src/funTest/kotlin/gradle/GradleLoggingSubprojectTest.kt
+++ analyzer/src/funTest/kotlin/gradle/GradleLoggingSubprojectTest.kt
@@ -1,6 +1,7 @@
 package com.here.ort.analyzer.integration
 
 import com.here.ort.model.Package
+import java.io.File
 
 class GradleLoggingSubprojectTest : BaseGradleSpec() {
     override val pkg = Package(
@@ -18,7 +19,24 @@ class GradleLoggingSubprojectTest : BaseGradleSpec() {
             hash = "",
             vcsRevision = "",
             vcsPath = "subprojects/logging/"
-            )
+    )
 
-    override val expectedResultsDir = "src/funTest/assets/projects/synthetic/gradle-expected-results/gradle/"
+    override val expectedResultsDir = "src/funTest/assets/projects/external/gradle-submodules"
+
+    override val expectedResultsDirsMap = mapOf(
+            "${expectedResultsDir}/subprojects/logging/src/integTest/resources/org/gradle/internal/logging/LoggingIntegrationTest/logging/build-gradle-dependencies.yml"
+                    to File("src/funTest/assets/projects/external/gradle-submodules/logging/expected-logging-integration-test-dependencies.yml"),
+            "${expectedResultsDir}/subprojects/logging/src/integTest/resources/org/gradle/internal/logging/LoggingIntegrationTest/logging/buildSrc/build-gradle-dependencies.yml"
+                    to File("src/funTest/assets/projects/external/gradle-submodules/logging/expected-logging-integration-test-build-src-dependencies.yml"),
+            "${expectedResultsDir}/subprojects/logging/src/integTest/resources/org/gradle/internal/logging/LoggingIntegrationTest/logging/nestedBuild/build-gradle-dependencies.yml"
+                    to File("src/funTest/assets/projects/external/gradle-submodules/logging/expected-logging-integration-test-nested-build-dependencies.yml "),
+            "${expectedResultsDir}/subprojects/logging/src/integTest/resources/org/gradle/internal/logging/LoggingIntegrationTest/logging/nestedBuild/buildSrc/build-gradle-dependencies.yml"
+                    to File("src/funTest/assets/projects/external/gradle-submodules/logging/expected-logging-integration-test-nested-build-build-src-dependencies.yml"),
+            "${expectedResultsDir}/subprojects/logging/src/integTest/resources/org/gradle/internal/logging/LoggingIntegrationTest/logging/project1/build-gradle-dependencies.yml"
+                    to File("src/funTest/assets/projects/external/gradle-submodules/logging/expected-logging-integration-test-project1-dependencies.yml"),
+            "${expectedResultsDir}/subprojects/logging/src/integTest/resources/org/gradle/internal/logging/LoggingIntegrationTest/logging/project2/build-gradle-dependencies.yml"
+                    to File("src/funTest/assets/projects/external/gradle-submodules/logging/expected-logging-integration-test-project2-dependencies.yml"),
+            "${expectedResultsDir}/subprojects/logging/src/integTest/resources/org/gradle/internal/logging/LoggingIntegrationTest/multiThreaded/build-gradle-dependencies.yml"
+                    to File("src/funTest/assets/projects/external/gradle-submodules/logging/expected-logging-integration-test-multi-threaded-dependencies.yml")
+    )
 }
