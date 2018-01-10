63cab251b578ebfb1cc3ee27d9ab2c354767169f
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-01-10 16:18:50 +0100

    MercurialTest: Rework tests to be more complete and stricter
    Compare the full list of files instead of indices of individual files. Add
    tests for downloading based on a version.

--- downloader/src/funTest/kotlin/MercurialTest.kt
+++ downloader/src/funTest/kotlin/MercurialTest.kt
@@ -23,19 +23,17 @@ import com.here.ort.model.VcsInfo
 import com.here.ort.utils.Expensive
 
 import io.kotlintest.TestCaseContext
-import io.kotlintest.matchers.beGreaterThan
-import io.kotlintest.matchers.should
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
 import java.io.File
 
 private const val REPO_URL = "https://bitbucket.org/creaceed/mercurial-xcode-plugin"
-private const val REPO_REV = "02098fc8bdac"
-private const val REPO_SUBDIR = "Classes"
-private const val REPO_SUBDIR_OMITTED = "Resources"
+private const val REPO_REV = "02098fc8bdaca4739ec52cbcb8ed51654eacee25"
+private const val REPO_PATH = "Classes"
 private const val REPO_VERSION = "1.1"
-private const val REPO_VERSION_REV = "562fed42b4f3"
+private const val REPO_REV_FOR_VERSION = "562fed42b4f3dceaacf6f1051963c865c0241e28"
+private const val REPO_PATH_FOR_VERSION = "Resources"
 
 class MercurialTest : StringSpec() {
     private lateinit var outputDir: File
@@ -53,27 +51,83 @@ class MercurialTest : StringSpec() {
     }
 
     init {
-        "Mercurial can download entire repo" {
-            Mercurial.download(VcsInfo("Mercurial", REPO_URL, "", ""), "", outputDir)
-            Mercurial.getWorkingTree(outputDir).getProvider() shouldBe "Mercurial"
-        }.config(tags = setOf(Expensive))
+        "Mercurial can download a given revision" {
+            val vcs = VcsInfo("Mercurial", REPO_URL, REPO_REV, "")
 
-        "Mercurial can download single revision" {
-            val workingTree = Mercurial.download(VcsInfo("Mercurial", REPO_URL, REPO_REV, ""), "", outputDir)
+            val workingTree = Mercurial.download(vcs, "", outputDir)
             workingTree.getRevision() shouldBe REPO_REV
+
+            val expectedFiles = listOf(
+                    ".hg",
+                    ".hgsub",
+                    ".hgsubstate",
+                    "Classes",
+                    "LICENCE.md",
+                    "MercurialPlugin.xcodeproj",
+                    "README.md",
+                    "Resources",
+                    "Script"
+            )
+            val actualFiles = workingTree.workingDir.list()
+            actualFiles.joinToString("\n") shouldBe expectedFiles.joinToString("\n")
         }.config(tags = setOf(Expensive))
 
-        "Mercurial can download sub path" {
-            Mercurial.download(VcsInfo("Mercurial", REPO_URL, "", REPO_SUBDIR), "", outputDir)
+        "Mercurial can download only a single path" {
+            val vcs = VcsInfo("Mercurial", REPO_URL, REPO_REV, REPO_PATH)
 
-            val outputDirList = Mercurial.getWorkingTree(outputDir).workingDir.list()
-            outputDirList.indexOf(REPO_SUBDIR) should beGreaterThan(-1)
-            outputDirList.indexOf(REPO_SUBDIR_OMITTED) shouldBe -1
-        }.config(tags = setOf(Expensive), enabled = Mercurial.isAtLeastVersion("4.3"))
+            val workingTree = Mercurial.download(vcs, "", outputDir)
+            workingTree.getRevision() shouldBe REPO_REV
 
-        "Mercurial can download version" {
-            val workingTree = Mercurial.download(VcsInfo("Mercurial", REPO_URL, "", ""), REPO_VERSION, outputDir)
-            workingTree.getRevision() shouldBe REPO_VERSION_REV
+            val expectedFiles = listOf(
+                    File(".hgsub"), // We always get these configuration files, if present.
+                    File(".hgsubstate"),
+                    File(REPO_PATH, "MercurialPlugin.h"),
+                    File(REPO_PATH, "MercurialPlugin.m"),
+                    File("Script", "git.py"), // As a submodule, "Script" is always included.
+                    File("Script", "gpl-2.0.txt"),
+                    File("Script", "install_bridge.sh"),
+                    File("Script", "README"),
+                    File("Script", "sniff.py"),
+                    File("Script", "uninstall_bridge.sh")
+            )
+            val actualFiles = workingTree.workingDir.walkBottomUp()
+                    .onEnter { it.name != ".hg" }
+                    .filter { it.isFile }
+                    .map { it.relativeTo(outputDir) }
+            actualFiles.joinToString("\n") shouldBe expectedFiles.joinToString("\n")
+        }.config(tags = setOf(Expensive))
+
+        "Mercurial can download based on a version" {
+            val vcs = VcsInfo("Mercurial", REPO_URL, "", "")
+
+            val workingTree = Mercurial.download(vcs, REPO_VERSION, outputDir)
+            workingTree.getRevision() shouldBe REPO_REV_FOR_VERSION
+        }.config(tags = setOf(Expensive))
+
+        "Mercurial can download only a single path based on a version" {
+            val vcs = VcsInfo("Mercurial", REPO_URL, "", REPO_PATH_FOR_VERSION)
+
+            val workingTree = Mercurial.download(vcs, REPO_VERSION, outputDir)
+            workingTree.getRevision() shouldBe REPO_REV_FOR_VERSION
+
+            val expectedFiles = listOf(
+                    File(".hgsub"), // We always get these configuration files, if present.
+                    File(".hgsubstate"),
+                    File(REPO_PATH_FOR_VERSION, "icon.icns"),
+                    File(REPO_PATH_FOR_VERSION, "icon_blank.icns"),
+                    File(REPO_PATH_FOR_VERSION, "Info.plist"),
+                    File("Script", "git.py"), // As a submodule, "Script" is always included.
+                    File("Script", "gpl-2.0.txt"),
+                    File("Script", "install_bridge.sh"),
+                    File("Script", "README"),
+                    File("Script", "sniff.py"),
+                    File("Script", "uninstall_bridge.sh")
+            )
+            val actualFiles = workingTree.workingDir.walkBottomUp()
+                    .onEnter { it.name != ".hg" }
+                    .filter { it.isFile }
+                    .map { it.relativeTo(outputDir) }
+            actualFiles.joinToString("\n") shouldBe expectedFiles.joinToString("\n")
         }.config(tags = setOf(Expensive))
     }
 }
