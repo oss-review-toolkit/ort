af27ebb4984aebf73e867be0873146a5ce50e051
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-01-11 12:58:07 +0100

    DownloaderTest: Remove VCS-related tests
    These are now covered by the reworked VCS-specific functional tests.

--- downloader/src/funTest/kotlin/DownloaderTest.kt
+++ downloader/src/funTest/kotlin/DownloaderTest.kt
@@ -19,16 +19,12 @@
 
 package com.here.ort.downloader
 
-import com.here.ort.downloader.vcs.Git
-import com.here.ort.downloader.vcs.Mercurial
 import com.here.ort.model.Package
 import com.here.ort.model.RemoteArtifact
 import com.here.ort.model.VcsInfo
 import com.here.ort.utils.Expensive
 
 import io.kotlintest.TestCaseContext
-import io.kotlintest.matchers.beGreaterThan
-import io.kotlintest.matchers.should
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.matchers.shouldThrow
 import io.kotlintest.specs.StringSpec
@@ -51,74 +47,6 @@ class DownloaderTest : StringSpec() {
     }
 
     init {
-        "Downloads subpath of package at specified revision from git" {
-            val submoduleName = "model"
-            val revision = "4d842ea6eda2d1a21db161ceb7ceabbcc23d8a85"
-            val pkg = Package(
-                    packageManager = "Gradle",
-                    namespace = "",
-                    name = "project :model",
-                    version = "",
-                    declaredLicenses = sortedSetOf(),
-                    description = "",
-                    homepageUrl = "",
-                    binaryArtifact = RemoteArtifact.EMPTY,
-                    sourceArtifact = RemoteArtifact.EMPTY,
-                    vcs = VcsInfo("Git", "https://github.com/heremaps/oss-review-toolkit.git", revision, submoduleName)
-            )
-
-            val downloadDir = Main.download(pkg, outputDir)
-            val workingTree = Git.getWorkingTree(downloadDir)
-            val outputDirList = workingTree.workingDir.list()
-
-            workingTree.isValid() shouldBe true
-            workingTree.getRevision() shouldBe revision
-            outputDirList.indexOf(submoduleName) should beGreaterThan(-1)
-            outputDirList.indexOf("downloader") shouldBe -1
-        }.config(tags = setOf(Expensive))
-
-        "Downloads whole package from git" {
-            val pkg = Package(
-                    packageManager = "Gradle",
-                    namespace = "",
-                    name = "oss-review-toolkit",
-                    version = "",
-                    declaredLicenses = sortedSetOf(),
-                    description = "",
-                    homepageUrl = "",
-                    binaryArtifact = RemoteArtifact.EMPTY,
-                    sourceArtifact = RemoteArtifact.EMPTY,
-                    vcs = VcsInfo("Git", "https://github.com/heremaps/oss-review-toolkit.git", "", "")
-            )
-
-            val downloadDir = Main.download(pkg, outputDir)
-            val workingTree = Git.getWorkingTree(downloadDir)
-
-            workingTree.isValid() shouldBe true
-        }.config(tags = setOf(Expensive))
-
-        "Downloads revision for package version from mercurial" {
-            val version = "1.1"
-            val revisionForVersion = "562fed42b4f3"
-            val pkg = Package(
-                    packageManager = "",
-                    namespace = "",
-                    name = "mercurial-xcode-plugin",
-                    version = version,
-                    declaredLicenses = sortedSetOf(),
-                    description = "",
-                    homepageUrl = "",
-                    binaryArtifact = RemoteArtifact.EMPTY,
-                    sourceArtifact = RemoteArtifact.EMPTY,
-                    vcs = VcsInfo("Mercurial", "https://bitbucket.org/creaceed/mercurial-xcode-plugin", "", "")
-            )
-
-            val downloadDir = Main.download(pkg, outputDir)
-            val workingTree = Mercurial.getWorkingTree(downloadDir)
-
-            workingTree.getRevision() shouldBe revisionForVersion
-        }.config(tags = setOf(Expensive))
-
         "Downloads and unpacks JAR source package" {
             val pkg = Package(
                     packageManager = "Gradle",
