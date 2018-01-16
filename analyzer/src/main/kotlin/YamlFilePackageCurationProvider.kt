d67f934574187cf80c22b3effb9a48d58e568b64
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-01-16 13:19:01 +0100

    analyzer: Add a provider for package curations from a YAML file
    Add a provider for package curations that reads all curations from a single
    YAML file.

---
+++ analyzer/src/main/kotlin/YamlFilePackageCurationProvider.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.analyzer
+
+import com.here.ort.model.Identifier
+import com.here.ort.model.PackageCuration
+import com.here.ort.utils.yamlMapper
+
+import java.io.File
+
+/**
+ * A [PackageCurationProvider] that loads [PackageCuration]s from a single YAML file.
+ */
+class YamlFilePackageCurationProvider(
+        curationFile: File
+) : PackageCurationProvider {
+    internal val packageCurations: List<PackageCuration> by lazy {
+        val valueType = yamlMapper.typeFactory.constructCollectionType(List::class.java, PackageCuration::class.java)
+        yamlMapper.readValue(curationFile, valueType) as List<PackageCuration>
+    }
+
+    override fun getCurationsFor(identifier: Identifier) = packageCurations.filter { it.id.matches(identifier) }
+}
