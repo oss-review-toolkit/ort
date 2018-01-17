78faa9a69a307ac6f83fbd71e6434ab5555592ad
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-01-17 14:58:50 +0100

    downloader: Add a functional test to download Babel from VCS

---
+++ downloader/src/funTest/kotlin/BabelTest.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.downloader
+
+import com.here.ort.model.Identifier
+import com.here.ort.model.Package
+import com.here.ort.model.RemoteArtifact
+import com.here.ort.model.VcsInfo
+import com.here.ort.utils.normalizeVcsUrl
+import com.here.ort.utils.safeDeleteRecursively
+
+import io.kotlintest.TestCaseContext
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldNotBe
+import io.kotlintest.specs.StringSpec
+
+import java.io.File
+
+class BabelTest : StringSpec() {
+    private lateinit var outputDir: File
+
+    // Required to make lateinit of outputDir work.
+    override val oneInstancePerTest = false
+
+    override fun interceptTestCase(context: TestCaseContext, test: () -> Unit) {
+        outputDir = createTempDir()
+        try {
+            super.interceptTestCase(context, test)
+        } finally {
+            outputDir.safeDeleteRecursively()
+        }
+    }
+
+    init {
+        "Babel packages should be correctly downloaded" {
+            val vcsFromPackage = VcsInfo(
+                    provider = "git",
+                    url = "https://github.com/babel/babel/tree/master/packages/babel-cli",
+                    revision = "",
+                    path = ""
+            )
+            val vcsFromUrl = VersionControlSystem.splitUrl(normalizeVcsUrl(vcsFromPackage.url))
+            val vcsMerged = vcsFromUrl.merge(vcsFromPackage)
+
+            val pkg = Package(
+                    id = Identifier(
+                            packageManager = "NPM",
+                            namespace = "",
+                            name = "babel-cli",
+                            version = "6.26.0"
+                    ),
+                    declaredLicenses = sortedSetOf("MIT"),
+                    description = "Babel command line.",
+                    homepageUrl = "https://babeljs.io/",
+                    binaryArtifact = RemoteArtifact(
+                            url = "https://registry.npmjs.org/babel-cli/-/babel-cli-6.26.0.tgz",
+                            hash = "502ab54874d7db88ad00b887a06383ce03d002f1",
+                            hashAlgorithm = ""
+                    ),
+                    sourceArtifact = RemoteArtifact.EMPTY,
+                    vcs = vcsFromPackage,
+                    vcsProcessed = vcsMerged
+            )
+
+            val downloadDir = Main.download(pkg, outputDir)
+            val workingTree = VersionControlSystem.forDirectory(downloadDir)
+
+            workingTree shouldNotBe null
+            workingTree!!.isValid() shouldBe true
+            workingTree.getRevision() shouldBe "cee4cde53e4f452d89229986b9368ecdb41e00da"
+
+            val babelCliDir = File(downloadDir, "packages/babel-cli")
+            babelCliDir.isDirectory shouldBe true
+            babelCliDir.walkTopDown().count() shouldBe 242
+        }
+    }
+}
