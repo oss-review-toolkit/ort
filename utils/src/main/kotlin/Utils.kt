47a3c5056040cc21e1e087437797d6cf01a17249
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-01-30 14:56:32 +0100

    Revert "normalizeVcsUrl: Simplify the regex to fixup scp-like URLs a bit"
    This reverts commit b741dd3 as it breaks SSH URLs with ports. Add a test
    on top of the revert.

--- utils/src/main/kotlin/Utils.kt
+++ utils/src/main/kotlin/Utils.kt
@@ -131,7 +131,7 @@ fun normalizeVcsUrl(vcsUrl: String): String {
     // URLs to Git repos may omit the scheme and use an scp-like URL that uses ":" to separate the host from the path,
     // see https://git-scm.com/docs/git-clone#_git_urls_a_id_urls_a. Make this an explicit ssh URL so it can be parsed
     // by Java's URI class.
-    url = url.replace(Regex("^(.*)(\\w+):(\\w+)(.*)$")) {
+    url = url.replace(Regex("^(.*)([a-zA-Z]+):([a-zA-Z]+)(.*)$")) {
         val tail = "${it.groupValues[1]}${it.groupValues[2]}/${it.groupValues[3]}${it.groupValues[4]}"
         if (url.contains("://")) tail else "ssh://" + tail
     }
