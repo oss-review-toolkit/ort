970c9822745682dfc355029a40b94aabedf25d2d
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-01-30 09:21:07 +0100

    downloader: Add CVS support
    Resolves #112.

---
+++ downloader/src/funTest/kotlin/CvsDownloadTest.kt
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.downloader.vcs
+
+import com.here.ort.model.VcsInfo
+import com.here.ort.utils.ExpensiveTag
+import com.here.ort.utils.safeDeleteRecursively
+
+import io.kotlintest.TestCaseContext
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+import java.io.File
+
+private const val REPO_URL = ":pserver:anonymous@xmlenc.cvs.sourceforge.net:/cvsroot/xmlenc"
+private const val REPO_REV = "RELEASE_0_52"
+private const val REPO_PATH = "xmlenc/src/history"
+private const val REPO_VERSION = "0.52"
+private const val REPO_PATH_FOR_VERSION = "xmlenc/build.xml"
+
+class CvsDownloadTest : StringSpec() {
+    private lateinit var outputDir: File
+
+    // Required to make lateinit of outputDir work.
+    override val oneInstancePerTest = false
+
+    override fun interceptTestCase(context: TestCaseContext, test: () -> Unit) {
+        outputDir = createTempDir()
+        try {
+            super.interceptTestCase(context, test)
+        } finally {
+            outputDir.safeDeleteRecursively()
+        }
+    }
+
+    init {
+        "CVS can download a given revision" {
+            val vcs = VcsInfo("CVS", REPO_URL, REPO_REV, "")
+            val expectedFiles = listOf(
+                    "CVS",
+                    "xmlenc"
+            )
+
+            val workingTree = Cvs.download(vcs, "", outputDir)
+            val actualFiles = workingTree.workingDir.list().sorted()
+
+            // Use forward slashes also on Windows as the CVS client comes from MSYS2.
+            val buildXmlFile = "xmlenc/build.xml"
+            val buildXmlStatus = Cvs.runCvsCommand(outputDir, "status", buildXmlFile)
+
+            workingTree.isValid() shouldBe true
+            actualFiles.joinToString("\n") shouldBe expectedFiles.joinToString("\n")
+
+            // Only tag "RELEASE_0_52" has revision 1.159 of "xmlenc/build.xml".
+            buildXmlStatus.stdout().contains("Working revision:\t1.159") shouldBe true
+        }.config(tags = setOf(ExpensiveTag))
+
+        "CVS can download only a single path" {
+            val vcs = VcsInfo("CVS", REPO_URL, REPO_REV, REPO_PATH)
+            val expectedFiles = listOf(
+                    File(REPO_PATH, "changes.xml")
+            )
+
+            val workingTree = Cvs.download(vcs, "", outputDir)
+            val actualFiles = workingTree.workingDir.walkBottomUp()
+                    .onEnter { it.name != "CVS" }
+                    .filter { it.isFile }
+                    .map { it.relativeTo(outputDir) }
+                    .sortedBy { it.path }
+
+            workingTree.isValid() shouldBe true
+            actualFiles.joinToString("\n") shouldBe expectedFiles.joinToString("\n")
+        }.config(tags = setOf(ExpensiveTag))
+
+        "CVS can download based on a version" {
+            val vcs = VcsInfo("CVS", REPO_URL, "", "")
+
+            val workingTree = Cvs.download(vcs, REPO_VERSION, outputDir)
+
+            // Use forward slashes also on Windows as the CVS client comes from MSYS2.
+            val buildXmlFile = "xmlenc/build.xml"
+            val buildXmlStatus = Cvs.runCvsCommand(outputDir, "status", buildXmlFile)
+
+            workingTree.isValid() shouldBe true
+
+            // Only tag "RELEASE_0_52" has revision 1.159 of "xmlenc/build.xml".
+            buildXmlStatus.stdout().contains("Working revision:\t1.159") shouldBe true
+        }.config(tags = setOf(ExpensiveTag))
+
+        "CVS can download only a single path based on a version" {
+            val vcs = VcsInfo("CVS", REPO_URL, "", REPO_PATH_FOR_VERSION)
+            val expectedFiles = listOf(
+                    File(REPO_PATH_FOR_VERSION)
+            )
+
+            val workingTree = Cvs.download(vcs, REPO_VERSION, outputDir)
+            val actualFiles = workingTree.workingDir.walkBottomUp()
+                    .onEnter { it.name != "CVS" }
+                    .filter { it.isFile }
+                    .map { it.relativeTo(outputDir) }
+                    .sortedBy { it.path }
+
+            workingTree.isValid() shouldBe true
+            actualFiles.joinToString("\n") shouldBe expectedFiles.joinToString("\n")
+        }.config(tags = setOf(ExpensiveTag))
+    }
+}
---
+++ downloader/src/main/kotlin/vcs/Cvs.kt
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.downloader.vcs
+
+import ch.frankel.slf4k.*
+
+import com.here.ort.downloader.VersionControlSystem
+import com.here.ort.model.VcsInfo
+import com.here.ort.utils.ProcessCapture
+import com.here.ort.utils.getCommandVersion
+import com.here.ort.utils.log
+import com.here.ort.utils.safeDeleteRecursively
+
+import org.apache.commons.codec.binary.Hex
+import org.apache.commons.codec.digest.DigestUtils
+
+import java.io.File
+import java.io.IOException
+import java.util.regex.Pattern
+
+typealias CvsFileRevisions = List<Pair<String, String>>
+
+object Cvs : VersionControlSystem() {
+    override val movingRevisionNames = emptyList<String>()
+
+    override fun getVersion(): String {
+        val versionRegex = Pattern.compile("Concurrent Versions System \\(CVS\\) (?<version>[\\d.]+).+")
+
+        return getCommandVersion("cvs") {
+            versionRegex.matcher(it.lineSequence().first()).let {
+                if (it.matches()) {
+                    it.group("version")
+                } else {
+                    ""
+                }
+            }
+        }
+    }
+
+    override fun getWorkingTree(vcsDirectory: File) =
+            object : WorkingTree(vcsDirectory) {
+                private val cvsDirectory = File(workingDir, "CVS")
+
+                override fun isValid(): Boolean {
+                    if (!workingDir.isDirectory) {
+                        return false
+                    }
+
+                    return ProcessCapture(workingDir, "cvs", "status", "-l").exitValue() == 0
+                }
+
+                override fun isShallow() = false
+
+                override fun getRemoteUrl() = File(cvsDirectory, "Root").useLines { it.first() }
+
+                override fun getRevision() =
+                        // CVS does not have the concept of a global revision, but each file has its own revision. As
+                        // we just use the revision to uniquely identify the state of a working tree, simply create an
+                        // artificial single revision based on the revisions of all files.
+                        getFileRevisionsHash(getFileRevisions())
+
+                private fun getFileRevisions(): CvsFileRevisions {
+                    val cvsLog = runCvsCommand(workingDir, "log", "-h")
+
+                    var currentWorkingFile = ""
+                    return cvsLog.stdout().lines().mapNotNull { line ->
+                        var value = line.removePrefix("Working file: ")
+                        if (value.length < line.length) {
+                            currentWorkingFile = value
+                        } else {
+                            value = line.removePrefix("head: ")
+                            if (value.length < line.length) {
+                                if (currentWorkingFile.isNotBlank() && value.isNotBlank()) {
+                                    return@mapNotNull Pair(currentWorkingFile, value)
+                                }
+                            }
+                        }
+
+                        null
+                    }.sortedBy { it.first }
+                }
+
+                private fun getFileRevisionsHash(fileRevisions: CvsFileRevisions): String {
+                    val digest = fileRevisions.fold(DigestUtils.getSha1Digest()) { digest, pair ->
+                        DigestUtils.updateDigest(digest, pair.first)
+                        DigestUtils.updateDigest(digest, pair.second)
+                    }.digest()
+
+                    return Hex.encodeHexString(digest)
+                }
+
+                override fun getRootPath(): String {
+                    // Walk the path up towards the root until we find a CVS directory.
+                    var parentDir = workingDir.toPath()
+
+                    val rootDir = workingDir.toPath().asIterable().reversed().map {
+                        parentDir = parentDir.parent
+                        parentDir.resolve(it)
+                    }.find {
+                        it.resolve("CVS").toFile().isDirectory
+                    }?.toString() ?: ""
+
+                    return rootDir.replace(File.separatorChar, '/')
+                }
+
+                override fun listRemoteTags(): List<String> {
+                    val cvsLog = runCvsCommand(workingDir, "log", "-h")
+                    var tagsSectionStarted = false
+
+                    val tagsList = cvsLog.stdout().lines().mapNotNull {
+                        if (tagsSectionStarted) {
+                            if (it.startsWith('\t')) {
+                                it.split(':').map { it.trim() }.firstOrNull()
+                            } else {
+                                tagsSectionStarted = false
+                                null
+                            }
+                        } else {
+                            if (it == "symbolic names:") {
+                                tagsSectionStarted = true
+                            }
+                            null
+                        }
+                    }
+
+                    return tagsList.toSortedSet().toList()
+                }
+            }
+
+    override fun isApplicableProvider(vcsProvider: String) = vcsProvider.equals("cvs", true)
+
+    override fun isApplicableUrl(vcsUrl: String) = vcsUrl.matches("^:(ext|pserver):[^@]+@.+$".toRegex())
+
+    override fun download(vcs: VcsInfo, version: String, targetDir: File, allowMovingRevisions: Boolean): WorkingTree {
+        log.info { "Using $this version ${getVersion()}." }
+
+        val path = if (vcs.path.isBlank()) "." else vcs.path
+
+        // Create a "fake" checkout as described at https://stackoverflow.com/a/3448891/1127485.
+        runCvsCommand(targetDir, "-z3", "-d", vcs.url, "checkout", "-l", ".")
+        val workingTree = getWorkingTree(targetDir)
+
+        val revision = if (allowMovingRevisions || isFixedRevision(vcs.revision)) {
+            vcs.revision
+        } else {
+            // Create all working tree directories in order to be able to query the log.
+            runCvsCommand(targetDir, "update", "-d")
+
+            log.info { "Trying to guess a $this revision for version '$version'." }
+
+            workingTree.guessRevisionNameForVersion(version).also {
+                if (it.isBlank()) {
+                    throw IOException("Unable to determine a revision to checkout.")
+                }
+
+                log.warn {
+                    "Using guessed $this revision '$it' for version '$version'. This might cause the " +
+                            "downloaded source code to not match the package version."
+                }
+
+                // Clean the temporarily updated working tree again.
+                targetDir.listFiles().forEach {
+                    if (it.isDirectory) {
+                        if (it.name != "CVS") it.safeDeleteRecursively()
+                    } else {
+                        it.delete()
+                    }
+                }
+            }
+        }
+
+        // Checkout the working tree of the desired revision.
+        runCvsCommand(targetDir, "checkout", "-r", revision, path)
+
+        return workingTree
+    }
+
+    fun runCvsCommand(workingDir: File, vararg args: String) =
+            ProcessCapture(workingDir, "cvs", *args).requireSuccess()
+}
