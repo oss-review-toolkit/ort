2c87c3795db2af0485ccd8f0ce2dc0bc39301c2a
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-02-09 15:10:03 +0100

    ScanPathTest: Make each scanner wrapper scan its own wrapper code

--- scanner/src/funTest/kotlin/ScanPathTest.kt
+++ scanner/src/funTest/kotlin/ScanPathTest.kt
@@ -48,12 +48,12 @@ class ScanPathTest : StringSpec() {
 
     init {
         "Licensee recognizes our own LICENSE" {
-            val result = Licensee.scan(File("../LICENSE"), outputDir)
+            val result = Licensee.scan(File("src/main/kotlin/scanners/Licensee.kt"), outputDir)
             result.licenses shouldBe setOf("Apache License 2.0")
         }.config(tags = setOf(ExpensiveTag))
 
         "ScanCode recognizes our own LICENSE" {
-            val result = ScanCode.scan(File("../LICENSE"), outputDir)
+            val result = ScanCode.scan(File("src/main/kotlin/scanners/ScanCode.kt"), outputDir)
             result.licenses shouldBe setOf("Apache-2.0")
         }.config(tags = setOf(ExpensiveTag, ScanCodeTag))
     }
