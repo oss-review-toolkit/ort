32c8f85f0c4ed83f55f335f957ef3218e742e51a
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-03-06 12:05:17 +0100

    analyzer: Add a test for AnalyzerResult

---
+++ model/src/test/kotlin/AnalyzerResultTest.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model
+
+import io.kotlintest.matchers.should
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class AnalyzerResultTest : StringSpec({
+    "collectErrors" should {
+        "find all errors" {
+            val result = AnalyzerResult(
+                    allowDynamicVersions = true,
+                    project = Project(
+                            id = Identifier("provider", "namespace", "name", "version"),
+                            declaredLicenses = sortedSetOf(),
+                            aliases = listOf(),
+                            vcs = VcsInfo.EMPTY,
+                            vcsProcessed = VcsInfo.EMPTY,
+                            homepageUrl = "",
+                            scopes = sortedSetOf(
+                                    Scope(
+                                            name = "scope 1",
+                                            delivered = true,
+                                            dependencies = sortedSetOf(
+                                                    PackageReference(
+                                                            namespace = "namespace1",
+                                                            name = "name1",
+                                                            version = "version1",
+                                                            dependencies = sortedSetOf(
+                                                                    PackageReference(
+                                                                            namespace = "namespace2",
+                                                                            name = "name2",
+                                                                            version = "version2",
+                                                                            dependencies = sortedSetOf(),
+                                                                            errors = listOf("2.1", "2.2")
+                                                                    )
+                                                            ),
+                                                            errors = listOf("1.1", "1.2")
+                                                    ),
+                                                    PackageReference(
+                                                            namespace = "namespace3",
+                                                            name = "name3",
+                                                            version = "version3",
+                                                            dependencies = sortedSetOf(),
+                                                            errors = listOf("3.1", "3.2")
+                                                    )
+                                            )
+                                    )
+                            )
+                    ),
+                    packages = sortedSetOf(),
+                    errors = listOf("a", "b")
+            )
+
+            val errors = result.collectErrors()
+            errors.size shouldBe 4
+            errors["provider:namespace:name:version"] shouldBe listOf("a", "b")
+            errors["provider:namespace1:name1:version1"] shouldBe listOf("1.1", "1.2")
+            errors["provider:namespace2:name2:version2"] shouldBe listOf("2.1", "2.2")
+            errors["provider:namespace3:name3:version3"] shouldBe listOf("3.1", "3.2")
+        }
+    }
+})
