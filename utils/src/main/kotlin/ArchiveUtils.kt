d17e19c0a3ded3e50301f0946ce2ff5364e256a3
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-03-21 13:00:10 +0100

    ArchiveUtils: Add a function to zip a file

--- utils/src/main/kotlin/ArchiveUtils.kt
+++ utils/src/main/kotlin/ArchiveUtils.kt
@@ -21,9 +21,17 @@ package com.here.ort.utils
 
 import java.io.File
 import java.io.IOException
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+import java.util.zip.Deflater
 
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
 
@@ -98,5 +106,32 @@ fun File.unpackZip(targetDirectory: File) {
                 it.copyTo(output)
             }
         }
+    }
+}
+
+/**
+ * Pack the file into a zip file. If it is a directory its content is recursively added to the zip file. The compression
+ * level used is [Deflater.BEST_COMPRESSION].
+ *
+ * @param targetFile The target zip file, must not exist.
+ * @param prefix A prefix to add to the file names in the zip file.
+ */
+fun File.packZip(targetFile: File, prefix: String = "") {
+    require(!targetFile.exists()) {
+        "The target zip file '${targetFile.absolutePath}' must not exist."
+    }
+
+    ZipArchiveOutputStream(targetFile).use {
+        it.setLevel(Deflater.BEST_COMPRESSION)
+        Files.walkFileTree(this.toPath(), object : SimpleFileVisitor<Path>() {
+            override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                val entry = ZipArchiveEntry(file.toFile(), "$prefix${this@packZip.toPath().relativize(file)}")
+                it.putArchiveEntry(entry)
+                file.toFile().inputStream().copyTo(it)
+                it.closeArchiveEntry()
+
+                return FileVisitResult.CONTINUE
+            }
+        })
     }
 }
