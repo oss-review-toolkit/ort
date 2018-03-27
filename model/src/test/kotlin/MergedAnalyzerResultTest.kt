627c08c0faf061961321b80213cdd26e0e01e4b9
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-03-27 23:18:38 +0200

    MergedAnalyzerResultTest: Add set of test data
    Add a set of constant test data that can be reused in different tests. This
    reduces the code in tests and simplifies the assertion of results.

--- model/src/test/kotlin/MergedAnalyzerResultTest.kt
+++ model/src/test/kotlin/MergedAnalyzerResultTest.kt
@@ -23,75 +23,50 @@ import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.WordSpec
 
 class MergedAnalyzerResultTest : WordSpec() {
+
+    val directoryDetails = ScannedDirectoryDetails("name", "/absolute/path", VcsInfo.EMPTY)
+
+    val package1 = Package.EMPTY.copy(id = Identifier("provider-1", "namespace-1", "package-1", "1.0"))
+    val package2 = Package.EMPTY.copy(id = Identifier("provider-2", "namespace-2", "package-2", "2.0"))
+    val package3 = Package.EMPTY.copy(id = Identifier("provider-3", "namespace-3", "package-3", "3.0"))
+
+    val pkgRef1 = package1.toReference()
+    val pkgRef2 = package2.toReference(sortedSetOf(package3.toReference()))
+
+    val scope1 = Scope("scope-1", true, sortedSetOf(pkgRef1))
+    val scope2 = Scope("scope-2", true, sortedSetOf(pkgRef2))
+
+    val project1 = Project.EMPTY.copy(
+            id = Identifier("provider-1", "namespace-1", "project-1", "1.0"),
+            scopes = sortedSetOf(scope1)
+    )
+    val project2 = Project.EMPTY.copy(
+            id = Identifier("provider-2", "namespace-2", "project-2", "2.0"),
+            scopes = sortedSetOf(scope1, scope2)
+    )
+
+    val analyzerResult1 = AnalyzerResult(true, project1, sortedSetOf(package1), listOf("error-1", "error-2"))
+    val analyzerResult2 = AnalyzerResult(true, project2, sortedSetOf(package1, package2, package3), listOf("error-2"))
+
     init {
         "MergedResultsBuilder" should {
             "merge results from all files" {
-                val builder = MergedResultsBuilder(true,
-                        ScannedDirectoryDetails("name", "/absolute/path", VcsInfo.EMPTY))
-                val project1 = createTestProject(1)
-                val project2 = createTestProject(2)
+                val builder = MergedResultsBuilder(true, directoryDetails)
 
-                builder.addResult("/analyzer-results/project-1.yml",
-                        AnalyzerResult(
-                                true,
-                                project1,
-                                sortedSetOf(createTestPackage(1)),
-                                emptyList()
-                        )
-                )
-                builder.addResult("/analyzer-results/project-2.yml",
-                        AnalyzerResult(
-                                true,
-                                project2,
-                                sortedSetOf(createTestPackage(2), createTestPackage(1)),
-                                listOf("Some error that occurred.")
-                        )
-                )
+                builder.addResult("/analyzer-result-1.yml", analyzerResult1)
+                builder.addResult("/analyzer-result-2.yml", analyzerResult2)
 
                 val mergedResults = builder.build()
 
-                mergedResults.errors.size shouldBe 2
-                mergedResults.errors[project1.id]?.size shouldBe 0
-                mergedResults.errors[project2.id]?.size shouldBe 1
-                mergedResults.projectResultsFiles.size shouldBe 2
-                mergedResults.packages.size shouldBe 2
-                mergedResults.projects.size shouldBe 2
+                mergedResults.allowDynamicVersions shouldBe true
+                mergedResults.repository shouldBe directoryDetails
+                mergedResults.projects shouldBe sortedSetOf(project1, project2)
+                mergedResults.projectResultsFiles shouldBe
+                        sortedMapOf(project1.id to "/analyzer-result-1.yml", project2.id to "/analyzer-result-2.yml")
+                mergedResults.packages shouldBe sortedSetOf(package1, package2, package3)
+                mergedResults.errors shouldBe
+                        sortedMapOf(project1.id to analyzerResult1.errors, project2.id to analyzerResult2.errors)
             }
         }
     }
-
-    private fun createTestPackage(i: Int) = Package(
-            id = Identifier(
-                    provider = "provider-$i",
-                    namespace = "namespace-$i",
-                    name = "package-$i",
-                    version = "$i"
-            ),
-            declaredLicenses = sortedSetOf(),
-            description = "",
-            homepageUrl = "",
-            binaryArtifact = RemoteArtifact.EMPTY,
-            sourceArtifact = RemoteArtifact.EMPTY,
-            vcs = VcsInfo.EMPTY
-    )
-
-    private fun createTestProject(i: Int) = Project(
-            id = Identifier(
-                    provider = "provider-$i",
-                    namespace = "namespace-$i",
-                    name = "project-$i",
-                    version = "$i"),
-            declaredLicenses = sortedSetOf("license-$i"),
-            aliases = emptyList(),
-            vcs = VcsInfo.EMPTY,
-            vcsProcessed = VcsInfo.EMPTY,
-            homepageUrl = "",
-            scopes = sortedSetOf(
-                    Scope(
-                            "scope-$i",
-                            true,
-                            sortedSetOf(createTestPackage(i).toReference())
-                    )
-            )
-    )
 }
