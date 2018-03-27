ce0f7fbe2557aa901e87e206e0399a91f9733703
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-03-27 15:47:28 +0200

    Identifier: Always serialize as a String
    The Identifier is now always serialized as a String, so configure the
    serialization of the Identifier in the class itself instead of in classes
    using the Identifier.

--- model/src/main/kotlin/PackageReference.kt
+++ model/src/main/kotlin/PackageReference.kt
@@ -20,8 +20,6 @@
 package com.here.ort.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
 
 import java.util.SortedSet
 
@@ -34,8 +32,6 @@ data class PackageReference(
         /**
          * The identifier of the package.
          */
-        @JsonDeserialize(using = IdentifierFromStringDeserializer::class)
-        @JsonSerialize(using = IdentifierToStringSerializer::class)
         val id: Identifier,
 
         /**
