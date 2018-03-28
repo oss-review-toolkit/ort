1444da8d2d9ccb692cb15ef5cefff1c72614b48e
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-03-28 09:53:18 +0200

    model: Rename ScannedDirectoryDetails to Repository
    The old name was misleading because the class has nothing to do with the
    scanner, instead it represents a source code repository.

--- model/src/main/kotlin/MergedAnalyzerResult.kt
+++ model/src/main/kotlin/MergedAnalyzerResult.kt
@@ -43,7 +43,7 @@ data class MergedAnalyzerResult(
         /**
          * Description of the scanned repository, with VCS information if available.
          */
-        val repository: ScannedDirectoryDetails,
+        val repository: Repository,
 
         /**
          * Sorted set of the projects, as they appear in the individual analyzer results.
@@ -79,7 +79,7 @@ data class MergedAnalyzerResult(
 
 class MergedResultsBuilder(
         private val allowDynamicVersions: Boolean,
-        private val directoryDetails: ScannedDirectoryDetails
+        private val directoryDetails: Repository
 ) {
     private val projects = sortedSetOf<Project>()
     private val projectResultsFiles = sortedMapOf<Identifier, String>()
@@ -98,7 +98,7 @@ class MergedResultsBuilder(
     }
 }
 
-data class ScannedDirectoryDetails(
+data class Repository(
         /**
          * Name of top level input directory
          */
