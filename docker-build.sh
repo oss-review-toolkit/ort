921ae4fb046f138b7315c2ce8e33ea86ba9f63b8
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-04-20 11:11:03 +0200

    Remove Docker related files as they have their own repository now
    See https://github.com/heremaps/oss-review-toolkit-docker.

--- docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-DOCKER_IMAGE="oss-review-toolkit:latest"
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-"$SCRIPT_DIR/gradlew" distTar
-docker build -t "$DOCKER_IMAGE" "$SCRIPT_DIR"
