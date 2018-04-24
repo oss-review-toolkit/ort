eb8bca4ce8f1ad27d5a05fab3e109b25ed238809
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-04-24 14:34:10 +0200

    model: Add the class CuratedPackage
    Add the class CuratedPackage that contains the Package after applying
    curations plus the list of PackageCurationResults to be able to trace back
    how the original data was modified.

---
+++ model/src/main/kotlin/CuratedPackage.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model
+
+/**
+ * A [Package] plus the [PackageCurationResult]s to be able to trace how the raw metadata of the package was modified
+ * by applying [PackageCuration]s.
+ */
+data class CuratedPackage(
+        val pkg: Package,
+        val curations: List<PackageCurationResult>
+) : Comparable<CuratedPackage> {
+    /**
+     * A comparison function to sort packages by their identifier.
+     */
+    override fun compareTo(other: CuratedPackage) = pkg.id.compareTo(other.pkg.id)
+}
