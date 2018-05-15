c861fe8283eb3bfc89aa8c6b1870702f1c20e8f0
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-05-15 11:45:57 +0200

    reporter: Add a stub for the Excel reporter

--- reporter/src/main/kotlin/Main.kt
+++ reporter/src/main/kotlin/Main.kt
@@ -26,6 +26,7 @@ import com.beust.jcommander.ParameterException
 
 import com.here.ort.model.ScanRecord
 import com.here.ort.model.mapper
+import com.here.ort.reporter.reporters.ExcelReporter
 import com.here.ort.utils.PARAMETER_ORDER_HELP
 import com.here.ort.utils.PARAMETER_ORDER_LOGGING
 import com.here.ort.utils.PARAMETER_ORDER_MANDATORY
@@ -45,7 +46,7 @@ object Main {
     const val TOOL_NAME = "scanner"
 
     private enum class ReportFormat {
-        ;
+        EXCEL;
 
         companion object {
             /**
@@ -143,5 +144,11 @@ object Main {
         val scanRecord = scanRecordFile?.let {
             it.mapper().readValue(it, ScanRecord::class.java)
         } ?: throw IllegalArgumentException("Could not read scan record from '$scanRecordFile'.")
+
+        reportFormats.forEach {
+            when (it) {
+                ReportFormat.EXCEL -> ExcelReporter().generateReport(scanRecord, outputDir)
+            }
+        }
     }
 }
