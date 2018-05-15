cd4a86636a8206caea8926d007088d9264dcd8dc
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-05-15 11:45:57 +0200

    reporter: Add a stub for the Excel reporter

---
+++ reporter/src/main/kotlin/reporters/Reporter.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.reporter.reporters
+
+import com.here.ort.model.ScanRecord
+
+import java.io.File
+
+/**
+ * A reporter that creates a human readable report from a [ScanRecord].
+ */
+interface Reporter {
+    fun generateReport(scanRecord: ScanRecord, outputDir: File)
+}
