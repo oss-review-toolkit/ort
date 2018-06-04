93179c5f3fe620d955146c47ff4cab3734b21cf7
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-06-04 22:31:36 +0200

    Associate ScanCode's license findings with copyright findings

--- scanner/src/test/kotlin/ScanCodeTest.kt
+++ scanner/src/test/kotlin/ScanCodeTest.kt
@@ -19,6 +19,8 @@
 
 package com.here.ort.scanner
 
+import com.here.ort.model.LicenseFinding
+import com.here.ort.model.jsonMapper
 import com.here.ort.scanner.scanners.ScanCode
 
 import io.kotlintest.shouldBe
@@ -33,6 +35,7 @@ class ScanCodeTest : WordSpec({
             val resultFile = File("src/test/assets/esprima-2.7.3_scancode-2.2.1.post277.4d68f9377.json")
             val result = ScanCode.getResult(resultFile)
             val summary = ScanCode.generateSummary(Instant.now(), Instant.now(), result)
+
             ScanCode.mapTimeoutErrors(summary.errors) shouldBe true
             summary.errors.joinToString("\n") shouldBe listOf(
                     "ERROR: Timeout after 300 seconds while scanning file " +
@@ -67,6 +70,7 @@ class ScanCodeTest : WordSpec({
             val resultFile = File("src/test/assets/esprima-2.7.3_scancode-2.2.1.json")
             val result = ScanCode.getResult(resultFile)
             val summary = ScanCode.generateSummary(Instant.now(), Instant.now(), result)
+
             ScanCode.mapTimeoutErrors(summary.errors) shouldBe false
         }
     }
@@ -76,6 +80,7 @@ class ScanCodeTest : WordSpec({
             val resultFile = File("src/test/assets/very-long-json-lines_scancode-2.2.1.post277.4d68f9377.json")
             val result = ScanCode.getResult(resultFile)
             val summary = ScanCode.generateSummary(Instant.now(), Instant.now(), result)
+
             ScanCode.mapUnknownErrors(summary.errors) shouldBe true
             summary.errors.joinToString("\n") shouldBe listOf(
                     "ERROR: MemoryError while scanning file 'data.json'."
@@ -86,6 +91,7 @@ class ScanCodeTest : WordSpec({
             val resultFile = File("src/test/assets/kotlin-annotation-processing-gradle-1.2.21_scancode.json")
             val result = ScanCode.getResult(resultFile)
             val summary = ScanCode.generateSummary(Instant.now(), Instant.now(), result)
+
             ScanCode.mapUnknownErrors(summary.errors) shouldBe false
             summary.errors.joinToString("\n") shouldBe listOf(
                     "ERROR: AttributeError while scanning file 'compiler/testData/cli/js-dce/withSourceMap.js.map' " +
@@ -97,7 +103,128 @@ class ScanCodeTest : WordSpec({
             val resultFile = File("src/test/assets/esprima-2.7.3_scancode-2.2.1.json")
             val result = ScanCode.getResult(resultFile)
             val summary = ScanCode.generateSummary(Instant.now(), Instant.now(), result)
+
             ScanCode.mapUnknownErrors(summary.errors) shouldBe false
+        }
+    }
+
+    "getRootLicense()" should {
+        "succeed for a result containing a LICENSE file" {
+            val resultFile = File("src/test/assets/oss-review-toolkit-license-and-readme_scancode-2.9.2.json")
+            val result = jsonMapper.readTree(resultFile)
+
+            ScanCode.getRootLicense(result) shouldBe "Apache-2.0"
+        }
+
+        "succeed for a result containing a LICENSE.BSD file" {
+            val resultFile = File("src/test/assets/esprima-2.7.3_scancode-2.2.1.post277.4d68f9377.json")
+            val result = jsonMapper.readTree(resultFile)
+
+            ScanCode.getRootLicense(result) shouldBe "BSD-2-Clause"
+        }
+    }
+
+    "getClosestCopyrightStatements()" should {
+        "properly return closest jquery copyright statements" {
+            val resultFile = File("src/test/assets/esprima-2.7.3_scancode-2.2.1.json")
+            val result = jsonMapper.readTree(resultFile)
+            val copyrights = result["files"].find {
+                it["path"].asText() == "test/3rdparty/jquery-1.9.1.js"
+            }!!.get("copyrights")
+
+            ScanCode.getClosestCopyrightStatements(copyrights, 5) shouldBe
+                    sortedSetOf("Copyright 2005, 2012 jQuery Foundation, Inc.")
+            ScanCode.getClosestCopyrightStatements(copyrights, 3690) shouldBe
+                    sortedSetOf("Copyright 2012 jQuery Foundation")
+        }
+
+        "properly return closest mootools copyright statements" {
+            val resultFile = File("src/test/assets/esprima-2.7.3_scancode-2.2.1.json")
+            val result = jsonMapper.readTree(resultFile)
+            val copyrights = result["files"].find {
+                it["path"].asText() == "test/3rdparty/mootools-1.4.5.js"
+            }!!.get("copyrights")
+
+            ScanCode.getClosestCopyrightStatements(copyrights, 28) shouldBe sortedSetOf(
+                    "Copyright (c) 2005-2007 Sam Stephenson",
+                    "Copyright (c) 2006 Dean Edwards, GNU Lesser General Public",
+                    "Copyright (c) 2006-2012 Valerio Proietti"
+            )
+        }
+    }
+
+    "associateFindings()" should {
+        "properly associate a separate copyright to a root license" {
+            val resultFile = File("src/test/assets/oss-review-toolkit-license-and-readme_scancode-2.9.2.json")
+            val result = jsonMapper.readTree(resultFile)
+
+            ScanCode.associateFindings(result) shouldBe sortedSetOf(
+                    LicenseFinding("Apache-2.0", sortedSetOf("Copyright (c) 2017-2018 HERE Europe B.V."))
+            )
+        }
+
+        "properly associate licenses to copyrights" {
+            val resultFile = File("src/test/assets/esprima-2.7.3_scancode-2.2.1.json")
+            val result = ScanCode.getResult(resultFile)
+
+            val expectedFindings = sortedSetOf(
+                    LicenseFinding(
+                            "BSD-2-Clause",
+                            sortedSetOf(
+                                    "Copyright (c) jQuery Foundation, Inc. and Contributors"
+                            )
+                    ),
+                    LicenseFinding(
+                            "BSD-3-Clause",
+                            sortedSetOf(
+                                    "copyright (c) 2012 Scott Jehl, Paul Irish, Nicholas Zakas.",
+                                    "Copyright 2013 Yahoo! Inc."
+                            )
+                    ),
+                    LicenseFinding(
+                            "GPL-1.0+",
+                            sortedSetOf(
+                                    "Copyright (c) 2010 Cowboy Ben Alman",
+                                    "Copyright 2005, 2012 jQuery Foundation, Inc.",
+                                    "Copyright 2010, 2014 jQuery Foundation, Inc.",
+                                    "Copyright 2013 jQuery Foundation"
+                            )
+                    ),
+                    LicenseFinding(
+                            "LGPL-2.0+",
+                            sortedSetOf(
+                                    "Copyright (c) 2005-2007 Sam Stephenson",
+                                    "Copyright (c) 2006 Dean Edwards, GNU Lesser General Public",
+                                    "Copyright (c) 2006-2012 Valerio Proietti"
+                            )
+                    ),
+                    LicenseFinding(
+                            "MIT",
+                            sortedSetOf(
+                                    "(c) 2007-2008 Steven Levithan",
+                                    "(c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & " +
+                                            "Editors Underscore",
+                                    "(c) 2010-2011 Jeremy Ashkenas, DocumentCloud Inc.",
+                                    "(c) 2010-2014 Google, Inc. http://angularjs.org",
+                                    "(c) 2011-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & " +
+                                            "Editors Backbone",
+                                    "Copyright (c) 2005-2007 Sam Stephenson",
+                                    "Copyright (c) 2006 Dean Edwards, GNU Lesser General Public",
+                                    "Copyright (c) 2006-2012 Valerio Proietti",
+                                    "Copyright (c) 2010 Cowboy Ben Alman",
+                                    "copyright (c) 2012 Scott Jehl, Paul Irish, Nicholas Zakas.",
+                                    "Copyright 2005, 2012 jQuery Foundation, Inc.",
+                                    "Copyright 2010, 2014 jQuery Foundation, Inc.",
+                                    "Copyright 2010-2012 Mathias Bynens",
+                                    "Copyright 2012 jQuery Foundation",
+                                    "Copyright 2013 jQuery Foundation",
+                                    "copyright Robert Kieffer"
+                            )
+                    )
+            )
+            val actualFindings = ScanCode.associateFindings(result)
+
+            actualFindings shouldBe expectedFindings
         }
     }
 })
