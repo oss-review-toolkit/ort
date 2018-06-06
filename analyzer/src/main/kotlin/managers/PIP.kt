360f36d4f3d8757ecda314a3def418e64affd973
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-06-06 14:56:53 +0200

    PIP: Remove a superfluous list item
    The destructuring declaration only has three variables.

--- analyzer/src/main/kotlin/managers/PIP.kt
+++ analyzer/src/main/kotlin/managers/PIP.kt
@@ -164,7 +164,7 @@ class PIP : PackageManager() {
         } else {
             // In case of "requirements*.txt" there is no meta-data at all available, so use the parent directory name
             // as the project name.
-            listOf(definitionFile.parentFile.name, "", "", "")
+            listOf(definitionFile.parentFile.name, "", "")
         }
 
         val packages = sortedSetOf<Package>()
