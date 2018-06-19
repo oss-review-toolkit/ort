729754d59289f5be50bcb5a8257f13d62315f7fa
Misha Vakulich <misha@codescoop.com>
2018-06-19 12:04:26 +0300

    add redux state management

---
+++ reporter/src/web-app/js/src/store/reducers.js
@@ -0,0 +1,11 @@
+// Parse JSON report data embedded in HTML page
+const jsonNode = document.querySelector("script[type='application/json']");
+const jsonText = jsonNode.textContent;
+const reportData = JSON.parse(jsonText);
+
+export default (initialState = reportData, action) => {
+    switch(action.type) {
+        default:
+            return initialState;
+    }
+}
