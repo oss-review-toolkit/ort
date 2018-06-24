a25c51824bc840e5149aea54db560daf62633157
Daniel Landau <daniel@landau.fi>
2018-06-24 11:32:52 +0300

    reporter: Add Array.includes polyfill for IE support

--- reporter/src/web-app/js/src/index.js
+++ reporter/src/web-app/js/src/index.js
@@ -21,6 +21,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './polyfills'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
