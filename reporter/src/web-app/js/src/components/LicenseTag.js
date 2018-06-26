203f4b7dd6836d5e8a651e4e7bac0238412bd590
Thomas Steenbergen <thomas.steenbergen@here.com>
2018-06-26 16:02:57 +0200

    reporter: Display Choosealicense.com license summary on click of LicenseTag

--- reporter/src/web-app/js/src/components/LicenseTag.js
+++ reporter/src/web-app/js/src/components/LicenseTag.js
@@ -18,179 +18,141 @@
  */
 
 import React from 'react';
-import { Badge, Card, Col, Modal, Popover, Tag, Tooltip, Row } from 'antd';
-import { config } from '../config';
-import { choosealicense } from '../data/choosealicense/index';
+import { List, Modal, Table, Tabs, Tag, Tooltip } from 'antd';
+import { LicenseSummaryCard } from './LicenseSummaryCard';
+import { LICENSES } from '../data/licenses';
 import 'antd/dist/antd.css';
 
-var correctToSPDX = require('spdx-correct'),
-  licenses = {},
-  licensesDataFromConfig = (config && config.hasOwnProperty('licenses')) ? config.licenses : {}, 
-  licensesDataFromSPDX = require('spdx-license-list/full');
-
-  // FIXME Remove window vars added for debugging purposes
-  window.config = config;
-  window.correctToSPDX = correctToSPDX;
-  window.licenses = licenses;
-  window.licensesDataFromConfig = licensesDataFromConfig
-  window.licensesDataFromSPDX = licensesDataFromSPDX;
-  window.choosealicense = choosealicense;
+const TabPane = Tabs.TabPane;
 
 export class LicenseTag extends React.Component {
   constructor(props) {
     super(props);
 
-    let licenseValueHandler = {
-        get: function(target, name) {
-          let licenseDataFromConfig,
-              licenseDataFromSPDX,
-              spdxId = target.spdxId;
-      
-          if (!target.hasOwnProperty(name)) {
-
-            // Check if property name has been defined in Reporter's config
-            licenseDataFromConfig = licensesDataFromConfig[spdxId] ? licensesDataFromConfig[spdxId] : licensesDataFromConfig[this.tagText];
-
-            if (licenseDataFromConfig && licenseDataFromConfig.hasOwnProperty(name)) {
-              return licenseDataFromConfig[name];
-            }
-
-            // If property name is not found in user specified config try to see if it's in SPDX licenses metadata
-            licenseDataFromSPDX = licensesDataFromSPDX[spdxId];
-        
-            if (licenseDataFromSPDX && licenseDataFromSPDX.hasOwnProperty(name)) {
-              return licenseDataFromSPDX[name];
-            }
-
-            return '';
-          } else {
-            return target[name];
-          }
-        }
-      };
-
     this.tagText = props.text;
-
-    /* Using ES6 Proxy to create Object that with default values for specific attribute coming for SPDX license list e.g.
-     *
-     * licenses[Apache-2.0] = {
-     *  spdxId: Apache-2.0
-     * }
-     *
-     * results in effectively
-     * 
-     * licenses[Apache-2.0] = {
-     *   spdxId: Apache-2.0,
-     *.  name: Apache License 2.0,
-     *.  licenseText: "Apache License↵Version 2.0, January 2004↵http://www.apache.org/licenses/ …"
-     *.  osiApproved: true,
-     *.  url: "http://www.apache.org/licenses/LICENSE-2.0"
-     *.}
-     *
-     * For more details on ES6 proxy please see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
-     */
-    if (!licenses[this.tagText]) {
-      licenses[this.tagText] = new Proxy({
-        spdxId: correctToSPDX(this.tagText),
-      }, licenseValueHandler);
-    }
-
-    this.licenseColor = (licenses[this.tagText] && licenses[this.tagText].color) ? licenses[this.tagText].color : '';
     
-    this.showLicenseInfoModal = () => {
-      let license = licenses[props.text],
-          licenseName,
-          licenseText;
+    if (this.tagText) {
+      this.license = LICENSES[this.tagText];
 
-      if (!license.modal) {
-        licenseName = license.name ? license.name : props.text;
-        licenseText = licenses[props.text].licenseText.split('\n').map(
-          function (elem) {
-            // FIXME Throws "Warning: Encountered two children with the same key..."
-            return React.createElement('p', {key: elem}, elem);
+      this.showLicenseInfoModal = () => {
+        if (!this.license) {
+          return;
+        }
+
+        if (this.license.name !== 'NONE') {
+          if (!this.license.modal) {
+            this.license.modal = {
+              title: this.license.name,
+              className: 'reporter-license-info',
+              content: (<LicenseInfo license={this.license}/>),
+              onOk() {},
+              okText: "Close",
+              maskClosable: true,
+              width: 800
+            };
           }
-        );
-        
-        license.modal = {
-          title: licenseName,
-          className: 'reporter-license-info',
-          content: (<LicenseInfo name={licenseName}/>),
-          onOk() {},
-          okText: "Close",
-          maskClosable: true,
-          width: 800
-        };
-      }
-      
-      if (licenseName !== 'NONE') {
-        Modal.info(license.modal);
+
+          Modal.info(this.license.modal);
+        }
       }
     }
   }
 
   render() {
-    /*
-      FIXME Use Button instead of Tag?
-      <Button className="reporter-license-btn red" onClick={this.test}>{this.tagText}</Button>
-    */
-    return (
-      <Tooltip placement="left" title={licenses[this.tagText].name}>
-        <Tag className="reporter-license"
-          color={this.licenseColor}
-          checked="true"
-          onClick={this.showLicenseInfoModal}>{this.tagText}</Tag>
-      </Tooltip>
-    );
+    if (this.license && this.tagText) {
+      return (
+        <Tooltip placement="left" title={this.license.name}>
+          <Tag className="reporter-license"
+            color={this.license.color}
+            checked="true"
+            onClick={this.showLicenseInfoModal}>{this.tagText}</Tag>
+        </Tooltip>
+      );
+    } else {
+      return(<div>No data</div>);
+    }
   }
-}
+};
 
 // Generates the HTML for the additional license information
 const LicenseInfo = function (props) {
-  
+  const license = props.license,
+        licenseDescription = license.description ? 
+          license.description : 'No description available for this license';
+
+  if (!license && !license.summary) {
+    return (<div>No summary data for this license</div>);
+  }
+
+  // Transform array of license summaries by provider so 
+  // we can display and attribute each provider's license summary
+  var summaryProviders = ((summary = license.summary) => {
+    let providers = {};
+
+    for (let i = 0; i < summary.length; i++) {
+      let provider = summary[i].provider;
+
+      if (provider) {
+        if (!providers.hasOwnProperty(provider)) {
+          providers[provider] = [];
+        }
+
+        providers[provider].push(summary[i]);
+      }
+    }
+
+    return Object.values(providers);
+  })();
+
   return (
     <div className="reporter-license-info">
-      <p className="reporter-license-description">
-        A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
-      </p>
-      <div className="reporter-license-obligations">
-        <Row gutter={16}>
-          <Col span={8}>
-            <Card title="Permissions" bordered={false}>
-              <ul>
-                <li>
-                  <Popover content="This Software and its derivates may be used for commercial purposes" title="Permission">
-                     <Badge status="success" text="commercial-use"/>
-                  </Popover>
-                </li>
-                <li><Badge status="success" text="modifications"/></li>
-                <li><Badge status="success" text="distribution"/></li>
-                <li><Badge status="success" text="patent-use"/></li>
-                <li><Badge status="success" text="private-use"/></li>
-              </ul>
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Conditions" bordered={false}>
-              <ul>
-                <li><Badge status="warning" text="include-copyright"/></li>
-                <li><Badge status="warning" text="document-changes"/></li>
-              </ul>
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Limitations" bordered={false}>
-              <ul>
-                <li><Badge status="error" text="trademark-use"/></li>
-                <li><Badge status="error" text="liability"/></li>
-                <li><Badge status="error" text="warranty"/></li>
-              </ul>
-            </Card>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          Source: Choosealicense.com
-        </Row>
-      </div>
+      <Tabs>
+        <TabPane tab="Summary" key="1">
+          <p className="reporter-license-description">
+            {licenseDescription}
+          </p>
+          <div className="reporter-license-obligations">
+            <List
+              grid={{ gutter: 16, column: 1 }}
+              itemLayout="vertical"
+              size="small"
+              pagination={{
+                hideOnSinglePage: true,
+                pageSize: 1,
+                size: "small"
+              }}
+              dataSource={summaryProviders}
+              renderItem={summary => (
+                <List.Item>
+                  <LicenseSummaryCard summary={summary}/> 
+                </List.Item>
+              )}
+            />
+          </div>
+        </TabPane>
+        <TabPane tab="Fulltext" key="2">
+          <Table 
+            columns={[{
+              title: license.name,
+              dataIndex: 'text',
+              render: (text, row, index) => {
+                return(<pre className="reporter-license-fulltext">{text}</pre>)
+              }
+            }]}
+            dataSource={[{
+              key: 1,
+              dataIndex: 'text',
+              text: license.licenseText
+            }]}
+            pagination={{
+              hideOnSinglePage: true
+             }}
+            scroll={{
+              y: 365
+            }}
+            showHeader={false}/>
+        </TabPane>
+      </Tabs>
     </div>
   );
-}
+};
