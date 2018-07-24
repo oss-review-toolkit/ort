8e70932b136202ebc0c67d5d721c90d2ed866bad
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-07-24 11:16:57 +0200

    model: Add classes for repository configuration
    Add model classes for the content of the ".ort.yml" file that can be added
    to a repository to configure the scan of this repository.

---
+++ model/src/main/kotlin/config/Excludes.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model.config
+
+import com.fasterxml.jackson.annotation.JsonInclude
+
+/**
+ * Defines which parts of a repository should be excluded.
+ */
+data class Excludes(
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val projects: List<ProjectExclude>,
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val packages: List<PackageExclude>,
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val scopes: List<ScopeExclude>,
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val errors: List<ErrorExclude>
+)
---
+++ model/src/main/kotlin/config/RepositoryConfiguration.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model.config
+
+import com.fasterxml.jackson.annotation.JsonInclude
+
+/**
+ * A repository specific configuration for ORT. This configuration is parsed from a ".ort.yml" file in the input
+ * directory of the analyzer. It will be included in the analyzer result and can be further processed by the other
+ * tools.
+ */
+data class RepositoryConfiguration(
+        /**
+         * Defines which parts of the repository will be excluded. Note that excluded parts will still be analyzed and
+         * scanned, but related errors will be marked as resolved in the reporter output.
+         */
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        val excludes: Excludes?
+)
