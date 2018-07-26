d11f680ff35a3cb471cc57ca3415c75547fc93fc
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-07-26 14:17:15 +0200

    model: Add classes for new output format OrtResult
    Add classes for the new unified output format OrtResult. It provides
    additional fields for the environment and better separates information
    about the processed repository and the results from analyzer and scanner.
    Also using an unified output format for both analyzer and scanner
    simplifies processing their results in other tools, e.g. to show only the
    analyzer results in the reporter.

---
+++ model/src/main/kotlin/OrtResult.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model
+
+/**
+ * The common output format for the analyzer and scanner. It contains information about the scanned repository, and the
+ * analyzer and scanner will add their result to it.
+ */
+data class OrtResult(
+        val repository: Repository,
+        val analyzer: AnalyzerRun? = null,
+        val scanner: ScannerRun? = null
+) : CustomData()
