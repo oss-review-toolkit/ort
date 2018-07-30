82f74e31ff374607c5c05a05c060d2687da1a15e
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-07-30 13:06:03 +0200

    Add model classes for the scanner configuration
    As part of this also remove the unused code for supporting different cache
    types. A proper implementation of this should be added once another cache
    type is implemented.

--- scanner/src/test/kotlin/ScanResultsCacheTest.kt
+++ scanner/src/test/kotlin/ScanResultsCacheTest.kt
@@ -19,7 +19,7 @@
 
 package com.here.ort.scanner
 
-import com.here.ort.model.yamlMapper
+import com.here.ort.model.config.ArtifactoryCacheConfiguration
 
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
@@ -41,63 +41,27 @@ class ScanResultsCacheTest : WordSpec() {
 
     init {
         "ScanResultsCache.configure" should {
-            "fail if the cache type is missing" {
+            "fail if the Artifactory URL is empty" {
                 val exception = shouldThrow<IllegalArgumentException> {
-                    val config = yamlMapper.readTree("""
-                        scanner:
-                          cache:
-                    """)
-                    ScanResultsCache.configure(config)
-                }
-                exception.message shouldBe "Cache type is missing."
-            }
+                    val config = ArtifactoryCacheConfiguration("", "someApiToken")
 
-            "fail if the cache type is unknown" {
-                val exception = shouldThrow<IllegalArgumentException> {
-                    val config = yamlMapper.readTree("""
-                        scanner:
-                          cache:
-                            type: abcd
-                    """)
-                    ScanResultsCache.configure(config)
-                }
-                exception.message shouldBe "Cache type 'abcd' unknown."
-            }
-
-            "fail if the Artifactory URL is missing" {
-                val exception = shouldThrow<IllegalArgumentException> {
-                    val config = yamlMapper.readTree("""
-                        scanner:
-                          cache:
-                            type: Artifactory
-                            apiToken: someApiToken
-                    """)
                     ScanResultsCache.configure(config)
                 }
                 exception.message shouldBe "URL for Artifactory cache is missing."
             }
 
-            "fail if the Artifactory apiToken is missing" {
+            "fail if the Artifactory apiToken is empty" {
                 val exception = shouldThrow<IllegalArgumentException> {
-                    val config = yamlMapper.readTree("""
-                        scanner:
-                          cache:
-                            type: Artifactory
-                            url: someUrl
-                    """)
+                    val config = ArtifactoryCacheConfiguration("someUrl", "")
+
                     ScanResultsCache.configure(config)
                 }
                 exception.message shouldBe "API token for Artifactory cache is missing."
             }
 
             "configure the Artifactory cache correctly" {
-                val config = yamlMapper.readTree("""
-                        scanner:
-                          cache:
-                            type: Artifactory
-                            apiToken: someApiToken
-                            url: someUrl
-                    """)
+                val config = ArtifactoryCacheConfiguration("someUrl", "someApiToken")
+
                 ScanResultsCache.configure(config)
 
                 ScanResultsCache.cache shouldNotBe null
