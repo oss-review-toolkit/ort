b01f77a252d6146442e959f69b93fe09be109324
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-08-31 08:36:27 +0200

    ProcessCapture: Improve logging of stdout / stderr
    Only log the note about limited log output if log output actually was
    limited.

--- utils/src/main/kotlin/ProcessCapture.kt
+++ utils/src/main/kotlin/ProcessCapture.kt
@@ -38,6 +38,17 @@ class ProcessCapture(workingDir: File?, environment: Map<String, String>, vararg
     companion object {
         private const val MAX_LOG_LINES = 20
         private const val MAX_LOG_LINES_MESSAGE = "(Above output is limited to $MAX_LOG_LINES lines.)"
+
+        private fun logLinesLimited(prefix: String, lines: Sequence<String>, limit: Int, logLine: (String) -> Unit) {
+            var count = 0
+            val i = lines.iterator()
+
+            while (i.hasNext() && count++ < limit) {
+                logLine("$prefix: ${i.next()}")
+            }
+
+            if (i.hasNext()) logLine(MAX_LOG_LINES_MESSAGE)
+        }
     }
 
     val commandLine = command.joinToString(" ")
@@ -89,23 +100,17 @@ class ProcessCapture(workingDir: File?, environment: Map<String, String>, vararg
         process.waitFor()
 
         if (log.isDebugEnabled) {
-            // No need to use curly-braces-syntax for logging here as the log level check is already done above.
+            // No need to use curly-braces-syntax for logging below as the log level check is already done above.
 
             if (stdoutFile.length() > 0L) {
                 stdoutFile.useLines { lines ->
-                    lines.take(MAX_LOG_LINES).forEach { line ->
-                        log.debug("stdout: $line")
-                    }
-                    log.debug(MAX_LOG_LINES_MESSAGE)
+                    logLinesLimited("stdout", lines, MAX_LOG_LINES) { log.debug(it) }
                 }
             }
 
             if (stderrFile.length() > 0L) {
                 stderrFile.useLines { lines ->
-                    lines.take(MAX_LOG_LINES).forEach { line ->
-                        log.debug("stderr: $line")
-                    }
-                    log.debug(MAX_LOG_LINES_MESSAGE)
+                    logLinesLimited("stderr", lines, MAX_LOG_LINES) { log.debug(it) }
                 }
             }
         }
