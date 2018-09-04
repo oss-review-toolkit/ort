b4ddefb6b1c81381b3a24e2755057e6e9d4c4071
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-09-04 21:47:36 +0200

    reporter-web-app: Trim trailing whitespace in JavaScript code
    As reported by eslint.

--- reporter-web-app/config/webpack.config.prod.js
+++ reporter-web-app/config/webpack.config.prod.js
@@ -94,7 +94,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -154,7 +154,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
           },
