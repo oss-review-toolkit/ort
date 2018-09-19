c8a1a3264a74fd13ed372e62fcbd7d8c8379ac42
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-09-19 16:13:31 +0200

    reporter: Show excludes in the Excel report
    Show the exclude reasons for projects and scopes in the Excel report.
    
    Also gray out excluded projects and dependencies to better show what is
    excluded.

--- reporter/src/main/kotlin/reporters/ExcelReporter.kt
+++ reporter/src/main/kotlin/reporters/ExcelReporter.kt
@@ -23,20 +23,25 @@ import ch.frankel.slf4k.*
 
 import com.here.ort.model.ScanRecord
 import com.here.ort.model.VcsInfo
+import com.here.ort.model.config.ProjectExclude
 import com.here.ort.utils.isValidUrl
 import com.here.ort.utils.log
 
 import org.apache.poi.common.usermodel.HyperlinkType
 import org.apache.poi.ss.usermodel.BorderStyle
+import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.CellStyle
 import org.apache.poi.ss.usermodel.CreationHelper
 import org.apache.poi.ss.usermodel.FillPatternType
+import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.VerticalAlignment
 import org.apache.poi.ss.util.CellRangeAddress
 import org.apache.poi.ss.util.CellUtil
 import org.apache.poi.ss.util.WorkbookUtil
 import org.apache.poi.xssf.usermodel.XSSFCellStyle
 import org.apache.poi.xssf.usermodel.XSSFColor
+import org.apache.poi.xssf.usermodel.XSSFFont
+import org.apache.poi.xssf.usermodel.XSSFRichTextString
 import org.apache.poi.xssf.usermodel.XSSFSheet
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
@@ -52,14 +57,21 @@ class ExcelReporter : TableReporter() {
 
     private val borderColor = XSSFColor(Color(211, 211, 211))
     private val errorColor = XSSFColor(Color(240, 128, 128))
+    private val excludedColor = XSSFColor(Color(180, 180, 180))
+    private val excludedFontColor = XSSFColor(Color(100, 100, 100))
     private val successColor = XSSFColor(Color(173, 216, 230))
     private val warningColor = XSSFColor(Color(255, 255, 224))
 
     private lateinit var defaultStyle: CellStyle
+    private lateinit var excludedStyle: CellStyle
     private lateinit var headerStyle: CellStyle
     private lateinit var successStyle: CellStyle
     private lateinit var warningStyle: CellStyle
     private lateinit var errorStyle: CellStyle
+
+    private lateinit var defaultFont: XSSFFont
+    private lateinit var headerFont: XSSFFont
+    private lateinit var excludedFont: XSSFFont
 
     private lateinit var creationHelper: CreationHelper
 
@@ -74,8 +86,18 @@ class ExcelReporter : TableReporter() {
             setBorderColor(borderColor)
         }
 
-        val headerFont = workbook.createFont().apply {
+        defaultFont = workbook.createFont().apply {
+            fontHeightInPoints = 11
+        }
+
+        headerFont = workbook.createFont().apply {
+            fontHeightInPoints = 11
             bold = true
+        }
+
+        excludedFont = workbook.createFont().apply {
+            fontHeightInPoints = 11
+            setColor(excludedFontColor)
         }
 
         headerStyle = workbook.createCellStyle().apply {
@@ -98,6 +120,12 @@ class ExcelReporter : TableReporter() {
         errorStyle = workbook.createCellStyle().apply {
             cloneStyleFrom(defaultStyle)
             setFillForegroundColor(errorColor)
+            setFillPattern(FillPatternType.SOLID_FOREGROUND)
+        }
+
+        excludedStyle = workbook.createCellStyle().apply {
+            cloneStyleFrom(defaultStyle)
+            setFillForegroundColor(excludedColor)
             setFillPattern(FillPatternType.SOLID_FOREGROUND)
         }
 
@@ -156,26 +184,86 @@ class ExcelReporter : TableReporter() {
 
         val sheet = workbook.createSheet(sheetName)
 
-        val headerRows = createHeader(sheet, name, file, vcsInfo, extraColumns)
+        val headerRows = createHeader(sheet, name, null, file, vcsInfo, extraColumns)
         var currentRow = headerRows
 
         table.rows.forEach { row ->
+            val isExcluded = row.scopes.all { (id, scopes) ->
+                table.projectExcludes[id] != null || (scopes.isNotEmpty() && scopes.all { it.value.isNotEmpty() })
+            }
+
+            val font = if (isExcluded) excludedFont else defaultFont
+
             val cellStyle = when {
+                isExcluded -> excludedStyle
                 row.analyzerErrors.isNotEmpty() || row.scanErrors.isNotEmpty() -> errorStyle
                 row.declaredLicenses.isEmpty() && row.detectedLicenses.isEmpty() -> warningStyle
                 else -> successStyle
             }
 
-            sheet.createRow(currentRow).apply {
-                CellUtil.createCell(this, 0, row.id.toString(), cellStyle)
-                CellUtil.createCell(this, 1, row.scopes.keys.joinWithLimit(), cellStyle)
-                CellUtil.createCell(this, 2, row.declaredLicenses.joinWithLimit(), cellStyle)
-                CellUtil.createCell(this, 3, row.detectedLicenses.joinWithLimit(), cellStyle)
-                CellUtil.createCell(this, 4, row.analyzerErrors.map { it.toString() }.joinWithLimit(), cellStyle)
-                CellUtil.createCell(this, 5, row.scanErrors.map { it.toString() }.joinWithLimit(), cellStyle)
+            val scopesText = XSSFRichTextString()
 
-                val maxLines = listOf(row.scopes.values, row.declaredLicenses, row.detectedLicenses,
-                        row.analyzerErrors.values, row.scanErrors.values).map { it.size }.max() ?: 1
+            row.scopes.entries.sortedWith(compareBy({
+                table.projectExcludes[it.key] != null || it.value.values.isNotEmpty()
+            }, { it.key })).forEach { (id, scopes) ->
+                val projectExclude = table.projectExcludes[id]
+
+                if (projectExclude != null) {
+                    scopesText.append("$id (Excluded: ${projectExclude.reason} - ${projectExclude.comment})\n",
+                            excludedFont)
+                } else {
+                    scopesText.append("$id\n", font)
+                }
+
+                scopes.entries.sortedWith(compareBy({ it.value.isNotEmpty() }, { it.key }))
+                        .forEach { (scope, excludes) ->
+                            scopesText.append("  $scope\n",
+                                    if (projectExclude != null || excludes.isNotEmpty()) excludedFont else font)
+
+                            excludes.forEach { exclude ->
+                                scopesText.append("    Excluded: ${exclude.reason} - ${exclude.comment}\n",
+                                        excludedFont)
+                            }
+                        }
+            }
+
+            val scopesLines = row.scopes.size + row.scopes.toList().sumBy { it.second.size } +
+                    row.scopes.flatMap { it.value.values }.size
+
+            val analyzerErrorsText = buildString {
+                row.analyzerErrors.forEach { (id, errors) ->
+                    append("$id\n")
+
+                    errors.forEach { error ->
+                        append("  $error\n")
+                    }
+                }
+            }
+
+            val analyzerErrorsLines = row.analyzerErrors.size + row.analyzerErrors.flatMap { it.value }.size
+
+            val scanErrorsText = buildString {
+                row.scanErrors.forEach { (id, errors) ->
+                    append("$id\n")
+
+                    errors.forEach { error ->
+                        append("  $error\n")
+                    }
+                }
+            }
+
+            val scanErrorsLines = row.scanErrors.size + row.scanErrors.flatMap { it.value }.size
+
+            sheet.createRow(currentRow).apply {
+                createCell(this, 0, row.id.toString(), font, cellStyle)
+                createCell(this, 1, scopesText, cellStyle)
+                createCell(this, 2, row.declaredLicenses.joinWithLimit(), font, cellStyle)
+                createCell(this, 3, row.detectedLicenses.joinWithLimit(), font, cellStyle)
+                createCell(this, 4, analyzerErrorsText, font, cellStyle)
+                createCell(this, 5, scanErrorsText, font, cellStyle)
+
+                val maxLines = listOf(scopesLines, row.declaredLicenses.size, row.detectedLicenses.size,
+                        analyzerErrorsLines, scanErrorsLines).max() ?: 1
                 heightInPoints = maxLines * getSheet().defaultRowHeightInPoints
             }
             ++currentRow
@@ -190,26 +278,45 @@ class ExcelReporter : TableReporter() {
 
         val sheet = workbook.createSheet(sheetName)
 
-        val headerRows = createHeader(sheet, name, file, vcsInfo, extraColumns)
+        val headerRows = createHeader(sheet, name, table.exclude, file, vcsInfo, extraColumns)
         var currentRow = headerRows
 
         table.rows.forEach { row ->
+            val isExcluded = table.exclude != null
+                    || (row.scopes.values.let { it.isNotEmpty() && it.all { it.isNotEmpty() } })
+
+            val font = if (isExcluded) excludedFont else defaultFont
+
             val cellStyle = when {
+                isExcluded -> excludedStyle
                 row.analyzerErrors.isNotEmpty() || row.scanErrors.isNotEmpty() -> errorStyle
                 row.declaredLicenses.isEmpty() && row.detectedLicenses.isEmpty() -> warningStyle
                 else -> successStyle
             }
 
-            sheet.createRow(currentRow).apply {
-                CellUtil.createCell(this, 0, row.id.toString(), cellStyle)
-                CellUtil.createCell(this, 1, row.scopes.keys.joinWithLimit(), cellStyle)
-                CellUtil.createCell(this, 2, row.declaredLicenses.joinWithLimit(), cellStyle)
-                CellUtil.createCell(this, 3, row.detectedLicenses.joinWithLimit(), cellStyle)
-                CellUtil.createCell(this, 4, row.analyzerErrors.map { it.toString() }.joinWithLimit(), cellStyle)
-                CellUtil.createCell(this, 5, row.scanErrors.map { it.toString() }.joinWithLimit(), cellStyle)
+            val scopesText = XSSFRichTextString()
 
-                val maxLines = listOf(row.scopes.values, row.declaredLicenses, row.detectedLicenses,
-                        row.analyzerErrors, row.scanErrors).map { it.size }.max() ?: 1
+            row.scopes.entries.sortedWith(compareBy({ it.value.isNotEmpty() }, { it.key }))
+                    .forEach { (scope, excludes) ->
+                        scopesText.append("$scope\n", if (excludes.isNotEmpty()) excludedFont else font)
+
+                        excludes.forEach { exclude ->
+                            scopesText.append("  Excluded: ${exclude.reason} - ${exclude.comment}\n", excludedFont)
+                        }
+                    }
+
+            val scopesLines = row.scopes.size + row.scopes.flatMap { it.value }.size
+
+            sheet.createRow(currentRow).apply {
+                createCell(this, 0, row.id.toString(), font, cellStyle)
+                createCell(this, 1, scopesText, cellStyle)
+                createCell(this, 2, row.declaredLicenses.joinWithLimit(), font, cellStyle)
+                createCell(this, 3, row.detectedLicenses.joinWithLimit(), font, cellStyle)
+                createCell(this, 4, row.analyzerErrors.map { it.toString() }.joinWithLimit(), font, cellStyle)
+                createCell(this, 5, row.scanErrors.map { it.toString() }.joinWithLimit(), font, cellStyle)
+
+                val maxLines = listOf(scopesLines, row.declaredLicenses.size, row.detectedLicenses.size,
+                        row.analyzerErrors.size, row.scanErrors.size).max() ?: 1
                 heightInPoints = maxLines * getSheet().defaultRowHeightInPoints
             }
             ++currentRow
@@ -218,13 +325,16 @@ class ExcelReporter : TableReporter() {
         sheet.finalize(headerRows, currentRow, defaultColumns + extraColumns.size)
     }
 
-    private fun createHeader(sheet: XSSFSheet, name: String, file: String, vcsInfo: VcsInfo,
+    private fun createHeader(sheet: XSSFSheet, name: String, exclude: ProjectExclude?, file: String, vcsInfo: VcsInfo,
                              extraColumns: List<String>): Int {
         val columns = defaultColumns + extraColumns.size
 
         sheet.createRow(0).apply {
+            val text = exclude?.let { "$name - This project is excluded: ${exclude.reason} - ${exclude.comment}" }
+                    ?: name
+
             CellUtil.createCell(this, 0, "Project:", headerStyle)
-            CellUtil.createCell(this, 1, name, headerStyle)
+            CellUtil.createCell(this, 1, text, headerStyle)
         }
         sheet.addMergedRegion(CellRangeAddress(0, 0, 1, columns))
 
@@ -281,6 +391,21 @@ class ExcelReporter : TableReporter() {
 
         return ++rows
     }
+
+    private fun createCell(row: Row, column: Int, value: XSSFRichTextString, style: CellStyle?): Cell? {
+        val cell = CellUtil.getCell(row, column)
+
+        cell.setCellValue(value)
+
+        if (style != null) {
+            cell.cellStyle = style
+        }
+
+        return cell
+    }
+
+    private fun createCell(row: Row, column: Int, value: String, font: XSSFFont, cellStyle: CellStyle) =
+            createCell(row, column, XSSFRichTextString().apply { append(value, font) }, cellStyle)
 }
 
 // Use the same name as in XSSFWorkbook.MAX_SENSITIVE_SHEET_NAME_LEN, which is private.
