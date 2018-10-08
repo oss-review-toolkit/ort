06070346cc51c563804a0784df48d1e351638130
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-10-08 17:23:34 +0200

    Move scripts and tools for static analysis to new folder `tools`
    ...to keep the root directory clean.

--- tools/ktlint.sh
+++ tools/ktlint.sh
@@ -2,7 +2,9 @@
 
 # Run ktlint but exclude rules that are currently not applied to this project.
 
-if ./ktlint | grep -Ev "(Wildcard import|Unexpected spacing before)"; then
+BASEDIR=$(dirname "$0")
+
+if "$BASEDIR/ktlint" | grep -Ev "(Wildcard import|Unexpected spacing before)"; then
   echo "Please correct above ktlint issues."
   exit 1
 else
