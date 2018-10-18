052ec77d6a963d1c008859dc0dd64deec356c5d7
Thomas Steenbergen <thomas.steenbergen@here.com>
2018-10-18 17:07:11 +0200

    reporter-web-app: Add license table to SummaryView
    Adding table besides a pie chart enables users
    to more quickly obtain an overview of declared/detected licenses
    
    Signed-off-by: Thomas Steenbergen <thomas.steenbergen@here.com>

--- reporter-web-app/src/components/SummaryViewLicenseCharts.js
+++ reporter-web-app/src/components/SummaryViewLicenseCharts.js
@@ -18,7 +18,7 @@
  */
 
 import React from 'react';
-import { Tabs } from 'antd';
+import { Icon, Table, Tabs } from 'antd';
 import PropTypes from 'prop-types';
 import LicenseChart from './LicenseChart';
 
@@ -27,6 +27,54 @@ const { TabPane } = Tabs;
 // Generates the HTML to display summary of licenses
 const SummaryViewLicenseCharts = (props) => {
     const { data } = props;
+    const columns = [
+        {
+            title: 'License',
+            dataIndex: 'name',
+            sorter: (a, b) => {
+                const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+                const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+
+                // names must be equal
+                return 0;
+            },
+            key: 'name'
+        }, {
+            title: 'Packages',
+            dataIndex: 'value',
+            sorter: (a, b) => a.value - b.value,
+            key: 'value',
+            width: 150
+        }
+    ];
+    const pagination = {
+        hideOnSinglePage: true,
+        pageSize: 50,
+        pageSizeOptions: ['50', '100', '250', '500'],
+        position: 'bottom',
+        showSizeChanger: true,
+        showQuickJumper: true
+    };
+    const sortReverseLicensesByName = (licenses) => {
+        const arr = Object.keys(licenses).sort().reduce((accumulator, key) => {
+            accumulator.push(licenses[key]);
+
+            return accumulator;
+        }, []).reverse();
+
+        return arr;
+    };
+    const sortReverseLicensesByValue = (licenses) => {
+        const arr = Object.values(licenses);
+
+        return arr.sort((a, b) => a.value - b.value).reverse();
+    };
 
     return (
         <Tabs tabPosition="top">
@@ -40,12 +88,36 @@ const SummaryViewLicenseCharts = (props) => {
                 )}
                 key="1"
             >
-                <LicenseChart
-                    label="Detected Licenses"
-                    licenses={data.detectedLicenses}
-                    width={800}
-                    height={500}
-                />
+                <Tabs tabPosition="left">
+                    <TabPane
+                        tab={<Icon type="pie-chart" theme="outlined" />}
+                        key="ort-dectected-licenses-pie-chart"
+                    >
+                        <LicenseChart
+                            label="Detected Licenses"
+                            licenses={sortReverseLicensesByName(data.detectedLicenses)}
+                            width={800}
+                            height={500}
+                        />
+                    </TabPane>
+                    <TabPane
+                        tab={<Icon type="table" theme="outlined" />}
+                        key="ort-dectected-licenses-table"
+                    >
+                        <Table
+                            bordered={false}
+                            columns={columns}
+                            dataSource={sortReverseLicensesByValue(data.detectedLicenses)}
+                            locale={{
+                                emptyText: 'No detected licenses'
+                            }}
+                            pagination={pagination}
+                            size="small"
+                            rowClassName="ort-dectected-licenses-table-row"
+                            rowKey="name"
+                        />
+                    </TabPane>
+                </Tabs>
             </TabPane>
             <TabPane
                 tab={(
@@ -57,12 +129,36 @@ const SummaryViewLicenseCharts = (props) => {
                 )}
                 key="2"
             >
-                <LicenseChart
-                    label="Declared licenses"
-                    licenses={data.declaredLicenses}
-                    width={800}
-                    height={500}
-                />
+                <Tabs tabPosition="left">
+                    <TabPane
+                        tab={<Icon type="pie-chart" theme="outlined" />}
+                        key="ort-declared-licenses-pie-chart"
+                    >
+                        <LicenseChart
+                            label="Declared licenses"
+                            licenses={sortReverseLicensesByName(data.declaredLicenses)}
+                            width={800}
+                            height={500}
+                        />
+                    </TabPane>
+                    <TabPane
+                        tab={<Icon type="table" theme="outlined" />}
+                        key="ort-declared-licenses-table"
+                    >
+                        <Table
+                            bordered={false}
+                            columns={columns}
+                            dataSource={sortReverseLicensesByValue(data.declaredLicenses)}
+                            locale={{
+                                emptyText: 'No declared licenses'
+                            }}
+                            pagination={pagination}
+                            size="small"
+                            rowClassName="ort-dectected-licenses-table-row"
+                            rowKey="name"
+                        />
+                    </TabPane>
+                </Tabs>
             </TabPane>
         </Tabs>
     );
