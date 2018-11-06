8b325cbfd131f1c9c071695397aca521e63a72ae
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-11-06 14:30:38 +0100

    EvaluatorCommand: Require scripts to populate a list of errors
    This way the actual errors source can be returned instead of just a
    boolean.

--- cli/src/main/resources/rules/no_gpl_declared.kts
+++ cli/src/main/resources/rules/no_gpl_declared.kts
@@ -2,8 +2,13 @@
 //
 // $ ort evaluate -i scanner/src/funTest/assets/file-counter-expected-output-for-analyzer-result.yml --rules-resource /rules/no_gpl_declared.kts
 
-ortResult.analyzer?.result?.packages?.all { curatedPkg ->
-    curatedPkg.pkg.declaredLicenses.none { license ->
+val pkgWithGpl = ortResult.analyzer?.result?.packages?.filter { curatedPkg ->
+    curatedPkg.pkg.declaredLicenses.any { license ->
         license == "GPL" || license.startsWith("GPL ") || license.startsWith("GPL-")
     }
+}
+
+// Populate the list of errors to return.
+pkgWithGpl?.forEach { (pkg, _) ->
+    evalErrors += Error(source = pkg.id.toString(), message = "This package is declared under a GPL license.")
 }
