f96ae3c91f2c65e68f1bc82085d1b36052e995d1
Frank Viernau <frank.viernau@here.com>
2018-11-21 11:20:17 +0100

    NoticeReporterTest.kt: Move temp dir handling to setup / tear down
    This reduces redundancy a bit and makes the code of the individual
    test case a bit more compact.
    
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

--- reporter/src/test/kotlin/reporters/NoticeReporterTest.kt
+++ reporter/src/test/kotlin/reporters/NoticeReporterTest.kt
@@ -22,63 +22,77 @@ package com.here.ort.reporter.reporters
 import com.here.ort.model.OrtResult
 import com.here.ort.model.readValue
 import com.here.ort.reporter.DefaultResolutionProvider
+import com.here.ort.utils.safeDeleteRecursively
+
+import io.kotlintest.Description
+import io.kotlintest.TestResult
 
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
 import java.io.File
 
-class NoticeReporterTest : WordSpec({
-    "Notices reporter" should {
-        "generate the correct license notes" {
-            val expectedResultFile = File("src/test/assets/NPM-is-windows-1.0.2-expected-NOTICE")
-            val expectedText = expectedResultFile.readText()
-            val scanRecordFile = File("src/test/assets/NPM-is-windows-1.0.2-scan-result.json")
-            val ortResult = scanRecordFile.readValue<OrtResult>()
-            val outputDir = createTempDir().also { it.deleteOnExit() }
+class NoticeReporterTest : WordSpec() {
+    private lateinit var tempDir: File
 
-            NoticeReporter().generateReport(ortResult, DefaultResolutionProvider(), outputDir)
+    override fun beforeTest(description: Description) {
+        super.beforeTest(description)
+        tempDir = createTempDir()
+    }
 
-            val resultFile = File(outputDir, "NOTICE")
-            val actualText = resultFile.readText()
+    override fun afterTest(description: Description, result: TestResult) {
+        tempDir.safeDeleteRecursively()
+        super.afterTest(description, result)
+    }
 
-            actualText shouldBe expectedText
-        }
+    init {
+        "Notices reporter" should {
+            "generate the correct license notes" {
+                val expectedResultFile = File("src/test/assets/NPM-is-windows-1.0.2-expected-NOTICE")
+                val expectedText = expectedResultFile.readText()
+                val scanRecordFile = File("src/test/assets/NPM-is-windows-1.0.2-scan-result.json")
+                val ortResult = scanRecordFile.readValue<OrtResult>()
 
-        "contain all licenses without excludes" {
-            val expectedResultFile = File("src/test/assets/npm-test-without-exclude-expected-NOTICE")
-            val scanRecordFile = File("src/test/assets/npm-test-without-exclude-scan-results.yml")
-            val ortResult = scanRecordFile.readValue<OrtResult>()
-            val outputDir = createTempDir().also { it.deleteOnExit() }
+                NoticeReporter().generateReport(ortResult, DefaultResolutionProvider(), tempDir)
 
-            NoticeReporter().generateReport(ortResult, DefaultResolutionProvider(), outputDir)
+                val resultFile = File(tempDir, "NOTICE")
+                val actualText = resultFile.readText()
 
-            val resultFile = File(outputDir, "NOTICE")
+                actualText shouldBe expectedText
+            }
 
-            resultFile.readText() shouldBe expectedResultFile.readText()
-        }
+            "contain all licenses without excludes" {
+                val expectedResultFile = File("src/test/assets/npm-test-without-exclude-expected-NOTICE")
+                val scanRecordFile = File("src/test/assets/npm-test-without-exclude-scan-results.yml")
+                val ortResult = scanRecordFile.readValue<OrtResult>()
 
-        "not contain licenses of excluded packages" {
-            val expectedResultFile = File("src/test/assets/npm-test-with-exclude-expected-NOTICE")
-            val scanRecordFile = File("src/test/assets/npm-test-with-exclude-scan-results.yml")
-            val ortResult = scanRecordFile.readValue<OrtResult>()
-            val outputDir = createTempDir().also { it.deleteOnExit() }
+                NoticeReporter().generateReport(ortResult, DefaultResolutionProvider(), tempDir)
 
-            NoticeReporter().generateReport(ortResult, DefaultResolutionProvider(), outputDir)
+                val resultFile = File(tempDir, "NOTICE")
 
-            val resultFile = File(outputDir, "NOTICE")
+                resultFile.readText() shouldBe expectedResultFile.readText()
+            }
 
-            resultFile.readText() shouldBe expectedResultFile.readText()
-        }
+            "not contain licenses of excluded packages" {
+                val expectedResultFile = File("src/test/assets/npm-test-with-exclude-expected-NOTICE")
+                val scanRecordFile = File("src/test/assets/npm-test-with-exclude-scan-results.yml")
+                val ortResult = scanRecordFile.readValue<OrtResult>()
 
-        "evaluate the provided post-processing script" {
-            val expectedResultFile = File("src/test/assets/post-processed-NOTICE")
-            val expectedText = expectedResultFile.readText()
-            val scanRecordFile = File("src/test/assets/NPM-is-windows-1.0.2-scan-result.json")
-            val ortResult = scanRecordFile.readValue<OrtResult>()
-            val outputDir = createTempDir().also { it.deleteOnExit() }
 
-            val postProcessingScript = """
+                NoticeReporter().generateReport(ortResult, DefaultResolutionProvider(), tempDir)
+
+                val resultFile = File(tempDir, "NOTICE")
+
+                resultFile.readText() shouldBe expectedResultFile.readText()
+            }
+
+            "evaluate the provided post-processing script" {
+                val expectedResultFile = File("src/test/assets/post-processed-NOTICE")
+                val expectedText = expectedResultFile.readText()
+                val scanRecordFile = File("src/test/assets/NPM-is-windows-1.0.2-scan-result.json")
+                val ortResult = scanRecordFile.readValue<OrtResult>()
+
+                val postProcessingScript = """
                 headers += "Header 1\n"
                 headers += "Header 2\n"
 
@@ -88,12 +102,13 @@ class NoticeReporterTest : WordSpec({
                 footers += "Footer 2\n"
             """.trimIndent()
 
-            NoticeReporter().generateReport(ortResult, DefaultResolutionProvider(), outputDir, postProcessingScript)
+                NoticeReporter().generateReport(ortResult, DefaultResolutionProvider(), tempDir, postProcessingScript)
 
-            val resultFile = File(outputDir, "NOTICE")
-            val actualText = resultFile.readText()
+                val resultFile = File(tempDir, "NOTICE")
+                val actualText = resultFile.readText()
 
-            actualText shouldBe expectedText
+                actualText shouldBe expectedText
+            }
         }
     }
-})
+}
