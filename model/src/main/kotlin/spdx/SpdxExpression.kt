50732ee1e96e69a8c80bc83018e7955e6cb7671b
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-11-28 11:06:45 +0100

    model: Add a parser for SPDX expressions
    Implement a parser for SPDX expressions as specified in [1] using ANTLR
    [2]. This is a prerequisite for implementing the evaluation of SPDX
    expressions which will be used by upcoming features like evaluating the
    compatibility of a license choice with an SPDX expression.
    
    [1] https://spdx.org/spdx-specification-21-web-version#h.jxpfx0ykyb60
    [2] https://www.antlr.org/
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ model/src/main/kotlin/spdx/SpdxExpression.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model.spdx
+
+import org.antlr.v4.runtime.BaseErrorListener
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.RecognitionException
+import org.antlr.v4.runtime.Recognizer
+
+sealed class SpdxExpression
+
+data class SpdxCompoundExpression(
+        val left: SpdxExpression,
+        val operator: SpdxOperator,
+        val right: SpdxExpression
+) : SpdxExpression()
+
+data class SpdxLicenseExceptionExpression(
+        val id: String
+) : SpdxExpression()
+
+data class SpdxLicenseIdExpression(
+        val id: String,
+        val anyLaterVersion: Boolean = false
+) : SpdxExpression()
+
+data class SpdxLicenseRefExpression(
+        val id: String
+) : SpdxExpression()
+
+enum class SpdxOperator {
+    AND,
+    OR,
+    WITH
+}
+
+fun parseSpdxExpression(expression: String): SpdxExpression {
+    val charStream = CharStreams.fromString(expression)
+    val lexer = SpdxExpressionLexer(charStream)
+
+    lexer.addErrorListener(object : BaseErrorListener() {
+        override fun syntaxError(
+                recognizer: Recognizer<*, *>?,
+                offendingSymbol: Any?,
+                line: Int,
+                charPositionInLine: Int,
+                msg: String?,
+                e: RecognitionException?
+        ) {
+            throw SpdxException(msg)
+        }
+    })
+
+    val tokenStream = CommonTokenStream(lexer)
+    val parser = SpdxExpressionParser(tokenStream)
+    val visitor = SpdxExpressionDefaultVisitor()
+
+    return visitor.visit(parser.licenseExpression())
+}
