07e6d5f1d051c0795587b2160e56660c5e165d4b
Frank Viernau <frank.viernau@here.com>
2018-11-29 12:16:52 +0100

    ResolvableError: Do not misuse toString() anymore
    The error description to be put into the HTML report deserves a dedicated
    function which leads to more readable client code and brings all the
    advantages of not relying on a 'toString()' implementation.
    
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

--- reporter/src/main/kotlin/reporters/TableReporter.kt
+++ reporter/src/main/kotlin/reporters/TableReporter.kt
@@ -222,7 +222,7 @@ abstract class TableReporter : Reporter() {
             val error: OrtIssue,
             val resolutions: List<ErrorResolution>
     ) {
-        override fun toString() =
+        fun getDescription() =
                 buildString {
                     append(error)
                     if (resolutions.isNotEmpty()) {
