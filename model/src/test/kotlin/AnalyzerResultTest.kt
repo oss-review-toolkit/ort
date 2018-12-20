bc41ef11562be5d4c7b8e1be3ac7238da2bf18a7
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2018-12-20 16:23:35 +0100

    AnalyzerResult: Add a function to collect all errors
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

--- model/src/test/kotlin/AnalyzerResultTest.kt
+++ model/src/test/kotlin/AnalyzerResultTest.kt
@@ -25,12 +25,19 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
 class AnalyzerResultTest : WordSpec() {
+    private val error1 = OrtIssue(source = "source-1", message = "message-1")
+    private val error2 = OrtIssue(source = "source-2", message = "message-2")
+    private val error3 = OrtIssue(source = "source-3", message = "message-3")
+    private val error4 = OrtIssue(source = "source-4", message = "message-4")
+
     private val package1 = Package.EMPTY.copy(id = Identifier("type-1", "namespace-1", "package-1", "version-1"))
     private val package2 = Package.EMPTY.copy(id = Identifier("type-2", "namespace-2", "package-2", "version-2"))
     private val package3 = Package.EMPTY.copy(id = Identifier("type-3", "namespace-3", "package-3", "version-3"))
 
-    private val pkgRef1 = package1.toReference()
-    private val pkgRef2 = package2.toReference(dependencies = sortedSetOf(package3.toReference()))
+    private val pkgRef1 = package1.toReference(errors = listOf(error1))
+    private val pkgRef2 = package2.toReference(
+            dependencies = sortedSetOf(package3.toReference(errors = listOf(error2)))
+    )
 
     private val scope1 = Scope("scope-1", sortedSetOf(pkgRef1))
     private val scope2 = Scope("scope-2", sortedSetOf(pkgRef2))
@@ -44,14 +51,11 @@ class AnalyzerResultTest : WordSpec() {
             scopes = sortedSetOf(scope1, scope2)
     )
 
-    private val error1 = OrtIssue(source = "source-1", message = "message-1")
-    private val error2 = OrtIssue(source = "source-2", message = "message-2")
-
     private val analyzerResult1 = ProjectAnalyzerResult(project1, sortedSetOf(package1.toCuratedPackage()),
-            listOf(error1, error2))
+            listOf(error3, error4))
     private val analyzerResult2 = ProjectAnalyzerResult(project2,
             sortedSetOf(package1.toCuratedPackage(), package2.toCuratedPackage(), package3.toCuratedPackage()),
-            listOf(error2))
+            listOf(error4))
 
     init {
         "AnalyzerResult" should {
@@ -65,6 +69,22 @@ class AnalyzerResultTest : WordSpec() {
                 val deserializedMergedResults = yamlMapper.readValue<AnalyzerResult>(serializedMergedResults)
 
                 deserializedMergedResults shouldBe mergedResults
+            }
+        }
+
+        "collectErrors" should {
+            "find all errors" {
+                val analyzerResult = AnalyzerResultBuilder()
+                        .addResult(analyzerResult1)
+                        .addResult(analyzerResult2)
+                        .build()
+
+                analyzerResult.collectErrors() shouldBe mapOf(
+                        package1.id to setOf(error1),
+                        package3.id to setOf(error2),
+                        project1.id to setOf(error3, error4),
+                        project2.id to setOf(error4)
+                )
             }
         }
 
