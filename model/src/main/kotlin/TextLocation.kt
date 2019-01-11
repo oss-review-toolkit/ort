7cb45a9570f4fcdc5883d72148a415db789902a1
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-01-11 14:03:38 +0100

    model: Add `TextLocation` class
    The class can be used to reference text located in a file.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ model/src/main/kotlin/TextLocation.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2017-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model
+
+/**
+ * A [TextLocation] references text located in a file.
+ */
+data class TextLocation(
+        /**
+         * The path of the file that contains the text.
+         */
+        val path: String,
+
+        /**
+         * The line the text is starting at.
+         */
+        val startLine: Int,
+
+        /**
+         * The line the text is ending at.
+         */
+        val endLine: Int
+) : Comparable<TextLocation> {
+    companion object {
+        private val COMPARATOR = compareBy<TextLocation>({ it.path }, { it.startLine }, { it.endLine })
+    }
+
+    override fun compareTo(other: TextLocation) = COMPARATOR.compare(this, other)
+}
