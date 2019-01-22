f614eb58b3fd243921e135a4910268d229494bb5
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-01-22 14:33:36 +0100

    Add timestamps to `EvaluatorRun`
    Add timestamps for the start and end time to the `EvaluatorRun` object.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ model/src/test/kotlin/EvaluatorRunTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model
+
+import com.fasterxml.jackson.module.kotlin.readValue
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+import java.time.Instant
+
+class EvaluatorRunTest : StringSpec() {
+    init {
+        "EvaluatorRun without timestamps can be deserialized" {
+            val yaml = """
+                ---
+                errors: []
+            """.trimIndent()
+
+            val evaluatorRun = yamlMapper.readValue<EvaluatorRun>(yaml)
+
+            evaluatorRun.startTime shouldBe Instant.EPOCH
+            evaluatorRun.endTime shouldBe Instant.EPOCH
+        }
+
+        "EvaluatorRun with timestamps can be deserialized" {
+            val yaml = """
+                ---
+                start_time: "1970-01-01T00:00:10Z"
+                end_time: "1970-01-01T00:00:10Z"
+                errors: []
+            """.trimIndent()
+
+            val evaluatorRun = yamlMapper.readValue<EvaluatorRun>(yaml)
+
+            evaluatorRun.startTime shouldBe Instant.ofEpochSecond(10)
+            evaluatorRun.endTime shouldBe Instant.ofEpochSecond(10)
+        }
+    }
+}
