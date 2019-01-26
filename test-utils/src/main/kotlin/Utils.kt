a36af92d658411c108e062a7e2c799bffb420300
Sebastian Schuberth <sebastian.schuberth@here.com>
2019-01-26 13:48:57 +0100

    test-utils: Do the custom replacement before any standard replacement
    This way a custom replacement can override any standard replacement.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@here.com>

--- test-utils/src/main/kotlin/Utils.kt
+++ test-utils/src/main/kotlin/Utils.kt
@@ -43,8 +43,8 @@ fun patchExpectedResult(result: File, custom: Pair<String, String>? = null, defi
             if (newValue != null) replace(oldValue, newValue) else this
 
     return result.readText()
-            .replaceIfNotNull("<REPLACE_OS>", System.getProperty("os.name"))
             .replaceIfNotNull(custom)
+            .replaceIfNotNull("<REPLACE_OS>", System.getProperty("os.name"))
             .replaceIfNotNull("<REPLACE_DEFINITION_FILE_PATH>", definitionFilePath)
             .replaceIfNotNull("<REPLACE_URL>", url)
             .replaceIfNotNull("<REPLACE_REVISION>", revision)
