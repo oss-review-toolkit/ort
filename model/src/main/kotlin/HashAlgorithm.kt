ac3d9f85f092dae29783c120c09095ec8fb258c8
Sebastian Schuberth <sebastian.schuberth@here.com>
2019-02-02 13:39:10 +0100

    HashAlgorithm: Document the whole enum
    Also trivially rename the fromHash() parameter to "value" as that makes a
    more readable documentation when referring to it.
    
    Note that overriding toString() for this enum, which otherwise is
    discouraged as its implicit calls are hard to refactor, is the correct way
    to associate an enum entry with a custom string. The name as a string can
    still be determined by calling name().
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@here.com>

--- model/src/main/kotlin/HashAlgorithm.kt
+++ model/src/main/kotlin/HashAlgorithm.kt
@@ -22,15 +22,44 @@ package com.here.ort.model
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
 
+/**
+ * An enum of supported hash algorithms.
+ */
 enum class HashAlgorithm(private vararg val aliases: String) {
+    /**
+     * An unknown hash algorithm.
+     */
     UNKNOWN("", "UNKNOWN"),
+
+    /**
+     * The Message-Digest 5 hash algorithm, see https://en.wikipedia.org/wiki/MD5.
+     */
     MD5("MD5"),
+
+    /**
+     * The Secure Hash Algorithm 1, see https://en.wikipedia.org/wiki/SHA-1.
+     */
     SHA1("SHA-1", "SHA1"),
+
+    /**
+     * The Secure Hash Algorithm 2 with 256 bits, see https://en.wikipedia.org/wiki/SHA-256.
+     */
     SHA256("SHA-256", "SHA256"),
+
+    /**
+     * The Secure Hash Algorithm 2 with 384 bits, see https://en.wikipedia.org/wiki/SHA-384.
+     */
     SHA384("SHA-384", "SHA384"),
+
+    /**
+     * The Secure Hash Algorithm 2 with 512 bits, see https://en.wikipedia.org/wiki/SHA-512.
+     */
     SHA512("SHA-512", "SHA512");
 
     companion object {
+        /**
+         * Create a hash algorithm from one of its [alias] names.
+         */
         @JsonCreator
         @JvmStatic
         fun fromString(alias: String) =
@@ -38,10 +67,13 @@ enum class HashAlgorithm(private vararg val aliases: String) {
                     alias.toUpperCase() in it.aliases
                 } ?: UNKNOWN
 
-        fun fromHash(hash: String): HashAlgorithm {
-            if (hash.isBlank()) return HashAlgorithm.UNKNOWN
+        /**
+         * Create a hash algorithm from a hash [value].
+         */
+        fun fromHash(value: String): HashAlgorithm {
+            if (value.isBlank()) return HashAlgorithm.UNKNOWN
 
-            return when (hash.length) {
+            return when (value.length) {
                 128 -> SHA512
                 96 -> SHA384
                 64 -> SHA256
@@ -52,6 +84,9 @@ enum class HashAlgorithm(private vararg val aliases: String) {
         }
     }
 
+    /**
+     * Convert the hash algorithm to a string representation.
+     */
     @JsonValue
     override fun toString(): String = aliases.first()
 }
