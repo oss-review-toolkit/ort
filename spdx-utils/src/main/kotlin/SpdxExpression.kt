83bc1d43b38506cf35901ff2cf4f1af20ecb111f
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-02-11 17:38:12 +0100

    SpdxLicenseMapping: Change mapping to SpdxExpression
    Instead of mapping declared licenses to sets of SPDX licenses map directly
    to SPDX expressions. This allows to keep the operators from the original
    declared license.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

--- spdx-utils/src/main/kotlin/SpdxExpression.kt
+++ spdx-utils/src/main/kotlin/SpdxExpression.kt
@@ -116,6 +116,16 @@ sealed class SpdxExpression {
      * ([ALLOW_CURRENT][Strictness.ALLOW_CURRENT]). Throws an [SpdxException] if validation fails.
      */
     abstract fun validate(strictness: Strictness)
+
+    /**
+     * Concatenate [this][SpdxExpression] and [other] using [SpdxOperator.AND].
+     */
+    infix fun and(other: SpdxExpression) = SpdxCompoundExpression(this, SpdxOperator.AND, other)
+
+    /**
+     * Concatenate [this][SpdxExpression] and [other] using [SpdxOperator.OR].
+     */
+    infix fun or(other: SpdxExpression) = SpdxCompoundExpression(this, SpdxOperator.OR, other)
 }
 
 /**
@@ -203,6 +213,11 @@ data class SpdxLicenseIdExpression(
             Strictness.ALLOW_CURRENT -> license?.takeUnless { license.deprecated }
         } ?: throw SpdxException("'$id' is not a valid SPDX license id.")
     }
+
+    /**
+     * Concatenate [this][SpdxLicenseIdExpression] and [other] using [SpdxOperator.WITH].
+     */
+    infix fun with(other: SpdxLicenseExceptionExpression) = SpdxCompoundExpression(this, SpdxOperator.WITH, other)
 
     override fun toString() =
             buildString {
