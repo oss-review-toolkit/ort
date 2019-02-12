31e969145c480e3fbaa2d8d612111d6d076921aa
Sebastian Schuberth <sebastian.schuberth@here.com>
2019-02-12 21:38:39 +0100

    Environment: Revise the list of relevant environment variables
    OSTYPE does not seem to be set on Linux, add TERM instead, plus GOPATH
    and JAVA_HOME.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@here.com>

--- model/src/main/kotlin/Environment.kt
+++ model/src/main/kotlin/Environment.kt
@@ -52,6 +52,16 @@ data class Environment(
     companion object {
         val ORT_VERSION = this::class.java.getResource("/VERSION").readText()
 
-        private val RELEVANT_VARIABLES = listOf("COMSPEC", "OS", "OSTYPE", "SHELL")
+        private val RELEVANT_VARIABLES = listOf(
+                // Windows variables.
+                "OS",
+                "COMSPEC",
+                // Unix variables.
+                "SHELL",
+                "TERM",
+                // General variables.
+                "GOPATH",
+                "JAVA_HOME"
+        )
     }
 }
