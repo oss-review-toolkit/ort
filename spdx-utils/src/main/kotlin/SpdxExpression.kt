b435c6482b0a646f5ac7ad85f6c886cb874968dc
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-02-13 14:24:53 +0100

    SpdxExpression: Add a normalize function
    Add a normalize function that uses the `SpdxLicenseIdMapping` to fix
    misspelled SPDX license IDs.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

--- spdx-utils/src/main/kotlin/SpdxExpression.kt
+++ spdx-utils/src/main/kotlin/SpdxExpression.kt
@@ -110,6 +110,13 @@ sealed class SpdxExpression {
     abstract fun spdxLicenses(): EnumSet<SpdxLicense>
 
     /**
+     * Normalize all license IDs using a mapping containing common misspellings of license IDs. The result of this
+     * function is not guaranteed to contain only valid IDs. Use [validate] to check the returned [SpdxExpression] for
+     * validity afterwards.
+     */
+    abstract fun normalize(): SpdxExpression
+
+    /**
      * Validate this expression. [strictness] defines whether only the syntax is checked
      * ([ALLOW_ANY][Strictness.ALLOW_ANY]), or semantics are also checked but deprecated license identifiers are
      * allowed ([ALLOW_DEPRECATED][Strictness.ALLOW_DEPRECATED]) or only current license identifiers are allowed
@@ -139,6 +146,8 @@ data class SpdxCompoundExpression(
     override fun licenses() = left.licenses() + right.licenses()
 
     override fun spdxLicenses() = left.spdxLicenses() + right.spdxLicenses()
+
+    override fun normalize() = SpdxCompoundExpression(left.normalize(), operator, right.normalize())
 
     override fun validate(strictness: Strictness) {
         left.validate(strictness)
@@ -181,6 +190,8 @@ data class SpdxLicenseExceptionExpression(
 
     override fun spdxLicenses() = enumSetOf<SpdxLicense>()
 
+    override fun normalize() = this
+
     override fun validate(strictness: Strictness) {
         val licenseException = SpdxLicenseException.forId(id)
         when (strictness) {
@@ -204,6 +215,8 @@ data class SpdxLicenseIdExpression(
     override fun licenses() = listOf(id)
 
     override fun spdxLicenses() = SpdxLicense.forId(id)?.let { enumSetOf(it) } ?: enumSetOf()
+
+    override fun normalize() = SpdxLicenseIdMapping.map(id)?.toExpression() ?: this
 
     override fun validate(strictness: Strictness) {
         val license = SpdxLicense.forId(id)
@@ -236,6 +249,8 @@ data class SpdxLicenseReferenceExpression(
     override fun licenses() = listOf(id)
 
     override fun spdxLicenses() = enumSetOf<SpdxLicense>()
+
+    override fun normalize() = this
 
     override fun validate(strictness: Strictness) {
         if (!(id.startsWith("LicenseRef-") ||
