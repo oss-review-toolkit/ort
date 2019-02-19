25c203c7ef15eb98b525690a4fa20a8b6762905a
Sebastian Schuberth <sebastian.schuberth@here.com>
2019-02-19 10:10:01 +0100

    utils: Add a test for ProcessCapture
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@here.com>

---
+++ utils/src/test/kotlin/ProcessCaptureTest.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.utils
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class ProcessCaptureTest : StringSpec({
+    "Environment variables should be passed correctly" {
+        val env = mapOf("PREFIX" to "This is some path: ", "SOME_PATH" to "/foo/bar")
+        val proc = if (OS.isWindows) {
+            ProcessCapture("cmd.exe", "/c", "echo %PREFIX%%SOME_PATH%", environment = env)
+        } else {
+            ProcessCapture("sh", "-c", "echo \$PREFIX\$SOME_PATH", environment = env)
+        }
+
+        proc.exitValue shouldBe 0
+        proc.stdout.trimEnd() shouldBe "This is some path: /foo/bar"
+    }
+})
