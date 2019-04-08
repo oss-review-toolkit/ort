3b6706e02bed018c1243a8a1063d06df3b919395
Sebastian Schuberth <sebastian.schuberth@here.com>
2019-04-08 09:12:18 +0200

    Gradle: Migrate the "analyzer" module's build file to Kotlin script
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@here.com>

---
+++ analyzer/build.gradle.kts
@@ -0,0 +1,39 @@
+val digraphVersion: String by project
+val jacksonVersion: String by project
+val mavenVersion: String by project
+val mavenResolverVersion: String by project
+val semverVersion: String by project
+val toml4jVersion: String by project
+
+plugins {
+    // Apply core plugins.
+    id("java-library")
+}
+
+repositories {
+    maven("https://repo.gradle.org/gradle/libs-releases-local/")
+}
+
+dependencies {
+    api(project(":model"))
+
+    implementation(project(":downloader"))
+    implementation(project(":utils"))
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.moandjiezana.toml:toml4j:$toml4jVersion")
+    implementation("com.paypal.digraph:digraph-parser:$digraphVersion")
+    implementation("com.vdurmont:semver4j:$semverVersion")
+
+    implementation("org.apache.maven:maven-core:$mavenVersion")
+    implementation("org.apache.maven:maven-compat:$mavenVersion")
+
+    // The classes from the maven-resolver dependencies are not used directly but initialized by the Plexus IoC
+    // container automatically. They are required on the classpath for Maven dependency resolution to work.
+    implementation("org.apache.maven.resolver:maven-resolver-connector-basic:$mavenResolverVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-transport-file:$mavenResolverVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-transport-http:$mavenResolverVersion")
+    implementation("org.apache.maven.resolver:maven-resolver-transport-wagon:$mavenResolverVersion")
+
+    implementation("org.gradle:gradle-tooling-api:${gradle.gradleVersion}")
+}
