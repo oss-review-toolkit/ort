91588a35ce22f6387511c80911499c7e21d55d56
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-04-09 18:40:51 +0200

    evaluator: Add a DSL for writing rules
    Add a DSL for describing evaluator rules. This is the first iteration
    that likely needs more refactoring in the near future. Documentation
    will be added later, once the API reaches a stable state.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ evaluator/src/main/kotlin/DependencyRule.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.evaluator
+
+import com.here.ort.model.Identifier
+import com.here.ort.model.LicenseFinding
+import com.here.ort.model.Package
+import com.here.ort.model.PackageLinkage
+import com.here.ort.model.PackageReference
+import com.here.ort.model.Project
+import com.here.ort.model.Scope
+
+/**
+ * A [Rule] to check a single [dependency][PackageReference].
+ */
+class DependencyRule(
+    ruleSet: RuleSet,
+    name: String,
+    pkg: Package,
+    detectedLicenses: List<LicenseFinding>,
+
+    /**
+     * The [dependency][PackageReference] to check.
+     */
+    val dependency: PackageReference,
+
+    /**
+     * The ancestors of the [dependency] in the dependency tree. The first entry is the root of the tree, the last entry
+     * (at [level] - 1) is the direct parent.
+     */
+    val ancestors: List<PackageReference>,
+
+    /**
+     * The level of the [dependency] inside the dependency tree. Starts with 0 for root level entries.
+     */
+    val level: Int,
+
+    /**
+     * The [Scope] that contains the [dependency].
+     */
+    val scope: Scope,
+
+    /**
+     * The [Project] that contains the [dependency].
+     */
+    val project: Project
+) : PackageRule(ruleSet, name, pkg, detectedLicenses) {
+    override val description =
+        "Evaluating rule '$name' for dependency '${dependency.id.toCoordinates()}' " +
+                "(project=${project.id.toCoordinates()}, scope=${scope.name}, level=$level)."
+
+    /**
+     * A [RuleMatcher] that checks if the level of the [dependency] inside the dependency tree equals [level].
+     */
+    fun isAtTreeLevel(level: Int) =
+        object : RuleMatcher {
+            override val description = "isAtTreeLevel($level)"
+
+            override fun matches() = this@DependencyRule.level == level
+        }
+
+    /**
+     * A [RuleMatcher] that checks if the [identifier][Project.id] of the [project] belongs to one of the provided
+     * [orgs][Identifier.isFromOrg].
+     */
+    fun isProjectFromOrg(vararg names: String) =
+        object : RuleMatcher {
+            override val description = "isProjectFromOrg(${names.joinToString()})"
+
+            override fun matches() = project.id.isFromOrg(*names)
+        }
+
+    /**
+     * A [RuleMatcher] that checks if the [dependency] is [statically linked][PackageLinkage].
+     */
+    fun isStaticallyLinked() =
+        object : RuleMatcher {
+            override val description = "isStaticallyLinked()"
+
+            override fun matches() = dependency.linkage in listOf(PackageLinkage.STATIC, PackageLinkage.PROJECT_STATIC)
+        }
+}
---
+++ evaluator/src/main/kotlin/RuleMatcher.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.evaluator
+
+/**
+ * A rule matcher, used to determine if a [Rule] applies.
+ */
+interface RuleMatcher {
+    /**
+     * A string describing this matcher.
+     */
+    val description: String
+
+    /**
+     * Return true if the rule applies, false otherwise.
+     */
+    fun matches(): Boolean
+}
+
+/**
+ * A [RuleMatcher] that requires all of the provided [matchers] to match.
+ */
+class AllOf(vararg val matchers: RuleMatcher) : RuleMatcher {
+    override val description = "(${matchers.joinToString(" && ") { it.description }})"
+
+    override fun matches() = matchers.all { it.matches() }
+}
+
+/**
+ * A [RuleMatcher] that requires at least one of the provided [matchers] to match.
+ */
+class AnyOf(vararg val matchers: RuleMatcher) : RuleMatcher {
+    override val description = "(${matchers.joinToString(" || ") { it.description }})"
+
+    override fun matches() = matchers.any { it.matches() }
+}
+
+/**
+ * A [RuleMatcher] that inverts the result of the provided [matcher].
+ */
+class Not(val matcher: RuleMatcher) : RuleMatcher {
+    override val description = "!(${matcher.description})"
+
+    override fun matches() = !matcher.matches()
+}
