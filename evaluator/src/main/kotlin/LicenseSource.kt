0bdffa6d3cc089d25f53b0a250719df8a96b6cf6
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-04-09 18:40:51 +0200

    evaluator: Add a DSL for writing rules
    Add a DSL for describing evaluator rules. This is the first iteration
    that likely needs more refactoring in the near future. Documentation
    will be added later, once the API reaches a stable state.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ evaluator/src/main/kotlin/LicenseSource.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.evaluator
+
+import com.here.ort.model.Package
+
+/**
+ * The source where a license originates from.
+ */
+enum class LicenseSource {
+    /**
+     * Licenses which are part of the [concluded license][Package.concludedLicense] of a [Package].
+     */
+    CONCLUDED,
+
+    /**
+     * Licenses which are part of the [(processed)][Package.declaredLicensesProcessed]
+     * [declared licenses][Package.declaredLicenses] of a [Package].
+     */
+    DECLARED,
+
+    /**
+     * Licenses which were detected by a license scanner.
+     */
+    DETECTED
+}
