1b8ba335554efdb07e210a08f4e9a0d1300d5261
Sebastian Schuberth <sebastian.schuberth@here.com>
2019-04-09 11:32:35 +0200

    Gradle: Migrate the settings file to Kotlin script
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@here.com>

---
+++ settings.gradle.kts
@@ -0,0 +1,37 @@
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            // Work around https://github.com/gradle/gradle/issues/1697.
+            if (requested.id.namespace != null && requested.version == null) {
+                val versionPropertyName = if (requested.id.id == "org.jetbrains.kotlin.jvm") {
+                    "kotlinPluginVersion"
+                } else {
+                    val pluginName = requested.id.name.split('-').joinToString("") { it.capitalize() }.decapitalize()
+                    "${pluginName}PluginVersion"
+                }
+
+                logger.info("Checking for plugin version property '$versionPropertyName'.")
+
+                gradle.rootProject.properties[versionPropertyName]?.let { version ->
+                    logger.info("Setting '${requested.id.id}' plugin version to $version.")
+                    useVersion(version.toString())
+                } ?: logger.warn("No version specified for plugin '${requested.id.id}' and property " +
+                        "'$versionPropertyName' does not exist.")
+            }
+        }
+    }
+}
+
+rootProject.name = "oss-review-toolkit"
+
+include("analyzer")
+include("cli")
+include("downloader")
+include("evaluator")
+include("model")
+include("reporter")
+include("reporter-web-app")
+include("scanner")
+include("spdx-utils")
+include("test-utils")
+include("utils")
