db58e3a4e6fee0b1a86f7362bddbb88170d49906
Sebastian Schuberth <sebastian.schuberth@here.com>
2019-04-25 11:36:03 +0200

    DotNet/NuGet: Make code to read XML more Kotlin-ish
    Note that we cannot use our xmlMapper intended for writing / reading
    OrtResults here as it uses the wrong property naming strategy.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@here.com>

--- analyzer/src/funTest/kotlin/NuGetTest.kt
+++ analyzer/src/funTest/kotlin/NuGetTest.kt
@@ -69,7 +69,7 @@ class NuGetTest : StringSpec() {
 
         "Definition File is correctly mapped" {
             val mapper = XmlMapper().registerKotlinModule()
-            val result: NuGet.Companion.PackagesConfig = mapper.readValue(packageFile)
+            val result = mapper.readValue<NuGet.Companion.PackagesConfig>(packageFile)
 
             result shouldNotBe null
             result.packages shouldNotBe null
