f7a1a83d3833d2c211e30d3b9c5fba7bdec86814
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-05-02 20:27:32 +0200

    analyzer: Update expected results for DotNet and NuGet tests
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

--- analyzer/src/funTest/kotlin/DotNetSupportTest.kt
+++ analyzer/src/funTest/kotlin/DotNetSupportTest.kt
@@ -47,7 +47,7 @@ class DotNetSupportTest : StringSpec() {
                             type = "nuget",
                             namespace = "",
                             name = "jQuery",
-                            version = "3.3.1"
+                            version = "3.4.0"
                         )
                     )
                 )
