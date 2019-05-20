523d2d9eed4da742395da170dbc58f1f19ba6149
Sebastian Schuberth <sschuberth@gmail.com>
2019-05-20 17:11:04 +0200

    scanner: Rename getResult() to getRawResult()
    To better match the purpose and the corresponding "rawResult" property
    of the ScanResult class.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- scanner/src/main/kotlin/scanners/BoyterLc.kt
+++ scanner/src/main/kotlin/scanners/BoyterLc.kt
@@ -145,7 +145,7 @@ class BoyterLc(name: String, config: ScannerConfiguration) : LocalScanner(name, 
 
         with(process) {
             if (isSuccess) {
-                val result = getResult(resultsFile)
+                val result = getRawResult(resultsFile)
                 val summary = generateSummary(startTime, endTime, result)
                 return ScanResult(Provenance(), getDetails(), summary, result)
             } else {
@@ -154,7 +154,7 @@ class BoyterLc(name: String, config: ScannerConfiguration) : LocalScanner(name, 
         }
     }
 
-    override fun getResult(resultsFile: File): JsonNode {
+    override fun getRawResult(resultsFile: File): JsonNode {
         return if (resultsFile.isFile && resultsFile.length() > 0L) {
             jsonMapper.readTree(resultsFile)
         } else {
