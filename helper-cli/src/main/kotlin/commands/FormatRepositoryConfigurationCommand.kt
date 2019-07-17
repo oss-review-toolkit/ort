681bd649b1b16e5ec8f0bf6130940cf15324986a
Frank Viernau <frank.viernau@here.com>
2019-07-17 15:11:01 +0200

    Introduce a `cli-helpers` module along with an initial command
    The purpose of this new module is to provide a command line interface
    offering helper commands for working with ORT such as e.g. working with
    global or local ORT configuration files and OrtResult files.
    
    It is important to note that ORT configuration files are likely to be
    put under version control. This should be considered when deciding about
    the granularity when designing (new) helper commands, e.g. if a command
    does many things at once then the output would make up a non-atomic commit.
    
    Thus it makes sense to have an initial command which just applies the default
    fromatting used across all (future) helper commands.
    
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

---
+++ helper-cli/src/main/kotlin/commands/FormatRepositoryConfigurationCommand.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.helper.commands
+
+import com.beust.jcommander.JCommander
+import com.beust.jcommander.Parameter
+import com.beust.jcommander.Parameters
+import com.beust.jcommander.converters.FileConverter
+
+import com.here.ort.CommandWithHelp
+import com.here.ort.helper.common.writeAsYaml
+import com.here.ort.model.config.RepositoryConfiguration
+import com.here.ort.model.readValue
+import com.here.ort.utils.PARAMETER_ORDER_MANDATORY
+
+import java.io.File
+
+@Parameters(
+    commandNames = ["format-repository-configuration"],
+    commandDescription = "Applies the formatting used by all ort-helper commands and strips all YAML comments." +
+        "The output is written to the given repository configuration file."
+)
+internal class FormatRepositoryConfigurationCommand : CommandWithHelp() {
+    @Parameter(
+        description = "repository configuration file.",
+        required = true,
+        order = PARAMETER_ORDER_MANDATORY,
+        converter = FileConverter::class
+    )
+    private lateinit var repositoryConfigurationFile: File
+
+    override fun runCommand(jc: JCommander): Int {
+        repositoryConfigurationFile
+            .readValue<RepositoryConfiguration>()
+            .writeAsYaml(repositoryConfigurationFile)
+
+        return 0
+    }
+}
