37a4733a3e77d02adbcacd67cae8367146daf07a
Sebastian Schuberth <sebastian.schuberth@bosch-si.com>
2019-08-06 13:40:06 +0200

    Mercurial: Port to the new finer-grained VCS API
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch-si.com>

--- downloader/src/main/kotlin/vcs/Mercurial.kt
+++ downloader/src/main/kotlin/vcs/Mercurial.kt
@@ -21,15 +21,15 @@ package com.here.ort.downloader.vcs
 
 import ch.frankel.slf4k.*
 
-import com.here.ort.downloader.DownloadException
 import com.here.ort.downloader.VersionControlSystem
 import com.here.ort.downloader.WorkingTree
-import com.here.ort.model.Package
+import com.here.ort.model.VcsInfo
 import com.here.ort.model.VcsType
 import com.here.ort.spdx.LICENSE_FILE_NAMES
 import com.here.ort.utils.CommandLineTool
 import com.here.ort.utils.ProcessCapture
 import com.here.ort.utils.log
+import com.here.ort.utils.showStackTrace
 
 import java.io.File
 import java.io.IOException
@@ -99,78 +99,56 @@ class Mercurial : VersionControlSystem(), CommandLineTool {
     override fun isApplicableUrlInternal(vcsUrl: String) =
         ProcessCapture("hg", "identify", vcsUrl).isSuccess
 
-    override fun download(
-        pkg: Package, targetDir: File, allowMovingRevisions: Boolean,
-        recursive: Boolean
-    ): WorkingTree {
-        log.info { "Using $type version ${getVersion()}." }
+    override fun initWorkingTree(targetDir: File, vcs: VcsInfo): WorkingTree {
+        // We cannot detect beforehand if the Large Files extension would be required, so enable it by default.
+        val extensionsList = mutableListOf(MERCURIAL_LARGE_FILES_EXTENSION)
 
-        try {
-            // We cannot detect beforehand if the Large Files extension would be required, so enable it by default.
-            val extensionsList = mutableListOf(MERCURIAL_LARGE_FILES_EXTENSION)
+        if (vcs.path.isNotBlank() && isAtLeastVersion("4.3")) {
+            // Starting with version 4.3 Mercurial has experimental built-in support for sparse checkouts, see
+            // https://www.mercurial-scm.org/wiki/WhatsNew#Mercurial_4.3_.2F_4.3.1_.282017-08-10.29
+            extensionsList += MERCURIAL_SPARSE_EXTENSION
+        }
 
-            if (pkg.vcsProcessed.path.isNotBlank() && isAtLeastVersion("4.3")) {
-                // Starting with version 4.3 Mercurial has experimental built-in support for sparse checkouts, see
-                // https://www.mercurial-scm.org/wiki/WhatsNew#Mercurial_4.3_.2F_4.3.1_.282017-08-10.29
-                extensionsList += MERCURIAL_SPARSE_EXTENSION
-            }
-
-            run(targetDir, "init")
-            File(targetDir, ".hg/hgrc").writeText(
-                """
+        run(targetDir, "init")
+        File(targetDir, ".hg/hgrc").writeText(
+            """
                 [paths]
-                default = ${pkg.vcsProcessed.url}
+                default = ${vcs.url}
                 [extensions]
 
                 """.trimIndent() + extensionsList.joinToString("\n")
+        )
+
+        if (MERCURIAL_SPARSE_EXTENSION in extensionsList) {
+            log.info { "Configuring Mercurial to do sparse checkout of path '${vcs.path}'." }
+            run(
+                targetDir, "debugsparse", "-I", "${vcs.path}/**",
+                *LICENSE_FILE_NAMES.flatMap { listOf("-I", it) }.toTypedArray()
             )
+        }
 
-            if (MERCURIAL_SPARSE_EXTENSION in extensionsList) {
-                log.info { "Configuring Mercurial to do sparse checkout of path '${pkg.vcsProcessed.path}'." }
-                run(
-                    targetDir, "debugsparse", "-I", "${pkg.vcsProcessed.path}/**",
-                    *LICENSE_FILE_NAMES.flatMap { listOf("-I", it) }.toTypedArray()
-                )
-            }
+        return getWorkingTree(targetDir)
+    }
 
-            val workingTree = getWorkingTree(targetDir)
-
-            val revision = if (allowMovingRevisions || isFixedRevision(workingTree, pkg.vcsProcessed.revision)) {
-                pkg.vcsProcessed.revision
-            } else {
-                log.info { "Trying to guess a $type revision for version '${pkg.id.version}'." }
-                try {
-                    workingTree.guessRevisionName(pkg.id.name, pkg.id.version).also { revision ->
-                        log.warn {
-                            "Using guessed $type revision '$revision' for version '${pkg.id.version}'. This might " +
-                                    "cause the downloaded source code to not match the package version."
-                        }
-                    }
-                } catch (e: IOException) {
-                    throw IOException("Unable to determine a revision to checkout.", e)
-                }
-            }
-
+    override fun updateWorkingTree(workingTree: WorkingTree, revision: String, recursive: Boolean): Boolean {
+        return try {
             // To safe network bandwidth, only pull exactly the revision we want. Do not use "-u" to update the
             // working tree just yet, as Mercurial would only update if new changesets were pulled. But that might
             // not be the case if the requested revision is already available locally.
-            run(targetDir, "pull", "-r", revision)
+            run(workingTree.workingDir, "pull", "-r", revision)
 
             // Explicitly update the working tree to the desired revision.
-            run(targetDir, "update", revision)
+            run(workingTree.workingDir, "update", revision)
 
-            pkg.vcsProcessed.path.let {
-                if (it.isNotEmpty() && !workingTree.workingDir.resolve(it).exists()) {
-                    throw DownloadException(
-                        "The $type working directory at '${workingTree.workingDir}' does not " +
-                                "contain the requested path '$it'."
-                    )
-                }
-            }
+            // TODO: Implement updating of subrepositories.
 
-            return workingTree
+            true
         } catch (e: IOException) {
-            throw DownloadException("$type failed to download from ${pkg.vcsProcessed.url}.", e)
+            e.showStackTrace()
+
+            log.warn { "Failed to update $type working tree to revision '$revision': ${e.message}" }
+
+            false
         }
     }
 }
