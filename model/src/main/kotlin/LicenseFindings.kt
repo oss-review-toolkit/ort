6de0123fcfbfdc5267e90113c9c37d3a5711d4c8
Frank Viernau <frank.viernau@here.com>
2019-08-29 15:28:21 +0200

    LicenseFindings: Remove the unused LicenseFindingsDeserializer
    Due to recent changes in the ScanSummary class the LicenseFindings are
    not serialized anymore at all and thus this deserializer is not needed
    anymore.
    
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

--- model/src/main/kotlin/LicenseFindings.kt
+++ model/src/main/kotlin/LicenseFindings.kt
@@ -19,19 +19,11 @@
 
 package com.here.ort.model
 
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer
-import com.fasterxml.jackson.module.kotlin.treeToValue
-
 import com.here.ort.model.config.CopyrightGarbage
 import com.here.ort.utils.CopyrightStatementsProcessor
 
 import java.util.SortedMap
 import java.util.SortedSet
-import java.util.TreeSet
 
 /**
  * A map that associates licenses with their belonging copyrights. This is provided mostly for convenience as creating
@@ -56,7 +48,6 @@ fun LicenseFindingsMap.removeGarbage(copyrightGarbage: CopyrightGarbage) =
  * A class to store a [license] finding along with its belonging [copyrights] and the [locations] where the license was
  * found.
  */
-@JsonDeserialize(using = LicenseFindingsDeserializer::class)
 data class LicenseFindings(
     val license: String,
     val locations: SortedSet<TextLocation>,
@@ -70,32 +61,4 @@ data class LicenseFindings(
                 .thenBy(TextLocation.SORTED_SET_COMPARATOR, LicenseFindings::locations)
                 .thenBy(CopyrightFindings.SORTED_SET_COMPARATOR, LicenseFindings::copyrights)
         ) { it }
-}
-
-/**
- * Custom deserializer to support old versions of the [LicenseFindings] class.
- */
-class LicenseFindingsDeserializer : StdDeserializer<LicenseFindings>(LicenseFindings::class.java) {
-    override fun deserialize(p: JsonParser, ctxt: DeserializationContext): LicenseFindings {
-        val node = p.codec.readTree<JsonNode>(p)
-
-        val license = jsonMapper.treeToValue<String>(node["license"])
-
-        val copyrights = jsonMapper.readValue<TreeSet<CopyrightFindings>>(
-            jsonMapper.treeAsTokens(node["copyrights"]),
-            CopyrightFindings.TREE_SET_TYPE
-        )
-
-        val locations = deserializeLocations(node)
-
-        return LicenseFindings(license, locations, copyrights)
-    }
-
-    private fun deserializeLocations(node: JsonNode) =
-        node["locations"]?.let { locations ->
-            jsonMapper.readValue<TreeSet<TextLocation>>(
-                jsonMapper.treeAsTokens(locations),
-                TextLocation.TREE_SET_TYPE
-            )
-        } ?: sortedSetOf()
 }
