fdf30713ac77270719db10bf94018ead4d2d1909
Sebastian Schuberth <sebastian.schuberth@bosch-si.com>
2019-09-04 10:22:56 +0200

    Licensee: Implement setting the path in locations for findings
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch-si.com>

--- scanner/src/funTest/kotlin/scanners/LicenseeScannerTest.kt
+++ scanner/src/funTest/kotlin/scanners/LicenseeScannerTest.kt
@@ -34,6 +34,9 @@ class LicenseeScannerTest : AbstractScannerTest() {
             resultsFile.isFile shouldBe true
             result.summary.fileCount shouldBe 1
             result.summary.licenses shouldBe sortedSetOf("Apache-2.0")
+            result.summary.licenseFindings.all {
+                it.location.path == licenseFilePath.invariantSeparatorsPath
+            } shouldBe true
         }
     }
 }
