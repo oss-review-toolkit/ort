3bbf2f8a391bf621ceb9f88b26f84b412b2da74a
Sebastian Schuberth <sebastian.schuberth@bosch-si.com>
2019-09-15 11:30:16 +0200

    spdx-utils: Add tests for mappings to contain only valid keys
    To not accidentally mix up values for SpdxDeclaredLicenseMapping vs.
    SpdxLicenseAliasMapping.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch-si.com>

---
+++ spdx-utils/src/test/kotlin/SpdxLicenseAliasMappingTest.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2019 Bosch Software Innovations GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.spdx
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class SpdxLicenseAliasMappingTest : StringSpec({
+    "Mapping contains only parsable keys" {
+        val unparsableLicenses = SpdxLicenseAliasMapping.mapping.filterNot { (declaredLicense, _) ->
+            try {
+                // Be as lenient as possible when parsing declared licenses as we really only want to check for general
+                // parsability here.
+                SpdxExpression.parse(declaredLicense)
+                true
+            } catch (e: SpdxException) {
+                false
+            }
+        }
+
+        unparsableLicenses shouldBe emptyMap()
+    }
+})
