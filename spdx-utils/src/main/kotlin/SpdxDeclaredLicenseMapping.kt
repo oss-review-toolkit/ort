1ff90bde7dad88a0b63213ba205edc0588b5ef76
Sebastian Schuberth <sebastian.schuberth@bosch-si.com>
2019-09-17 19:41:35 +0200

    SpdxDeclaredLicenseMapping: Correct sorting of an MIT mapping
    This is a fixup for 195e7a2 to correct the sorting after removal of the
    Python license classification prefixes.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch-si.com>

--- spdx-utils/src/main/kotlin/SpdxDeclaredLicenseMapping.kt
+++ spdx-utils/src/main/kotlin/SpdxDeclaredLicenseMapping.kt
@@ -218,12 +218,12 @@ object SpdxDeclaredLicenseMapping {
         "LGPLv3 or later" to LGPL_3_0_OR_LATER.toExpression(),
         "Lesser General Public License (LGPL)" to LGPL_2_1_ONLY.toExpression(),
         "Lesser General Public License, version 3 or greater" to LGPL_3_0_OR_LATER.toExpression(),
-        "MIT License (http://opensource.org/licenses/MIT)" to MIT.toExpression(),
         "License Agreement For Open Source Computer Vision Library (3-clause BSD License)"
                 to BSD_3_CLAUSE.toExpression(),
         "MIT / http://rem.mit-license.org" to MIT.toExpression(),
         "MIT Licence" to MIT.toExpression(),
         "MIT License" to MIT.toExpression(),
+        "MIT License (http://opensource.org/licenses/MIT)" to MIT.toExpression(),
         "MIT Licensed. http://www.opensource.org/licenses/mit-license.php" to MIT.toExpression(),
         "MIT licence" to MIT.toExpression(),
         "MIT license" to MIT.toExpression(),
