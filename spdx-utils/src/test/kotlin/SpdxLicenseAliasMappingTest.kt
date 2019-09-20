e4cbad8ba56db38ce297ec66e82667ddd17d374f
Sebastian Schuberth <sebastian.schuberth@bosch-si.com>
2019-09-20 17:21:59 +0200

    SpdxLicenseAliasMappingTest: Really use SpdxLicenseAliasMapping
    By copy & paste mistake SpdxDeclaredLicenseMapping was used instead.
    This is a fixup for 969a971.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch-si.com>

--- spdx-utils/src/test/kotlin/SpdxLicenseAliasMappingTest.kt
+++ spdx-utils/src/test/kotlin/SpdxLicenseAliasMappingTest.kt
@@ -42,7 +42,7 @@ class SpdxLicenseAliasMappingTest : WordSpec({
 
         "not contain plain SPDX license ids" {
             assertSoftly {
-                SpdxDeclaredLicenseMapping.mapping.forEach { (declaredLicense, _) ->
+                SpdxLicenseAliasMapping.mapping.forEach { (declaredLicense, _) ->
                     SpdxLicense.forId(declaredLicense) shouldBe null
                 }
             }
