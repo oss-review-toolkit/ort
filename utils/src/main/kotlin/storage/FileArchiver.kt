3a214c3e4d3eef6fd07b583303335e98bfe0dc2f
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-10-29 11:05:31 +0100

    utils: Refactor `FileSaver` to `FileArchiver`
    Instead of saving all files separately like it was done in the
    `FileSaver`, add them to a ZIP file and only upload this archive to the
    `FileStorage` in `FileArchiver`.
    The main reason for this change is that the name of the archive file can
    be known beforehand which makes it easier to download the file in a
    later step. As a side effect uploading the compressed ZIP file takes
    less space on in the storage.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ utils/src/main/kotlin/storage/FileArchiver.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.utils.storage
+
+import com.here.ort.utils.FileMatcher
+import com.here.ort.utils.log
+import com.here.ort.utils.packZip
+
+import java.io.File
+
+/**
+ * A class to archive files matched by provided [patterns] in a ZIP file that is stored in a [FileStorage][storage].
+ */
+class FileArchiver(
+    /**
+     * A list of globs to match the paths of files that shall be archived. For details about the glob pattern see
+     * [java.nio.file.FileSystem.getPathMatcher].
+     */
+    val patterns: List<String>,
+
+    /**
+     * The [FileStorage] to use for archiving files.
+     */
+    val storage: FileStorage
+) {
+    companion object {
+        private const val ARCHIVE_FILE_NAME = "archive.zip"
+    }
+
+    private val matcher = FileMatcher(patterns)
+
+    /**
+     * Archive all files in [directory] matching any of the configured [patterns] in the [storage]. The archived files
+     * are zipped in the file '[storagePath]/[ARCHIVE_FILE_NAME]'.
+     */
+    fun archive(directory: File, storagePath: String) {
+        val zipFile = createTempFile(prefix = "ort", suffix = ".zip")
+        zipFile.deleteOnExit()
+
+        directory.packZip(zipFile, overwrite = true) { path ->
+            val relativePath = directory.toPath().relativize(path)
+            matcher.matches(relativePath.toString()).also { result ->
+                if (result) {
+                    log.debug { "Adding '$relativePath' to archive." }
+                } else {
+                    log.debug { "Not adding '$relativePath' to archive." }
+                }
+            }
+        }
+
+        storage.write("${storagePath.removeSuffix("/")}/$ARCHIVE_FILE_NAME", zipFile.inputStream())
+    }
+}
