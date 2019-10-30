ddudnik/oss-review-toolkit

Move the logic to create a file storage to a factory method
This allows to re-use the logic in other places.

Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

model/src/main/kotlin/config/FileStorageConfiguration.kt
@@ -19,6 +19,7 @@
 
 package com.here.ort.model.config
 
+import com.here.ort.utils.expandTilde
 import com.here.ort.utils.storage.FileStorage
 import com.here.ort.utils.storage.HttpFileStorage
 import com.here.ort.utils.storage.LocalFileStorage
@@ -36,4 +37,20 @@ data class FileStorageConfiguration(
      * The configuration of a [LocalFileStorage].
      */
     val localFileStorage: LocalFileStorageConfiguration? = null
-)
+) {
+    /**
+     * Create a [FileStorage] based on this configuration.
+     */
+    fun create(): FileStorage {
+        require((httpFileStorage == null) xor (localFileStorage == null)) {
+            "Exactly one implementation must be configured for a FileStorage."
+        }
+
+        return httpFileStorage?.let { httpFileStorageConfiguration ->
+            HttpFileStorage(httpFileStorageConfiguration.url, httpFileStorageConfiguration.headers)
+        } ?: localFileStorage!!.let { localFileStorageConfiguration ->
+            val directory = localFileStorageConfiguration.directory.expandTilde()
+            LocalFileStorage(directory)
+        }
+    }
+}

scanner/src/main/kotlin/ScanResultsStorage.kt
@@ -29,7 +29,6 @@ import com.here.ort.model.ScannerDetails
 import com.here.ort.model.config.FileBasedStorageConfiguration
 import com.here.ort.model.config.PostgresStorageConfiguration
 import com.here.ort.scanner.storages.*
-import com.here.ort.utils.expandTilde
 import com.here.ort.utils.log
 import com.here.ort.utils.storage.HttpFileStorage
 import com.here.ort.utils.storage.LocalFileStorage
@@ -66,17 +65,13 @@ abstract class ScanResultsStorage {
          * Configure a [FileBasedStorage] as the current storage backend.
          */
         fun configure(config: FileBasedStorageConfiguration) {
-            require((config.backend.httpFileStorage == null) xor (config.backend.localFileStorage == null)) {
-                "Exactly one storage backend must be configured for a FileBasedStorage."
-            }
+            val backend = config.backend.create()
 
-            val backend = config.backend.httpFileStorage?.let { httpFileStorageConfiguration ->
-                log.info { "Using file based storage with HTTP backend '${httpFileStorageConfiguration.url}'." }
-                HttpFileStorage(httpFileStorageConfiguration.url, httpFileStorageConfiguration.headers)
-            } ?: config.backend.localFileStorage!!.let { localFileStorageConfiguration ->
-                val directory = localFileStorageConfiguration.directory.expandTilde()
-                log.info { "Using file based storage with local directory '${directory.invariantSeparatorsPath}'." }
-                LocalFileStorage(directory)
+            when (backend) {
+                is HttpFileStorage -> log.info { "Using file based storage with HTTP backend '${backend.url}'." }
+                is LocalFileStorage -> log.info {
+                    "Using file based storage with local directory '${backend.directory.invariantSeparatorsPath}'."
+                }
             }
 
             storage = FileBasedStorage(backend)

oss-review-toolkit/ort

model/src/main/kotlin/config/FileStorageConfiguration.kt
@@ -19,6 +19,7 @@
 
 package com.here.ort.model.config
 
+import com.here.ort.utils.expandTilde
 import com.here.ort.utils.storage.FileStorage
 import com.here.ort.utils.storage.HttpFileStorage
 import com.here.ort.utils.storage.LocalFileStorage
@@ -36,4 +37,20 @@ data class FileStorageConfiguration(
      * The configuration of a [LocalFileStorage].
      */
     val localFileStorage: LocalFileStorageConfiguration? = null
-)
+) {
+    /**
+     * Create a [FileStorage] based on this configuration.
+     */
+    fun createFileStorage(): FileStorage {
+        require((httpFileStorage == null) xor (localFileStorage == null)) {
+            "Exactly one implementation must be configured for a FileStorage."
+        }
+
+        return httpFileStorage?.let { httpFileStorageConfiguration ->
+            HttpFileStorage(httpFileStorageConfiguration.url, httpFileStorageConfiguration.headers)
+        } ?: localFileStorage!!.let { localFileStorageConfiguration ->
+            val directory = localFileStorageConfiguration.directory.expandTilde()
+            LocalFileStorage(directory)
+        }
+    }
+}

scanner/src/main/kotlin/ScanResultsStorage.kt
@@ -29,7 +29,6 @@ import com.here.ort.model.ScannerDetails
 import com.here.ort.model.config.FileBasedStorageConfiguration
 import com.here.ort.model.config.PostgresStorageConfiguration
 import com.here.ort.scanner.storages.*
-import com.here.ort.utils.expandTilde
 import com.here.ort.utils.log
 import com.here.ort.utils.storage.HttpFileStorage
 import com.here.ort.utils.storage.LocalFileStorage
@@ -66,17 +65,13 @@ abstract class ScanResultsStorage {
          * Configure a [FileBasedStorage] as the current storage backend.
          */
         fun configure(config: FileBasedStorageConfiguration) {
-            require((config.backend.httpFileStorage == null) xor (config.backend.localFileStorage == null)) {
-                "Exactly one storage backend must be configured for a FileBasedStorage."
-            }
+            val backend = config.backend.createFileStorage()
 
-            val backend = config.backend.httpFileStorage?.let { httpFileStorageConfiguration ->
-                log.info { "Using file based storage with HTTP backend '${httpFileStorageConfiguration.url}'." }
-                HttpFileStorage(httpFileStorageConfiguration.url, httpFileStorageConfiguration.headers)
-            } ?: config.backend.localFileStorage!!.let { localFileStorageConfiguration ->
-                val directory = localFileStorageConfiguration.directory.expandTilde()
-                log.info { "Using file based storage with local directory '${directory.invariantSeparatorsPath}'." }
-                LocalFileStorage(directory)
+            when (backend) {
+                is HttpFileStorage -> log.info { "Using file based storage with HTTP backend '${backend.url}'." }
+                is LocalFileStorage -> log.info {
+                    "Using file based storage with local directory '${backend.directory.invariantSeparatorsPath}'."
+                }
             }
 
             storage = FileBasedStorage(backend)