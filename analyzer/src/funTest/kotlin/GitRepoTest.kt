4dd2f32ee214f692f2c1a566208c8e2f4673b293
Sebastian Schuberth <sebastian.schuberth@bosch-si.com>
2019-11-07 10:24:53 +0100

    GitRepoTest: Temporarily disable the test on Travis
    For unknown reasons, Travis has problems running this test and reports
    
        java.lang.OutOfMemoryError: Java heap space
    
    even though the test passes e.g. on AppVeyor.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch-si.com>

--- analyzer/src/funTest/kotlin/GitRepoTest.kt
+++ analyzer/src/funTest/kotlin/GitRepoTest.kt
@@ -25,6 +25,7 @@ import com.here.ort.model.Package
 import com.here.ort.model.VcsInfo
 import com.here.ort.model.VcsType
 import com.here.ort.model.yamlMapper
+import com.here.ort.utils.Ci
 import com.here.ort.utils.safeDeleteRecursively
 import com.here.ort.utils.test.DEFAULT_ANALYZER_CONFIGURATION
 import com.here.ort.utils.test.patchActualResult
@@ -57,7 +58,7 @@ class GitRepoTest : StringSpec() {
     }
 
     init {
-        "Analyzer correctly reports VcsInfo for git-repo projects" {
+        "Analyzer correctly reports VcsInfo for git-repo projects".config(enabled = !Ci.isTravis) {
             val ortResult = Analyzer(DEFAULT_ANALYZER_CONFIGURATION).analyze(outputDir)
             val actualResult = yamlMapper.writeValueAsString(ortResult)
             val expectedResult = patchExpectedResult(
@@ -69,7 +70,7 @@ class GitRepoTest : StringSpec() {
             patchActualResult(actualResult, patchStartAndEndTime = true) shouldBe expectedResult
         }
 
-        "GitRepo correctly lists submodules" {
+        "GitRepo correctly lists submodules".config(enabled = !Ci.isTravis) {
             val expectedSubmodules = listOf(
                 "spdx-tools",
                 "submodules",
