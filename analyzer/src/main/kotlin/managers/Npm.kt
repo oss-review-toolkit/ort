c5564ab0ef5c2992422131675d47ac89b60d58a6
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-11-13 13:15:37 +0100

    Npm: Support current stable version 6.13.0
    Also upgrade the CI to use this version.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

--- analyzer/src/main/kotlin/managers/Npm.kt
+++ analyzer/src/main/kotlin/managers/Npm.kt
@@ -98,7 +98,7 @@ open class Npm(
 
     override fun command(workingDir: File?) = if (Os.isWindows) "npm.cmd" else "npm"
 
-    override fun getVersionRequirement(): Requirement = Requirement.buildNPM("5.7.* - 6.9.*")
+    override fun getVersionRequirement(): Requirement = Requirement.buildNPM("5.7.* - 6.13.*")
 
     override fun mapDefinitionFiles(definitionFiles: List<File>) =
         PackageJsonUtils.mapDefinitionFilesForNpm(definitionFiles).toList()
