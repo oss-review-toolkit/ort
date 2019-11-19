f65ad21ac4cf38df847c4a41c2898af92c3187b6
Sebastian Schuberth <sebastian.schuberth@bosch-si.com>
2019-11-19 13:00:50 +0100

    reporter-web-app: Use Kotlin's Yarn plugin to bootstrap Node.js and Yarn
    This allows to remove Node.js and Yarn from the list of prerequisites
    to build ORT, which means now nothing except for a JDK is required.
    
    Unfortunately, the Yarn plugin always installs itself to the root project
    (even if applied in a subproject) [1], and it also creates a "check" task
    in the root project which clashes with our existing "check" task. As for
    some reason the "check" task created by the Yarn plugin cannot be further
    configured to depend on our "checkCopyright" task, simply remove our
    "check" task which is not used anymore as of 6d994fd anyway.
    
    Finally, as the Node.js binary installed by Gradle depends on glibc, but
    our Alpine-based Dockerfile uses musl, switch to an Alpine base image
    that comes with glibc (and Java 8).
    
    [1] https://github.com/JetBrains/kotlin/blob/2f90742/libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/yarn/YarnPlugin.kt#L27-L31
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch-si.com>

--- reporter-web-app/build.gradle.kts
+++ reporter-web-app/build.gradle.kts
@@ -1,13 +1,47 @@
 import org.apache.tools.ant.taskdefs.condition.Os
 
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsSetupTask
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnSetupTask
+
+// The Yarn plugin is only applied programmatically for Kotlin projects that target JavaScript. As we do not target
+// JavaScript from Kotlin (yet), manually apply the plugin to make its setup tasks available.
+YarnPlugin.apply(project).version = "1.17.3"
+
+// The Yarn plugin registers tasks always on the root project, see
+// https://github.com/JetBrains/kotlin/blob/2f90742/libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/yarn/YarnPlugin.kt#L27-L31
+val kotlinNodeJsSetup by rootProject.tasks.existing(NodeJsSetupTask::class)
+val kotlinYarnSetup by rootProject.tasks.existing(YarnSetupTask::class)
+
+val nodeDir = kotlinNodeJsSetup.get().destination
+val nodeBinDir = if (Os.isFamily(Os.FAMILY_WINDOWS)) nodeDir else nodeDir.resolve("bin")
+val nodeExecutable = if (Os.isFamily(Os.FAMILY_WINDOWS)) nodeBinDir.resolve("node.exe") else nodeBinDir.resolve("node")
+
+val yarnDir = kotlinYarnSetup.get().destination
+val yarnJs = yarnDir.resolve("bin/yarn.js")
+
+kotlinNodeJsSetup {
+    logger.quiet("Will use the Node executable file from '$nodeExecutable'.")
+
+    // If the node binary is missing, force a re-download of the NodeJs distribution, see
+    // https://youtrack.jetbrains.com/issue/KT-34989.
+    if (!nodeExecutable.isFile && nodeDir.deleteRecursively()) {
+        logger.info("Successfully deleted the incomplete '$nodeDir' directory to trigger the NodeJs download.")
+    }
+}
+
+kotlinYarnSetup {
+    logger.quiet("Will use the Yarn JavaScript file from '$yarnJs'.")
+}
+
 tasks.addRule("Pattern: yarn<Command>") {
     val taskName = this
     if (taskName.startsWith("yarn")) {
-        val yarn = if (Os.isFamily(Os.FAMILY_WINDOWS)) "yarn.cmd" else "yarn"
         val command = taskName.removePrefix("yarn").decapitalize()
 
         tasks.register<Exec>(taskName) {
-            commandLine = listOf(yarn, command)
+            // Execute the Yarn version downloaded by Gradle using the NodeJs version downloaded by Gradle.
+            commandLine = listOf(nodeExecutable.path, yarnJs.path, command)
         }
     }
 }
@@ -20,6 +54,8 @@ tasks {
     "yarnInstall" {
         description = "Use Yarn to install the Node.js dependencies."
         group = "Node"
+
+        dependsOn(kotlinYarnSetup)
 
         inputs.files(listOf("package.json", "yarn.lock"))
         outputs.dir("node_modules")
