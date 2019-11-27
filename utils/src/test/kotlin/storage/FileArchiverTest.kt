f271f4a56965c8a35ef6a46c76951f685968ff91
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-11-27 17:27:56 +0100

    FileArchiver: Add a function to get archived files
    Add the function `unarchive` to get previously archived files from the
    storage.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

--- utils/src/test/kotlin/storage/FileArchiverTest.kt
+++ utils/src/test/kotlin/storage/FileArchiverTest.kt
@@ -19,56 +19,94 @@
 
 package com.here.ort.utils.storage
 
+import com.here.ort.utils.safeDeleteRecursively
 import com.here.ort.utils.safeMkdirs
 import com.here.ort.utils.unpack
 
+import io.kotlintest.TestCase
+import io.kotlintest.TestResult
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class FileArchiverTest : StringSpec({
-    "All files matching any of the patterns are archived" {
-        val dir = createTempDir("ort")
+import java.io.File
 
-        fun createFile(path: String) {
-            val file = dir.resolve(path)
-            file.parentFile.safeMkdirs()
-            file.writeText(path)
-        }
+class FileArchiverTest : StringSpec() {
+    private lateinit var workingDir: File
+    private lateinit var storageDir: File
+    private lateinit var targetDir: File
+    private lateinit var storage: LocalFileStorage
 
-        createFile("a")
-        createFile("b")
-        createFile("d/a")
-        createFile("d/b")
-
-        val storageDir = createTempDir("ort")
-        val storage = LocalFileStorage(storageDir)
-        val archiver = FileArchiver(listOf("a", "**/a"), storage)
-
-        archiver.archive(dir, "save")
-
-        val archiveFile = storageDir.resolve("save/archive.zip")
-
-        archiveFile.isFile shouldBe true
-
-        val unzipDir = createTempDir()
-        unzipDir.deleteOnExit()
-        archiveFile.unpack(unzipDir)
-
-        fun assertFileSaved(path: String) {
-            val file = unzipDir.resolve(path)
-            file.isFile shouldBe true
-            file.readText() shouldBe path
-        }
-
-        assertFileSaved("a")
-        assertFileSaved("d/a")
-
-        fun assertFileNotSaved(path: String) {
-            val file = storageDir.resolve("save/$path")
-            file.exists() shouldBe false
-        }
-
-        assertFileNotSaved("b")
-        assertFileNotSaved("d/b")
+    override fun beforeTest(testCase: TestCase) {
+        workingDir = createTempDir("ort")
+        storageDir = createTempDir("ort")
+        targetDir = createTempDir("ort")
+        storage = LocalFileStorage(storageDir)
     }
-})
+
+    override fun afterTest(testCase: TestCase, result: TestResult) {
+        workingDir.safeDeleteRecursively()
+        storageDir.safeDeleteRecursively()
+        targetDir.safeDeleteRecursively()
+    }
+
+    private fun createFile(path: String) {
+        val file = workingDir.resolve(path)
+        file.parentFile.safeMkdirs()
+        file.writeText(path)
+    }
+
+    private fun File.assertFileContent(path: String) {
+        val file = resolve(path)
+        file.isFile shouldBe true
+        file.readText() shouldBe path
+    }
+
+    init {
+        "All files matching any of the patterns are archived" {
+            createFile("a")
+            createFile("b")
+            createFile("d/a")
+            createFile("d/b")
+
+            val archiver = FileArchiver(listOf("a", "**/a"), storage)
+
+            archiver.archive(workingDir, "save")
+
+            val archiveFile = storageDir.resolve("save/archive.zip")
+
+            archiveFile.isFile shouldBe true
+
+            archiveFile.unpack(targetDir)
+
+            targetDir.assertFileContent("a")
+            targetDir.assertFileContent("d/a")
+
+            fun assertFileNotSaved(path: String) {
+                val file = storageDir.resolve("save/$path")
+                file.exists() shouldBe false
+            }
+
+            assertFileNotSaved("b")
+            assertFileNotSaved("d/b")
+        }
+
+        "All archived files are unarchived" {
+            createFile("a")
+            createFile("b")
+            createFile("c/a")
+            createFile("c/b")
+
+            val storagePath = "save"
+            val archiver = FileArchiver(listOf("**"), storage)
+            archiver.archive(workingDir, storagePath)
+
+            val result = archiver.unarchive(targetDir, storagePath)
+
+            result shouldBe true
+            targetDir.assertFileContent("a")
+            targetDir.assertFileContent("b")
+            targetDir.assertFileContent("c/a")
+            targetDir.assertFileContent("c/b")
+        }
+    }
+}
