f182c6879018ac5f32fccdba2d921ddb239d39de
Frank Viernau <frank.viernau@here.com>
2019-12-05 00:14:03 +0100

    GoMod: Test analyzing a synthetic project
    Co-authored-by: Roman Shatsov <roman.shatsov@here.com>
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

---
+++ analyzer/src/funTest/assets/projects/synthetic/gomod/go.mod
@@ -0,0 +1,13 @@
+module github.com/heremaps/oss-review-toolkit/gomod-synthetic-test-project
+
+go 1.13
+
+require (
+	github.com/fatih/color v1.7.0
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/hashicorp/hcl2 v0.0.0-20190503213020-640445e16309 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	golang.org/x/mod v0.1.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421 // indirect
+)
---
+++ analyzer/src/funTest/kotlin/managers/GoModTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.analyzer.managers
+
+import com.here.ort.downloader.VersionControlSystem
+import com.here.ort.model.yamlMapper
+import com.here.ort.utils.normalizeVcsUrl
+import com.here.ort.utils.test.DEFAULT_ANALYZER_CONFIGURATION
+import com.here.ort.utils.test.DEFAULT_REPOSITORY_CONFIGURATION
+import com.here.ort.utils.test.USER_DIR
+import com.here.ort.utils.test.patchExpectedResult
+
+import io.kotlintest.matchers.beEmpty
+import io.kotlintest.should
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
+import io.kotlintest.specs.StringSpec
+
+import java.io.File
+
+class GoModTest : StringSpec() {
+    private val projectDir = File("src/funTest/assets/projects/synthetic/gomod").absoluteFile
+    private val vcsDir = VersionControlSystem.forDirectory(projectDir)!!
+    private val vcsUrl = vcsDir.getRemoteUrl()
+    private val vcsRevision = vcsDir.getRevision()
+
+    init {
+        "Project dependencies are detected correctly" {
+            val definitionFile = File(projectDir, "go.mod")
+            val vcsPath = vcsDir.getPathToRoot(projectDir)
+            val expectedResult = patchExpectedResult(
+                File(projectDir.parentFile, "gomod-expected-output.yml"),
+                definitionFilePath = "$vcsPath/go.mod",
+                path = vcsPath,
+                revision = vcsRevision,
+                url = normalizeVcsUrl(vcsUrl)
+            )
+
+            val result = createGoMod().resolveDependencies(listOf(definitionFile))[definitionFile]
+
+            result shouldNotBe null
+            result!!.errors should beEmpty()
+            yamlMapper.writeValueAsString(result) shouldBe expectedResult
+        }
+    }
+
+    private fun createGoMod() =
+        GoMod("GoMod", USER_DIR, DEFAULT_ANALYZER_CONFIGURATION, DEFAULT_REPOSITORY_CONFIGURATION)
+}
