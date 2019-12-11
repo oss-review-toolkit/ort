27902162e2d4b51f67d1c413abf79ddbe4fd7f99
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2019-12-11 17:57:45 +0100

    reporter: Add a notice reporter that reports licenses by package
    Add an alternative notice reporter implementation that reports the
    licenses for each package separately.
    
    For each package the license files archived by the scanner plus
    copyrights detected for the licenses contained in those files are added
    to the file. Afterwards all detected licenses and copyrights which are
    not contained in any license file are listed.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ reporter/src/main/kotlin/reporters/NoticeByPackageReporter.kt
@@ -0,0 +1,283 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.reporter.reporters
+
+import com.here.ort.model.Identifier
+import com.here.ort.model.LicenseFindingsMap
+import com.here.ort.model.ScanResult
+import com.here.ort.model.merge
+import com.here.ort.model.processStatements
+import com.here.ort.model.removeGarbage
+import com.here.ort.reporter.ReporterInput
+import com.here.ort.utils.CopyrightStatementsProcessor
+import com.here.ort.utils.FileMatcher
+import com.here.ort.utils.LICENSE_FILENAMES
+import com.here.ort.utils.getUserOrtDirectory
+import com.here.ort.utils.log
+import com.here.ort.utils.storage.FileArchiver
+import com.here.ort.utils.storage.LocalFileStorage
+import com.here.ort.utils.toHexString
+
+import java.io.File
+import java.nio.file.Path
+import java.security.MessageDigest
+
+/**
+ * Creates a notice file containing the licenses for all non-excluded projects and packages, listed by their identifier.
+ * Detected copyrights are listed next to the licenses.
+ *
+ * If a [FileArchiver] is configured for the scanner the reporter will try to download any archived license files and
+ * include them in the notice file. Licenses detected in those license files will not be listed again, instead all
+ * copyrights associated to those licenses will be listed below the license file's content.
+ */
+class NoticeByPackageReporter : AbstractNoticeReporter() {
+    override val reporterName = "NoticeByPackage"
+    override val defaultFilename = "NOTICE_BY_PACKAGE"
+
+    override fun createProcessor(input: ReporterInput): NoticeProcessor = NoticeByPackageProcessor(input)
+}
+
+class NoticeByPackageProcessor(input: ReporterInput) : AbstractNoticeReporter.NoticeProcessor(input) {
+    companion object {
+        private val DEFAULT_ARCHIVE_DIR by lazy { getUserOrtDirectory().resolve("scanner/archive") }
+        private const val LICENSE_SEPARATOR = "\n  --\n\n"
+    }
+
+    override fun process(model: AbstractNoticeReporter.NoticeReportModel): List<() -> String> =
+        mutableListOf<() -> String>().apply {
+            add { model.headers.joinToString(AbstractNoticeReporter.NOTICE_SEPARATOR) }
+
+            if (model.headers.isNotEmpty()) {
+                add { AbstractNoticeReporter.NOTICE_SEPARATOR }
+            }
+
+            val projectIds = input.ortResult.getProjects().map { it.id }
+            val projectFindings = model.findings
+                .filter { (id, licenseFindingsMap) ->
+                    id in projectIds && licenseFindingsMap.isNotEmpty()
+                }
+            val packageFindings = model.findings
+                .filterNot { (id, _) -> id in projectIds }
+
+            addProjectFindings(projectFindings)
+
+            if (packageFindings.isNotEmpty() && projectFindings.isNotEmpty()) {
+                add { AbstractNoticeReporter.NOTICE_SEPARATOR }
+            }
+
+            if (packageFindings.isEmpty()) {
+                add { model.headerWithoutLicenses }
+            } else {
+                add { model.headerWithLicenses }
+
+                addPackageFindings(packageFindings)
+            }
+
+            model.footers.forEach { footer ->
+                add { AbstractNoticeReporter.NOTICE_SEPARATOR }
+                add { footer }
+            }
+        }
+
+    private fun MutableList<() -> String>.addProjectFindings(findings: Map<Identifier, LicenseFindingsMap>) {
+        val processedFindings = findings.values
+            .merge()
+            .removeGarbage(input.copyrightGarbage)
+            .processStatements()
+            .filter { (license, _) ->
+                input.licenseTextProvider.hasLicenseText(license).also {
+                    if (!it) {
+                        log.warn {
+                            "No license text found for license '$license', it will be omitted from the report."
+                        }
+                    }
+                }
+            }.toSortedMap()
+
+        if (processedFindings.isNotEmpty()) {
+            add { "This software includes external packages and source code.\n" }
+            add { "The applicable license information is listed below:\n" }
+        }
+
+        add { AbstractNoticeReporter.NOTICE_SEPARATOR }
+
+        addProcessedFindings(processedFindings)
+    }
+
+    private fun MutableList<() -> String>.addPackageFindings(findings: Map<Identifier, LicenseFindingsMap>) {
+        val archiver = input.ortConfig.scanner?.archive?.createFileArchiver() ?: FileArchiver(
+            LICENSE_FILENAMES,
+            LocalFileStorage(DEFAULT_ARCHIVE_DIR)
+        )
+
+        findings.forEach { (id, licenseFindingsMap) ->
+            add { AbstractNoticeReporter.NOTICE_SEPARATOR }
+
+            add { "Package: " }
+            if (id.namespace.isNotBlank()) {
+                add { "${id.namespace}:" }
+            }
+            add { "${id.name}:${id.version}\n\n" }
+
+            val scanResult = input.ortResult.getScanResultsForId(id).firstOrNull()
+            val archiveDir = createTempDir(prefix = "notice").also { it.deleteOnExit() }
+
+            val licenseFileFindings = if (scanResult != null) {
+                val provenanceBytes = scanResult.provenance.toString().toByteArray()
+                val provenanceHash = MessageDigest.getInstance("SHA-1").digest(provenanceBytes).toHexString()
+                val path = "${id.toPath()}/$provenanceHash"
+                if (archiver.unarchive(archiveDir, path)) {
+                    getFindingsForLicenseFiles(scanResult, archiveDir, licenseFindingsMap)
+                } else {
+                    emptyMap()
+                }
+            } else {
+                emptyMap<String, LicenseFindingsMap>()
+            }
+
+            addLicenseFileFindings(licenseFileFindings, archiveDir)
+
+            val licensesInNoticeFiles = licenseFileFindings.values.map { it.keys }.flatten().toSet()
+            val processedFindings = licenseFindingsMap.removeGarbage(input.copyrightGarbage).processStatements()
+                .filter { (license, _) -> license !in licensesInNoticeFiles }
+                .filter { (license, _) ->
+                    input.licenseTextProvider.hasLicenseText(license).also {
+                        if (!it) {
+                            log.warn {
+                                "No license text found for license '$license', it will be omitted from the report."
+                            }
+                        }
+                    }
+                }.toSortedMap()
+
+            if (processedFindings.isNotEmpty()) {
+                if (licenseFileFindings.isNotEmpty()) {
+                    add { LICENSE_SEPARATOR }
+                }
+
+                add { "The following copyrights and licenses were found in the source code of this package:\n\n" }
+            }
+
+            addProcessedFindings(processedFindings)
+
+            if (licenseFileFindings.isEmpty() && processedFindings.isEmpty()) {
+                log.error { "No license information was added for package ${id.toCoordinates()}." }
+            }
+        }
+    }
+
+    private fun MutableList<() -> String>.addProcessedFindings(processedFindings: LicenseFindingsMap) {
+        processedFindings.entries.forEachIndexed { index, (license, copyrights) ->
+            input.licenseTextProvider.getLicenseText(license)?.let { licenseText ->
+                copyrights.forEach { copyright ->
+                    add { "$copyright\n" }
+                }
+                if (copyrights.isNotEmpty()) {
+                    add { "\n" }
+                }
+
+                add { licenseText }
+
+                if (index < processedFindings.size - 1) {
+                    add { LICENSE_SEPARATOR }
+                }
+            }
+        }
+    }
+
+    private fun MutableList<() -> String>.addLicenseFileFindings(
+        licenseFileFindings: Map<String, LicenseFindingsMap>,
+        archiveDir: File
+    ) {
+        licenseFileFindings.forEach { (file, findings) ->
+            add { "This package contains the file $file with the following contents:\n\n" }
+            add { "${archiveDir.resolve(file).readText()}\n" }
+            val allCopyrights = findings.values.flatten().toSet()
+            val processedCopyrights = CopyrightStatementsProcessor().process(allCopyrights).toMutableSet()
+            if (processedCopyrights.isNotEmpty()) {
+                add { "The following copyright holder information relates to the license(s) above:\n\n" }
+            }
+            processedCopyrights.forEach { copyright ->
+                add { "$copyright\n" }
+            }
+        }
+    }
+
+    /**
+     * Get the license and copyright findings from [scanResult] for all license files in [archiveDir].
+     */
+    private fun getFindingsForLicenseFiles(
+        scanResult: ScanResult,
+        archiveDir: File,
+        licenseFindingsMap: LicenseFindingsMap
+    ): MutableMap<String, LicenseFindingsMap> {
+        val matcher = FileMatcher(LICENSE_FILENAMES)
+        val licenseFiles = mutableMapOf<String, LicenseFindingsMap>()
+
+        archiveDir.walkTopDown().forEach { file ->
+            val relativePath = archiveDir.toPath().relativize(file.toPath())
+            if (matcher.matches(relativePath.toString())) {
+                licenseFiles[relativePath.toString()] =
+                    getFindingsForLicenseFile(scanResult, relativePath, licenseFindingsMap)
+            }
+        }
+
+        return licenseFiles
+    }
+
+    fun getFindingsForLicenseFile(
+        scanResult: ScanResult,
+        path: Path,
+        licenseFindingsMap: LicenseFindingsMap
+    ): LicenseFindingsMap {
+        val licenses = findLicensesForFile(scanResult, path)
+        val copyrightsInFile = findCopyrightsForFile(scanResult, path)
+
+        return licenses.associateWith { license ->
+            // Remove copyrights which are already contained in the license file.
+            val allCopyrights = (licenseFindingsMap[license].orEmpty() + copyrightsInFile).toSet()
+            val processCopyrightsResult = CopyrightStatementsProcessor().process(allCopyrights)
+            val processedCopyrightsNotInLicenseFile =
+                processCopyrightsResult.processedStatements.filterNot { (_, sources) ->
+                    copyrightsInFile.any { it in sources }
+                }
+            val unprocessedCopyrightsNotInLicenseFile =
+                processCopyrightsResult.unprocessedStatements - copyrightsInFile
+
+            (processedCopyrightsNotInLicenseFile.keys + unprocessedCopyrightsNotInLicenseFile).toMutableSet()
+        }.toSortedMap()
+    }
+
+    private fun findCopyrightsForFile(scanResult: ScanResult, relativePath: Path): Set<String> {
+        return scanResult.summary.copyrightFindings.filter {
+            it.location.path == relativePath.toString()
+        }.mapTo(mutableSetOf()) {
+            it.statement
+        }
+    }
+
+    private fun findLicensesForFile(scanResult: ScanResult, relativePath: Path): Set<String> {
+        return scanResult.summary.licenseFindings.filter {
+            it.location.path == relativePath.toString()
+        }.mapTo(mutableSetOf()) {
+            it.license
+        }
+    }
+}
