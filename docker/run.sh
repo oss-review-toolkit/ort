oss-review-toolkit/ort

commit a96df4b3b776824201767ef33f7957d540da6b8b

docker: Support running without arguments
Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch-si.com>

docker/run.sh
@@ -33,8 +33,10 @@ fi
 echo Running ORT version $ORT_VERSION...
 
 DOCKER_ARGS=$1
-shift
-ORT_ARGS=$@
+if [ -n "$DOCKER_ARGS" ]; then
+    shift
+    ORT_ARGS=$@
+fi
 
 (cd $PROJECT_DIR && \
     . docker/lib && \

commit e4d3f379e6d41758940474ac366e42e24f0ab330

docker: Fix running without arguments
The change in a96df4b broke running the script with empty Docker
arguments like

    $ ./run.sh "" requirements

so fix the check to use the argument count instead.

Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch-si.com>

docker/run.sh
@@ -35,7 +35,7 @@ fi
 echo "Running ORT version $ORT_VERSION..."
 
 DOCKER_ARGS=$1
-if [ -n "$DOCKER_ARGS" ]; then
+if [ $# -gt 0 ]; then
     shift
     ORT_ARGS=$@
 fi