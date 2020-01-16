9b3b83beba0c2b54c95692090fc9d45fa774ae07
Frank Viernau <frank.viernau@here.com>
2020-01-16 11:25:02 +0100

    Statstics: Add a class to provide a basic summary
    Resolutions and excludes are currently applied only in the reporters and
    thus the result of applying these is not contained in the OrtResult.
    Applying excludes, resolutions and further configuration should ideally
    be done without (code) redundancy and thus a report model shared
    accross the reporters is needed. This change introduces a stats class
    which can be considered as the summary part of a shared report model.
    
    Naturally this class would be introduced together with or even after a
    shared report model but as the stats are required first we decided to do
    the other way round.
    
    Note: This class is intentionally not added to the (OrtResult) model to
    keep that OrtResult free of any redundancies and also to follow the
    design approach that only the reporter knows about (global) resolutions.
    
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

---
+++ reporter/src/main/kotlin/model/Statistics.kt
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2017-2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model
+
+import java.util.SortedSet
+
+/**
+ * A class containing statistics for an [OrtResult].
+ */
+data class Statistics(
+    /**
+     * The number of [OrtIssue]s by severity which are not resolved and not excluded.
+     */
+    val openIssues: IssueStatistics,
+
+    /**
+     * The number of [RuleViolation]s by severity which are not resolved.
+     */
+    val openRuleViolations: IssueStatistics,
+
+    /**
+     * Statistics for the dependency tree.
+     */
+    val dependencyTree: DependencyTreeStatistics
+)
+
+/**
+ * A class containing the amount of issues per severity.
+ */
+data class IssueStatistics(
+    /**
+     * The number of issues with [Severity.ERROR] as severity.
+     */
+    val errors: Int,
+
+    /**
+     * The number of issues with [Severity.WARNING] as severity.
+     */
+    val warnings: Int,
+
+    /**
+     * The number of issues with [Severity.HINT] as severity.
+     */
+    val hints: Int
+)
+
+/**
+ * A class containing statistics about the dependency trees.
+ */
+data class DependencyTreeStatistics(
+    /**
+     * The number of included [Project]s.
+     */
+    val includedProjects: Int,
+
+    /**
+     * The number of excluded [Project]s.
+     */
+    val excludedProjects: Int,
+
+    /**
+     * The number of included [Package]s.
+     */
+    val includedPackages: Int,
+
+    /**
+     * The number of excluded [Package]s.
+     */
+    val excludesPackages: Int,
+
+    /**
+     * The total depth of the deepest tree in the forest.
+     */
+    val totalTreeDepth: Int,
+
+    /**
+     * The depth of the deepest tree in the forest disregarding excluded tree nodes.
+     */
+    val includedTreeDepth: Int,
+
+    /**
+     * The set of scopes names which have at least one not excluded corresponding [Scope].
+     */
+    val includedScopes: SortedSet<String>,
+
+    /**
+     * The set of scope names which do not have a single not excluded corresponding [Scope].
+     */
+    val excludedScopes: SortedSet<String>
+)
