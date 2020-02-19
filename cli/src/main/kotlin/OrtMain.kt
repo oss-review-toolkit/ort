42dc4cebf4f1ffe82ff3b416d10d253c0b6c7f9a
Frank Viernau <frank.viernau@here.com>
2020-02-20 00:33:23 +0100

    OrtMain: Remove an unused import
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

--- cli/src/main/kotlin/OrtMain.kt
+++ cli/src/main/kotlin/OrtMain.kt
@@ -21,7 +21,6 @@ package com.here.ort
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.context
-import com.github.ajalt.clikt.core.findOrSetObject
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.output.CliktHelpFormatter
 import com.github.ajalt.clikt.output.HelpFormatter
