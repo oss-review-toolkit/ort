0376b27bb67c731dd258cfdef8267e10c9e1a309
Thomas Steenbergen <thomas.steenbergen@here.com>
2020-04-15 11:10:02 +0200

    reporter-web-app: Fix displaying a package's scan results
    Prior to this change the scan results of a package would show
    the scan results of a previously shown package.
    This was due to a state object not properly updating which
    is now fixed by simply removing state from `PackageFindingsTable`.
    
    Signed-off-by: Thomas Steenbergen <thomas.steenbergen@here.com>

--- reporter-web-app/src/components/PackageFindingsTable.js
+++ reporter-web-app/src/components/PackageFindingsTable.js
@@ -23,128 +23,111 @@ import { Table } from 'antd';
 import { CopyrightOutlined, FileTextOutlined } from '@ant-design/icons';
 
 // Generates the HTML to display scanFindings as a Table
-class PackageFindingsTable extends React.Component {
-    constructor(props) {
-        super(props);
-
-        const {
-            webAppPackage
-        } = props;
-
-        this.state = {
-            webAppPackage
-        };
-    }
-
-    render() {
-        const {
-            webAppPackage
-        } = this.state;
-        const { findings } = webAppPackage;
-
-        const columns = [
-            {
-                align: 'right',
-                dataIndex: 'type',
-                filters: (() => [
-                    {
-                        text: (
-                            <span>
-                                <CopyrightOutlined />
-                                {' '}
-                                Copyright
-                            </span>
-                        ),
-                        value: 'COPYRIGHT'
-                    },
-                    {
-                        text: (
-                            <span>
-                                <FileTextOutlined />
-                                {' '}
-                                License
-                            </span>
-                        ),
-                        value: 'LICENSE'
-                    }
-                ])(),
-                key: 'type',
-                onFilter: (value, row) => value === row.type,
-                render: (type) => (
-                    <span className="ort-scan-finding-type">
-                        {type === 'LICENSE' && (<FileTextOutlined />)}
-                        {type === 'COPYRIGHT' && (<CopyrightOutlined />)}
-                    </span>
-                ),
-                width: '1em'
-            },
-            {
-                title: 'Value',
-                dataIndex: 'value',
-                filters: (
-                    () => Array.from(webAppPackage.detectedLicenses)
-                        .map((license) => ({ text: license, value: license }))
-                        .sort((a, b) => a.text.localeCompare(b.text))
-                )(),
-                onFilter: (value, row) => value === row.value,
-                key: 'value',
-                render: (value) => (
-                    <span className="ort-word-break-wrap">
-                        {value}
-                    </span>
-                )
-            },
-            {
-                title: 'Path',
-                dataIndex: 'path',
-                defaultSortOrder: 'ascend',
-                key: 'path',
-                sorter: (a, b) => a.path.length - b.path.length,
-                render: (path) => (
-                    <div className="ort-word-break-wrap">
-                        {path}
-                    </div>
-                ),
-                style: { minWidth: '50%' }
-            },
-            {
-                title: 'Start',
-                dataIndex: 'startLine',
-                key: 'startLine',
-                align: 'center'
-            },
-            {
-                title: 'End',
-                dataIndex: 'endLine',
-                key: 'endLine',
-                align: 'center'
-            }
-        ];
-
-        return (
-            <Table
-                columns={columns}
-                dataSource={findings}
-                locale={{
-                    emptyText: 'No scan results'
-                }}
-                pagination={
-                    {
-                        defaultPageSize: 250,
-                        hideOnSinglePage: true,
-                        pageSizeOptions: ['50', '100', '250', '500'],
-                        position: 'bottom',
-                        showQuickJumper: true,
-                        showSizeChanger: true,
-                        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} results`
-                    }
+const PackageFindingsTable = (props) => {
+    const { webAppPackage } = props;
+    const { findings } = webAppPackage;
+    const columns = [
+        {
+            align: 'right',
+            dataIndex: 'type',
+            filters: (() => [
+                {
+                    text: (
+                        <span>
+                            <CopyrightOutlined />
+                            {' '}
+                            Copyright
+                        </span>
+                    ),
+                    value: 'COPYRIGHT'
+                },
+                {
+                    text: (
+                        <span>
+                            <FileTextOutlined />
+                            {' '}
+                            License
+                        </span>
+                    ),
+                    value: 'LICENSE'
                 }
-                rowKey="key"
-                size="small"
-            />
-        );
-    }
-}
+            ])(),
+            key: 'type',
+            onFilter: (value, row) => value === row.type,
+            render: (type) => (
+                <span className="ort-scan-finding-type">
+                    {type === 'LICENSE' && (<FileTextOutlined />)}
+                    {type === 'COPYRIGHT' && (<CopyrightOutlined />)}
+                </span>
+            ),
+            width: '1em'
+        },
+        {
+            title: 'Value',
+            dataIndex: 'value',
+            filters: (
+                () => Array.from(webAppPackage.detectedLicenses)
+                    .map((license) => ({ text: license, value: license }))
+                    .sort((a, b) => a.text.localeCompare(b.text))
+            )(),
+            onFilter: (value, row) => value === row.value,
+            key: 'value',
+            render: (value) => (
+                <span className="ort-word-break-wrap">
+                    {value}
+                </span>
+            )
+        },
+        {
+            title: 'Path',
+            dataIndex: 'path',
+            defaultSortOrder: 'ascend',
+            key: 'path',
+            sorter: (a, b) => a.path.length - b.path.length,
+            render: (path) => (
+                <div className="ort-word-break-wrap">
+                    {path}
+                </div>
+            ),
+            style: { minWidth: '50%' }
+        },
+        {
+            title: 'Start',
+            dataIndex: 'startLine',
+            key: 'startLine',
+            align: 'center'
+        },
+        {
+            title: 'End',
+            dataIndex: 'endLine',
+            key: 'endLine',
+            align: 'center'
+        }
+    ];
+
+    return (
+        <Table
+            columns={columns}
+            dataSource={findings}
+            locale={{
+                emptyText: 'No scan results'
+            }}
+            pagination={
+                {
+                    defaultPageSize: 250,
+                    hideOnSinglePage: true,
+                    pageSizeOptions: ['50', '100', '250', '500'],
+                    position: 'bottom',
+                    showQuickJumper: true,
+                    showSizeChanger: true,
+                    showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} results`
+                }
+            }
+            rowKey="key"
+            size="small"
+        />
+    );
+};
 
 PackageFindingsTable.propTypes = {
     webAppPackage: PropTypes.object.isRequired
