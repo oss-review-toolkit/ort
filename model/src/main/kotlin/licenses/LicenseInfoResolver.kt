a2eac4d2fbace58276c15fde8ef8e2133c45bcbd
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2020-05-15 17:02:45 +0200

    model: Add the class `LicenseInfoResolver`
    Add the model for resolved license information and a related resolver
    class. Resolved in this context means that all processing is done, for
    example license finding curations are applied, and copyright statements
    are processed.
    
    The resolved license model contains all related data with the goal to be
    completely transparent with regard to how license data was modified.
    This information will later be needed by the reports to be able to show
    the effects of configuration options like license finding curations to
    the user.
    
    The resolver class converts the raw license info into the resolved
    license info and caches the results to avoid computing them repeatedly.
    This commit only provides the basic implementation of the resolver, most
    features need to be implemented in later commits. The initial
    implementation also always returns the full data sets. This will later
    be extended with filters to only get the required data, for example to
    filter out license locations excluded by path excludes, or to get only
    declared licenses.
    
    At this point the implementation is completely separate from existing
    license functions like the `LicenseResolver` or the license related
    extension functions `OrtResultExtensions`. The plan is that all code in
    ORT that works with licenses will gradually be migrated to the new API,
    adding the required features to the API on the fly.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ model/src/main/kotlin/licenses/LicenseInfoResolver.kt
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2017-2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.model.licenses
+
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
+
+import org.ossreviewtoolkit.model.CopyrightFinding
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.LicenseSource
+import org.ossreviewtoolkit.model.Provenance
+import org.ossreviewtoolkit.model.utils.FindingsMatcher
+import org.ossreviewtoolkit.spdx.SpdxSingleLicenseExpression
+
+class LicenseInfoResolver(val provider: LicenseInfoProvider) {
+    private val resolvedLicenseInfo: ConcurrentMap<Identifier, ResolvedLicenseInfo> = ConcurrentHashMap()
+
+    /**
+     * Get the [ResolvedLicenseInfo] for the project or package identified by [id].
+     * TODO: Add options to filter output, e.g. "filter excluded findings".
+     */
+    fun resolveLicenseInfo(id: Identifier) = resolvedLicenseInfo.getOrPut(id) { createLicenseInfo(id) }
+
+    private fun createLicenseInfo(id: Identifier): ResolvedLicenseInfo {
+        val licenseInfo = provider.get(id)
+
+        val concludedLicenses = licenseInfo.concludedLicenseInfo.concludedLicense?.decompose().orEmpty()
+        val declaredLicenses = licenseInfo.declaredLicenseInfo.processed.spdxExpression?.decompose().orEmpty()
+        val detectedLicenses = licenseInfo.detectedLicenseInfo.findings.flatMapTo(mutableSetOf()) { findings ->
+            findings.licenses.flatMap { it.license.decompose() }
+        }
+
+        val resolvedLicenses = mutableMapOf<SpdxSingleLicenseExpression, ResolvedLicenseBuilder>()
+
+        fun SpdxSingleLicenseExpression.builder() =
+            resolvedLicenses.getOrPut(this) { ResolvedLicenseBuilder(this) }
+
+        // Handle concluded licenses.
+        concludedLicenses.forEach { license ->
+            license.builder().sources += LicenseSource.CONCLUDED
+        }
+
+        // Handle declared licenses.
+        declaredLicenses.forEach { license ->
+            license.builder().apply {
+                sources += LicenseSource.DECLARED
+
+                originalDeclaredLicenses.addAll(
+                    licenseInfo.declaredLicenseInfo.processed.mapped.filterValues { it == license }.keys
+                )
+            }
+        }
+
+        // Handle detected licenses.
+        val unmatchedCopyrights = mutableMapOf<Provenance, MutableSet<CopyrightFinding>>()
+        val resolvedLocations = resolveLocations(licenseInfo.detectedLicenseInfo, unmatchedCopyrights)
+
+        detectedLicenses.forEach { license ->
+            license.builder().apply {
+                sources += LicenseSource.DETECTED
+                resolvedLocations[license]?.let { locations.addAll(it) }
+            }
+        }
+
+        return ResolvedLicenseInfo(id, resolvedLicenses.values.map { it.build() }, unmatchedCopyrights)
+    }
+
+    private fun resolveLocations(
+        detectedLicenseInfo: DetectedLicenseInfo,
+        unmatchedCopyrights: MutableMap<Provenance, MutableSet<CopyrightFinding>>
+    ): Map<SpdxSingleLicenseExpression, Set<ResolvedLicenseLocation>> {
+        val resolvedLocations = mutableMapOf<SpdxSingleLicenseExpression, MutableSet<ResolvedLicenseLocation>>()
+
+        detectedLicenseInfo.findings.forEach { findings ->
+            // TODO: Add copyrights.
+            // TODO: Filter copyright garbage.
+            // TODO: Process copyright statements.
+            // TODO: Apply license finding curations.
+            // TODO: Apply path excludes.
+            val matchResult = FindingsMatcher().matchFindings(findings.licenses, findings.copyrights)
+            matchResult.matchedFindings.forEach { (licenseFinding, _) ->
+                licenseFinding.license.decompose().forEach { singleLicense ->
+                    resolvedLocations.getOrPut(singleLicense) { mutableSetOf() } += ResolvedLicenseLocation(
+                        findings.provenance,
+                        licenseFinding.location.path,
+                        licenseFinding.location.startLine,
+                        licenseFinding.location.endLine,
+                        appliedCuration = null,
+                        matchingPathExcludes = emptyList(),
+                        copyrights = emptySet()
+                    )
+                }
+            }
+
+            unmatchedCopyrights.getOrPut(findings.provenance) { mutableSetOf() } += matchResult.unmatchedCopyrights
+        }
+
+        return resolvedLocations
+    }
+}
+
+private class ResolvedLicenseBuilder(val license: SpdxSingleLicenseExpression) {
+    val sources = mutableSetOf<LicenseSource>()
+    var originalDeclaredLicenses = mutableSetOf<String>()
+    var locations = mutableSetOf<ResolvedLicenseLocation>()
+
+    fun build() = ResolvedLicense(license, sources, originalDeclaredLicenses, locations)
+}
---
+++ model/src/main/kotlin/licenses/ResolvedLicenseInfo.kt
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2017-2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.model.licenses
+
+import org.ossreviewtoolkit.model.CopyrightFinding
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.LicenseSource
+import org.ossreviewtoolkit.model.Provenance
+import org.ossreviewtoolkit.model.TextLocation
+import org.ossreviewtoolkit.model.config.LicenseFindingCuration
+import org.ossreviewtoolkit.model.config.PathExclude
+import org.ossreviewtoolkit.spdx.SpdxSingleLicenseExpression
+import org.ossreviewtoolkit.utils.CopyrightStatementsProcessor
+import org.ossreviewtoolkit.utils.DeclaredLicenseProcessor
+
+/**
+ * Resolved license information about a package (or project).
+ */
+data class ResolvedLicenseInfo(
+    /**
+     * The identifier of the package (or project).
+     */
+    val id: Identifier,
+
+    /**
+     * The list of [ResolvedLicense]s for this package (or project).
+     */
+    val licenses: List<ResolvedLicense>,
+
+    val unmatchedCopyrights: Map<Provenance, Set<CopyrightFinding>>
+) : Iterable<ResolvedLicense> by licenses {
+    operator fun get(license: SpdxSingleLicenseExpression): ResolvedLicense? = find { it.license == license }
+}
+
+/**
+ * Resolved information for a single license.
+ */
+data class ResolvedLicense(
+    /**
+     * The license.
+     */
+    val license: SpdxSingleLicenseExpression,
+
+    /**
+     * The sources where this license was found.
+     */
+    val sources: Set<LicenseSource>,
+
+    /**
+     * The list of original declared license that were [processed][DeclaredLicenseProcessor] to this [license], or an
+     * empty list, if this [license] was not modified during processing.
+     */
+    val originalDeclaredLicenses: Set<String>,
+
+    /**
+     * All text locations where this license was found.
+     */
+    val locations: Set<ResolvedLicenseLocation>
+)
+
+/**
+ * A resolved text location.
+ */
+data class ResolvedLicenseLocation(
+    /**
+     * The provenance of the file.
+     */
+    val provenance: Provenance,
+
+    /**
+     * The path of the file.
+     */
+    val path: String,
+
+    /**
+     * The start line of the text location.
+     */
+    val startLine: Int,
+
+    /**
+     * The end line of the text location.
+     */
+    val endLine: Int,
+
+    /**
+     * The applied [LicenseFindingCuration], or null if none were applied.
+     */
+    val appliedCuration: LicenseFindingCuration?,
+
+    /**
+     * All matching [PathExclude]s matching this [path].
+     */
+    val matchingPathExcludes: List<PathExclude>,
+
+    /**
+     * All copyright findings associated to this license location.
+     */
+    val copyrights: Set<ResolvedCopyrightFinding>
+)
+
+/**
+ * A resolved copyright finding.
+ */
+data class ResolvedCopyrightFinding(
+    /**
+     * The resolved copyright statement.
+     */
+    val statement: String,
+
+    /**
+     * The original copyright statements, if this statement was created by the [CopyrightStatementsProcessor].
+     */
+    val originalStatements: Set<String>,
+
+    /**
+     * All text locations where this copyright was found.
+     */
+    val locations: Set<TextLocation>
+)
