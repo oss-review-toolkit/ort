oss-review-toolkit/ort

commit bb5e2b6f64c69b599d7383d060b15230a85cc1dc

cli: Use clikt instead of JCommander for command line parsing
In contrast to claims by the maintainer, JCommander is not really
well-maintained anymore [1], see e.g. the number of open PRs and the
recent releases which lack proper release notes and Git tags.

Use clikt [2] instead for its native Kotlin features and maintainer's
responsiveness.

[1] https://github.com/cbeust/jcommander/issues/466
[2] https://kotlin.link/?q=clikt

Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

cli/src/main/kotlin/commands/ReporterCommand.kt
@@ -19,13 +19,16 @@
 
 package com.here.ort.commands
 
-import com.beust.jcommander.IStringConverter
-import com.beust.jcommander.JCommander
-import com.beust.jcommander.Parameter
-import com.beust.jcommander.ParameterException
-import com.beust.jcommander.Parameters
+import com.github.ajalt.clikt.core.BadParameterValue
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.UsageError
+import com.github.ajalt.clikt.core.requireObject
+import com.github.ajalt.clikt.parameters.options.convert
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.options.split
+import com.github.ajalt.clikt.parameters.types.file
 
-import com.here.ort.CommandWithHelp
 import com.here.ort.model.OrtResult
 import com.here.ort.model.config.CopyrightGarbage
 import com.here.ort.model.config.OrtConfiguration
@@ -38,105 +41,73 @@ import com.here.ort.reporter.DefaultLicenseTextProvider
 import com.here.ort.reporter.DefaultResolutionProvider
 import com.here.ort.reporter.Reporter
 import com.here.ort.reporter.ReporterInput
-import com.here.ort.utils.PARAMETER_ORDER_MANDATORY
-import com.here.ort.utils.PARAMETER_ORDER_OPTIONAL
 import com.here.ort.utils.collectMessagesAsString
 import com.here.ort.utils.expandTilde
-import com.here.ort.utils.log
 import com.here.ort.utils.safeMkdirs
 import com.here.ort.utils.showStackTrace
 
 import java.io.File
 
-@Parameters(
-    commandNames = ["report"],
-    commandDescription = "Present Analyzer and Scanner results in various formats."
-)
-object ReporterCommand : CommandWithHelp() {
-    private class ReporterConverter : IStringConverter<Reporter> {
-        companion object {
-            // Map upper-cased reporter names to their instances.
-            val REPORTERS = Reporter.ALL.associateBy { it.reporterName.toUpperCase() }
-        }
+class ReporterCommand : CliktCommand(
+    name = "report",
+    help = "Present Analyzer and Scanner results in various formats."
+) {
+    private val ortFile by option(
+        "--ort-file", "-i",
+        help = "The ORT result file to use."
+    ).file(exists = true, fileOkay = true, folderOkay = false, writable = false, readable = true).required()
 
-        override fun convert(name: String): Reporter =
-            REPORTERS[name.toUpperCase()]
-                ?: throw ParameterException("Reporters must be contained in ${REPORTERS.keys}.")
-    }
+    private val outputDir by option(
+        "--output-dir", "-o",
+        help = "The output directory to store the generated reports in."
+    ).file(exists = false, fileOkay = false, folderOkay = true, writable = false, readable = false).required()
 
-    @Parameter(
-        description = "The ORT result file to use.",
-        names = ["--ort-file", "-i"],
-        required = true,
-        order = PARAMETER_ORDER_MANDATORY
-    )
-    private lateinit var ortFile: File
+    private val reporters by option(
+        "--report-formats", "-f",
+        help = "The list of report formats that will be generated."
+    ).convert { reporterName ->
+        // Map upper-cased reporter names to their instances.
+        val reporters = Reporter.ALL.associateBy { it.reporterName.toUpperCase() }
+        reporters[reporterName.toUpperCase()]
+            ?: throw BadParameterValue("Reporters must be contained in ${reporters.keys}.")
+    }.split(",").required()
 
-    @Parameter(
-        description = "The output directory to store the generated reports in.",
-        names = ["--output-dir", "-o"],
-        required = true,
-        order = PARAMETER_ORDER_MANDATORY
-    )
-    @Suppress("LateinitUsage")
-    private lateinit var outputDir: File
+    private val resolutionsFile by option(
+        "--resolutions-file",
+        help = "A file containing error resolutions."
+    ).file(exists = true, fileOkay = true, folderOkay = false, writable = false, readable = true)
 
-    @Parameter(
-        description = "The list of report formats that will be generated.",
-        names = ["--report-formats", "-f"],
-        converter = ReporterConverter::class,
-        required = true,
-        order = PARAMETER_ORDER_MANDATORY
-    )
-    private lateinit var reporters: List<Reporter>
+    private val preProcessingScript by option(
+        "--pre-processing-script",
+        help = "The path to a Kotlin script to pre-process the notice report before writing it to disk."
+    ).file(exists = true, fileOkay = true, folderOkay = false, writable = false, readable = true)
 
-    @Parameter(
-        description = "A file containing error resolutions.",
-        names = ["--resolutions-file"],
-        order = PARAMETER_ORDER_OPTIONAL
-    )
-    private var resolutionsFile: File? = null
+    private val copyrightGarbageFile by option(
+        "--copyright-garbage-file",
+        help = "A file containing garbage copyright statements entries which are to be ignored."
+    ).file(exists = true, fileOkay = true, folderOkay = false, writable = false, readable = true)
 
-    @Parameter(
-        description = "The path to a Kotlin script to pre-process the notice report before writing it to disk.",
-        names = ["--pre-processing-script"],
-        order = PARAMETER_ORDER_OPTIONAL
-    )
-    private var preProcessingScript: File? = null
+    private val repositoryConfigurationFile by option(
+        "--repository-configuration-file",
+        help = "A file containing the repository configuration. If set the .ort.yml overrides the repository " +
+                "configuration contained in the ort result from the input file."
+    ).file(exists = true, fileOkay = true, folderOkay = false, writable = false, readable = true)
 
-    @Parameter(
-        description = "A file containing garbage copyright statements entries which are to be ignored.",
-        names = ["--copyright-garbage-file"],
-        order = PARAMETER_ORDER_OPTIONAL
-    )
-    private var copyrightGarbageFile: File? = null
+    private val customLicenseTextsDir by option(
+        "--custom-license-texts-dir",
+        help = "A directory which maps custom license IDs to license texts. It should contain one text file per " +
+                "license with the license ID as the filename. A custom license text is used only if its ID has a " +
+                "'LicenseRef-' prefix and if the respective license text is not known by ORT."
+    ).file(exists = false, fileOkay = false, folderOkay = true, writable = false, readable = false)
 
-    @Parameter(
-        description = "A file containing the repository configuration. If set the .ort.yml " +
-                "overrides the repository configuration contained in the ort result from the input file.",
-        names = ["--repository-configuration-file"],
-        order = PARAMETER_ORDER_OPTIONAL
-    )
-    private var repositoryConfigurationFile: File? = null
+    private val licenseConfigurationFile by option(
+        "--license-configuration-file",
+        help = "A file containing the license configuration."
+    ).file(exists = true, fileOkay = true, folderOkay = false, writable = false, readable = true)
 
-    @Parameter(
-        description = "A directory which maps custom license IDs to license texts. " +
-                "It should contain one text file per license with the license ID as the filename. " +
-                "A custom license text is used only if its ID has a 'LicenseRef-' prefix and if " +
-                "the respective license text is not known by ORT.",
-        names = ["--custom-license-texts-dir"],
-        order = PARAMETER_ORDER_OPTIONAL
-    )
-    private var customLicenseTextsDir: File? = null
+    private val config by requireObject<OrtConfiguration>()
 
-    @Parameter(
-        description = "A file containing the license configuration.",
-        names = ["--license-configuration-file"],
-        order = PARAMETER_ORDER_OPTIONAL
-    )
-    private var licenseConfigurationFile: File? = null
-
-    override fun runCommand(jc: JCommander, config: OrtConfiguration): Int {
+    override fun run() {
         val absoluteOutputDir = outputDir.expandTilde().normalize()
 
         val reports = reporters.associateWith { reporter ->
@@ -145,8 +116,7 @@ object ReporterCommand : CommandWithHelp() {
 
         val existingReportFiles = reports.values.filter { it.exists() }
         if (existingReportFiles.isNotEmpty()) {
-            log.error { "None of the report files $existingReportFiles must exist yet." }
-            return 2
+            throw UsageError("None of the report files $existingReportFiles must exist yet.", statusCode = 2)
         }
 
         var ortResult = ortFile.expandTilde().readValue<OrtResult>()
@@ -161,8 +131,6 @@ object ReporterCommand : CommandWithHelp() {
         val copyrightGarbage = copyrightGarbageFile?.expandTilde()?.readValue<CopyrightGarbage>().orEmpty()
 
         val licenseConfiguration = licenseConfigurationFile?.expandTilde()?.readValue<LicenseConfiguration>().orEmpty()
-
-        var exitCode = 0
 
         absoluteOutputDir.safeMkdirs()
 
@@ -185,12 +153,9 @@ object ReporterCommand : CommandWithHelp() {
             } catch (e: Exception) {
                 e.showStackTrace()
 
-                log.error { "Could not create '${reporter.reporterName}' report: ${e.collectMessagesAsString()}" }
-
-                exitCode = 1
+                throw UsageError("Could not create '${reporter.reporterName}' report: ${e.collectMessagesAsString()}",
+                    statusCode = 1)
             }
         }
-
-        return exitCode
     }
 }

cli/build.gradle.kts
@@ -18,8 +18,8 @@
  * License-Filename: LICENSE
  */
 
+val cliktVersion: String by project
 val config4kVersion: String by project
-val jcommanderVersion: String by project
 val kotlintestVersion: String by project
 val log4jCoreVersion: String by project
 val reflectionsVersion: String by project
@@ -31,7 +31,7 @@ plugins {
 
 application {
     applicationName = "ort"
-    mainClassName = "com.here.ort.OrtMain"
+    mainClassName = "com.here.ort.OrtMainKt"
 }
 
 tasks.named<CreateStartScripts>("startScripts") {
@@ -60,7 +60,7 @@ dependencies {
     implementation(project(":scanner"))
     implementation(project(":utils"))
 
-    implementation("com.beust:jcommander:$jcommanderVersion")
+    implementation("com.github.ajalt:clikt:$cliktVersion")
     implementation("io.github.config4k:config4k:$config4kVersion")
     implementation("org.apache.logging.log4j:log4j-core:$log4jCoreVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")

cli/src/funTest/kotlin/OrtMainTest.kt
@@ -56,18 +56,17 @@ class OrtMainTest : StringSpec() {
         "Activating only Gradle works" {
             val inputDir = File(projectDir, "gradle")
 
-            val runResult = runMain(
+            val stdout = runMain(
                 "analyze",
                 "-m", "Gradle",
                 "-i", inputDir.path,
                 "-o", File(outputDir, "gradle").path
             )
-            val iterator = runResult.stdout.iterator()
+            val iterator = stdout.iterator()
             while (iterator.hasNext()) {
                 if (iterator.next() == "The following package managers are activated:") break
             }
 
-            runResult.exitCode shouldBe 0
             iterator.hasNext() shouldBe true
             iterator.next() shouldBe "\tGradle"
         }
@@ -75,18 +74,17 @@ class OrtMainTest : StringSpec() {
         "Activating only NPM works" {
             val inputDir = File(projectDir, "npm/package-lock")
 
-            val runResult = runMain(
+            val stdout = runMain(
                 "analyze",
                 "-m", "NPM",
                 "-i", inputDir.path,
                 "-o", File(outputDir, "package-lock").path
             )
-            val iterator = runResult.stdout.iterator()
+            val iterator = stdout.iterator()
             while (iterator.hasNext()) {
                 if (iterator.next() == "The following package managers are activated:") break
             }
 
-            runResult.exitCode shouldBe 0
             iterator.hasNext() shouldBe true
             iterator.next() shouldBe "\tNPM"
         }
@@ -94,16 +92,15 @@ class OrtMainTest : StringSpec() {
         "Output formats are deduplicated" {
             val inputDir = File(projectDir, "gradle")
 
-            val runResult = runMain(
+            val stdout = runMain(
                 "analyze",
                 "-m", "Gradle",
                 "-i", inputDir.path,
                 "-o", File(outputDir, "gradle").path,
                 "-f", "json,yaml,json"
             )
-            val lines = runResult.stdout.filter { it.startsWith("Writing analyzer result to ") }
+            val lines = stdout.filter { it.startsWith("Writing analyzer result to ") }
 
-            runResult.exitCode shouldBe 0
             lines.count() shouldBe 2
         }
 
@@ -116,7 +113,7 @@ class OrtMainTest : StringSpec() {
                 urlProcessed = normalizeVcsUrl(vcsUrl)
             )
 
-            val runResult = runMain(
+            runMain(
                 "analyze",
                 "-m", "Gradle",
                 "-i", File(projectDir, "gradle").absolutePath,
@@ -124,7 +121,6 @@ class OrtMainTest : StringSpec() {
             )
             val analyzerResult = File(analyzerOutputDir, "analyzer-result.yml").readText()
 
-            runResult.exitCode shouldBe 0
             patchActualResult(analyzerResult, patchStartAndEndTime = true) shouldBe expectedResult
         }
 
@@ -137,7 +133,7 @@ class OrtMainTest : StringSpec() {
                 urlProcessed = normalizeVcsUrl(vcsUrl)
             )
 
-            val runResult = runMain(
+            runMain(
                 "analyze",
                 "-m", "Gradle",
                 "-i", File(projectDir, "gradle").absolutePath,
@@ -146,24 +142,16 @@ class OrtMainTest : StringSpec() {
             )
             val analyzerResult = File(analyzerOutputDir, "analyzer-result.yml").readText()
 
-            runResult.exitCode shouldBe 0
             patchActualResult(analyzerResult, patchStartAndEndTime = true) shouldBe expectedResult
         }
 
         "Requirements are listed correctly" {
-            val runResult = runMain("requirements")
-            val errorLogs = runResult.stdout.find { it.contains(" ERROR - ") }
+            val stdout = runMain("requirements")
+            val errorLogs = stdout.find { it.contains(" ERROR - ") }
 
             errorLogs shouldBe null
-            runResult.exitCode shouldBe 0
         }
     }
 
-    private data class Result(val stdout: Sequence<String>, val exitCode: Int)
-
-    private fun runMain(vararg args: String): Result {
-        var exitCode = 0
-        val output = redirectStdout { exitCode = OrtMain.run(args.asList().toTypedArray()) }
-        return Result(output.lineSequence(), exitCode)
-    }
+    private fun runMain(vararg args: String) = redirectStdout { OrtMain().parse(args.asList()) }.lineSequence()
 }

cli/src/main/kotlin/CommandWithHelp.kt
@@ -1,62 +1,0 @@
-/*
- * Copyright (C) 2017-2019 HERE Europe B.V.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- * License-Filename: LICENSE
- */
-
-package com.here.ort
-
-import com.beust.jcommander.JCommander
-import com.beust.jcommander.Parameter
-
-import com.here.ort.model.config.OrtConfiguration
-
-import com.here.ort.utils.PARAMETER_ORDER_HELP
-
-/**
- * A JCommander command that offers command line help.
- */
-abstract class CommandWithHelp {
-    @Parameter(
-        description = "Display the command line help.",
-        names = ["--help", "-h"],
-        help = true,
-        order = PARAMETER_ORDER_HELP
-    )
-    private var help = false
-
-    /**
-     * Run the command after processing command line help.
-     */
-    fun run(jc: JCommander, config: OrtConfiguration): Int {
-        if (jc.parsedCommand == null) {
-            jc.usage()
-            return 0
-        }
-
-        if (help) {
-            jc.usageFormatter.usage(jc.parsedCommand)
-            return 0
-        }
-
-        return runCommand(jc, config)
-    }
-
-    /**
-     * Run the underlying command.
-     */
-    protected abstract fun runCommand(jc: JCommander, config: OrtConfiguration): Int
-}

cli/src/main/kotlin/OrtMain.kt
@@ -19,15 +19,24 @@
 
 package com.here.ort
 
-import com.beust.jcommander.DynamicParameter
-import com.beust.jcommander.JCommander
-import com.beust.jcommander.Parameter
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.core.findOrSetObject
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.output.CliktHelpFormatter
+import com.github.ajalt.clikt.output.HelpFormatter
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.multiple
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.pair
+import com.github.ajalt.clikt.parameters.options.switch
+import com.github.ajalt.clikt.parameters.options.versionOption
+import com.github.ajalt.clikt.parameters.types.file
 
 import com.here.ort.commands.*
 import com.here.ort.model.Environment
 import com.here.ort.model.config.OrtConfiguration
-import com.here.ort.utils.PARAMETER_ORDER_LOGGING
-import com.here.ort.utils.PARAMETER_ORDER_OPTIONAL
 import com.here.ort.utils.getUserOrtDirectory
 import com.here.ort.utils.expandTilde
 import com.here.ort.utils.printStackTrace
@@ -37,8 +46,6 @@ import com.typesafe.config.ConfigFactory
 import io.github.config4k.extract
 
 import java.io.File
-
-import kotlin.system.exitProcess
 
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.core.config.Configurator
@@ -51,128 +58,84 @@ const val TOOL_NAME = "ort"
 const val ORT_USER_HOME_ENV = "ORT_USER_HOME"
 
 /**
- * The main entry point of the application.
+ * Helper class for mutually exclusive command line options of different types.
  */
-object OrtMain : CommandWithHelp() {
-    @Parameter(
-        description = "The path to a configuration file.",
-        names = ["--config", "-c"],
-        order = PARAMETER_ORDER_OPTIONAL
-    )
-    private var configFile: File? = null
+sealed class GroupTypes {
+    data class FileType(val file: File) : GroupTypes()
+    data class StringType(val string: String) : GroupTypes()
+}
 
-    @Parameter(
-        description = "Enable info logging.",
-        names = ["--info"],
-        order = PARAMETER_ORDER_LOGGING
-    )
-    private var info = false
+class OrtMain : CliktCommand(name = TOOL_NAME, epilog = "* denotes required options.") {
+    private val configFile by option("--config", "-c", help = "The path to a configuration file.")
+        .file(exists = true, fileOkay = true, folderOkay = false, writable = false, readable = true)
 
-    @Parameter(
-        description = "Enable debug logging and keep any temporary files.",
-        names = ["--debug"],
-        order = PARAMETER_ORDER_LOGGING
-    )
-    private var debug = false
+    private val logLevel by option(help = "Set the verbosity level of log output.").switch(
+        "--info" to Level.INFO,
+        "--debug" to Level.DEBUG
+    ).default(Level.WARN)
 
-    @Parameter(
-        description = "Print out the stacktrace for all exceptions.",
-        names = ["--stacktrace"],
-        order = PARAMETER_ORDER_LOGGING
-    )
-    private var stacktrace = false
+    private val stacktrace by option(help = "Print out the stacktrace for all exceptions.").flag()
 
-    @Parameter(
-        description = "Show version information and exit.",
-        names = ["--version", "-v"],
-        order = PARAMETER_ORDER_OPTIONAL
-    )
-    private var version = false
+    private val configArguments by option("-P", help = "Allows to override configuration parameters.").pair().multiple()
 
-    @DynamicParameter(
-        description = "Allows to override configuration parameters.",
-        names = ["-P"]
-    )
-    private var configArguments = mutableMapOf<String, String>()
+    private val env = Environment()
 
-    /**
-     * The entry point for the application.
-     *
-     * @param args The list of application arguments.
-     */
-    @JvmStatic
-    fun main(args: Array<String>) {
-        fixupUserHomeProperty()
-        exitProcess(run(args))
+    private inner class OrtHelpFormatter : CliktHelpFormatter(requiredOptionMarker = "*", showDefaultValues = true) {
+        override fun formatHelp(
+            prolog: String,
+            epilog: String,
+            parameters: List<HelpFormatter.ParameterHelp>,
+            programName: String
+        ) =
+            buildString {
+                // If help is invoked without a subcommand, the main run() is not invoked and no header is printed, so
+                // we need to do that manually here.
+                if (context.invokedSubcommand == null) appendln(getVersionHeader(env.ortVersion))
+                append(super.formatHelp(prolog, epilog, parameters, programName))
+            }
     }
 
-    /**
-     * Check if the "user.home" property is set to a sane value and otherwise set it to the value of the ORT_USER_HOME
-     * environment variable, if set, or to the value of an (OS-specific) environment variable for the user home
-     * directory. This works around the issue that esp. in certain Docker scenarios "user.home" is set to "?", see
-     * https://bugs.openjdk.java.net/browse/JDK-8193433 for some background information.
-     */
-    fun fixupUserHomeProperty() {
-        val userHome = System.getProperty("user.home")
-        val checkedUserHome = sequenceOf(
-            userHome,
-            System.getenv(ORT_USER_HOME_ENV),
-            System.getenv("HOME"),
-            System.getenv("USERPROFILE")
-        ).first {
-            it != null && it.isNotBlank() && it != "?"
+    init {
+        context {
+            expandArgumentFiles = false
+            helpFormatter = OrtHelpFormatter()
         }
 
-        if (checkedUserHome != userHome) System.setProperty("user.home", checkedUserHome)
+        subcommands(
+            AnalyzerCommand(),
+            ClearlyDefinedUploadCommand(),
+            DownloaderCommand(),
+            EvaluatorCommand(),
+            ReporterCommand(),
+            RequirementsCommand(),
+            ScannerCommand()
+        )
+
+        versionOption(
+            version = env.ortVersion,
+            names = setOf("--version", "-v"),
+            help = "Show version information and exit.",
+            message = ::getVersionHeader
+        )
     }
 
-    /**
-     * Run the ORT CLI with the provided [args] and return the exit code of [CommandWithHelp.run].
-     */
-    fun run(args: Array<String>): Int {
-        val jc = JCommander(this).apply {
-            programName = TOOL_NAME
-            setExpandAtSign(false)
-            addCommand(AnalyzerCommand)
-            addCommand(ClearlyDefinedUploadCommand)
-            addCommand(DownloaderCommand)
-            addCommand(EvaluatorCommand)
-            addCommand(ReporterCommand)
-            addCommand(RequirementsCommand)
-            addCommand(ScannerCommand)
-            parse(*args)
-        }
-
-        println(getVersionHeader(jc.parsedCommand))
-
-        val config = loadConfig()
-
-        return if (version) 0 else run(jc, config)
-    }
-
-    override fun runCommand(jc: JCommander, config: OrtConfiguration): Int {
-        when {
-            debug -> Configurator.setRootLevel(Level.DEBUG)
-            info -> Configurator.setRootLevel(Level.INFO)
-        }
+    override fun run() {
+        Configurator.setRootLevel(logLevel)
 
         // Make the parameter globally available.
         printStackTrace = stacktrace
 
-        // JCommander already validates the command names.
-        val command = jc.commands[jc.parsedCommand]!!
-        val commandObject = command.objects.first() as CommandWithHelp
+        // Make the OrtConfiguration available to subcommands.
+        context.findOrSetObject { loadConfig() }
 
-        // Delegate running actions to the specified command.
-        return commandObject.run(jc, config)
+        println(getVersionHeader(env.ortVersion))
     }
 
-    private fun getVersionHeader(commandName: String?): String {
-        val env = Environment()
-
+    private fun getVersionHeader(version: String): String {
         val variables = mutableListOf("$ORT_USER_HOME_ENV = ${getUserOrtDirectory()}")
         env.variables.entries.mapTo(variables) { (key, value) -> "$key = $value" }
 
+        val commandName = context.invokedSubcommand?.commandName
         val command = commandName?.let { " '$commandName'" }.orEmpty()
         val with = if (variables.isNotEmpty()) " with" else "."
 
@@ -181,7 +144,7 @@ object OrtMain : CommandWithHelp() {
         val header = mutableListOf<String>()
         """
             ________ _____________________
-            \_____  \\______   \__    ___/ the OSS Review Toolkit, version ${env.ortVersion}.
+            \_____  \\______   \__    ___/ the OSS Review Toolkit, version $version.
              /   |   \|       _/ |    |    Running$command under Java ${env.javaVersion} on ${env.os}$with
             /    |    \    |   \ |    |    ${variables.getOrElse(variableIndex++) { "" }}
             \_______  /____|_  / |____|    ${variables.getOrElse(variableIndex++) { "" }}
@@ -198,12 +161,8 @@ object OrtMain : CommandWithHelp() {
     }
 
     private fun loadConfig(): OrtConfiguration {
-        val argsConfig = ConfigFactory.parseMap(configArguments, "Command line").withOnlyPath("ort")
+        val argsConfig = ConfigFactory.parseMap(configArguments.toMap(), "Command line").withOnlyPath("ort")
         val fileConfig = configFile?.expandTilde()?.let {
-            require(it.isFile) {
-                "The provided configuration file '$it' is not actually a file."
-            }
-
             ConfigFactory.parseFile(it).withOnlyPath("ort")
         }
         val defaultConfig = ConfigFactory.parseResources("default.conf")
@@ -216,4 +175,32 @@ object OrtMain : CommandWithHelp() {
 
         return combinedConfig.extract("ort")
     }
+}
+
+/**
+ * Check if the "user.home" property is set to a sane value and otherwise set it to the value of the ORT_USER_HOME
+ * environment variable, if set, or to the value of an (OS-specific) environment variable for the user home
+ * directory. This works around the issue that esp. in certain Docker scenarios "user.home" is set to "?", see
+ * https://bugs.openjdk.java.net/browse/JDK-8193433 for some background information.
+ */
+fun fixupUserHomeProperty() {
+    val userHome = System.getProperty("user.home")
+    val checkedUserHome = sequenceOf(
+        userHome,
+        System.getenv(ORT_USER_HOME_ENV),
+        System.getenv("HOME"),
+        System.getenv("USERPROFILE")
+    ).first {
+        it != null && it.isNotBlank() && it != "?"
+    }
+
+    if (checkedUserHome != userHome) System.setProperty("user.home", checkedUserHome)
+}
+
+/**
+ * The entry point for the application with [args] being the list of arguments.
+ */
+fun main(args: Array<String>) {
+    fixupUserHomeProperty()
+    return OrtMain().main(args)
 }

commit af486a2f3179002ba364341417b14fcddca3c5a6

ReporterCommand: Continue creating other reports even if some failed
This fixes a regression introduced in bb5e2b6.

Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

cli/src/main/kotlin/commands/ReporterCommand.kt
@@ -202,6 +202,8 @@ class ReporterCommand : CliktCommand(
             reportSpecificOptionsMap[option.first] = option.second
         }
 
+        var statusCode = 0
+
         reports.forEach { (reporter, file) ->
             @Suppress("TooGenericExceptionCaught")
             try {
@@ -217,8 +219,14 @@ class ReporterCommand : CliktCommand(
                 if (file.length() == 0L) file.delete()
 
                 log.error { "Could not create '${reporter.reporterName}' report: ${e.collectMessagesAsString()}" }
-                throw ProgramResult(1)
+
+                statusCode = 1
             }
+        }
+
+        if (statusCode != 0) {
+            println("Not all reports were created successfully.")
+            throw ProgramResult(statusCode)
         }
     }
 }