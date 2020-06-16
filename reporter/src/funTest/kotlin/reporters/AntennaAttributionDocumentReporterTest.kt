b0566b1faf57b81455e9a8d35e84825e46b52b69
Onur Demirci <onur.demirci@bosch.io>
2020-06-17 00:13:57 +0200

    reporter: Add a test for the AntennaAttributionDocumentReporter
    The AntennaAttributionDocumentReporterTest tests only the size of the
    generated report because comparing the contents does not work due to
    added timestamps.
    
    Signed-off-by: Onur Demirci <onur.demirci@bosch.io>

---
+++ reporter/src/funTest/kotlin/reporters/AntennaAttributionDocumentReporterTest.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2020 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.reporter.reporters
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+import org.ossreviewtoolkit.model.OrtResult
+import org.ossreviewtoolkit.reporter.ReporterInput
+import org.ossreviewtoolkit.utils.ORT_NAME
+import org.ossreviewtoolkit.utils.test.readOrtResult
+
+class AntennaAttributionDocumentReporterTest : WordSpec({
+    "AntennaAttributionDocumentReporter" should {
+        "successfully generate the PDF output" {
+            val ortResult = readOrtResult(
+                "../scanner/src/funTest/assets/file-counter-expected-output-for-analyzer-result.yml"
+            )
+
+            val actualReport = generateReport(ortResult)
+
+            actualReport.size shouldBe 53797
+        }
+    }
+})
+
+private fun generateReport(ortResult: OrtResult): ByteArray {
+    val outputDir = createTempDir(
+        ORT_NAME, AntennaAttributionDocumentReporterTest::class.simpleName
+    ).apply { deleteOnExit() }
+
+    return AntennaAttributionDocumentReporter().generateReport(ReporterInput(ortResult), outputDir).single().readBytes()
+}
