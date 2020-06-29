0ecbb74c1f6de7d0ec12c1a44b6f4f60aa3c2bdb
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2020-06-29 17:50:24 +0200

    analyzer: Add a generic SPDX-document-based "fake" package manager
    See the discussion at https://github.com/spdx/spdx-spec/issues/439 for
    details.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

---
+++ analyzer/src/funTest/assets/projects/synthetic/spdx/package/README.md
@@ -0,0 +1,2 @@
+This is the root of a project that stores `package.spdx.yml` files in the directories of its dependencies (one file per
+dependency package).
---
+++ analyzer/src/funTest/kotlin/managers/SpdxDocumentFileTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2020 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.analyzer.managers
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+import java.io.File
+
+import org.ossreviewtoolkit.downloader.VersionControlSystem
+import org.ossreviewtoolkit.utils.normalizeVcsUrl
+import org.ossreviewtoolkit.utils.test.DEFAULT_ANALYZER_CONFIGURATION
+import org.ossreviewtoolkit.utils.test.DEFAULT_REPOSITORY_CONFIGURATION
+import org.ossreviewtoolkit.utils.test.USER_DIR
+import org.ossreviewtoolkit.utils.test.patchExpectedResult
+
+class SpdxDocumentFileTest : StringSpec() {
+    private val projectDir = File("src/funTest/assets/projects/synthetic/spdx").absoluteFile
+    private val vcsDir = VersionControlSystem.forDirectory(projectDir)!!
+    private val vcsUrl = vcsDir.getRemoteUrl()
+    private val vcsRevision = vcsDir.getRevision()
+
+    init {
+        "Project dependencies are detected correctly" {
+            val expectedResult = patchExpectedResult(
+                projectDir.parentFile.resolve("spdx-project-expected-output.yml"),
+                url = vcsUrl,
+                urlProcessed = normalizeVcsUrl(vcsUrl),
+                revision = vcsRevision
+            )
+
+            val definitionFile = projectDir.resolve("project/project.spdx.yml")
+            val actualResult = createSpdxDocumentFile().resolveSingleProject(definitionFile).toYaml()
+
+            actualResult shouldBe expectedResult
+        }
+
+        // TODO: Add a test for "package.spdx.yml".
+
+        // TODO: Test that we can read in files written by SpdxDocumentReporter.
+    }
+
+    private fun createSpdxDocumentFile() =
+        SpdxDocumentFile(
+            "SpdxDocumentFile",
+            USER_DIR,
+            DEFAULT_ANALYZER_CONFIGURATION,
+            DEFAULT_REPOSITORY_CONFIGURATION
+        )
+}
