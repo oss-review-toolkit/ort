9af63f4dde18d63d7bb10fe2e328079ecd9a4c1c
Thomas Steenbergen <thomas.steenbergen@here.com>
2020-07-03 22:41:08 +0200

    SpdxDocumentReporter: Add purl as SPDX externalRefs
    Signed-off-by: Thomas Steenbergen <thomas.steenbergen@here.com>

--- spdx-utils/src/main/kotlin/model/SpdxExternalReference.kt
+++ spdx-utils/src/main/kotlin/model/SpdxExternalReference.kt
@@ -60,6 +60,22 @@ data class SpdxExternalReference(
         OTHER("OTHER");
     }
 
+    // TODO: Using new values as per https://github.com/spdx/spdx-spec/issues/451.
+    enum class Type(
+        val typeName: String,
+        val category: Category
+    ) {
+        BOWER("bower", Category.PACKAGE_MANAGER),
+        CPE_22_TYPE("cpe22Type", Category.SECURITY),
+        CPE_23_TYPE("cpe23Type", Category.SECURITY),
+        MAVEN_CENTRAL("maven-central", Category.PACKAGE_MANAGER),
+        NPM("npm", Category.PACKAGE_MANAGER),
+        NUGET("nuget", Category.PACKAGE_MANAGER),
+        PURL("purl", Category.PACKAGE_MANAGER),
+        SWH("swh", Category.PERSISTENT_ID),
+        OTHER("OTHER", Category.OTHER);
+    }
+
     init {
         require(referenceLocator.isNotBlank()) { "The referenceLocator must not be blank." }
 
