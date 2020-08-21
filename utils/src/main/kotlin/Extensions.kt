5d376bfa71e85e4b4fcc72e82ad3fdc3ad1ae067
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2020-08-21 13:49:19 +0200

    Extensions: Do not print a trailing comma when collecting exception messages
    As all lines are prefixed by "Suppressed: " having a trailing comma
    between these simply looks (arguably) odd. See e.g.
    
    Suppressed: DownloadException: Unsupported VCS type ''.,
    Suppressed: DownloadException: No source artifact URL provided for 'Downloader::ciam-ids4-centralids:'.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- utils/src/main/kotlin/Extensions.kt
+++ utils/src/main/kotlin/Extensions.kt
@@ -323,7 +323,7 @@ fun Throwable.collectMessages(): List<String> {
     val messages = mutableListOf<String>()
     var cause: Throwable? = this
     while (cause != null) {
-        val suppressed = cause.suppressed.joinToString { "\nSuppressed: ${it.javaClass.simpleName}: ${it.message}" }
+        val suppressed = cause.suppressed.joinToString("") { "\nSuppressed: ${it.javaClass.simpleName}: ${it.message}" }
         messages += "${cause.javaClass.simpleName}: ${cause.message}$suppressed"
         cause = cause.cause
     }
