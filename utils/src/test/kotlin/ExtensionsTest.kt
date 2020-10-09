421a1060afe5e713ab6571b285130a8a36be5d5e
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2020-10-09 09:27:55 +0200

    Extensions(Test): Fix alphabetical sorting of File.searchUpwardsForFile()
    This is a fixup for a53f102.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- utils/src/test/kotlin/ExtensionsTest.kt
+++ utils/src/test/kotlin/ExtensionsTest.kt
@@ -122,15 +122,6 @@ class ExtensionsTest : WordSpec({
         tempDir.safeDeleteRecursively()
     }
 
-    "File.searchUpwardsForSubdirectory" should {
-        "find the root Git directory" {
-            val gitRoot = File(".").searchUpwardsForSubdirectory(".git")
-
-            gitRoot.shouldNotBeNull()
-            gitRoot shouldBe File("..").absoluteFile.normalize()
-        }
-    }
-
     "File.searchUpwardsForFile" should {
         "find the README.md file case insensitive" {
             val readmeFile = File(".").searchUpwardsForFile("ReadMe.MD", true)
@@ -150,6 +141,15 @@ class ExtensionsTest : WordSpec({
             val readmeFile = File(".").searchUpwardsForFile("ReadMe.MD", false)
 
             readmeFile.shouldBeNull()
+        }
+    }
+
+    "File.searchUpwardsForSubdirectory" should {
+        "find the root Git directory" {
+            val gitRoot = File(".").searchUpwardsForSubdirectory(".git")
+
+            gitRoot.shouldNotBeNull()
+            gitRoot shouldBe File("..").absoluteFile.normalize()
         }
     }
 
