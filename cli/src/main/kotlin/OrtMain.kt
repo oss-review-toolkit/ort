457d3612cb28f2afa78173455b66c60f0aa68a7d
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2020-10-20 10:58:23 +0200

    WIP

--- cli/src/main/kotlin/OrtMain.kt
+++ cli/src/main/kotlin/OrtMain.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017-2020 HERE Europe B.V.
+ * test
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
