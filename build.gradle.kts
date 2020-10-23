83e8bdc933b558225fbb93b20841fd5db148b034
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2020-10-23 15:18:05 +0200

    Gradle: Account for the kotest plugin to create Gradle run configs now
    See https://github.com/kotest/kotest-intellij-plugin/commit/114ec28.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ import java.net.URL
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
-import org.jetbrains.gradle.ext.JUnit
+import org.jetbrains.gradle.ext.Gradle
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -62,10 +62,10 @@ idea {
     project {
         settings {
             runConfigurations {
-                defaults<JUnit> {
+                defaults<Gradle> {
                     // Disable "condensed" multi-line diffs when running tests from the IDE to more easily accept actual
                     // results as expected results.
-                    vmParameters = "-Dkotest.assertions.multi-line-diff=simple"
+                    jvmArgs = "-Dkotest.assertions.multi-line-diff=simple"
                 }
             }
         }
