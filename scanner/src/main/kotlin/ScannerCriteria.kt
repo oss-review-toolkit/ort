8436724e09215da4bdf482aeb78416aacf1ff506
Oliver Heger <oliver.heger@bosch.io>
2020-10-28 13:58:46 +0100

    Add ScannerCriteria data class
    This class is going to be used when querying results from a
    ScanResultsStorage. It contains properties that define, which results
    are acceptable. This mechanism is a replacement for the strict
    configuration-based compatibility check used currently. The idea is
    that users can configure to a certain extent, which existing scan
    results can be reused, even if the scanner that produced those results
    is not the same as the current one or uses a slightly different
    configuration.
    
    Currently, the properties available are restricted to basic use cases.
    This might be extended in future, for instance by criteria that define
    the data a scan result must contain.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ scanner/src/main/kotlin/ScannerCriteria.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2020 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.scanner
+
+import com.vdurmont.semver4j.Semver
+
+import org.ossreviewtoolkit.model.ScannerDetails
+
+/**
+ * Definition of a predicate to check whether the configuration of a scanner is compatible with the requirements
+ * specified by a [ScannerCriteria] object.
+ *
+ * When testing whether a scan result is compatible with specific criteria this function is invoked on the
+ * scanner configuration data stored in the result. By having different, scanner-specific matcher functions, this
+ * compatibility check can be made very flexible.
+ *
+ * TODO: Switch to a more advanced type than String to represent the scanner configuration.
+ */
+typealias ScannerConfigMatcher = (String) -> Boolean
+
+/**
+ * A data class defining selection criteria for scanners.
+ *
+ * An instance of this class is passed to a [ScanResultsStorage] to define the criteria a scan result must match,
+ * so that it can be used as a replacement for a result produced by an actual scanner. A scanner implementation
+ * creates a criteria object with its exact properties. Users can override some or all of these properties to
+ * state the criteria under which results from a storage are acceptable even if they deviate from the exact
+ * properties of the scanner. That way it can be configured for instance, that results produced by an older
+ * version of the scanner can be used.
+ */
+data class ScannerCriteria(
+    /**
+     * Criterion to match the scanner name. This string is interpreted as a regular expression. In the most basic
+     * form, it can be an exact scanner name, but by using features of regular expressions, a more advanced
+     * matching can be achieved. So it is possible for instance to select multiple scanners using an alternative ('|')
+     * expression or an arbitrary one using a wildcard ('.*').
+     */
+    val regScannerName: String,
+
+    /**
+     * Criterion to match for the minimum scanner version. Results are accepted if they are produced from scanners
+     * with at least this version.
+     */
+    val minVersion: Semver,
+
+    /**
+     * Criterion to match for the maximum scanner version. Results are accepted if they are produced from scanners
+     * with a version lower than this one. (This bound of the version range is excluding.)
+     */
+    val maxVersion: Semver,
+
+    /**
+     * A function to check whether the configuration of a scanner is compatible with this criteria object.
+     */
+    val configMatcher: ScannerConfigMatcher
+) {
+    companion object {
+        /**
+         * A matcher for scanner configurations that accepts all configurations passed in. This function can be
+         * used if the concrete configuration of a scanner is irrelevant.
+         */
+        val ALL_CONFIG_MATCHER: ScannerConfigMatcher = { true }
+
+        /**
+         * A matcher for scanner configurations that accepts only exact matches of the [originalConfig]. This
+         * function can be used by scanners that are extremely sensitive about their configuration.
+         */
+        fun exactConfigMatcher(originalConfig: String): ScannerConfigMatcher = { config -> originalConfig == config }
+    }
+
+    /** The regular expression to match for the scanner name. */
+    private val nameRegex: Regex by lazy { Regex(regScannerName) }
+
+    /**
+     * Check whether the [details] specified match the criteria stored in this object. Return true if and only if
+     * the result described by the [details] fulfills all the requirements expressed by the properties of this
+     * object.
+     */
+    fun matches(details: ScannerDetails): Boolean {
+        if (!nameRegex.matches(details.name)) {
+            return false
+        }
+
+        val version = Semver(details.version)
+        return minVersion <= version && maxVersion > version && configMatcher(details.configuration)
+    }
+}
---
+++ scanner/src/test/kotlin/ScannerCriteriaTest.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2020 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.scanner
+
+import com.vdurmont.semver4j.Semver
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+import org.ossreviewtoolkit.model.ScannerDetails
+
+class ScannerCriteriaTest : WordSpec({
+    "ScannerCriteria" should {
+        "provide a config matcher that accepts every configuration" {
+            ScannerCriteria.ALL_CONFIG_MATCHER("") shouldBe true
+            ScannerCriteria.ALL_CONFIG_MATCHER("foo") shouldBe true
+            ScannerCriteria.ALL_CONFIG_MATCHER("Supercalifragilisticexpialidocious") shouldBe true
+        }
+
+        "provide a config matcher that accepts only exact configuration matches" {
+            val orgConfig = "--info --copyright --licenses"
+            val matcher = ScannerCriteria.exactConfigMatcher(orgConfig)
+
+            matcher(orgConfig) shouldBe true
+            matcher("$orgConfig --more") shouldBe false
+        }
+    }
+
+    "ScannerCriteria.isCompatible()" should {
+        "accept matching details" {
+            matchingCriteria.matches(testDetails) shouldBe true
+        }
+
+        "detect a different name" {
+            val criteria = matchingCriteria.copy(regScannerName = testDetails.name + "_other")
+
+            criteria.matches(testDetails) shouldBe false
+        }
+
+        "use a regular expression to match the scanner name" {
+            val criteria = matchingCriteria.copy(regScannerName = "Sc.*Cr.+Te.t")
+
+            criteria.matches(testDetails) shouldBe true
+        }
+
+        "detect a scanner version that is too old" {
+            val criteria = matchingCriteria.copy(
+                minVersion = matchingCriteria.maxVersion,
+                maxVersion = Semver("2.0.0")
+            )
+
+            criteria.matches(testDetails) shouldBe false
+        }
+
+        "detect a scanner version that is too new" {
+            val criteria = matchingCriteria.copy(
+                minVersion = Semver("1.0.0"),
+                maxVersion = Semver(testDetails.version)
+            )
+
+            criteria.matches(testDetails) shouldBe false
+        }
+
+        "detect a difference reported by the config matcher" {
+            val criteria = matchingCriteria.copy(
+                configMatcher = ScannerCriteria.exactConfigMatcher(testDetails.configuration + "_other")
+            )
+
+            criteria.matches(testDetails) shouldBe false
+        }
+    }
+})
+
+/** Test details to match against. */
+private val testDetails = ScannerDetails("ScannerCriteriaTest", "1.2.3.beta-47", "a b c")
+
+/** A test instance which should accept the test details. */
+private val matchingCriteria = ScannerCriteria(
+    regScannerName = testDetails.name,
+    minVersion = Semver(testDetails.version),
+    maxVersion = Semver(testDetails.version).nextPatch(),
+    configMatcher = ScannerCriteria.exactConfigMatcher(testDetails.configuration)
+)
