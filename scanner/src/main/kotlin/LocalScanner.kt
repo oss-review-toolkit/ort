ff97c0688542048c0b9df4946af744a98d19a537
Frank Viernau <frank.viernau@here.com>
2020-11-05 12:45:56 +0100

    LocalScanner: Fix filtering the scan result by VCS path
    In case of Git Repo the VCS path points to the manifest file, thus it
    does not denote a directory to be sparse checked out. Thus, the
    filtering introduced by [1] has the effect that the scan results of
    Git Repo based project become empty.
    
    Fix this by not applying any filtering for Git Repo.
    
    [1] https://github.com/oss-review-toolkit/ort/pull/3258
    
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

--- scanner/src/main/kotlin/LocalScanner.kt
+++ scanner/src/main/kotlin/LocalScanner.kt
@@ -56,6 +56,7 @@ import org.ossreviewtoolkit.model.ScannerRun
 import org.ossreviewtoolkit.model.Severity
 import org.ossreviewtoolkit.model.Success
 import org.ossreviewtoolkit.model.VcsInfo
+import org.ossreviewtoolkit.model.VcsType
 import org.ossreviewtoolkit.model.config.ScannerConfiguration
 import org.ossreviewtoolkit.model.createAndLogIssue
 import org.ossreviewtoolkit.model.mapper
@@ -520,7 +521,7 @@ private fun ScanResultContainer.deduplicateScanResults(): ScanResultContainer {
 }
 
 private fun ScanResult.filterByVcsPath(): ScanResult {
-    val path = provenance.vcsInfo?.path.orEmpty()
+    val path = provenance.vcsInfo?.takeIf { it.type != VcsType.GIT_REPO }?.path.orEmpty()
 
     return filterPath(path)
 }
