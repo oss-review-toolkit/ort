447c516d86372ade868efc3c31fa34706e17291b
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2020-11-30 16:14:32 +0100

    test-utils: Introduce Proxy.toGenericString() for later reuse
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- test-utils/src/main/kotlin/Extensions.kt
+++ test-utils/src/main/kotlin/Extensions.kt
@@ -23,6 +23,12 @@ import io.kotest.matchers.Matcher
 import io.kotest.matchers.maps.MapContainsMatcher
 import io.kotest.matchers.nulls.shouldNotBeNull
 
+import java.net.InetSocketAddress
+import java.net.Proxy
+
+fun Proxy.toGenericString() =
+    (address() as? InetSocketAddress)?.let { address -> "${type()} @ ${address.hostString}:${address.port}" }
+
 fun <K, V> containExactly(vararg expected: Pair<K, V>): Matcher<Map<K, V>> = MapContainsMatcher(expected.toMap())
 
 infix fun <T> T?.shouldNotBeNull(block: T.() -> Unit) {
