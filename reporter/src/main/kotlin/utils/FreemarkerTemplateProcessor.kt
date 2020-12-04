0b81d0c83b1cc4a9dd7adc9c65470fd640c7d484
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2020-12-04 08:15:04 +0100

    FreemarkerTemplateProcessor: Move the default logic to NoticeTemplateReporter
    As the default template IDs are now format-specific, the
    NoticeTemplateReporter should determine the default ID to use.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- reporter/src/main/kotlin/utils/FreemarkerTemplateProcessor.kt
+++ reporter/src/main/kotlin/utils/FreemarkerTemplateProcessor.kt
@@ -53,8 +53,6 @@ class FreemarkerTemplateProcessor(
     companion object {
         const val OPTION_TEMPLATE_ID = "template.id"
         const val OPTION_TEMPLATE_PATH = "template.path"
-
-        const val DEFAULT_TEMPLATE_ID = "default"
     }
 
     /**
@@ -92,8 +90,7 @@ class FreemarkerTemplateProcessor(
         }
 
         val templatePaths = options[OPTION_TEMPLATE_PATH]?.split(",").orEmpty()
-        val templateIds = options[OPTION_TEMPLATE_ID]?.split(",")
-            ?: if (templatePaths.isEmpty()) listOf(DEFAULT_TEMPLATE_ID) else emptyList()
+        val templateIds = options[OPTION_TEMPLATE_ID]?.split(",").orEmpty()
 
         val templateFiles = templatePaths.map { path ->
             File(path).expandTilde().also {
