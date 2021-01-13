79b3c6747ac4caaae5fb398922395938c0fad622
Nicolas Nobelis <nicolas.nobelis@bosch.io>
2021-01-13 12:11:17 +0100

    Ignore leading './' in path exclude patterns
    Signed-off-by: Nicolas Nobelis <nicolas.nobelis@bosch.io>

---
+++ model/src/test/kotlin/config/PathExcludeTest.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.model.config
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+/**
+ * This class tests [PathExclude] members functions
+ */
+class PathExcludeTest : WordSpec() {
+    private val pathExcludeWithLeadingDot = PathExclude("./path1", PathExcludeReason.BUILD_TOOL_OF, "")
+
+    init {
+        "isPathExcluded" should {
+            "ignore leading './' in the matching path" {
+                pathExcludeWithLeadingDot.matches("path1") shouldBe true
+                pathExcludeWithLeadingDot.matches("path2") shouldBe false
+            }
+        }
+    }
+}
