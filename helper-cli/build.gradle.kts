398c1f254c517a72e7a74c58e29045eed5bf3e7f
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-03-15 21:16:48 +0100

    Gradle: Upgrade JGit to version 5.11.0.202103091610-r
    See [1]. Most notably, this by default enables "client-side protocol V2
    support for fetching".
    
    [1] https://projects.eclipse.org/projects/technology.jgit/releases/5.11.0
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- helper-cli/build.gradle.kts
+++ helper-cli/build.gradle.kts
@@ -53,6 +53,17 @@ repositories {
         }
     }
 
+    // TODO: Remove this again once this JGit release is properly mirrored to Maven Central.
+    exclusiveContent {
+        forRepository {
+            maven("https://repo.eclipse.org/content/repositories/jgit-releases/")
+        }
+
+        filter {
+            includeGroup("org.eclipse.jgit")
+        }
+    }
+
     exclusiveContent {
         forRepository {
             maven("https://repo.eclipse.org/content/repositories/sw360-releases/")
