c56835dd0bfa8d0bbf47af720ebff9cd05e1ec37
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-03-25 21:46:36 +0100

    Scanner: Rename a few variables for clarity
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- scanner/src/main/kotlin/Scanner.kt
+++ scanner/src/main/kotlin/Scanner.kt
@@ -119,14 +119,14 @@ abstract class Scanner(
 
         // Add the projects as packages to scan.
         val consolidatedProjects = consolidateProjectPackagesByVcs(ortResult.getProjects(skipExcluded))
-        val consolidatedReferencePackages = consolidatedProjects.keys.toList()
+        val projectPackages = consolidatedProjects.keys.toList()
 
-        val referencePackageIdentifiers = consolidatedReferencePackages.map { it.id }
+        val projectPackageIds = projectPackages.map { it.id }
         val packages = ortResult.getPackages(skipExcluded)
-            .filter { it.pkg.id !in referencePackageIdentifiers }
+            .filter { it.pkg.id !in projectPackageIds }
             .map { it.pkg }
 
-        val packagesToScan = consolidatedReferencePackages + packages
+        val packagesToScan = projectPackages + packages
         val scanResults = runBlocking {
             scanPackages(packagesToScan, outputDirectory, downloadDirectory).mapKeys { it.key.id }
         }.toSortedMap()
