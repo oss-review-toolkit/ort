3d6c5686743c4a46ca86bbac924fd25bc67ec786
Oliver Heger <oliver.heger@bosch.io>
2021-03-30 06:56:25 +0200

    Add a command to apply a JSLT transformation to an ORT result
    JSLT [1] supports powerful transformations on JSON data. This command
    is useful for instance if there are breaking changes in ORT's data
    format; by applying a corresponding transformation, stored results can
    be upgraded to the new format.
    
    [1] https://github.com/schibsted/jslt
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ helper-cli/src/main/kotlin/commands/TransformResultCommand.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.helper.commands
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.convert
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.file
+
+import com.schibsted.spt.data.jslt.Parser
+
+import org.ossreviewtoolkit.model.yamlMapper
+import org.ossreviewtoolkit.utils.expandTilde
+
+/**
+ * A command to transform an ORT result using a JSLT expression.
+ *
+ * See https://github.com/schibsted/jslt.
+ */
+class TransformResultCommand : CliktCommand(
+    name = "transform", help = "Implements a JSLT transformation on the given ORT result file."
+) {
+    private val ortResultFile by option(
+        "--ort-result-file", "-i",
+        help = "The ORT result file to read as input."
+    ).convert { it.expandTilde() }
+        .file(mustExist = true, canBeFile = true, canBeDir = false, mustBeWritable = false, mustBeReadable = true)
+        .convert { it.absoluteFile.normalize() }
+        .required()
+
+    private val transformationFile by option(
+        "--transformation-file",
+        help = "The file defining the JSLT transformation to apply."
+    ).convert { it.expandTilde() }
+        .file(mustExist = true, canBeFile = true, canBeDir = false, mustBeWritable = false, mustBeReadable = true)
+        .convert { it.absoluteFile.normalize() }
+        .required()
+
+    private val outputFile by option(
+        "--output-file", "-o",
+        help = "The file in which to write the result of the transformation."
+    ).convert { it.expandTilde() }
+        .file(mustExist = false, canBeFile = true, canBeDir = false, mustBeWritable = false, mustBeReadable = false)
+        .convert { it.absoluteFile.normalize() }
+        .required()
+
+    override fun run() {
+        val node = yamlMapper.readTree(ortResultFile)
+
+        val expression = Parser.compile(transformationFile)
+
+        val result = expression.apply(node)
+        yamlMapper.writeValue(outputFile, result)
+
+        println("Wrote result to $outputFile.")
+    }
+}
