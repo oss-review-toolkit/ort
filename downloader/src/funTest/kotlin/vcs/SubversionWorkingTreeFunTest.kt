927b488df9c51504a6c56d45b9790e2c00a77bd9
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-04-05 18:02:23 +0200

    SubversionWorkingTreeFunTest: Update the list of expected tags
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- downloader/src/funTest/kotlin/vcs/SubversionWorkingTreeFunTest.kt
+++ downloader/src/funTest/kotlin/vcs/SubversionWorkingTreeFunTest.kt
@@ -117,6 +117,7 @@ class SubversionWorkingTreeFunTest : StringSpec() {
                 "docutils-0.14rc2",
                 "docutils-0.15",
                 "docutils-0.16",
+                "docutils-0.17",
                 "docutils-0.3.7",
                 "docutils-0.3.9",
                 "docutils-0.4",
