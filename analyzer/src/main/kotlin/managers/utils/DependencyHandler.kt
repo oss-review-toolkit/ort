fbb380b114c0c47bde3e3a51fc16f4f826b3478a
Oliver Heger <oliver.heger@bosch.io>
2021-04-12 09:28:04 +0200

    Add DependencyHandler interface
    This interface plays a central role to generalize the
    GradleDependencyBuilder class to support other package managers as
    well. The idea is that each package manager can have its own, internal
    representation of a dependency. A DependencyHandler allows extracting
    information from such a representation, so that it can be processed by
    a generic builder, which does not need to have knowledge about the
    internals of a specific package manager implementation.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ analyzer/src/main/kotlin/managers/utils/DependencyHandler.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.analyzer.managers.utils
+
+import org.ossreviewtoolkit.analyzer.PackageManager
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.OrtIssue
+import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.PackageLinkage
+
+/**
+ * An interface used by [DependencyGraphBuilder] to handle the specific representations of concrete [PackageManager]
+ * implementations in a generic way.
+ *
+ * A package manager may use its own, internal representation of a type [D] of a dependency. When constructing the
+ * [org.ossreviewtoolkit.model.DependencyGraph] by passing the dependencies of the single scopes, the builder must be
+ * able to extract certain information from the dependency objects. This is done via an implementation of this
+ * interface.
+ */
+interface DependencyHandler<D> {
+    /**
+     * Construct a unique identifier for the given [dependency]. This identifier should be derived from the
+     * coordinates of the dependency. It is later converted to an [Identifier]; so it has to adhere to the
+     * string-representation of this class.
+     */
+    fun identifierFor(dependency: D): String
+
+    /**
+     * Return a collection with the dependencies of the given [dependency]. [DependencyGraphBuilder] invokes this
+     * function to construct the whole dependency tree spawned by this [dependency].
+     */
+    fun dependenciesFor(dependency: D): Collection<D>
+
+    /**
+     * Return the [PackageLinkage] for the given [dependency].
+     */
+    fun linkageFor(dependency: D): PackageLinkage
+
+    /**
+     * Create a [Package] to represent the [dependency] with the given [identifier]. This is used to populate the
+     * packages in the analyzer result. The creation of a package may fail, e.g. if the dependency cannot be resolved.
+     * In this case, a concrete implementation is expected to return a dummy [Package] with correct coordinates and
+     * add a corresponding issue to the provided [issues] list. If the [dependency] does not map to a package, an
+     * implementation should return *null*.
+     */
+    fun createPackage(identifier: String, dependency: D, issues: MutableList<OrtIssue>): Package?
+
+    /**
+     * Return a collection with known issues for the given [dependency]. Some package manager implementations may
+     * already encounter problems when obtaining dependency representations. These can be reported here. This base
+     * implementation returns an empty collection.
+     */
+    fun issuesForDependency(dependency: D): Collection<OrtIssue> = emptyList()
+}
