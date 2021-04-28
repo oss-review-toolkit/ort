f35a60084aefe02e7f69b8229d6ede108ba9b470
Piet Brauer-Kallenberg <piet.brauer-kallenberg@mhp.com>
2021-04-28 07:45:35 +0000

    analyzer: Add initial support for the CocoaPods dependency manager
    Constructing the dependency tree for any given `Podfile` solely from the
    output of the `pod` sub-commands requires running `pod` sub-commands
    which only run on macOS. This implementation avoids all macOS-only
    commands by constructing the dependency tree from the lockfile and
    therefore the analysis of any `Podfile` requires the precence of the
    corresponding `Podfile.lock`.
    
    In order to obtain the meta-data for a dependency, the corresponding
    `.podspec` file is determined via the command `pod spec which`. These
    paths always point under `~/cocoapods/repos` which is where CocoaPods
    sets up its so called Specs repositories. Which repositories are to be
    used is defined by the respective `Podfile` via the `source` property,
    which defaults to [1]. This implementation disregards the specified
    repositories and always assumes that default. That default specs
    repository is however not used via GitHub, but via CDN as this is much
    faster [2]. Cloning [1] takes 10-30 minutes and takes a bit more than
    5GB while the CDN approach initializes in no time and fetches only
    required data.
    
    Even with that CDN based approach the implementation can
    and should be extended to adhere to the specified repositories in the
    future.
    
    The CocoaPods repository cannot be checked out on Windows due to special
    characters in a path. So, installation is not possible on Windows which
    is why the tests are disabled on Windows for now.
    
    [1] https://github.com/CocoaPods/Specs
    [2] https://github.com/CocoaPods/CocoaPods/issues/7046
    
    Co-authored-by: Frank Viernau <frank.viernau@here.com>
    Signed-off-by: Piet Brauer-Kallenberg <piet.brauer-kallenberg@mhp.com>
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

---
+++ analyzer/src/funTest/kotlin/managers/CocoaPodsFunTest.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2017-2021 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.analyzer.managers
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+import java.io.File
+import java.time.Instant
+
+import org.ossreviewtoolkit.downloader.VersionControlSystem
+import org.ossreviewtoolkit.model.ProjectAnalyzerResult
+import org.ossreviewtoolkit.utils.Os
+import org.ossreviewtoolkit.utils.normalizeVcsUrl
+import org.ossreviewtoolkit.utils.test.DEFAULT_ANALYZER_CONFIGURATION
+import org.ossreviewtoolkit.utils.test.DEFAULT_REPOSITORY_CONFIGURATION
+import org.ossreviewtoolkit.utils.test.USER_DIR
+import org.ossreviewtoolkit.utils.test.patchExpectedResult
+
+private val SYNTHETIC_PROJECTS_DIR = File("src/funTest/assets/projects/synthetic")
+
+class CocoaPodsFunTest : WordSpec({
+    "resolveSingleProject()" should {
+        "determine dependencies from a Podfile without a dependency tree".config(enabled = !Os.isWindows) {
+            val definitionFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods/regular/Podfile").absoluteFile
+            val expectedResult = getExpectedResult(
+                definitionFile = definitionFile,
+                expectedResultFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods-regular-expected-output.yml")
+            )
+
+            val result = createCocoaPods().resolveSingleProject(definitionFile)
+
+            result.toYaml() shouldBe expectedResult
+        }
+
+        "determine dependencies from a Podfile with a dependency tree".config(enabled = !Os.isWindows) {
+            val definitionFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods/dep-tree/Podfile").absoluteFile
+            val expectedResult = getExpectedResult(
+                definitionFile = definitionFile,
+                expectedResultFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods-dep-tree-expected-output.yml")
+            )
+
+            val result = createCocoaPods().resolveSingleProject(definitionFile)
+
+            result.toYaml() shouldBe expectedResult
+        }
+
+        "return no dependencies along with an issue if the lockfile is absent".config(enabled = !Os.isWindows) {
+            val definitionFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods/no-lockfile/Podfile").absoluteFile
+            val expectedResult = getExpectedResult(
+                definitionFile = definitionFile,
+                expectedResultFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods-no-lockfile-expected-output.yml")
+            )
+
+            val result = createCocoaPods().resolveSingleProject(definitionFile)
+
+            result.replaceIssueTimestamps().toYaml() shouldBe expectedResult
+        }
+    }
+})
+
+private fun createCocoaPods(): CocoaPods =
+    CocoaPods.Factory().create(
+        analysisRoot = USER_DIR,
+        analyzerConfig = DEFAULT_ANALYZER_CONFIGURATION,
+        repoConfig = DEFAULT_REPOSITORY_CONFIGURATION
+    )
+
+private fun getExpectedResult(definitionFile: File, expectedResultFile: File): String {
+    val projectDir = definitionFile.parentFile
+    val vcsDir = VersionControlSystem.forDirectory(projectDir)!!
+    val vcsRevision = vcsDir.getRevision()
+    val vcsUrl = vcsDir.getRemoteUrl()
+    val vcsPath = vcsDir.getPathToRoot(projectDir)
+
+    return patchExpectedResult(
+        expectedResultFile,
+        definitionFilePath = "$vcsPath/${definitionFile.name}",
+        path = vcsPath,
+        revision = vcsRevision,
+        url = normalizeVcsUrl(vcsUrl)
+    )
+}
+
+private fun ProjectAnalyzerResult.replaceIssueTimestamps(): ProjectAnalyzerResult =
+    copy(issues = issues.map { it.copy(timestamp = Instant.EPOCH) })
