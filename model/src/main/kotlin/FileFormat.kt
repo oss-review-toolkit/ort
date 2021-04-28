oss-review-toolkit/ort

commit 3269e2eae6f578f2a0c2954d6fa85fa6f7f23652

Add and use an extension function to write values using the correct mapper
This is analog to the existing "File.readValue()". Note that while not
all original callers were using pretty printing, this is done now for
all calls, as there was no case identified where pretty printing would
not be wanted.

Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

model/src/main/kotlin/FileFormat.kt
@@ -24,6 +24,8 @@ import com.fasterxml.jackson.module.kotlin.readValue
 
 import java.io.File
 
+import org.ossreviewtoolkit.utils.safeMkdirs
+
 /**
  * An enumeration of supported file formats for (de-)serialization, their primary [fileExtension] and optional aliases
  * (not including the dot).
@@ -80,3 +82,12 @@ fun File.mapper() = FileFormat.forFile(this).mapper
  * Use the Jackson mapper returned from [File.mapper] to read an object of type [T] from this file.
  */
 inline fun <reified T : Any> File.readValue(): T = mapper().readValue(this)
+
+/**
+ * Use the Jackson mapper returned from [File.mapper] to write an object of type [T] to this file. [prettyPrint]
+ * indicates whether to use pretty printing or not. The function also ensures that the parent directory exists.
+ */
+inline fun <reified T : Any> File.writeValue(value: T, prettyPrint: Boolean = true) {
+    parentFile.safeMkdirs()
+    mapper().apply { if (prettyPrint) writerWithDefaultPrettyPrinter() }.writeValue(this, value)
+}

cli/src/main/kotlin/commands/AdvisorCommand.kt
@@ -37,8 +37,8 @@ import com.github.ajalt.clikt.parameters.types.file
 import org.ossreviewtoolkit.GlobalOptions
 import org.ossreviewtoolkit.advisor.Advisor
 import org.ossreviewtoolkit.model.FileFormat
-import org.ossreviewtoolkit.model.mapper
 import org.ossreviewtoolkit.model.utils.mergeLabels
+import org.ossreviewtoolkit.model.writeValue
 import org.ossreviewtoolkit.utils.expandTilde
 import org.ossreviewtoolkit.utils.safeMkdirs
 
@@ -116,7 +116,7 @@ class AdvisorCommand : CliktCommand(name = "advise", help = "Check dependencies 
 
         outputFiles.forEach { file ->
             println("Writing advisor result to '$file'.")
-            file.mapper().writerWithDefaultPrettyPrinter().writeValue(file, ortResult)
+            file.writeValue(ortResult)
         }
 
         val advisorResults = ortResult.advisor?.results

cli/src/main/kotlin/commands/AnalyzerCommand.kt
@@ -45,8 +45,8 @@ import org.ossreviewtoolkit.analyzer.curation.FallbackPackageCurationProvider
 import org.ossreviewtoolkit.analyzer.curation.FilePackageCurationProvider
 import org.ossreviewtoolkit.analyzer.curation.Sw360PackageCurationProvider
 import org.ossreviewtoolkit.model.FileFormat
-import org.ossreviewtoolkit.model.mapper
 import org.ossreviewtoolkit.model.utils.mergeLabels
+import org.ossreviewtoolkit.model.writeValue
 import org.ossreviewtoolkit.utils.ORT_CURATIONS_FILENAME
 import org.ossreviewtoolkit.utils.ORT_REPO_CONFIG_FILENAME
 import org.ossreviewtoolkit.utils.expandTilde
@@ -173,7 +173,7 @@ class AnalyzerCommand : CliktCommand(name = "analyze", help = "Determine depende
 
         outputFiles.forEach { file ->
             println("Writing analyzer result to '$file'.")
-            val duration = measureTime { file.mapper().writerWithDefaultPrettyPrinter().writeValue(file, ortResult) }
+            val duration = measureTime { file.writeValue(ortResult) }
 
             log.perf {
                 "Wrote ORT result to '${file.name}' (${file.formatSizeInMib}) in ${duration.inMilliseconds}ms."

cli/src/main/kotlin/commands/EvaluatorCommand.kt
@@ -54,9 +54,9 @@ import org.ossreviewtoolkit.model.licenses.DefaultLicenseInfoProvider
 import org.ossreviewtoolkit.model.licenses.LicenseClassifications
 import org.ossreviewtoolkit.model.licenses.LicenseInfoResolver
 import org.ossreviewtoolkit.model.licenses.orEmpty
-import org.ossreviewtoolkit.model.mapper
 import org.ossreviewtoolkit.model.readValue
 import org.ossreviewtoolkit.model.utils.mergeLabels
+import org.ossreviewtoolkit.model.writeValue
 import org.ossreviewtoolkit.utils.ORT_COPYRIGHT_GARBAGE_FILENAME
 import org.ossreviewtoolkit.utils.ORT_LICENSE_CLASSIFICATIONS_FILENAME
 import org.ossreviewtoolkit.utils.ORT_REPO_CONFIG_FILENAME
@@ -284,7 +284,7 @@ class EvaluatorCommand : CliktCommand(name = "evaluate", help = "Evaluate ORT re
             outputFiles.forEach { file ->
                 println("Writing evaluation result to '$file'.")
                 val writeDuration = measureTime {
-                    file.mapper().writerWithDefaultPrettyPrinter().writeValue(file, ortResultOutput)
+                    file.writeValue(ortResultOutput)
                 }
 
                 log.perf {

cli/src/main/kotlin/commands/ScannerCommand.kt
@@ -43,8 +43,8 @@ import org.ossreviewtoolkit.GlobalOptions
 import org.ossreviewtoolkit.model.FileFormat
 import org.ossreviewtoolkit.model.config.DownloaderConfiguration
 import org.ossreviewtoolkit.model.config.ScannerConfiguration
-import org.ossreviewtoolkit.model.mapper
 import org.ossreviewtoolkit.model.utils.mergeLabels
+import org.ossreviewtoolkit.model.writeValue
 import org.ossreviewtoolkit.scanner.LocalScanner
 import org.ossreviewtoolkit.scanner.ScanResultsStorage
 import org.ossreviewtoolkit.scanner.Scanner
@@ -194,7 +194,7 @@ class ScannerCommand : CliktCommand(name = "scan", help = "Run external license 
 
         outputFiles.forEach { file ->
             println("Writing scan result to '$file'.")
-            val duration = measureTime { file.mapper().writerWithDefaultPrettyPrinter().writeValue(file, ortResult) }
+            val duration = measureTime { file.writeValue(ortResult) }
 
             log.perf {
                 "Wrote ORT result to '${file.name}' (${file.formatSizeInMib}) in ${duration.inMilliseconds}ms."

helper-cli/src/main/kotlin/commands/SubtractScanResultsCommand.kt
@@ -31,8 +31,8 @@ import org.ossreviewtoolkit.model.Provenance
 import org.ossreviewtoolkit.model.RepositoryProvenance
 import org.ossreviewtoolkit.model.ScanSummary
 import org.ossreviewtoolkit.model.VcsType
-import org.ossreviewtoolkit.model.mapper
 import org.ossreviewtoolkit.model.readValue
+import org.ossreviewtoolkit.model.writeValue
 import org.ossreviewtoolkit.utils.expandTilde
 
 internal class SubtractScanResultsCommand : CliktCommand(
@@ -89,7 +89,7 @@ internal class SubtractScanResultsCommand : CliktCommand(
            )
        )
 
-       outputOrtFile.mapper().writerWithDefaultPrettyPrinter().writeValue(outputOrtFile, result)
+       outputOrtFile.writeValue(result)
     }
 }
 

helper-cli/src/main/kotlin/commands/TransformResultCommand.kt
@@ -27,6 +27,7 @@ import com.github.ajalt.clikt.parameters.types.file
 
 import com.schibsted.spt.data.jslt.Parser
 
+import org.ossreviewtoolkit.model.writeValue
 import org.ossreviewtoolkit.model.yamlMapper
 import org.ossreviewtoolkit.utils.expandTilde
 
@@ -68,7 +69,7 @@ class TransformResultCommand : CliktCommand(
         val expression = Parser.compile(transformationFile)
 
         val result = expression.apply(node)
-        yamlMapper.writeValue(outputFile, result)
+        outputFile.writeValue(result)
 
         println("Wrote result to $outputFile.")
     }

helper-cli/src/main/kotlin/common/Utils.kt
@@ -69,14 +69,13 @@ import org.ossreviewtoolkit.model.utils.FindingCurationMatcher
 import org.ossreviewtoolkit.model.utils.PackageConfigurationProvider
 import org.ossreviewtoolkit.model.utils.SimplePackageConfigurationProvider
 import org.ossreviewtoolkit.model.utils.createLicenseInfoResolver
-import org.ossreviewtoolkit.model.yamlMapper
+import org.ossreviewtoolkit.model.writeValue
 import org.ossreviewtoolkit.spdx.SpdxExpression
 import org.ossreviewtoolkit.spdx.SpdxSingleLicenseExpression
 import org.ossreviewtoolkit.utils.CopyrightStatementsProcessor
 import org.ossreviewtoolkit.utils.ORT_NAME
 import org.ossreviewtoolkit.utils.OkHttpClientHelper
 import org.ossreviewtoolkit.utils.isSymbolicLink
-import org.ossreviewtoolkit.utils.safeMkdirs
 import org.ossreviewtoolkit.utils.stripCredentialsFromUrl
 import org.ossreviewtoolkit.utils.withoutPrefix
 
@@ -561,8 +560,7 @@ internal fun RepositoryConfiguration.sortScopeExcludes(): RepositoryConfiguratio
  * Serialize a [RepositoryConfiguration] as YAML to the given target [File].
  */
 internal fun RepositoryConfiguration.writeAsYaml(targetFile: File) {
-    targetFile.absoluteFile.parentFile.safeMkdirs()
-    yamlMapper.writeValue(targetFile, this)
+    targetFile.writeValue(this)
 }
 
 /**
@@ -614,10 +612,7 @@ internal fun RepositoryLicenseFindingCurations.mergeLicenseFindingCurations(
  */
 @JvmName("writeRepositoryLicenseFindingCurationsAsYaml")
 internal fun RepositoryLicenseFindingCurations.writeAsYaml(targetFile: File) {
-    targetFile.parentFile.apply { safeMkdirs() }
-
-    yamlMapper.writeValue(
-        targetFile,
+    targetFile.writeValue(
         mapValues { (_, curations) ->
             curations.sortedBy { it.path.removePrefix("*").removePrefix("*") }
         }.toSortedMap()
@@ -669,10 +664,7 @@ internal fun RepositoryPathExcludes.mergePathExcludes(
  */
 @JvmName("writeRepositoryPathExcludesAsYaml")
 internal fun RepositoryPathExcludes.writeAsYaml(targetFile: File) {
-    targetFile.parentFile.apply { safeMkdirs() }
-
-    yamlMapper.writeValue(
-        targetFile,
+    targetFile.writeValue(
         mapValues { (_, pathExcludes) ->
             pathExcludes.sortedBy { it.pattern }
         }.toSortedMap()
@@ -834,8 +826,7 @@ internal fun RepositoryConfiguration.merge(
  * Serialize a [PackageConfiguration] as YAML to the given target [File].
  */
 internal fun PackageConfiguration.writeAsYaml(targetFile: File) {
-    targetFile.absoluteFile.parentFile.safeMkdirs()
-    yamlMapper.writeValue(targetFile, this)
+    targetFile.writeValue(this)
 }
 
 internal fun importPathExcludes(sourceCodeDir: File, pathExcludesFile: File): List<PathExclude> {

commit 4fcc907e65ec06a77b78bce6ec01ffa88b3bdc3d

FileFormat: Fix pretty-printing of (JSON) files
If pretty-printing is enabled, writeValue() needs to be called on the
ObjectWriter, not on the ObjectMapper. This got broken in 3269e2e.

Fixes #3948.

Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

model/src/main/kotlin/FileFormat.kt
@@ -89,5 +89,10 @@ inline fun <reified T : Any> File.readValue(): T = mapper().readValue(this)
  */
 inline fun <reified T : Any> File.writeValue(value: T, prettyPrint: Boolean = true) {
     parentFile.safeMkdirs()
-    mapper().apply { if (prettyPrint) writerWithDefaultPrettyPrinter() }.writeValue(this, value)
+
+    if (prettyPrint) {
+        mapper().writerWithDefaultPrettyPrinter().writeValue(this, value)
+    } else {
+        mapper().writeValue(this, value)
+    }
 }