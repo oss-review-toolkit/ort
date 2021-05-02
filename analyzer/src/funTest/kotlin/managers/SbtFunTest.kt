9117a92b9cc8e5037f64f5773929dcf1a46a22b3
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-05-02 11:34:55 +0200

    Disable running Sbt tests on Azure on Windows for now
    For some reason the existing work-around [1] to suppress a prompt when
    running Sbt does not work anymore with Sbt 1.5.0 which is now installed
    on Azure's "windows-2019" images. Disable related tests to unblock
    merging PRs until a solution is found.
    
    [1] https://github.com/oss-review-toolkit/ort/blob/79a687c/analyzer/src/main/kotlin/managers/Sbt.kt#L96-L100
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- analyzer/src/funTest/kotlin/managers/SbtFunTest.kt
+++ analyzer/src/funTest/kotlin/managers/SbtFunTest.kt
@@ -27,13 +27,16 @@ import java.io.File
 import org.ossreviewtoolkit.analyzer.Analyzer
 import org.ossreviewtoolkit.downloader.VersionControlSystem
 import org.ossreviewtoolkit.downloader.vcs.Git
+import org.ossreviewtoolkit.utils.Ci
 import org.ossreviewtoolkit.utils.normalizeVcsUrl
 import org.ossreviewtoolkit.utils.test.DEFAULT_ANALYZER_CONFIGURATION
 import org.ossreviewtoolkit.utils.test.patchActualResult
 import org.ossreviewtoolkit.utils.test.patchExpectedResult
 
 class SbtFunTest : StringSpec({
-    "Dependencies of the external 'directories' single project should be detected correctly" {
+    "Dependencies of the external 'directories' single project should be detected correctly".config(
+        enabled = !Ci.isAzureWindows // Disabled as a prompt in Sbt 1.5.0 blocks execution when getting the version.
+    ) {
         val projectName = "directories"
         val projectDir = File("src/funTest/assets/projects/external/$projectName").absoluteFile
         val expectedOutputFile = projectDir.resolveSibling("$projectName-expected-output.yml")
@@ -49,7 +52,9 @@ class SbtFunTest : StringSpec({
         patchActualResult(actualResult, patchStartAndEndTime = true) shouldBe expectedResult
     }
 
-    "Dependencies of the external 'sbt-multi-project-example' multi-project should be detected correctly" {
+    "Dependencies of the external 'sbt-multi-project-example' multi-project should be detected correctly".config(
+        enabled = !Ci.isAzureWindows // Disabled as a prompt in Sbt 1.5.0 blocks execution when getting the version.
+    ) {
         val projectName = "sbt-multi-project-example"
         val projectDir = File("src/funTest/assets/projects/external/$projectName").absoluteFile
         val expectedOutputFile = projectDir.parentFile.resolve("$projectName-expected-output.yml")
@@ -65,7 +70,9 @@ class SbtFunTest : StringSpec({
         patchActualResult(actualResult, patchStartAndEndTime = true) shouldBe expectedResult
     }
 
-    "Dependencies of the synthetic 'http4s-template' project should be detected correctly" {
+    "Dependencies of the synthetic 'http4s-template' project should be detected correctly".config(
+        enabled = !Ci.isAzureWindows // Disabled as a prompt in Sbt 1.5.0 blocks execution when getting the version.
+    ) {
         val projectName = "sbt-http4s-template"
         val projectDir = File("src/funTest/assets/projects/synthetic/$projectName").absoluteFile
         val expectedOutputFile = projectDir.parentFile.resolve("$projectName-expected-output.yml")
