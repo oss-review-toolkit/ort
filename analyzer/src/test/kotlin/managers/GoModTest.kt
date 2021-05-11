9503cea686d134a834f3ca7db5e1395ec8d14c43
Frank Viernau <frank.viernau@here.com>
2021-05-11 13:07:11 +0200

    GoMod: Extract the revision from versions in "pseudo version" format
    See https://golang.org/ref/mod#pseudo-versions.
    
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

---
+++ analyzer/src/test/kotlin/managers/GoModTest.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2021 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.analyzer.managers
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+class GoModTest : WordSpec({
+    "getVersion" should {
+        "return the SHA1 from a 'pseudo version'" {
+            // See https://golang.org/ref/mod#pseudo-versions.
+            val version = "v0.0.0-20191109021931-daa7c04131f5"
+
+            getRevision(version) shouldBe "daa7c04131f5"
+        }
+    }
+})
