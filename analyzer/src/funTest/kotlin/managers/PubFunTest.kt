ee3bc35cf47e67a6e0dd3997fb5c205243bc7c0f
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-05-17 17:19:39 +0200

    PubFunTest: Fix nested test case names
    These should start lower case as they continue the "should" sentence.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- analyzer/src/funTest/kotlin/managers/PubFunTest.kt
+++ analyzer/src/funTest/kotlin/managers/PubFunTest.kt
@@ -70,7 +70,7 @@ class PubFunTest : WordSpec() {
                 }
             }
 
-            "Resolve dependencies for a project with flutter correctly" {
+            "resolve dependencies for a project with flutter correctly" {
                 val workingDir = projectsDir.resolve("project-with-flutter")
                 val packageFile = workingDir.resolve("pubspec.yaml")
                 val expectedResultFile = projectsDir.parentFile.resolve("pub-expected-output-project-with-flutter.yml")
@@ -89,7 +89,7 @@ class PubFunTest : WordSpec() {
                 patchActualResult(result.toYaml()) shouldBe expectedResult
             }
 
-            "Resolve dependencies for a project with dependencies without a static version" {
+            "resolve dependencies for a project with dependencies without a static version" {
                 val workingDir = projectsDir.resolve("any-version")
                 val packageFile = workingDir.resolve("pubspec.yaml")
                 val expectedResultFile = projectsDir.parentFile.resolve("pub-expected-output-any-version.yml")
@@ -108,7 +108,7 @@ class PubFunTest : WordSpec() {
                 result.toYaml() shouldBe expectedResult
             }
 
-            "Error is shown when no lockfile is present" {
+            "show an error if no lockfile is present" {
                 val workingDir = projectsDir.resolve("no-lockfile")
                 val packageFile = workingDir.resolve("pubspec.yaml")
 
