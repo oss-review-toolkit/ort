oss-review-toolkit/ort

commit f2793cd91feca409190ef81ebf2b81399629aa3d

GoMod: Remove unneeded packages from the dependency graph
The current implementation makes use of the "go mod graph" command to
obtain all the dependencies of a project. This command, however,
yields a full dependency graph that might contain packages which are
not needed by the main module.

To fix this, run the "go mod why" command on the found packages and
filter out those packages that are reported to be unneeded.

Resolves #3934.

Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

analyzer/src/main/kotlin/managers/GoMod.kt
@@ -41,6 +41,7 @@ import org.ossreviewtoolkit.model.config.AnalyzerConfiguration
 import org.ossreviewtoolkit.model.config.RepositoryConfiguration
 import org.ossreviewtoolkit.utils.CommandLineTool
 import org.ossreviewtoolkit.utils.Os
+import org.ossreviewtoolkit.utils.log
 import org.ossreviewtoolkit.utils.stashDirectories
 import org.ossreviewtoolkit.utils.withoutSuffix
 
@@ -92,7 +93,8 @@ class GoMod(
         val projectDir = definitionFile.parentFile
 
         stashDirectories(projectDir.resolve("vendor")).use {
-            val graph = getDependencyGraph(projectDir)
+            val fullGraph = getDependencyGraph(projectDir)
+            val graph = fullGraph.subgraph(getUsedPackages(fullGraph, projectDir))
             val vendorModules = getVendorModules(projectDir)
 
             val (projectIds, packageIds) = graph.nodes().partition { it.version.isBlank() }
@@ -176,6 +178,27 @@ class GoMod(
         }
 
         return result
+    }
+
+    /**
+     * Determine the set of packages that are actually used by the main module by running the _go mod why_ command
+     * repeatedly in [projectDir].
+     */
+    private fun getUsedPackages(graph: Graph, projectDir: File): Set<Identifier> {
+        val usedPackageNames = mutableSetOf<String>()
+
+        graph.nodes().chunked(WHY_CHUNK_SIZE).forEach { ids ->
+            val pkgNames = ids.map { it.name }.toTypedArray()
+            usedPackageNames +=
+                parseWhyOutput(run(projectDir, "mod", "why", *pkgNames).requireSuccess().stdout)
+        }
+
+        val usedPackages = graph.nodes().filter { it.name in usedPackageNames }
+        if (usedPackages.size < graph.size()) {
+            log.debug { "Removing ${graph.size() - usedPackages.size} unused packages from the dependency graph." }
+        }
+
+        return usedPackages.toSet()
     }
 
     private fun createPackage(id: Identifier): Package {
@@ -292,6 +315,15 @@ private class Graph(private val nodeMap: MutableMap<Identifier, Set<Identifier>>
 // See https://golang.org/ref/mod#pseudo-versions.
 private val PSEUDO_VERSION_REGEX = "^v0.0.0-(?:[\\d]{14}-(?<sha1>[0-9a-f]+)$)".toRegex()
 
+/** Separator string indicating that data of a new package follows in the output of the go mod why command. */
+private const val PACKAGE_SEPARATOR = "# "
+
+/**
+ * Constant for the number of packages to pass to the _go mod why_ command in a single step. This value is chosen
+ * rather arbitrarily to keep the command's output to a reasonable size.
+ */
+private const val WHY_CHUNK_SIZE = 32
+
 private fun getRevision(version: String): String {
     version.withoutSuffix("+incompatible")?.let { return getRevision(it) }
 
@@ -305,4 +337,26 @@ private fun getRevision(version: String): String {
 internal fun Identifier.toVcsInfo(): VcsInfo {
     val vcsInfo = VcsHost.toVcsInfo("https://$name")
     return vcsInfo.copy(revision = getRevision(version))
+}
+
+/**
+ * Parse the given [output] generated by the _go mod why_ command and the names of packages that are reported to be
+ * used by the main module.
+ * See https://golang.org/ref/mod#go-mod-why.
+ */
+internal fun parseWhyOutput(output: String): Set<String> {
+    val usedPackages = mutableSetOf<String>()
+    var currentPackage: String? = null
+
+    output.lineSequence().forEach { line ->
+        if (line.startsWith(PACKAGE_SEPARATOR)) {
+            currentPackage = line.substring(PACKAGE_SEPARATOR.length)
+        } else {
+            if (!line.startsWith('(') && line.isNotBlank()) {
+                currentPackage?.let { usedPackages += it }
+            }
+        }
+    }
+
+    return usedPackages
 }

analyzer/src/funTest/assets/projects/synthetic/gomod-expected-output.yml
@@ -20,42 +20,13 @@ project:
     dependencies:
     - id: "GoMod::github.com/fatih/color:v1.7.0"
       linkage: "PROJECT_STATIC"
-    - id: "GoMod::github.com/hashicorp/go-multierror:v1.0.0"
-      linkage: "PROJECT_STATIC"
-      dependencies:
-      - id: "GoMod::github.com/hashicorp/errwrap:v1.0.0"
-        linkage: "PROJECT_STATIC"
     - id: "GoMod::github.com/mattn/go-colorable:v0.1.4"
       linkage: "PROJECT_STATIC"
       dependencies:
       - id: "GoMod::github.com/mattn/go-isatty:v0.0.8"
         linkage: "PROJECT_STATIC"
-        dependencies:
-        - id: "GoMod::golang.org/x/sys:v0.0.0-20190222072716-a9d3bda3a223"
-          linkage: "PROJECT_STATIC"
     - id: "GoMod::github.com/mattn/go-isatty:v0.0.10"
       linkage: "PROJECT_STATIC"
-      dependencies:
-      - id: "GoMod::golang.org/x/sys:v0.0.0-20191008105621-543471e840be"
-        linkage: "PROJECT_STATIC"
-    - id: "GoMod::golang.org/x/oauth2:v0.0.0-20190226205417-e64efc72b421"
-      linkage: "PROJECT_STATIC"
-      dependencies:
-      - id: "GoMod::cloud.google.com/go:v0.34.0"
-        linkage: "PROJECT_STATIC"
-      - id: "GoMod::golang.org/x/net:v0.0.0-20190108225652-1e06a53dbb7e"
-        linkage: "PROJECT_STATIC"
-      - id: "GoMod::golang.org/x/sync:v0.0.0-20181221193216-37e7f081c4d4"
-        linkage: "PROJECT_STATIC"
-      - id: "GoMod::google.golang.org/appengine:v1.4.0"
-        linkage: "PROJECT_STATIC"
-        dependencies:
-        - id: "GoMod::github.com/golang/protobuf:v1.2.0"
-          linkage: "PROJECT_STATIC"
-        - id: "GoMod::golang.org/x/net:v0.0.0-20180724234803-3673e40ba225"
-          linkage: "PROJECT_STATIC"
-        - id: "GoMod::golang.org/x/text:v0.3.0"
-          linkage: "PROJECT_STATIC"
   - name: "vendor"
     dependencies:
     - id: "GoMod::github.com/fatih/color:v1.7.0"
@@ -64,36 +35,7 @@ project:
       linkage: "PROJECT_STATIC"
     - id: "GoMod::github.com/mattn/go-isatty:v0.0.10"
       linkage: "PROJECT_STATIC"
-      dependencies:
-      - id: "GoMod::golang.org/x/sys:v0.0.0-20191008105621-543471e840be"
-        linkage: "PROJECT_STATIC"
 packages:
-- id: "GoMod::cloud.google.com/go:v0.34.0"
-  purl: "pkg:golang/cloud.google.com%2Fgo@v0.34.0"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: "https://proxy.golang.org/cloud.google.com/go/@v/v0.34.0.zip"
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-  vcs_processed:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
 - id: "GoMod::github.com/fatih/color:v1.7.0"
   purl: "pkg:golang/github.com%2Ffatih%2Fcolor@v1.7.0"
   declared_licenses: []
@@ -119,84 +61,6 @@ packages:
     type: "Git"
     url: "https://github.com/fatih/color.git"
     revision: "v1.7.0"
-    path: ""
-- id: "GoMod::github.com/golang/protobuf:v1.2.0"
-  purl: "pkg:golang/github.com%2Fgolang%2Fprotobuf@v1.2.0"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: "Git"
-    url: "https://github.com/golang/protobuf.git"
-    revision: "v1.2.0"
-    path: ""
-  vcs_processed:
-    type: "Git"
-    url: "https://github.com/golang/protobuf.git"
-    revision: "v1.2.0"
-    path: ""
-- id: "GoMod::github.com/hashicorp/errwrap:v1.0.0"
-  purl: "pkg:golang/github.com%2Fhashicorp%2Ferrwrap@v1.0.0"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: "Git"
-    url: "https://github.com/hashicorp/errwrap.git"
-    revision: "v1.0.0"
-    path: ""
-  vcs_processed:
-    type: "Git"
-    url: "https://github.com/hashicorp/errwrap.git"
-    revision: "v1.0.0"
-    path: ""
-- id: "GoMod::github.com/hashicorp/go-multierror:v1.0.0"
-  purl: "pkg:golang/github.com%2Fhashicorp%2Fgo-multierror@v1.0.0"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: "Git"
-    url: "https://github.com/hashicorp/go-multierror.git"
-    revision: "v1.0.0"
-    path: ""
-  vcs_processed:
-    type: "Git"
-    url: "https://github.com/hashicorp/go-multierror.git"
-    revision: "v1.0.0"
     path: ""
 - id: "GoMod::github.com/mattn/go-colorable:v0.1.4"
   purl: "pkg:golang/github.com%2Fmattn%2Fgo-colorable@v0.1.4"
@@ -275,212 +139,4 @@ packages:
     type: "Git"
     url: "https://github.com/mattn/go-isatty.git"
     revision: "v0.0.8"
-    path: ""
-- id: "GoMod::golang.org/x/net:v0.0.0-20180724234803-3673e40ba225"
-  purl: "pkg:golang/golang.org%2Fx%2Fnet@v0.0.0-20180724234803-3673e40ba225"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: "https://proxy.golang.org/golang.org/x/net/@v/v0.0.0-20180724234803-3673e40ba225.zip"
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-  vcs_processed:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-- id: "GoMod::golang.org/x/net:v0.0.0-20190108225652-1e06a53dbb7e"
-  purl: "pkg:golang/golang.org%2Fx%2Fnet@v0.0.0-20190108225652-1e06a53dbb7e"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: "https://proxy.golang.org/golang.org/x/net/@v/v0.0.0-20190108225652-1e06a53dbb7e.zip"
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-  vcs_processed:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-- id: "GoMod::golang.org/x/oauth2:v0.0.0-20190226205417-e64efc72b421"
-  purl: "pkg:golang/golang.org%2Fx%2Foauth2@v0.0.0-20190226205417-e64efc72b421"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: "https://proxy.golang.org/golang.org/x/oauth2/@v/v0.0.0-20190226205417-e64efc72b421.zip"
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-  vcs_processed:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-- id: "GoMod::golang.org/x/sync:v0.0.0-20181221193216-37e7f081c4d4"
-  purl: "pkg:golang/golang.org%2Fx%2Fsync@v0.0.0-20181221193216-37e7f081c4d4"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: "https://proxy.golang.org/golang.org/x/sync/@v/v0.0.0-20181221193216-37e7f081c4d4.zip"
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-  vcs_processed:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-- id: "GoMod::golang.org/x/sys:v0.0.0-20190222072716-a9d3bda3a223"
-  purl: "pkg:golang/golang.org%2Fx%2Fsys@v0.0.0-20190222072716-a9d3bda3a223"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: "https://proxy.golang.org/golang.org/x/sys/@v/v0.0.0-20190222072716-a9d3bda3a223.zip"
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-  vcs_processed:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-- id: "GoMod::golang.org/x/sys:v0.0.0-20191008105621-543471e840be"
-  purl: "pkg:golang/golang.org%2Fx%2Fsys@v0.0.0-20191008105621-543471e840be"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: "https://proxy.golang.org/golang.org/x/sys/@v/v0.0.0-20191008105621-543471e840be.zip"
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-  vcs_processed:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-- id: "GoMod::golang.org/x/text:v0.3.0"
-  purl: "pkg:golang/golang.org%2Fx%2Ftext@v0.3.0"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: "https://proxy.golang.org/golang.org/x/text/@v/v0.3.0.zip"
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-  vcs_processed:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-- id: "GoMod::google.golang.org/appengine:v1.4.0"
-  purl: "pkg:golang/google.golang.org%2Fappengine@v1.4.0"
-  declared_licenses: []
-  declared_licenses_processed: {}
-  description: ""
-  homepage_url: ""
-  binary_artifact:
-    url: ""
-    hash:
-      value: ""
-      algorithm: ""
-  source_artifact:
-    url: "https://proxy.golang.org/google.golang.org/appengine/@v/v1.4.0.zip"
-    hash:
-      value: ""
-      algorithm: ""
-  vcs:
-    type: ""
-    url: ""
-    revision: ""
-    path: ""
-  vcs_processed:
-    type: ""
-    url: ""
-    revision: ""
     path: ""

analyzer/src/test/kotlin/managers/GoModTest.kt
@@ -20,6 +20,9 @@
 package org.ossreviewtoolkit.analyzer.managers
 
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.beEmpty
+import io.kotest.matchers.collections.containExactlyInAnyOrder
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 
 import org.ossreviewtoolkit.model.Identifier
@@ -68,6 +71,51 @@ class GoModTest : WordSpec({
             val id = Identifier("GoMod::github.com/Azure/azure-sdk-for-go:v43.3.0+incompatible")
 
             id.toVcsInfo().revision shouldBe "v43.3.0"
+        }
+    }
+
+    "parseWhyOutput" should {
+        "detect packages that are used" {
+            val output = """
+                # cloud.google.com/go
+                foo
+                
+                # github.com/fatih/color
+                bar
+                
+                # github.com/hashicorp/go-multierror
+                baz
+            """.trimIndent()
+
+            parseWhyOutput(output) should containExactlyInAnyOrder(
+                "cloud.google.com/go",
+                "github.com/fatih/color",
+                "github.com/hashicorp/go-multierror"
+            )
+        }
+
+        "handle a missing current package" {
+            val output = "foo"
+
+            parseWhyOutput(output) should beEmpty()
+        }
+
+        "skip unused packages" {
+            val output = """
+                # cloud.google.com/go
+                foo
+                
+                # github.com/fatih/color
+                bar
+                
+                # github.com/hashicorp/go-multierror
+                (main module does not need package github.com/hashicorp/go-multierror
+            """.trimIndent()
+
+            parseWhyOutput(output) should containExactlyInAnyOrder(
+                "cloud.google.com/go",
+                "github.com/fatih/color"
+            )
         }
     }
 })

commit cf2cdd88ff17055e01315b79f73b3682ffddfdbb

GoMod: Do not filter out the main project package
This is a fix for a regression introduced in f2793cd. If the project's
root package does not contain any source code, it is reported to be
unused by "go mod why", and was therefore filtered out. This caused
an exception later because the project identifier could not be
determined.

Fix this by ensuring that the project package is always included in
the dependency graph.

Resolves #4139.

Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

analyzer/src/funTest/assets/projects/synthetic/gomod-subpkg/cmd/main.go
@@ -1,0 +1,8 @@
+package main
+
+import "github.com/fatih/color"
+
+func main() {
+	c := color.New(color.FgCyan).Add(color.Underline)
+	c.Println("Prints cyan text with an underline.")
+}

analyzer/src/funTest/assets/projects/synthetic/gomod-subpkg/go.mod
@@ -1,0 +1,11 @@
+module github.com/oss-review-toolkit/ort/gomod-synthetic-test-project
+
+go 1.13
+
+require (
+	github.com/fatih/color v1.7.0
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421 // indirect
+)

analyzer/src/funTest/assets/projects/synthetic/gomod-subpkg/go.sum
@@ -1,0 +1,29 @@
+cloud.google.com/go v0.34.0 h1:eOI3/cP2VTU6uZLDYAoic+eyzzB9YyGmJ7eIjl8rOPg=
+cloud.google.com/go v0.34.0/go.mod h1:aQUYkXzVsufM+DwF1aE+0xfcU+56JwCaLick0ClmMTw=
+github.com/fatih/color v1.7.0 h1:DkWD4oS2D8LGGgTQ6IvwJJXSL5Vp2ffcQg58nFV38Ys=
+github.com/fatih/color v1.7.0/go.mod h1:Zm6kSWBoL9eyXnKyktHP6abPY2pDugNf5KwzbycvMj4=
+github.com/golang/protobuf v1.2.0 h1:P3YflyNX/ehuJFLhxviNdFxQPkGK5cDcApsge1SqnvM=
+github.com/golang/protobuf v1.2.0/go.mod h1:6lQm79b+lXiMfvg/cZm0SGofjICqVBUtrP5yJMmIC1U=
+github.com/hashicorp/errwrap v1.0.0 h1:hLrqtEDnRye3+sgx6z4qVLNuviH3MR5aQ0ykNJa/UYA=
+github.com/hashicorp/errwrap v1.0.0/go.mod h1:YH+1FKiLXxHSkmPseP+kNlulaMuP3n2brvKWEqk/Jc4=
+github.com/hashicorp/go-multierror v1.0.0 h1:iVjPR7a6H0tWELX5NxNe7bYopibicUzc7uPribsnS6o=
+github.com/hashicorp/go-multierror v1.0.0/go.mod h1:dHtQlpGsu+cZNNAkkCN/P3hoUDHhCYQXV3UM06sGGrk=
+github.com/mattn/go-colorable v0.1.4 h1:snbPLB8fVfU9iwbbo30TPtbLRzwWu6aJS6Xh4eaaviA=
+github.com/mattn/go-colorable v0.1.4/go.mod h1:U0ppj6V5qS13XJ6of8GYAs25YV2eR4EVcfRqFIhoBtE=
+github.com/mattn/go-isatty v0.0.8/go.mod h1:Iq45c/XA43vh69/j3iqttzPXn0bhXyGjM0Hdxcsrc5s=
+github.com/mattn/go-isatty v0.0.10 h1:qxFzApOv4WsAL965uUPIsXzAKCZxN2p9UqdhFS4ZW10=
+github.com/mattn/go-isatty v0.0.10/go.mod h1:qgIWMr58cqv1PHHyhnkY9lrL7etaEgOFcMEpPG5Rm84=
+golang.org/x/net v0.0.0-20180724234803-3673e40ba225/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
+golang.org/x/net v0.0.0-20190108225652-1e06a53dbb7e h1:bRhVy7zSSasaqNksaRZiA5EEI+Ei4I1nO5Jh72wfHlg=
+golang.org/x/net v0.0.0-20190108225652-1e06a53dbb7e/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
+golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421 h1:Wo7BWFiOk0QRFMLYMqJGFMd9CgUAcGx7V+qEg/h5IBI=
+golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421/go.mod h1:gOpvHmFTYa4IltrdGE7lF6nIHvwfUNPOp7c8zoXwtLw=
+golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 h1:YUO/7uOKsKeq9UokNS62b8FYywz3ker1l1vDZRCRefw=
+golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
+golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223/go.mod h1:STP8DvDyc/dI5b8T5hshtkjS+E42TnysNCUPdjciGhY=
+golang.org/x/sys v0.0.0-20191008105621-543471e840be h1:QAcqgptGM8IQBC9K/RC4o+O9YmqEm0diQn9QmZw/0mU=
+golang.org/x/sys v0.0.0-20191008105621-543471e840be/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
+golang.org/x/text v0.3.0 h1:g61tztE5qeGQ89tm6NTjjM9VPIm088od1l6aSorWRWg=
+golang.org/x/text v0.3.0/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
+google.golang.org/appengine v1.4.0 h1:/wp5JvzpHIxhs/dumFmF7BXTf3Z+dd4uXta4kVyO508=
+google.golang.org/appengine v1.4.0/go.mod h1:xpcJRLb0r/rnEns0DIKYYv+WjYCduHsrkT7/EB5XEv4=

analyzer/src/main/kotlin/managers/GoMod.kt
@@ -94,18 +94,15 @@ class GoMod(
 
         stashDirectories(projectDir.resolve("vendor")).use {
             val fullGraph = getDependencyGraph(projectDir)
-            val graph = fullGraph.subgraph(getUsedPackages(fullGraph, projectDir))
+            val projectId = fullGraph.projectId()
+
+            val graph = fullGraph.subgraph(getUsedPackages(fullGraph, projectDir, projectId))
             val vendorModules = getVendorModules(projectDir)
 
-            val (projectIds, packageIds) = graph.nodes().partition { it.version.isBlank() }
-            require(projectIds.size == 1) {
-                "Expected exactly one unique package without version but got ${projectIds.joinToString()}."
-            }
-
-            val projectId = projectIds.first()
-            val projectVcs = processProjectVcs(projectDir)
-
+            val packageIds = graph.nodes() - projectId
             val packages = packageIds.mapTo(sortedSetOf()) { createPackage(it) }
+
+            val projectVcs = processProjectVcs(projectDir)
 
             val scopes = sortedSetOf(
                 Scope(
@@ -181,11 +178,11 @@ class GoMod(
     }
 
     /**
-     * Determine the set of packages that are actually used by the main module by running the _go mod why_ command
-     * repeatedly in [projectDir].
+     * Determine the set of packages that are actually used by the [main module][projectId] by running the _go mod why_
+     * command repeatedly in [projectDir].
      */
-    private fun getUsedPackages(graph: Graph, projectDir: File): Set<Identifier> {
-        val usedPackageNames = mutableSetOf<String>()
+    private fun getUsedPackages(graph: Graph, projectDir: File, projectId: Identifier): Set<Identifier> {
+        val usedPackageNames = mutableSetOf(projectId.name)
 
         graph.nodes().chunked(WHY_CHUNK_SIZE).forEach { ids ->
             val pkgNames = ids.map { it.name }.toTypedArray()
@@ -281,6 +278,19 @@ private class Graph(private val nodeMap: MutableMap<Identifier, Set<Identifier>>
     fun subgraph(subNodes: Set<Identifier>): Graph =
         Graph(nodeMap.filter { it.key in subNodes }
             .mapValuesTo(mutableMapOf()) { e -> e.value.filterTo(mutableSetOf()) { it in subNodes } })
+
+    /**
+     * Search for the single package that represents the main project. This is the only package without a version.
+     * Fail if no single package with this criterion can be found.
+     */
+    fun projectId(): Identifier =
+        nodes().filter { it.version.isBlank() }.let { idsWithoutVersion ->
+            require(idsWithoutVersion.size == 1) {
+                "Expected exactly one unique package without version but got ${idsWithoutVersion.joinToString()}."
+            }
+
+            idsWithoutVersion.first()
+        }
 
     /**
      * Convert this [Graph] to a set of [PackageReference]s that spawn the dependency trees of the direct dependencies

analyzer/src/funTest/kotlin/managers/GoModFunTest.kt
@@ -32,13 +32,34 @@ import org.ossreviewtoolkit.utils.test.USER_DIR
 import org.ossreviewtoolkit.utils.test.patchExpectedResult
 
 class GoModFunTest : StringSpec() {
-    private val projectDir = File("src/funTest/assets/projects/synthetic/gomod").absoluteFile
-    private val vcsDir = VersionControlSystem.forDirectory(projectDir)!!
-    private val vcsUrl = vcsDir.getRemoteUrl()
-    private val vcsRevision = vcsDir.getRevision()
+    private val testDir = File("src/funTest/assets/projects/synthetic").absoluteFile
 
     init {
         "Project dependencies are detected correctly" {
+            val projectDir = testDir.resolve("gomod")
+            val vcsDir = VersionControlSystem.forDirectory(projectDir)!!
+            val vcsUrl = vcsDir.getRemoteUrl()
+            val vcsRevision = vcsDir.getRevision()
+            val definitionFile = projectDir.resolve("go.mod")
+            val vcsPath = vcsDir.getPathToRoot(projectDir)
+            val expectedResult = patchExpectedResult(
+                projectDir.parentFile.resolve("gomod-expected-output.yml"),
+                definitionFilePath = "$vcsPath/go.mod",
+                path = vcsPath,
+                revision = vcsRevision,
+                url = normalizeVcsUrl(vcsUrl)
+            )
+
+            val result = createGoMod().resolveSingleProject(definitionFile)
+
+            result.toYaml() shouldBe expectedResult
+        }
+
+        "Project dependencies are detected correctly if the main package does not contain any code" {
+            val projectDir = testDir.resolve("gomod-subpkg")
+            val vcsDir = VersionControlSystem.forDirectory(projectDir)!!
+            val vcsUrl = vcsDir.getRemoteUrl()
+            val vcsRevision = vcsDir.getRevision()
             val definitionFile = projectDir.resolve("go.mod")
             val vcsPath = vcsDir.getPathToRoot(projectDir)
             val expectedResult = patchExpectedResult(