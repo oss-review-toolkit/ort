3d4d80334778e7bd969f10478e9c31382fe2d507
Marcel Bochtler <marcel.bochtler@bosch.io>
2021-06-11 11:57:56 +0200

    PackageCuration: Move split command to separate sub-sub-command
    To organise the curation helper commands better, as more commands will
    be added in a later commit.
    
    Renamed from `split-curations` as the curations context is now already
    given from the sub-command.
    
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.io>

---
+++ helper-cli/src/main/kotlin/commands/packagecuration/PackageCurationCommand.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.helper.commands.packagecuration
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
+
+internal class PackageCurationCommand : CliktCommand() {
+    init {
+        subcommands(
+            SplitCommand()
+        )
+    }
+
+    override fun run() {
+        // Intentionally empty, because all logic is implemented inside the sub commands.
+    }
+}
