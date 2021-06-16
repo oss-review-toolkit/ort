cfa52f595522884a7dcd935a2ebc7eea955791d7
Oliver Heger <oliver.heger@bosch.io>
2021-06-16 16:01:59 +0200

    ProjectAnalyzerResult: Remove sanity check about referenced packages
    This check is no longer applicable under all circumstances. Package
    managers using the dependency graph format typically pass in an empty
    set of packages for a single project, as all packages are aggregated
    globally. Also, the Project.collectDependencies() function used in
    this check is problematic because it only works for the classic
    dependency tree format.
    
    As this is related, update the Extensions class from the analyzer
    functional tests. A comment referencing the sanity check is no longer
    correct, and rework a usage of Project.collectDependencies().
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

--- model/src/main/kotlin/ProjectAnalyzerResult.kt
+++ model/src/main/kotlin/ProjectAnalyzerResult.kt
@@ -46,21 +46,6 @@ data class ProjectAnalyzerResult(
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     val issues: List<OrtIssue> = emptyList()
 ) {
-    init {
-        // Perform a sanity check to ensure we have no references to non-existing packages.
-        val packageIds = packages.map { it.id }
-        val referencedIds = project.collectDependencies {
-            // Exclude project dependencies in multi-projects from the check as these appear as references in the
-            // dependency tree but not in the list of packages used.
-            !it.hasIssues() && it.linkage !in PackageLinkage.PROJECT_LINKAGE
-        }
-
-        // Note that not all packageIds have to be contained in the referencedIds, e.g. for NPM optional dependencies.
-        require(packageIds.containsAll(referencedIds)) {
-            "The following references do not actually refer to packages: ${referencedIds - packageIds}."
-        }
-    }
-
     fun collectIssues(): Map<Identifier, List<OrtIssue>> {
         val collectedIssues = mutableMapOf<Identifier, MutableList<OrtIssue>>()
 
