83221e99a31b6e80d425cf56e9f5cddab1f4dc7a
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-06-17 14:58:38 +0200

    Pip: Only accept single-line licenses
    There are projects which put the full license text instead of just the
    license name into a license field. Omit such texts from the list of
    declared licenses by only accepting licenses that do not contain a
    newline character.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- analyzer/src/main/kotlin/managers/Pip.kt
+++ analyzer/src/main/kotlin/managers/Pip.kt
@@ -419,13 +419,12 @@ class Pip(
         return declaredLicenses
     }
 
-    private fun getLicenseFromLicenseField(value: String?): String? =
-        value?.let {
-            // Work-around for projects that declare licenses in classifier-style syntax.
-            getLicenseFromClassifier(it) ?: it
-        }?.takeUnless {
-            it.isBlank() || it == "UNKNOWN"
-        }
+    private fun getLicenseFromLicenseField(value: String?): String? {
+        if (value.isNullOrBlank() || value == "UNKNOWN" || value.lines().size > 1) return null
+
+        // Apply a work-around for projects that declare licenses in classifier-syntax in the license field.
+        return getLicenseFromClassifier(value) ?: value
+    }
 
     private fun getLicenseFromClassifier(classifier: String): String? {
         // Example license classifier:
