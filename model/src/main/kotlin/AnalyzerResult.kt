oss-review-toolkit/ort

commit d76ec51fb89fc42848dabd4843f10a68f6448103

AnalyzerResult: Always use dependency graphs on serialization
Add a custom serializer for AnalyzerResult objects that converts the
representation of dependencies if necessary before the result is
written.

This is part of the improvements of the handling of dependency graphs
(see #4069). The optimized dependency graph format is now used for all
package managers.

The change in the output format also causes some adaptations in tests.

Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

model/src/main/kotlin/AnalyzerResult.kt
@@ -22,14 +22,21 @@ package org.ossreviewtoolkit.model
 import com.fasterxml.jackson.annotation.JsonAlias
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 
 import java.util.SortedMap
 import java.util.SortedSet
+
+import org.ossreviewtoolkit.model.utils.DependencyGraphConverter
 
 /**
  * A class that merges all information from individual [ProjectAnalyzerResult]s created for each found definition file.
  */
 @JsonIgnoreProperties(value = ["has_issues", /* Backwards-compatibility: */ "has_errors"], allowGetters = true)
+@JsonSerialize(using = AnalyzerResultSerializer::class)
 data class AnalyzerResult(
     /**
      * Sorted set of the projects, as they appear in the individual analyzer results.
@@ -120,4 +127,31 @@ data class AnalyzerResult(
         } else {
             this
         }
+}
+
+/**
+ * A custom serializer for [AnalyzerResult] instances.
+ *
+ * This serializer makes sure that [AnalyzerResult]s always use the dependency graph representation when they are
+ * serialized. This is achieved by processing the result by [DependencyGraphConverter] before it is written out.
+ */
+private class AnalyzerResultSerializer : StdSerializer<AnalyzerResult>(AnalyzerResult::class.java) {
+    override fun serialize(result: AnalyzerResult, gen: JsonGenerator, provider: SerializerProvider?) {
+        val resultWithGraph = DependencyGraphConverter.convert(result)
+
+        gen.writeStartObject()
+
+        with(resultWithGraph) {
+            gen.writeObjectField("projects", projects)
+            gen.writeObjectField("packages", packages)
+
+            if (issues.isNotEmpty()) {
+                gen.writeObjectField("issues", issues)
+            }
+
+            gen.writeObjectField("dependency_graphs", dependencyGraphs)
+        }
+
+        gen.writeEndObject()
+    }
 }

analyzer/src/test/kotlin/AnalyzerResultBuilderTest.kt
@@ -119,7 +119,7 @@ class AnalyzerResultBuilderTest : WordSpec() {
                 val serializedMergedResults = yamlMapper.writeValueAsString(mergedResults)
                 val deserializedMergedResults = yamlMapper.readValue<AnalyzerResult>(serializedMergedResults)
 
-                deserializedMergedResults shouldBe mergedResults
+                deserializedMergedResults.withScopesResolved() shouldBe mergedResults
             }
 
             "be serialized and deserialized correctly with a dependency graph" {
@@ -157,6 +157,20 @@ class AnalyzerResultBuilderTest : WordSpec() {
                 val serializedResult2 = yamlMapper.writeValueAsString(deserializedResult)
 
                 serializedResult2 shouldBe serializedResult
+            }
+
+            "use the dependency graph representation on serialization" {
+                val mergedResults = AnalyzerResultBuilder()
+                    .addResult(analyzerResult1)
+                    .addResult(analyzerResult2)
+                    .build()
+
+                val serializedMergedResults = yamlMapper.writeValueAsString(mergedResults)
+                val resultTree = yamlMapper.readTree(serializedMergedResults)
+
+                resultTree["dependency_graphs"] shouldNotBeNull {
+                    count() shouldBe 2
+                }
             }
         }
 

analyzer/src/funTest/kotlin/GitRepoFunTest.kt
@@ -37,6 +37,7 @@ import org.ossreviewtoolkit.utils.Ci
 import org.ossreviewtoolkit.utils.ORT_NAME
 import org.ossreviewtoolkit.utils.safeDeleteRecursively
 import org.ossreviewtoolkit.utils.test.DEFAULT_ANALYZER_CONFIGURATION
+import org.ossreviewtoolkit.utils.test.convertToDependencyGraph
 import org.ossreviewtoolkit.utils.test.patchActualResult
 import org.ossreviewtoolkit.utils.test.patchExpectedResult
 
@@ -67,10 +68,12 @@ class GitRepoFunTest : StringSpec() {
         "Analyzer correctly reports VcsInfo for git-repo projects".config(enabled = !Ci.isAzureWindows) {
             val ortResult = Analyzer(DEFAULT_ANALYZER_CONFIGURATION).analyze(outputDir)
             val actualResult = ortResult.withResolvedScopes().toYaml()
-            val expectedResult = patchExpectedResult(
-                File("src/funTest/assets/projects/external/git-repo-expected-output.yml"),
-                revision = REPO_REV,
-                path = outputDir.invariantSeparatorsPath
+            val expectedResult = convertToDependencyGraph(
+                patchExpectedResult(
+                    File("src/funTest/assets/projects/external/git-repo-expected-output.yml"),
+                    revision = REPO_REV,
+                    path = outputDir.invariantSeparatorsPath
+                )
             )
 
             patchActualResult(actualResult, patchStartAndEndTime = true) shouldBe expectedResult

analyzer/src/funTest/kotlin/managers/SbtFunTest.kt
@@ -19,6 +19,8 @@
 
 package org.ossreviewtoolkit.analyzer.managers
 
+import com.fasterxml.jackson.module.kotlin.readValue
+
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
@@ -27,11 +29,14 @@ import java.io.File
 import org.ossreviewtoolkit.analyzer.Analyzer
 import org.ossreviewtoolkit.downloader.VersionControlSystem
 import org.ossreviewtoolkit.downloader.vcs.Git
+import org.ossreviewtoolkit.model.OrtResult
+import org.ossreviewtoolkit.model.yamlMapper
 import org.ossreviewtoolkit.utils.Ci
 import org.ossreviewtoolkit.utils.normalizeVcsUrl
 import org.ossreviewtoolkit.utils.test.DEFAULT_ANALYZER_CONFIGURATION
-import org.ossreviewtoolkit.utils.test.patchActualResult
+import org.ossreviewtoolkit.utils.test.patchActualResultObject
 import org.ossreviewtoolkit.utils.test.patchExpectedResult
+import org.ossreviewtoolkit.utils.test.readOrtResult
 
 class SbtFunTest : StringSpec({
     "Dependencies of the external 'directories' single project should be detected correctly".config(
@@ -46,10 +51,9 @@ class SbtFunTest : StringSpec({
 
         val ortResult = Analyzer(DEFAULT_ANALYZER_CONFIGURATION).analyze(projectDir, listOf(Sbt.Factory()))
 
-        val actualResult = ortResult.withResolvedScopes().toYaml()
-        val expectedResult = patchExpectedResult(expectedOutputFile)
+        val expectedResult = readOrtResult(expectedOutputFile)
 
-        patchActualResult(actualResult, patchStartAndEndTime = true) shouldBe expectedResult
+        patchActualResultObject(ortResult, patchStartAndEndTime = true).withResolvedScopes() shouldBe expectedResult
     }
 
     "Dependencies of the external 'sbt-multi-project-example' multi-project should be detected correctly".config(
@@ -64,10 +68,9 @@ class SbtFunTest : StringSpec({
 
         val ortResult = Analyzer(DEFAULT_ANALYZER_CONFIGURATION).analyze(projectDir, listOf(Sbt.Factory()))
 
-        val actualResult = ortResult.withResolvedScopes().toYaml()
-        val expectedResult = patchExpectedResult(expectedOutputFile)
+        val expectedResult = readOrtResult(expectedOutputFile)
 
-        patchActualResult(actualResult, patchStartAndEndTime = true) shouldBe expectedResult
+        patchActualResultObject(ortResult, patchStartAndEndTime = true).withResolvedScopes() shouldBe expectedResult
     }
 
     "Dependencies of the synthetic 'http4s-template' project should be detected correctly".config(
@@ -85,14 +88,15 @@ class SbtFunTest : StringSpec({
 
         val ortResult = Analyzer(DEFAULT_ANALYZER_CONFIGURATION).analyze(projectDir, listOf(Sbt.Factory()))
 
-        val actualResult = ortResult.withResolvedScopes().toYaml()
-        val expectedResult = patchExpectedResult(
-            expectedOutputFile,
-            url = vcsUrl,
-            revision = vcsRevision,
-            urlProcessed = normalizeVcsUrl(vcsUrl)
+        val expectedResult = yamlMapper.readValue<OrtResult>(
+            patchExpectedResult(
+                expectedOutputFile,
+                url = vcsUrl,
+                revision = vcsRevision,
+                urlProcessed = normalizeVcsUrl(vcsUrl)
+            )
         )
 
-        patchActualResult(actualResult, patchStartAndEndTime = true) shouldBe expectedResult
+        patchActualResultObject(ortResult, patchStartAndEndTime = true).withResolvedScopes() shouldBe expectedResult
     }
 })

cli/src/funTest/kotlin/OrtMainFunTest.kt
@@ -40,6 +40,7 @@ import org.ossreviewtoolkit.model.OrtResult
 import org.ossreviewtoolkit.model.readValue
 import org.ossreviewtoolkit.utils.normalizeVcsUrl
 import org.ossreviewtoolkit.utils.redirectStdout
+import org.ossreviewtoolkit.utils.test.convertToDependencyGraph
 import org.ossreviewtoolkit.utils.test.createTestTempDir
 import org.ossreviewtoolkit.utils.test.patchActualResult
 import org.ossreviewtoolkit.utils.test.patchExpectedResult
@@ -114,11 +115,13 @@ class OrtMainFunTest : StringSpec() {
 
         "Analyzer creates correct output" {
             val analyzerOutputDir = outputDir.resolve("merged-results")
-            val expectedResult = patchExpectedResult(
-                projectDir.resolve("gradle-all-dependencies-expected-result.yml"),
-                url = vcsUrl,
-                revision = vcsRevision,
-                urlProcessed = normalizeVcsUrl(vcsUrl)
+            val expectedResult = convertToDependencyGraph(
+                patchExpectedResult(
+                    projectDir.resolve("gradle-all-dependencies-expected-result.yml"),
+                    url = vcsUrl,
+                    revision = vcsRevision,
+                    urlProcessed = normalizeVcsUrl(vcsUrl)
+                )
             )
 
             runMain(
@@ -136,11 +139,13 @@ class OrtMainFunTest : StringSpec() {
 
         "Package curation data file is applied correctly" {
             val analyzerOutputDir = outputDir.resolve("curations")
-            val expectedResult = patchExpectedResult(
-                projectDir.resolve("gradle-all-dependencies-expected-result-with-curations.yml"),
-                url = vcsUrl,
-                revision = vcsRevision,
-                urlProcessed = normalizeVcsUrl(vcsUrl)
+            val expectedResult = convertToDependencyGraph(
+                patchExpectedResult(
+                    projectDir.resolve("gradle-all-dependencies-expected-result-with-curations.yml"),
+                    url = vcsUrl,
+                    revision = vcsRevision,
+                    urlProcessed = normalizeVcsUrl(vcsUrl)
+                )
             )
 
             runMain(

scanner/src/funTest/kotlin/scanners/FileCounterScannerFunTest.kt
@@ -29,6 +29,7 @@ import org.ossreviewtoolkit.model.config.ScannerConfiguration
 import org.ossreviewtoolkit.model.readValue
 import org.ossreviewtoolkit.model.yamlMapper
 import org.ossreviewtoolkit.scanner.ScanResultsStorage
+import org.ossreviewtoolkit.utils.test.convertToDependencyGraph
 import org.ossreviewtoolkit.utils.test.createTestTempDir
 import org.ossreviewtoolkit.utils.test.patchActualResult
 import org.ossreviewtoolkit.utils.test.patchExpectedResult
@@ -43,8 +44,10 @@ class FileCounterScannerFunTest : StringSpec() {
             outputDir = createTestTempDir()
 
             val analyzerResultFile = assetsDir.resolve("analyzer-result.yml")
-            val expectedResult = patchExpectedResult(
-                assetsDir.resolve("file-counter-expected-output-for-analyzer-result.yml")
+            val expectedResult = convertToDependencyGraph(
+                patchExpectedResult(
+                    assetsDir.resolve("file-counter-expected-output-for-analyzer-result.yml")
+                )
             )
 
             val scanner = FileCounter("FileCounter", ScannerConfiguration(), DownloaderConfiguration())

test-utils/src/main/kotlin/Utils.kt
@@ -93,4 +93,20 @@ fun patchActualResult(
 ): String =
     patchActualResult(yamlMapper.writeValueAsString(result), patchStartAndEndTime)
 
-fun readOrtResult(file: String) = File(file).let { it.mapper().readValue<OrtResult>(patchExpectedResult(it)) }
+fun patchActualResultObject(result: OrtResult, patchStartAndEndTime: Boolean = false): OrtResult =
+    yamlMapper.readValue(patchActualResult(result, patchStartAndEndTime))
+
+fun readOrtResult(file: String) = readOrtResult(File(file))
+
+fun readOrtResult(file: File) = file.mapper().readValue<OrtResult>(patchExpectedResult(file))
+
+/**
+ * Deserialize an [OrtResult] from the given [string representation][result] and then serialize it again. As for some
+ * of the components in the result custom serializers or deserializers are registered, this can cause some
+ * modifications and conversions on the original result. This function is also used to convert expected test results
+ * stored in the assets to the most recent serialization format.
+ */
+fun convertToDependencyGraph(result: String): String {
+    val ortResult = yamlMapper.readValue<OrtResult>(result)
+    return yamlMapper.writeValueAsString(ortResult)
+}

commit 1239c4131f27b3d61433a1aab7b4ea06db70797f

AnalyzerResult: Take hasIssues property into account on serialization
This fixes a regression introduced in d76ec51 with the custom
serializer for AnalyzerResult; the property was overlooked then.

Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

model/src/main/kotlin/AnalyzerResult.kt
@@ -149,6 +149,7 @@ private class AnalyzerResultSerializer : StdSerializer<AnalyzerResult>(AnalyzerR
             }
 
             gen.writeObjectField("dependency_graphs", dependencyGraphs)
+            gen.writeBooleanField("has_issues", hasIssues)
         }
 
         gen.writeEndObject()

analyzer/src/test/kotlin/AnalyzerResultBuilderTest.kt
@@ -171,6 +171,8 @@ class AnalyzerResultBuilderTest : WordSpec() {
                 resultTree["dependency_graphs"] shouldNotBeNull {
                     count() shouldBe 2
                 }
+
+                resultTree["has_issues"].asBoolean() shouldBe true
             }
         }
 