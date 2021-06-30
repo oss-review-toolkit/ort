27d3c27ef733b580c7ddb514596e52cb9fb132be
Oliver Heger <oliver.heger@bosch.io>
2021-06-30 15:53:38 +0200

    DependencyNavigator: Add an implementation for dependency graphs
    This implementation allows accessing and traversing over the
    dependency references contained in a DependencyGraph object. It can
    deal with graphs storing their data as DependencyReference objects
    and with graphs based on lists with nodes and edges.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ model/src/test/kotlin/DependencyGraphNavigatorLegacyTest.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.model
+
+/**
+ * Test class for [DependencyGraphNavigator] that checks whether the navigator implementation can handle results
+ * containing a dependency graph in the legacy format.
+ */
+class DependencyGraphNavigatorLegacyTest : AbstractDependencyNavigatorTest() {
+    override val resultFileName = "src/test/assets/sbt-multi-project-example-graph-old.yml"
+
+    override val resultWithIssuesFileName = "src/test/assets/result-with-issues-graph-old.yml"
+}
---
+++ model/src/test/kotlin/DependencyGraphNavigatorTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.model
+
+import io.kotest.assertions.throwables.shouldThrow
+
+import org.ossreviewtoolkit.model.config.AnalyzerConfiguration
+import org.ossreviewtoolkit.utils.Environment
+
+class DependencyGraphNavigatorTest : AbstractDependencyNavigatorTest() {
+    override val resultFileName = "src/test/assets/sbt-multi-project-example-graph.yml"
+
+    override val resultWithIssuesFileName = "src/test/assets/result-with-issues-graph.yml"
+
+    init {
+        "init" should {
+            "fail if there is no analyzer result" {
+                shouldThrow<IllegalArgumentException> {
+                    DependencyGraphNavigator(OrtResult.EMPTY)
+                }
+            }
+
+            "fail if the map with dependency graphs is empty" {
+                val result = OrtResult.EMPTY.copy(
+                    analyzer = AnalyzerRun(
+                        result = AnalyzerResult.EMPTY,
+                        environment = Environment(),
+                        config = AnalyzerConfiguration()
+                    )
+                )
+
+                shouldThrow<IllegalArgumentException> {
+                    DependencyGraphNavigator(result)
+                }
+            }
+        }
+    }
+}
