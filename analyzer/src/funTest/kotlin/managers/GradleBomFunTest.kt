5dd83b06262c66638c5db6b171997da63f518ea7
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2021-07-05 17:57:06 +0200

    analyzer: Add a test case to demonstrate how Gradle BOMs are handled
    Add a test case to demonstrate how imported Maven BOMs [1] are currently
    handled. This behavior will be changed in the next commit, adding this
    test case helps to better understand how the change will affect the
    expected result.
    
    [1] https://docs.gradle.org/current/userguide/platforms.html#sub:bom_import
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ analyzer/src/funTest/kotlin/managers/GradleBomFunTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2021 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.analyzer.managers
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+import java.io.File
+
+import org.ossreviewtoolkit.downloader.VersionControlSystem
+import org.ossreviewtoolkit.utils.normalizeVcsUrl
+import org.ossreviewtoolkit.utils.test.DEFAULT_ANALYZER_CONFIGURATION
+import org.ossreviewtoolkit.utils.test.DEFAULT_REPOSITORY_CONFIGURATION
+import org.ossreviewtoolkit.utils.test.USER_DIR
+import org.ossreviewtoolkit.utils.test.patchExpectedResult
+
+class GradleBomFunTest : StringSpec() {
+    private val projectDir = File("src/funTest/assets/projects/synthetic/gradle-bom").absoluteFile
+    private val vcsDir = VersionControlSystem.forDirectory(projectDir)!!
+    private val vcsUrl = vcsDir.getRemoteUrl()
+    private val vcsRevision = vcsDir.getRevision()
+
+    init {
+        "Gradle BOM dependencies are detected correctly" {
+            val packageFile = projectDir.resolve("build.gradle")
+            val expectedResult = patchExpectedResult(
+                projectDir.parentFile.resolve("gradle-bom-expected-output.yml"),
+                url = normalizeVcsUrl(vcsUrl),
+                revision = vcsRevision
+            )
+
+            val result = createGradle().resolveSingleProject(packageFile, resolveScopes = true)
+
+            result.toYaml() shouldBe expectedResult
+        }
+    }
+
+    private fun createGradle() =
+        Gradle("Gradle", USER_DIR, DEFAULT_ANALYZER_CONFIGURATION, DEFAULT_REPOSITORY_CONFIGURATION)
+}
