c43bcec9b76c3685e1824cf203253348b2d123ba
Oliver Heger <oliver.heger@bosch.io>
2021-07-05 12:58:54 +0200

    DependencyGraphConverter: Filter out empty projects
    Such projects are created as fall-back with a corresponding issue by
    PackageManager if resolving of dependencies fails. They can cause
    problems if there is already a dependency graph for their package
    manager type; then this graph is overridden by an empty one.
    
    Note that empty projects are not removed from the analyzer result;
    they stay there with with all of their issues. They are only no
    longer taken into account when the dependency graph is constructed.
    (They would not contribute to it anyway as they do not define any
    dependencies.)
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

--- model/src/main/kotlin/utils/DependencyGraphConverter.kt
+++ model/src/main/kotlin/utils/DependencyGraphConverter.kt
@@ -86,7 +86,8 @@ object DependencyGraphConverter {
      * Determine the projects in this [AnalyzerResult] that require a conversion. These are the projects that manage
      * their dependencies in a scope structure.
      */
-    private fun AnalyzerResult.projectsWithScopes(): List<Project> = projects.filter { it.scopeNames == null }
+    private fun AnalyzerResult.projectsWithScopes(): List<Project> =
+        projects.filter { it.scopeDependencies?.isNotEmpty() ?: false }
 
     /**
      * Convert the dependency representation used by this [Project] to the dependency graph format, i.e. a set of
