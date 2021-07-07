7df44c8c366d40eb56ed8e635b2290945346faa4
Oliver Heger <oliver.heger@bosch.io>
2021-07-07 15:36:31 +0200

    ClearlyDefinedPackageCurationProvider: Handle all exceptions
    In a concrete scenario, ClearlyDefined did not send a response within
    the configured timeout, causing a SocketTimeoutException to be thrown.
    This exception was not handled and eventually caused the analyzer to
    crash. To avoid this, add a catch block for all exceptions.
    
    Add a test case using a mock server to verify that timeout exceptions
    are now handled properly.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ analyzer/src/test/kotlin/curation/ClearlyDefinedPackageCurationProviderMockTest.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.analyzer.curation
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.anyUrl
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.beEmpty
+import io.kotest.matchers.should
+
+import java.time.Duration
+
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.utils.OkHttpClientHelper
+
+/**
+ * A test for [ClearlyDefinedPackageCurationProvider], which uses a mock server. This allows testing some specific
+ * error conditions.
+ */
+class ClearlyDefinedPackageCurationProviderMockTest : WordSpec({
+    val wiremock = WireMockServer(
+        WireMockConfiguration.options()
+            .dynamicPort()
+            .usingFilesUnderDirectory("src/test/assets/")
+    )
+
+    beforeSpec {
+        wiremock.start()
+        WireMock.configureFor(wiremock.port())
+    }
+
+    afterSpec {
+        wiremock.stop()
+    }
+
+    beforeTest {
+        wiremock.resetAll()
+    }
+
+    "ClearlyDefinedPackageCurationProvider" should {
+        "handle a SocketTimeoutException" {
+            wiremock.stubFor(get(anyUrl())
+                .willReturn(aResponse().withFixedDelay(2000)))
+            val client = OkHttpClientHelper.buildClient {
+                readTimeout(Duration.ofSeconds(1))
+            }
+
+            val provider = ClearlyDefinedPackageCurationProvider("http://localhost:${wiremock.port()}", client)
+
+            provider.getCurationsFor(Identifier("Maven:some-ns:some-component:1.2.3")) should beEmpty()
+        }
+    }
+})
