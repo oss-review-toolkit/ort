bosch-io/oss-review-toolkit

DependencyNavigator: Support calculating the dependency tree depth
This functionality is required by StatisticsCalculator. Adapt the
calculator to use the new functions of DependencyNavigator.

The functionality can already be implemented in a generic way in the
DependencyNavigator interface. Remove the no longer needed
getDependencyTreeDepth() function from Scope. As this is the only
function that was tested in ScopeTest, the test class can be removed,
too.

Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

model/src/main/kotlin/DependencyNavigator.kt
@@ -113,6 +113,13 @@ interface DependencyNavigator {
         scopeDependencies(project, matcher = MATCH_SUB_PROJECTS).collectDependencies()
 
     /**
+     * Return the depth of the dependency tree rooted at the given [project] associated with this [scopeName]. If the
+     * scope cannot be resolved, return -1.
+     */
+    fun dependencyTreeDepth(project: Project, scopeName: String): Int =
+        getTreeDepthRec(directDependencies(project, scopeName))
+
+    /**
      * Determine the map of shortest paths for all the dependencies of a project, given its map of [scopeDependencies]
      * and a [scopeResolver] function that retrieves the direct dependency nodes of a scope.
      */
@@ -162,4 +169,12 @@ interface DependencyNavigator {
 
         return result
     }
+
+    /**
+     * Traverse the given sequence of [dependencies] recursively to determine the depth of the dependency tree.
+     */
+    private fun getTreeDepthRec(dependencies: Sequence<DependencyNode>): Int =
+        dependencies.map { dependency ->
+            1 + dependency.visitDependencies { getTreeDepthRec(it) }
+        }.maxOrNull() ?: 0
 }

oss-review-toolkit/ort

model/src/main/kotlin/DependencyNavigator.kt
@@ -132,6 +132,13 @@ interface DependencyNavigator {
         scopeDependencies(project, matcher = MATCH_SUB_PROJECTS).collectDependencies()
 
     /**
+     * Return the depth of the dependency tree rooted at the given [project] associated with this [scopeName]. If the
+     * scope cannot be resolved, return -1.
+     */
+    fun dependencyTreeDepth(project: Project, scopeName: String): Int =
+        getTreeDepthRecursive(directDependencies(project, scopeName))
+
+    /**
      * Determine the map of shortest paths for all the dependencies of a [project], given its map of
      * [scopeDependencies].
      */
@@ -179,4 +186,12 @@ interface DependencyNavigator {
 
         return result
     }
+
+    /**
+     * Traverse the given sequence of [dependencies] recursively to determine the depth of the dependency tree.
+     */
+    private fun getTreeDepthRecursive(dependencies: Sequence<DependencyNode>): Int =
+        dependencies.map { dependency ->
+            1 + dependency.visitDependencies { getTreeDepthRecursive(it) }
+        }.maxOrNull() ?: 0
 }