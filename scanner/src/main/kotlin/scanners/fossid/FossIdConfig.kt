97bf28c57d717e90a77bb704b1b5861a2c9865f9
Oliver Heger <oliver.heger@bosch.io>
2021-07-30 08:43:37 +0200

    fossid-webapp: Add a configuration class
    FossIdConfig can read the configuration properties supported by FossId
    from the scanner configuration and stores them. It makes it easier to
    get an overview over the properties available.
    
    The class is going to be extended by functionality to create some
    helper objects used by FossId based on the current configuration. This
    will also be used to make FossId easier to test, as mock objects can
    be injected.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ scanner/src/main/kotlin/scanners/fossid/FossIdConfig.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.scanner.scanners.fossid
+
+import org.ossreviewtoolkit.model.config.ScannerConfiguration
+import org.ossreviewtoolkit.utils.log
+
+/**
+ * A data class that holds the configuration options supported by the [FossId] scanner. An instance of this class is
+ * created from the options contained in a [ScannerConfiguration] object.
+ */
+internal data class FossIdConfig(
+    /** The URL where the FossID service is running. */
+    val serverUrl: String,
+
+    /** The API key to access the FossID server. */
+    val apiKey: String,
+
+    /** The user to authenticate against the server. */
+    val user: String,
+
+    /** Flag whether the scanner should wait for the completion of FossID scans. */
+    val waitForResult: Boolean,
+
+    /** Filter for package namespaces (empty string if undefined). */
+    val packageNamespaceFilter: String,
+
+    /** Filter for package authors (empty string if undefined). */
+    val packageAuthorsFilter: String,
+
+    /** Flag whether credentials should be passed to FossID in URLs. */
+    val addAuthenticationToUrl: Boolean,
+
+    /** Flag whether delta scans should be triggered. */
+    val deltaScans: Boolean,
+
+    /** Stores the map with FossID-specific configuration options. */
+    private val options: Map<String, String>
+) {
+    companion object {
+        /** Name of the configuration property for the server URL. */
+        private const val SERVER_URL_PROPERTY = "serverUrl"
+
+        /** Name of the configuration property for the API key. */
+        private const val API_KEY_PROPERTY = "apiKey"
+
+        /** Name of the configuration property for the user name. */
+        private const val USER_PROPERTY = "user"
+
+        /** Name of the configuration property for the packages namespace filter. */
+        private const val NAMESPACE_FILTER_PROPERTY = "packageNamespaceFilter"
+
+        /** Name of the configuration property for the packages authros filter. */
+        private const val AUTHORS_FILTER_PROPERTY = "packageAuthorsFilter"
+
+        /** Name of the configuration property controlling that credentials are added to URLs. */
+        private const val CREDENTIALS_IN_URL_PROPERTY = "addAuthenticationToUrl"
+
+        /** Name of the configuration property controlling whether ORT should wait for FossID results. */
+        private const val WAIT_FOR_RESULT_PROPERTY = "waitForResult"
+
+        /** Name of the configuration property controlling whether delta scans are to be created. */
+        private const val DELTA_SCAN_PROPERTY = "deltaScans"
+
+        fun create(scannerConfig: ScannerConfiguration): FossIdConfig {
+            val fossIdScannerOptions = scannerConfig.options?.get("FossId")
+
+            requireNotNull(fossIdScannerOptions) { "No FossId Scanner configuration found." }
+
+            val serverUrl = fossIdScannerOptions[SERVER_URL_PROPERTY]
+                ?: throw IllegalArgumentException("No FossId server URL configuration found.")
+            val apiKey = fossIdScannerOptions[API_KEY_PROPERTY]
+                ?: throw IllegalArgumentException("No FossId API Key configuration found.")
+            val user = fossIdScannerOptions[USER_PROPERTY]
+                ?: throw IllegalArgumentException("No FossId User configuration found.")
+            val packageNamespaceFilter = fossIdScannerOptions[NAMESPACE_FILTER_PROPERTY].orEmpty()
+            val packageAuthorsFilter = fossIdScannerOptions[AUTHORS_FILTER_PROPERTY].orEmpty()
+            val addAuthenticationToUrl = fossIdScannerOptions[CREDENTIALS_IN_URL_PROPERTY].toBoolean()
+            val waitForResult = fossIdScannerOptions[WAIT_FOR_RESULT_PROPERTY]?.toBooleanStrictOrNull() ?: true
+            val deltaScans = fossIdScannerOptions[DELTA_SCAN_PROPERTY].toBoolean()
+
+            log.info { "waitForResult parameter is set to '$waitForResult'" }
+
+            return FossIdConfig(
+                serverUrl,
+                apiKey,
+                user,
+                waitForResult,
+                packageNamespaceFilter,
+                packageAuthorsFilter,
+                addAuthenticationToUrl,
+                deltaScans,
+                fossIdScannerOptions
+            )
+        }
+    }
+}
---
+++ scanner/src/test/kotlin/scanners/fossid/FossIdConfigTest.kt
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.scanner.scanners.fossid
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+import kotlin.IllegalArgumentException
+
+import org.ossreviewtoolkit.model.config.ScannerConfiguration
+
+class FossIdConfigTest : WordSpec({
+    "create" should {
+            "throw if no options for FossID are provided in the scanner configuration" {
+                val scannerConfig = ScannerConfiguration()
+
+                shouldThrow<IllegalArgumentException> { FossIdConfig.create(scannerConfig) }
+            }
+
+        "read all properties from the scanner configuration" {
+            val options = mapOf(
+                "serverUrl" to SERVER_URL,
+                "apiKey" to API_KEY,
+                "user" to USER,
+                "packageNamespaceFilter" to NAMESPACE_FILTER,
+                "packageAuthorsFilter" to AUTHOR_FILTER,
+                "waitForResult" to "false",
+                "deltaScans" to "true",
+                "addAuthenticationToUrl" to "false"
+            )
+            val scannerConfig = options.toScannerConfig()
+
+            val fossIdConfig = FossIdConfig.create(scannerConfig)
+
+            fossIdConfig shouldBe FossIdConfig(
+                serverUrl = SERVER_URL,
+                apiKey = API_KEY,
+                user = USER,
+                packageAuthorsFilter = AUTHOR_FILTER,
+                packageNamespaceFilter = NAMESPACE_FILTER,
+                waitForResult = false,
+                deltaScans = true,
+                addAuthenticationToUrl = false,
+                options = options
+            )
+        }
+
+        "set default values for optional properties" {
+            val options = mapOf(
+                "serverUrl" to SERVER_URL,
+                "apiKey" to API_KEY,
+                "user" to USER
+            )
+            val scannerConfig = options.toScannerConfig()
+
+            val fossIdConfig = FossIdConfig.create(scannerConfig)
+
+            fossIdConfig shouldBe FossIdConfig(
+                serverUrl = SERVER_URL,
+                apiKey = API_KEY,
+                user = USER,
+                packageAuthorsFilter = "",
+                packageNamespaceFilter = "",
+                waitForResult = true,
+                deltaScans = false,
+                addAuthenticationToUrl = false,
+                options = options
+            )
+        }
+
+        "throw if the server URL is missing" {
+            val scannerConfig = mapOf(
+                "apiKey" to API_KEY,
+                "user" to USER
+            ).toScannerConfig()
+
+            shouldThrow<IllegalArgumentException> { FossIdConfig.create(scannerConfig) }
+        }
+
+        "throw if the API key is missing" {
+            val scannerConfig = mapOf(
+                "serverUrl" to SERVER_URL,
+                "user" to USER
+            ).toScannerConfig()
+
+            shouldThrow<IllegalArgumentException> { FossIdConfig.create(scannerConfig) }
+        }
+
+        "throw if the user name is missing" {
+            val scannerConfig = mapOf(
+                "serverUrl" to SERVER_URL,
+                "apiKey" to API_KEY
+            ).toScannerConfig()
+
+            shouldThrow<IllegalArgumentException> { FossIdConfig.create(scannerConfig) }
+        }
+    }
+})
+
+private const val SERVER_URL = "https://www.example.org/fossid"
+private const val API_KEY = "test_api_key"
+private const val USER = "fossIdTestUser"
+private const val NAMESPACE_FILTER = "testFilterForNamespace"
+private const val AUTHOR_FILTER = "testFilterForAuthors"
+
+/**
+ * Return a [ScannerConfiguration] with this map as options for the FossID scanner.
+ */
+private fun Map<String, String>.toScannerConfig(): ScannerConfiguration {
+    val options = mapOf("FossId" to this)
+    return ScannerConfiguration(options = options)
+}
