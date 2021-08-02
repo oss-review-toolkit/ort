881f6179796e56a523a39c313d2a155f2ce732be
Oliver Heger <oliver.heger@bosch.io>
2021-08-02 15:00:25 +0200

    fossid-webapp: Move description of options to FossIdConfig class
    As this class now reads and stores the options, it feels natural to
    have their description located there as well. The class comment for
    FossId now links to this configuration class.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

--- scanner/src/main/kotlin/scanners/fossid/FossIdConfig.kt
+++ scanner/src/main/kotlin/scanners/fossid/FossIdConfig.kt
@@ -29,7 +29,27 @@ import retrofit2.converter.jackson.JacksonConverterFactory
 
 /**
  * A data class that holds the configuration options supported by the [FossId] scanner. An instance of this class is
- * created from the options contained in a [ScannerConfiguration] object.
+ * created from the options contained in a [ScannerConfiguration] object under the key _FossId_. It offers the
+ * following configuration options:
+ *
+ * * **"serverUrl":** The URL of the FossID server.
+ * * **"user":** The user to connect to the FossID server.
+ * * **"apiKey":** The API key of the user which connects to the FossID server.
+ * * **"packageNamespaceFilter":** If this optional filter is set, only packages having an identifier in given namespace
+ * will be scanned.
+ * * **"packageAuthorsFilter":** If this optional filter is set, only packages from a given author will be scanned.
+ * * **"addAuthenticationToUrl":** When set, ORT will add credentials from its Authenticator to the URLs sent to FossID.
+ * * **"waitForResult":** When set to false, ORT doesn't wait for repositories to be downloaded nor scans to be
+ * completed. As a consequence, scan results won't be available in ORT result.
+ * * **"deltaScans":** When set, ORT will create delta scans. When only changes in a repository need to be scanned,
+ * delta scans reuse the identifications of latest scan on this repository to reduce the amount of findings. If
+ * deltaScans is set and no scan exist yet, an initial scan called "origin" scan will be created.
+ *
+ * Naming conventions options. If they are not set, default naming convention are used.
+ * * **"namingProjectPattern":** A pattern for project names when projects are created on the FossID instance. Contains
+ * variables prefixed by "$" e.g. "$Var1_$Var2". Variables are also passed as options and are prefixed by
+ * [NAMING_CONVENTION_VARIABLE_PREFIX] e.g. namingVariableVar1 = "foo".
+ * * **"namingScanPattern":** A pattern for scan names when scans are created on the FossID instance.
  */
 internal data class FossIdConfig(
     /** The URL where the FossID service is running. */
