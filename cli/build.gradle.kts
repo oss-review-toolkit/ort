3b582464b8237707e21ce17768dbfc352d239899
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-08-23 18:01:46 +0200

    Gradle: Stricten the pattern for patching the start scripts
    Just to be on the safe side, to not patch a wrong location.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -58,7 +58,7 @@ tasks.named<CreateStartScripts>("startScripts").configure {
         // Work around the command line length limit on Windows when passing the classpath to Java, see
         // https://github.com/gradle/gradle/issues/1989#issuecomment-395001392.
         val windowsScriptText = windowsScript.readText(Charset.defaultCharset())
-        windowsScript.writeText(windowsScriptText.replace(Regex("set CLASSPATH=.*"),
+        windowsScript.writeText(windowsScriptText.replace(Regex("set CLASSPATH=%APP_HOME%\\\\lib\\\\.*"),
             "set CLASSPATH=%APP_HOME%\\\\lib\\\\*"))
     }
 }
