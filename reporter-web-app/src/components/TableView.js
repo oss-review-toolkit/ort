d66facd0097558bc2afd417ec494e079a1cfe2e8
Thomas Steenbergen <thomas.steenbergen@here.com>
2021-09-16 09:50:43 +0200

    reporter-web-app: Minor refactor of isExcluded table filters
    Signed-off-by: Thomas Steenbergen <thomas.steenbergen@here.com>

--- reporter-web-app/src/components/TableView.js
+++ reporter-web-app/src/components/TableView.js
@@ -123,15 +123,10 @@ class TableView extends React.Component {
                 filteredValue: filteredInfo.excludes || null,
                 key: 'excludes',
                 onFilter: (value, webAppPackage) => {
-                    if (value === 'excluded') {
-                        return webAppPackage.isExcluded;
-                    }
+                    const { isExcluded } = webAppPackage;
 
-                    if (value === 'included') {
-                        return !webAppPackage.isExcluded;
-                    }
+                    return (isExcluded && value === 'excluded') || (!isExcluded && value === 'included');
 
-                    return false;
                 },
                 render: (webAppPackage) => (
                     webAppPackage.isExcluded ? (
