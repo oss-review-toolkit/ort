f8b28d89eef8ca0909a51ccbaad6fa40d6e14590
Maximilian Huber <maximilian.huber@tngtech.com>
2021-09-17 15:48:38 +0200

    add helper script to run ort reporter for opossum on ort result
    Signed-off-by: Maximilian Huber <maximilian.huber@tngtech.com>

---
+++ opossumReportHelper.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+tag=ort/with_opossum
+
+buildDockerImg() (
+    if [[ "$(docker images -q "${tag}:latest" 2> /dev/null)" == "" ]]; then
+        cd "$(dirname "$0")"
+        DOCKER_BUILDKIT=1 docker build . --tag $tag --network=host
+    fi
+)
+
+analzyeDir() {
+    inputFile="$(readlink -f $1)"
+    inputDir="$(dirname "$inputFile")"
+
+    mkdir -p "$HOME/.ort/dockerHome/"
+
+    local dockerArgs=("run" "-i" "--rm")
+    dockerArgs+=("-v" "/etc/group:/etc/group:ro")
+    dockerArgs+=("-v" "/etc/passwd:/etc/passwd:ro")
+    dockerArgs+=("-v" "$HOME/.ort/dockerHome:$HOME")
+    dockerArgs+=("-u" "$(id -u):$(id -g)")
+    dockerArgs+=("-v" "$inputDir:$inputDir" "-w" "$inputDir")
+    dockerArgs+=("--net=host")
+    dockerArgs+=("${tag}:latest")
+    dockerArgs+=("--force-overwrite" "--performance")
+
+    set -x
+    docker "${dockerArgs[@]}" \
+        report -f Opossum \
+        -i "$inputFile" \
+        -o "$inputDir"
+}
+
+buildDockerImg
+
+for ortFile in "$@"; do
+    analzyeDir "$ortFile"
+done
