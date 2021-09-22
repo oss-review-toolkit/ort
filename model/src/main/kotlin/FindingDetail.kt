db0661cd8770442cfe158c88290b3a4dfb5f6789
Oliver Heger <oliver.heger@bosch.io>
2021-09-22 10:41:52 +0200

    FindingDetail: Add more properties to describe findings
    The new properties should enable other types of advisor
    implementations to report their findings. They are mainly from the
    domain of issue tracking systems, but should be sufficiently generic.
    
    An upcoming use case is the integration of a database for software
    defects with ORT.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

--- model/src/main/kotlin/FindingDetail.kt
+++ model/src/main/kotlin/FindingDetail.kt
@@ -19,7 +19,10 @@
 
 package org.ossreviewtoolkit.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
 import java.net.URI
+import java.time.Instant
 
 /**
  * A data class representing detailed information about an advisor finding obtained from a specific source.
@@ -30,6 +33,7 @@ import java.net.URI
  * with a list of details; each detail points to the source of the information and has some additional information
  * provided by this source.
  */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class FindingDetail(
     /**
      * The URI pointing to the source of this finding.
@@ -46,7 +50,43 @@ data class FindingDetail(
      * depends on the concrete scoring system. It could be a number, but also a constant like _LOW_ or _HIGH_. A
      * *null* value is possible as well, meaning that this object does not contain any information about the severity.
      */
-    val severity: String?
+    val severity: String?,
+
+    /**
+     * Contains a title for the associated finding if available. If this detail is just a reference to an external
+     * source of information, this field is *null*.
+     */
+    val title: String? = null,
+
+    /**
+     * Contains a description for the associated finding if available.
+     */
+    val description: String? = null,
+
+    /**
+     * A state of the associated finding. The concrete meaning of this string depends on the type of the finding and
+     * the source from where it was obtained. A typical use case would be the state of an issue tracker with possible
+     * values like *OPEN*, *IN PROGRESS*, *BLOCKED*, etc.
+     */
+    val state: String? = null,
+
+    /**
+     * Contains a creation date of the associated finding if available.
+     */
+    val createdAt: Instant? = null,
+
+    /**
+     * Contains a date of last modification of the associated finding if available. This information can be useful for
+     * instance to find out how up-to-date this finding might be.
+     */
+    val modifiedAt: Instant? = null,
+
+    /**
+     * A set with labels assigned to this finding. Labels allow a classification of findings based on defined criteria.
+     * The exact meaning of these labels depends on the type of the finding and source from where it was obtained. A
+     * typical use case could be a labeling system used by an issue tracker to assign additional information to issues.
+     */
+    val labels: Set<String> = emptySet()
 ) {
     companion object {
         /**
