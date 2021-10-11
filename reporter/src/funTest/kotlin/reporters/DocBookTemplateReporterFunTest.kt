9311ec8a150d3d9d813f774c45c8b4b10f5d75a6
Marcel Bochtler <marcel.bochtler@bosch.io>
2021-10-11 13:55:32 +0200

    Reporter: Add a DocBookTemplateReporter
    This is based on the AsciiDocTemplateReporter using DocBook as the
    backend.
    
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.io>

---
+++ reporter/src/funTest/kotlin/reporters/DocBookTemplateReporterFunTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.reporter.reporters
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+import java.io.File
+
+import org.ossreviewtoolkit.reporter.ORT_RESULT
+import org.ossreviewtoolkit.reporter.ReporterInput
+import org.ossreviewtoolkit.reporter.utils.patchAsciiDocTemplateResult
+import org.ossreviewtoolkit.utils.test.createTestTempDir
+
+class DocBookTemplateReporterFunTest : StringSpec({
+    "DocBook report is created from default template" {
+        val expectedText = File("src/funTest/assets/docbook-template-reporter-expected-result.xml").readText()
+
+        val reportContent =
+            DocBookTemplateReporter().generateReport(ReporterInput(ORT_RESULT), createTestTempDir()).single().readText()
+
+        reportContent.patchAsciiDocTemplateResult() shouldBe expectedText
+    }
+})
