89f327b2b5f4ca214afc3d58eb7de099bfb80eb1
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-10-13 08:52:21 +0200

    GitWorkingTreeFunTest: Update expected tags
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- downloader/src/funTest/kotlin/vcs/GitWorkingTreeFunTest.kt
+++ downloader/src/funTest/kotlin/vcs/GitWorkingTreeFunTest.kt
@@ -110,7 +110,8 @@ class GitWorkingTreeFunTest : StringSpec({
             "1.0.0",
             "2.0.0",
             "2.0.0b1",
-            "2.1.0"
+            "2.1.0",
+            "2.2.0"
         )
     }
 
