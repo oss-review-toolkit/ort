f0177b5087ac34cb0d23b1433a2adc79e6a5e10d
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2021-10-25 21:52:10 +0200

    FileArchiverTest: Use kotest style assertions
    Change the assertions functions to match with the style assertions are
    usually done with kotest.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

--- model/src/test/kotlin/utils/FileArchiverTest.kt
+++ model/src/test/kotlin/utils/FileArchiverTest.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2019-2021 HERE Europe B.V.
+ * Copyright (C) 2021 Bosch.IO GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,7 +64,10 @@ class FileArchiverTest : StringSpec() {
         file.writeText(path)
     }
 
-    private fun File.assertFileContent(path: String) {
+    /**
+     * Assert that this directory contains a file at [path] which contains the [path] as text.
+     */
+    private fun File.shouldContainFileWithContent(path: String) {
         val file = resolve(path)
         file.isFile shouldBe true
         file.readText() shouldBe path
@@ -81,16 +85,16 @@ class FileArchiverTest : StringSpec() {
             archiver.archive(workingDir, PROVENANCE)
             archiver.unarchive(targetDir, PROVENANCE)
 
-            targetDir.assertFileContent("a")
-            targetDir.assertFileContent("d/a")
+            targetDir.shouldContainFileWithContent("a")
+            targetDir.shouldContainFileWithContent("d/a")
 
-            fun assertFileNotSaved(path: String) {
+            fun shouldNotContainFile(path: String) {
                 val file = storageDir.resolve("save/$path")
                 file shouldNot exist()
             }
 
-            assertFileNotSaved("b")
-            assertFileNotSaved("d/b")
+            shouldNotContainFile("b")
+            shouldNotContainFile("d/b")
         }
 
         "All archived files are unarchived" {
@@ -106,10 +110,10 @@ class FileArchiverTest : StringSpec() {
 
             result shouldBe true
             with(targetDir) {
-                assertFileContent("a")
-                assertFileContent("b")
-                assertFileContent("c/a")
-                assertFileContent("c/b")
+                shouldContainFileWithContent("a")
+                shouldContainFileWithContent("b")
+                shouldContainFileWithContent("c/a")
+                shouldContainFileWithContent("c/b")
             }
         }
 
@@ -122,8 +126,8 @@ class FileArchiverTest : StringSpec() {
             archiver.unarchive(targetDir, PROVENANCE)
 
             with(targetDir) {
-                assertFileContent("LICENSE")
-                assertFileContent("path/LICENSE")
+                shouldContainFileWithContent("LICENSE")
+                shouldContainFileWithContent("path/LICENSE")
             }
         }
 
@@ -138,10 +142,10 @@ class FileArchiverTest : StringSpec() {
             archiver.unarchive(targetDir, PROVENANCE)
 
             with(targetDir) {
-                assertFileContent("a/LICENSE")
-                assertFileContent("b/License")
-                assertFileContent("c/license")
-                assertFileContent("d/LiCeNsE")
+                shouldContainFileWithContent("a/LICENSE")
+                shouldContainFileWithContent("b/License")
+                shouldContainFileWithContent("c/license")
+                shouldContainFileWithContent("d/LiCeNsE")
             }
         }
     }
