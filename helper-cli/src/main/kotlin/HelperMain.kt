c307b02ad8857a05e27bd42036dd36782a0b155b
Rainer Bieniek <extern.rainer.bieniek@porsche.de>
2021-11-15 03:57:15 +0100

    The rationale behind this helper command is the need to 'condense' the analyzer result files delivered by multiple product teams into one summary analyzer result to be processed by the ORT pipeline. In this specific use case, the exact relationship of dependencies does not matter and it is totally acceptable to work with a flat list of dependencies in the result reports.
    This commit extends the helper CLI with a command to merge multiple analyzer result files into
    one analyzer result file.
    
    The algorithm to condense the result file work the following way:
    1. Take the first and second item from the input list of analyzer result files
    2. Flatten the dependency tree per scope in an analyzer result into a simple list of dependencies, thus already eliminating eventual duplicates
    3. Merge VCS info, repository configurations   and dependency lists
    4. Put the result analyzer result back in the first position of the analyzer result files list
    5. Repeat with Step 1 unless the analyzer result file list contains only one entry
    
    After the merge, the resulting VCS info of the only analyzer result is patched into the top-level VCS info for the overall project
    
    Signed-Off: Rainer Bieniek <extern.rainer.bieniek@porsche.de>

--- helper-cli/src/main/kotlin/HelperMain.kt
+++ helper-cli/src/main/kotlin/HelperMain.kt
@@ -41,6 +41,7 @@ import org.ossreviewtoolkit.helper.commands.ListLicensesCommand
 import org.ossreviewtoolkit.helper.commands.ListPackagesCommand
 import org.ossreviewtoolkit.helper.commands.ListStoredScanResultsCommand
 import org.ossreviewtoolkit.helper.commands.MapCopyrightsCommand
+import org.ossreviewtoolkit.helper.commands.MergeAnalyzerResultsCommand
 import org.ossreviewtoolkit.helper.commands.MergeRepositoryConfigurationsCommand
 import org.ossreviewtoolkit.helper.commands.SetDependencyRepresentationCommand
 import org.ossreviewtoolkit.helper.commands.SetLabelsCommand
@@ -86,6 +87,7 @@ internal class HelperMain : CliktCommand(name = ORTH_NAME, epilog = "* denotes r
             ListPackagesCommand(),
             ListStoredScanResultsCommand(),
             MapCopyrightsCommand(),
+            MergeAnalyzerResultsCommand(),
             MergeRepositoryConfigurationsCommand(),
             PackageConfigurationCommand(),
             PackageCurationsCommand(),
