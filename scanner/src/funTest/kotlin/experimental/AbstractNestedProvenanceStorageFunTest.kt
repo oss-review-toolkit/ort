ae8a32ad375396398473d9ef476de2f08d786d08
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2021-11-19 15:36:43 +0100

    scanner: Add tests for the NestedProvenanceStorage implementations
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

---
+++ scanner/src/funTest/kotlin/experimental/AbstractNestedProvenanceStorageFunTest.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2021 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.scanner.experimental
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.core.test.TestCase
+import io.kotest.matchers.shouldBe
+
+import org.ossreviewtoolkit.model.KnownProvenance
+import org.ossreviewtoolkit.model.RepositoryProvenance
+import org.ossreviewtoolkit.model.VcsInfo
+import org.ossreviewtoolkit.model.VcsType
+
+abstract class AbstractNestedProvenanceStorageFunTest : WordSpec() {
+    private lateinit var storage: NestedProvenanceStorage
+
+    override fun beforeTest(testCase: TestCase) {
+        storage = createStorage()
+    }
+
+    protected abstract fun createStorage(): NestedProvenanceStorage
+
+    init {
+        "Adding a result" should {
+            "succeed" {
+                val root = createRepositoryProvenance()
+                val result = NestedProvenanceResolutionResult(createNestedProvenance(root), true)
+
+                storage.putNestedProvenance(root, result)
+
+                storage.readNestedProvenance(root) shouldBe result
+            }
+
+            "overwrite a previously stored result" {
+                val root = createRepositoryProvenance()
+                val result1 = NestedProvenanceResolutionResult(
+                    createNestedProvenance(root, mapOf("path" to root)), true
+                )
+                val result2 = NestedProvenanceResolutionResult(createNestedProvenance(root), true)
+
+                storage.putNestedProvenance(root, result1)
+                storage.putNestedProvenance(root, result2)
+
+                storage.readNestedProvenance(root) shouldBe result2
+            }
+        }
+    }
+}
+
+private fun createVcsInfo(
+    type: VcsType = VcsType.GIT,
+    url: String = "https://github.com/apache/logging-log4j2.git",
+    revision: String = "be881e503e14b267fb8a8f94b6d15eddba7ed8c4"
+) = VcsInfo(type, url, revision)
+
+private fun createRepositoryProvenance(
+    vcsInfo: VcsInfo = createVcsInfo(),
+    resolvedRevision: String = vcsInfo.revision
+) = RepositoryProvenance(vcsInfo, resolvedRevision)
+
+private fun createNestedProvenance(
+    root: KnownProvenance,
+    subRepositories: Map<String, RepositoryProvenance> = emptyMap()
+) = NestedProvenance(root, subRepositories)
