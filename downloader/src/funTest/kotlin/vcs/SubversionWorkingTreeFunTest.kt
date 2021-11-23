15904def7c33d3973b295ddcf9f77fbe2401e2ae
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2021-11-23 22:46:03 +0100

    SubversionWorkingTreeFunTest: Update expected remote tags
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- downloader/src/funTest/kotlin/vcs/SubversionWorkingTreeFunTest.kt
+++ downloader/src/funTest/kotlin/vcs/SubversionWorkingTreeFunTest.kt
@@ -105,6 +105,7 @@ class SubversionWorkingTreeFunTest : StringSpec({
             "docutils-0.17",
             "docutils-0.17.1",
             "docutils-0.18",
+            "docutils-0.18.1",
             "docutils-0.3.7",
             "docutils-0.3.9",
             "docutils-0.4",
