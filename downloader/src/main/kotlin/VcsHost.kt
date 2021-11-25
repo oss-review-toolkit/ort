oss-review-toolkit/ort

commit b39e446ac2a8e7d893ab698b00555d0eb7000e78

VcsHost: Return early in case of a non-URI project URL
Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

downloader/src/main/kotlin/VcsHost.kt
@@ -254,7 +254,8 @@ enum class VcsHost(
          * Return all [VcsInfo] that can be parsed from [projectUrl] without actually making a network request.
          */
         fun toVcsInfo(projectUrl: String): VcsInfo {
-            val vcsInfoFromHost = projectUrl.toUri { toVcsHost(it)?.toVcsInfoInternal(it) }.getOrNull()
+            val unknownVcs = VcsInfo(type = VcsType.UNKNOWN, url = projectUrl, revision = "")
+            val projectUri = projectUrl.toUri().getOrNull() ?: return unknownVcs
 
             // Fall back to generic URL detection for unknown VCS hosts.
             val svnBranchOrTagMatch = SVN_BRANCH_OR_TAG_PATTERN.matchEntire(projectUrl)
@@ -304,13 +305,10 @@ enum class VcsHost(
                     )
                 }
 
-                else -> VcsInfo(
-                    type = VcsType.UNKNOWN,
-                    url = projectUrl,
-                    revision = ""
-                )
+                else -> unknownVcs
             }
 
+            val vcsInfoFromHost = toVcsHost(projectUri)?.toVcsInfoInternal(projectUri)
             return vcsInfoFromHost?.merge(vcsInfoFromUrl) ?: vcsInfoFromUrl
         }
 

commit f8a3df5dd7b6b8ba994405b96fa68e26e5c0d923

VcsHost: Also exit early in case of a blank `projectUrl`
This is a fixup for b39e446, which caused a NPE triggered by 13b70a7.

Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

downloader/src/main/kotlin/VcsHost.kt
@@ -255,7 +255,7 @@ enum class VcsHost(
          */
         fun toVcsInfo(projectUrl: String): VcsInfo {
             val unknownVcs = VcsInfo(type = VcsType.UNKNOWN, url = projectUrl, revision = "")
-            val projectUri = projectUrl.toUri().getOrNull() ?: return unknownVcs
+            val projectUri = projectUrl.takeUnless { it.isBlank() }?.toUri()?.getOrNull() ?: return unknownVcs
 
             // Fall back to generic URL detection for unknown VCS hosts.
             val svnBranchOrTagMatch = SVN_BRANCH_OR_TAG_PATTERN.matchEntire(projectUrl)