8565f405f7d2c772faf83ea985149ae915ff574b
Frank Viernau <frank.viernau@here.com>
2021-11-30 16:02:30 +0100

    PackageManager: Improve the KDoc of resolveDependencies()
    Rewrite the comment to not mention the `cli` module because the API
    should not assume to be used from the `cli` only. This fixes a broken
    KDoc reference.
    
    Also clarify that the labels are actually not interpreted by ORT in
    anyway, but just passed along to enable injecting configuration
    parameters into third-party package manager implementations.
    
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

--- analyzer/src/main/kotlin/PackageManager.kt
+++ analyzer/src/main/kotlin/PackageManager.kt
@@ -219,9 +219,9 @@ abstract class PackageManager(
     /**
      * Return a tree of resolved dependencies (not necessarily declared dependencies, in case conflicts were resolved)
      * for all [definitionFiles] which were found by searching the [analysisRoot] directory. By convention, the
-     * [definitionFiles] must be absolute.
-     * [labels] are the labels given as parameters to the [org.ossreviewtoolkit.cli.commands.AnalyzerCommand]. They can
-     * be used by package manager implementations to decide how dependencies are resolved.
+     * [definitionFiles] must be absolute. The given [labels] are parameters to the overall analysis of the project and
+     * to further stages. They are not interpreted by ORT, but can be used to configure behavior of custom package
+     * manager implementations.
      */
     open fun resolveDependencies(definitionFiles: List<File>, labels: Map<String, String>): PackageManagerResult {
         definitionFiles.forEach { definitionFile ->
@@ -282,9 +282,9 @@ abstract class PackageManager(
 
     /**
      * Resolve dependencies for a single absolute [definitionFile] and return a list of [ProjectAnalyzerResult]s, with
-     * one result for each project found in the definition file.
-     * [labels] are the labels given as parameters to the [org.ossreviewtoolkit.cli.commands.AnalyzerCommand]. They can
-     * be used by package manager implementations to decide how dependencies are resolved.
+     * one result for each project found in the definition file. The given [labels] are parameters to the overall
+     * analysis of the project and to further stages. They are not interpreted by ORT, but can be used to configure
+     * behavior of custom package manager implementations.
      */
     abstract fun resolveDependencies(definitionFile: File, labels: Map<String, String>): List<ProjectAnalyzerResult>
 
