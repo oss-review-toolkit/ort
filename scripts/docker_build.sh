98d5f39ad6bd5b43d39aa98b5a488a9c49158ad0
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2022-01-06 12:47:21 +0100

    Use 10 instead of 7 digits for the version hash to match what JitPack uses
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -18,7 +18,7 @@
 # License-Filename: LICENSE
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
-GIT_VERSION=$(git describe --abbrev=7 --always --tags --dirty)
+GIT_VERSION=$(git describe --abbrev=10 --always --tags --dirty)
 
 echo "Setting ORT_VERSION to $GIT_VERSION."
 docker build -f $GIT_ROOT/Dockerfile -t ort --build-arg ORT_VERSION=$GIT_VERSION $GIT_ROOT
