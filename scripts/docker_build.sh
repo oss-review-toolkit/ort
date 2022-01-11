8aa5714656dcbaa91524aeaf237a53f295361385
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2022-01-11 12:44:18 +0100

    docker_build: Prefer `GIT_REVISION` as a variable name
    The previous `GIT_VERSION` read like the "version of Git".
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -18,7 +18,7 @@
 # License-Filename: LICENSE
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
-GIT_VERSION=$(git describe --abbrev=10 --always --tags --dirty)
+GIT_REVISION=$(git describe --abbrev=10 --always --tags --dirty)
 
-echo "Setting ORT_VERSION to $GIT_VERSION."
-DOCKER_BUILDKIT=1 docker build -f $GIT_ROOT/Dockerfile -t ort --build-arg ORT_VERSION=$GIT_VERSION $GIT_ROOT
+echo "Setting ORT_VERSION to $GIT_REVISION."
+DOCKER_BUILDKIT=1 docker build -f $GIT_ROOT/Dockerfile -t ort --build-arg ORT_VERSION=$GIT_REVISION $GIT_ROOT
