21205ba3242de4eb758d207fba4f2481178bffc5
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2022-02-24 13:02:49 +0100

    PackageRule: Simplify the `severityComparator` for vulnerabilities
    Using a `Comparator` that returns `Int`s < 0, 0 and > 0 seems overly
    complicated if in the end only values >= 0 are considered. So simplify
    writing rules for the user by making `severityComparator` simply return a
    `Boolean`.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- evaluator/src/test/kotlin/PackageRuleTest.kt
+++ evaluator/src/test/kotlin/PackageRuleTest.kt
@@ -196,7 +196,7 @@ class PackageRuleTest : WordSpec() {
             "return true if a severity of a vulnerability is higher than the threshold" {
                 val rule = createPackageRule(packageWithVulnerabilities)
                 val matcher = rule.hasVulnerability("8.9", "CVSS3") { value, threshold ->
-                    value.toFloat().compareTo(threshold.toFloat())
+                    value.toFloat() >= threshold.toFloat()
                 }
 
                 matcher.matches() shouldBe true
@@ -205,7 +205,7 @@ class PackageRuleTest : WordSpec() {
             "return false if a severity of a vulnerability is lower than the threshold" {
                 val rule = createPackageRule(packageWithVulnerabilities)
                 val matcher = rule.hasVulnerability("9.1", "CVSS3") { value, threshold ->
-                    value.toFloat().compareTo(threshold.toFloat())
+                    value.toFloat() >= threshold.toFloat()
                 }
 
                 matcher.matches() shouldBe false
@@ -214,7 +214,7 @@ class PackageRuleTest : WordSpec() {
             "return true if a severity of a vulnerability is the same as the threshold" {
                 val rule = createPackageRule(packageWithVulnerabilities)
                 val matcher = rule.hasVulnerability("9.0", "CVSS3") { value, threshold ->
-                    value.toFloat().compareTo(threshold.toFloat())
+                    value.toFloat() >= threshold.toFloat()
                 }
 
                 matcher.matches() shouldBe true
@@ -223,7 +223,7 @@ class PackageRuleTest : WordSpec() {
             "return true if a severity of a vulnerability is the same as the threshold without decimals" {
                 val rule = createPackageRule(packageWithVulnerabilities)
                 val matcher = rule.hasVulnerability("9", "CVSS3") { value, threshold ->
-                    value.toFloat().compareTo(threshold.toFloat())
+                    value.toFloat() >= threshold.toFloat()
                 }
 
                 matcher.matches() shouldBe true
@@ -232,7 +232,7 @@ class PackageRuleTest : WordSpec() {
             "return false if no vulnerability is found for the scoringSystem" {
                 val rule = createPackageRule(packageWithVulnerabilities)
                 val matcher = rule.hasVulnerability("10.0", "fake-scoring-system") { value, threshold ->
-                    value.toFloat().compareTo(threshold.toFloat())
+                    value.toFloat() >= threshold.toFloat()
                 }
 
                 matcher.matches() shouldBe false
