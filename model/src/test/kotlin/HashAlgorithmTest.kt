66ef661aa09a65cff3171498564e365d99d39284
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2022-04-19 16:17:47 +0200

    HashAlgorithm: Rename the `SHA1_GIT` enum entry to `SHA1GIT`
    This is to align with the name of the first alias since a45b944.
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- model/src/test/kotlin/HashAlgorithmTest.kt
+++ model/src/test/kotlin/HashAlgorithmTest.kt
@@ -34,18 +34,18 @@ class HashAlgorithmTest : StringSpec({
         HashAlgorithm.SHA1.calculate(file) shouldBe "c00ef43045659b53da5d71d49b8cd7e528c9d55b"
     }
 
-    "Calculating the SHA1-GIT on a file should yield the correct result" {
+    "Calculating the SHA1GIT on a file should yield the correct result" {
         // The expected hash was calculated with "git ls-tree HEAD".
-        HashAlgorithm.SHA1_GIT.calculate(file) shouldBe "3f4d322ebd76de0f1bbb9c867e1f818f5202efd3"
+        HashAlgorithm.SHA1GIT.calculate(file) shouldBe "3f4d322ebd76de0f1bbb9c867e1f818f5202efd3"
     }
 
-    "Calculating the SHA1-GIT on a resource should yield the correct result" {
+    "Calculating the SHA1GIT on a resource should yield the correct result" {
         // The expected hash was calculated with "git ls-tree HEAD".
-        HashAlgorithm.SHA1_GIT.calculate("/licenses/Apache-2.0") shouldBe
+        HashAlgorithm.SHA1GIT.calculate("/licenses/Apache-2.0") shouldBe
                 "261eeb9e9f8b2b4b0d119366dda99c6fd7d35c64"
     }
 
-    "Calculating the SHA1-GIT on non-existent field should return null" {
-        HashAlgorithm.SHA1_GIT.calculate("/license/DOESNOTEXIST") shouldBe null
+    "Calculating the SHA1GIT on non-existent field should return null" {
+        HashAlgorithm.SHA1GIT.calculate("/license/DOESNOTEXIST") shouldBe null
     }
 })
