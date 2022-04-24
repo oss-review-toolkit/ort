b9acce3e6c6ae040eefea1a5d970eab8ac5029d1
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2022-04-24 17:06:00 +0200

    Upgrade Batect to version 0.79.1
    See [1].
    
    [1]: https://github.com/batect/batect/releases/tag/0.79.1
    
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- batect.cmd
+++ batect.cmd
@@ -6,7 +6,7 @@ rem For more information, visit https://github.com/batect/batect.
 
 setlocal EnableDelayedExpansion
 
-set "version=0.78.0"
+set "version=0.79.1"
 
 if "%BATECT_CACHE_DIR%" == "" (
     set "BATECT_CACHE_DIR=%USERPROFILE%\.batect\cache"
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'^
 
 ^
 
-$Version='0.78.0'^
+$Version='0.79.1'^
 
 ^
 
@@ -48,7 +48,7 @@ $UrlEncodedVersion = [Uri]::EscapeDataString($Version)^
 
 $DownloadUrl = getValueOrDefault $env:BATECT_DOWNLOAD_URL "$DownloadUrlRoot/$UrlEncodedVersion/batect-$UrlEncodedVersion.jar"^
 
-$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM 'bc65ac8e00dd23dd9d68130e2109e429993b769b79e081d262a79798c8c3784b'^
+$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '8d7de395863cddecc660933fa05d67af54129b06a7fea2307e409c7cd3c04686'^
 
 ^
 
