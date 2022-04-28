826acfdbe291ec57ae84b72b87bc8d54f135fe65
Helio Chissini de Castro <helio.chissini-de-castro@bmw.de>
2022-04-28 15:36:15 +0200

    Dockerfile: Add tooling versions in docker_build.sh to easy updates
    Signed-off-by: Helio Chissini de Castro <helio.chissini-de-castro@bmw.de>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -28,5 +28,23 @@ echo "Setting ORT_VERSION to $GIT_VERSION."
 DOCKER_BUILDKIT=1 docker build -f "$GIT_ROOT"/Dockerfile \
     -t "$ORT_DOCKER" \
     --build-arg ORT_VERSION="$GIT_VERSION" \
+    --build-arg ANDROID_CMD_VERSION=7583922 \
+    --build-arg BOWER_VERSION="1.8.12" \
+    --build-arg COCOAPODS_VERSION="1.11.2" \
+    --build-arg COMPOSER_VERSION="1.10.1-1" \
+    --build-arg CONAN_VERSION="1.44.0" \
+    --build-arg GO_DEP_VERSION="0.5.4" \
+    --build-arg GO_VERSION="1.17.3" \
+    --build-arg HASKELL_STACK_VERSION="2.1.3" \
+    --build-arg NODEJS_VERSION="16.13.2" \
+    --build-arg NPM_VERSION="7.20.6" \
+    --build-arg PIPTOOL_VERSION="21.3.1" \
+    --build-arg PYTHON_VERSION="3.10.4" \
+    --build-arg PYTHON2_VERSION="" \
+    --build-arg PYTHON_VIRTUALENV_VERSION="20.13.0" \
+    --build-arg SCANCODE_VERSION="30.1.0" \
+    --build-arg RUBY_VERSION="2.7.4" \
+    --build-arg SBT_VERSION="1.6.1" \
+    --build-arg YARN_VERSION="1.22.10" \
     "$@" \
     "$GIT_ROOT"
