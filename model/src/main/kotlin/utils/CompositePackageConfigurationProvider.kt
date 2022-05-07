ebc74bf0b38f93f7f1742e59d21c8822a1c91ebd
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2022-05-07 12:50:53 +0200

    model: Add a CompositePackageConfigurationProvider
    Add a provider for package configurations that combines multiple other
    providers.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

---
+++ model/src/main/kotlin/utils/CompositePackageConfigurationProvider.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2022 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.model.utils
+
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.Provenance
+import org.ossreviewtoolkit.model.config.PackageConfiguration
+
+/**
+ * A [PackageConfigurationProvider] that combines the provided [providers] into a single provider. The order of the
+ * [providers] determines the order in which they are queried when requesting a [PackageConfiguration] in
+ * [getPackageConfiguration].
+ */
+class CompositePackageConfigurationProvider(
+    private val providers: List<PackageConfigurationProvider>
+) : PackageConfigurationProvider {
+    constructor(vararg providers: PackageConfigurationProvider) : this(providers.asList())
+
+    override fun getPackageConfigurations(): List<PackageConfiguration> =
+        providers.flatMap { it.getPackageConfigurations() }
+
+    override fun getPackageConfiguration(packageId: Identifier, provenance: Provenance): PackageConfiguration? =
+        providers.firstNotNullOfOrNull { it.getPackageConfiguration(packageId, provenance) }
+}
