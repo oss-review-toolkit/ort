0ba2b52c8f45e2cd3ed191430f03f8fc374d6a92
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2022-05-07 13:34:39 +0200

    PackageConfigurationProvider: Remove an unused function
    Remove the function `getPackageConfigurations()` because it is not used
    anymore.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

--- model/src/main/kotlin/utils/PackageConfigurationProvider.kt
+++ model/src/main/kotlin/utils/PackageConfigurationProvider.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017-2020 HERE Europe B.V.
+ * Copyright (C) 2022 Bosch.IO GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,11 +28,6 @@ import org.ossreviewtoolkit.model.config.PackageConfiguration
  * A provider for [PackageConfiguration]s.
  */
 interface PackageConfigurationProvider {
-    /**
-     * Return all [PackageConfiguration]s of this provider.
-     */
-    fun getPackageConfigurations(): List<PackageConfiguration>
-
     /**
      * Return the first matching [PackageConfiguration] for the given [packageId] and [provenance] if any.
      */
