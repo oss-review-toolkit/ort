d8e6465c5b4880e85b43dd8e588b1d12822e2444
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2022-05-16 10:18:21 +0200

    ArchiveUtils: Rearrange enums and functions for a better overview
    Signed-off-by: Sebastian Schuberth <sebastian.schuberth@bosch.io>

--- utils/common/src/main/kotlin/ArchiveUtils.kt
+++ utils/common/src/main/kotlin/ArchiveUtils.kt
@@ -48,13 +48,16 @@ import org.apache.commons.compress.compressors.xz.XZCompressorInputStream
 import org.apache.commons.compress.utils.SeekableInMemoryByteChannel
 
 enum class ArchiveType(vararg val extensions: String) {
+    SEVENZIP(".7z"),
+    ZIP(".aar", ".egg", ".jar", ".war", ".whl", ".zip"),
+
     TAR(".gem", ".tar"),
     TAR_BZIP2(".tar.bz2", ".tbz2"),
     TAR_GZIP(".crate", ".tar.gz", ".tgz"),
     TAR_XZ(".tar.xz", ".txz"),
-    ZIP(".aar", ".egg", ".jar", ".war", ".whl", ".zip"),
-    SEVENZIP(".7z"),
+
     DEB(".deb", ".udeb"),
+
     NONE("");
 
     companion object {
@@ -138,10 +141,46 @@ fun File.unpack7Zip(targetDirectory: File, filter: (ArchiveEntry) -> Boolean = {
 }
 
 /**
+ * Unpack the [ByteArray] assuming it is a Zip archive, ignoring entries not matched by [filter].
+ */
+fun ByteArray.unpackZip(targetDirectory: File, filter: (ArchiveEntry) -> Boolean = { true }) =
+    ZipFile(SeekableInMemoryByteChannel(this)).unpack(targetDirectory, filter)
+
+/**
  * Unpack the [File] assuming it is a Zip archive ignoring all entries not matched by [filter].
  */
 fun File.unpackZip(targetDirectory: File, filter: (ArchiveEntry) -> Boolean = { true }) =
     ZipFile(this).unpack(targetDirectory, filter)
+
+/**
+ * Unpack the [ZipFile]. In contrast to [InputStream.unpackZip] this properly parses the ZIP's central directory, see
+ * https://commons.apache.org/proper/commons-compress/zip.html#ZipArchiveInputStream_vs_ZipFile.
+ */
+private fun ZipFile.unpack(targetDirectory: File, filter: (ArchiveEntry) -> Boolean) =
+    use { zipFile ->
+        val entries = zipFile.entries
+
+        while (entries.hasMoreElements()) {
+            val entry = entries.nextElement()
+
+            @Suppress("ComplexCondition")
+            if (entry.isDirectory || entry.isUnixSymlink || File(entry.name).isAbsolute || !filter(entry)) {
+                continue
+            }
+
+            val target = targetDirectory.resolve(entry.name)
+
+            // There is no guarantee that directory entries appear before file entries, so ensure that the parent
+            // directory for a file exists.
+            target.parentFile.safeMkdirs()
+
+            target.outputStream().use { output ->
+                zipFile.getInputStream(entry).copyTo(output)
+            }
+
+            copyExecutableModeBit(target, entry.unixMode)
+        }
+    }
 
 /**
  * A list with file names that are expected to be contained in a Debian package archive file.
@@ -175,10 +214,74 @@ fun File.unpackDeb(targetDirectory: File, filter: (ArchiveEntry) -> Boolean = { 
 }
 
 /**
- * Unpack the [ByteArray] assuming it is a Zip archive, ignoring entries not matched by [filter].
+ * Unpack the [InputStream] to [targetDirectory] assuming that it is a ZIP archive. This implementation ignores empty
+ * directories and symbolic links.
  */
-fun ByteArray.unpackZip(targetDirectory: File, filter: (ArchiveEntry) -> Boolean = { true }) =
-    ZipFile(SeekableInMemoryByteChannel(this)).unpack(targetDirectory, filter)
+fun InputStream.unpackZip(targetDirectory: File) =
+    ZipArchiveInputStream(this).unpack(
+        targetDirectory,
+        { entry -> (entry as ZipArchiveEntry).let { it.isDirectory || it.isUnixSymlink || File(it.name).isAbsolute } },
+        { entry -> (entry as ZipArchiveEntry).unixMode }
+    )
+
+/**
+ * Unpack the [InputStream] to [targetDirectory] assuming that it is a tape archive (TAR). This implementation ignores
+ * empty directories and symbolic links and all archive entries not accepted by [filter].
+ */
+fun InputStream.unpackTar(targetDirectory: File, filter: (ArchiveEntry) -> Boolean = { true }) =
+    TarArchiveInputStream(this).unpack(
+        targetDirectory,
+        { entry -> !(entry as TarArchiveEntry).isFile || File(entry.name).isAbsolute || !filter(entry) },
+        { entry -> (entry as TarArchiveEntry).mode }
+    )
+
+/**
+ * Unpack this [ArchiveInputStream] to the [targetDirectory], skipping all entries for which [shouldSkip] returns true,
+ * and using what [mode] returns as the file mode bits.
+ */
+private fun ArchiveInputStream.unpack(
+    targetDirectory: File,
+    shouldSkip: (ArchiveEntry) -> Boolean,
+    mode: (ArchiveEntry) -> Int
+) =
+    use { input ->
+        var processed = false
+
+        while (true) {
+            val entry = input.nextEntry ?: break
+            processed = true
+
+            if (shouldSkip(entry)) continue
+
+            val target = targetDirectory.resolve(entry.name)
+
+            // There is no guarantee that directory entries appear before file entries, so ensure that the parent
+            // directory for a file exists.
+            target.parentFile.safeMkdirs()
+
+            target.outputStream().use { output ->
+                input.copyTo(output)
+            }
+
+            copyExecutableModeBit(target, mode(entry))
+        }
+
+        if (!processed) throw IOException("Unsupported archive format or empty archive.")
+    }
+
+/**
+ * Copy the executable bit contained in [mode] to the [target] file's mode bits.
+ */
+private fun copyExecutableModeBit(target: File, mode: Int) {
+    if (Os.isWindows) return
+
+    // Note: In contrast to Java, Kotlin does not support octal literals, see
+    // https://kotlinlang.org/docs/reference/basic-types.html#literal-constants.
+    // The bit-triplets from left to right stand for user, groups, other, respectively.
+    if (mode and 0b001_000_001 != 0) {
+        target.setExecutable(true, (mode and 0b000_000_001) == 0)
+    }
+}
 
 /**
  * Pack the file into a ZIP [targetFile] using [Deflater.BEST_COMPRESSION]. If the file is a directory its content is
@@ -216,103 +319,3 @@ fun File.packZip(
 
     return targetFile
 }
-
-/**
- * Unpack the [InputStream] to [targetDirectory] assuming that it is a tape archive (TAR). This implementation ignores
- * empty directories and symbolic links and all archive entries not accepted by [filter].
- */
-fun InputStream.unpackTar(targetDirectory: File, filter: (ArchiveEntry) -> Boolean = { true }) =
-    TarArchiveInputStream(this).unpack(
-        targetDirectory,
-        { entry -> !(entry as TarArchiveEntry).isFile || File(entry.name).isAbsolute || !filter(entry) },
-        { entry -> (entry as TarArchiveEntry).mode }
-    )
-
-/**
- * Unpack the [InputStream] to [targetDirectory] assuming that it is a ZIP archive. This implementation ignores empty
- * directories and symbolic links.
- */
-fun InputStream.unpackZip(targetDirectory: File) =
-    ZipArchiveInputStream(this).unpack(
-        targetDirectory,
-        { entry -> (entry as ZipArchiveEntry).let { it.isDirectory || it.isUnixSymlink || File(it.name).isAbsolute } },
-        { entry -> (entry as ZipArchiveEntry).unixMode }
-    )
-
-/**
- * Copy the executable bit contained in [mode] to the [target] file's mode bits.
- */
-private fun copyExecutableModeBit(target: File, mode: Int) {
-    if (Os.isWindows) return
-
-    // Note: In contrast to Java, Kotlin does not support octal literals, see
-    // https://kotlinlang.org/docs/reference/basic-types.html#literal-constants.
-    // The bit-triplets from left to right stand for user, groups, other, respectively.
-    if (mode and 0b001_000_001 != 0) {
-        target.setExecutable(true, (mode and 0b000_000_001) == 0)
-    }
-}
-
-/**
- * Unpack this [ArchiveInputStream] to the [targetDirectory], skipping all entries for which [shouldSkip] returns true,
- * and using what [mode] returns as the file mode bits.
- */
-private fun ArchiveInputStream.unpack(
-    targetDirectory: File,
-    shouldSkip: (ArchiveEntry) -> Boolean,
-    mode: (ArchiveEntry) -> Int
-) =
-    use { input ->
-        var processed = false
-
-        while (true) {
-            val entry = input.nextEntry ?: break
-            processed = true
-
-            if (shouldSkip(entry)) continue
-
-            val target = targetDirectory.resolve(entry.name)
-
-            // There is no guarantee that directory entries appear before file entries, so ensure that the parent
-            // directory for a file exists.
-            target.parentFile.safeMkdirs()
-
-            target.outputStream().use { output ->
-                input.copyTo(output)
-            }
-
-            copyExecutableModeBit(target, mode(entry))
-        }
-
-        if (!processed) throw IOException("Unsupported archive format or empty archive.")
-    }
-
-/**
- * Unpack the [ZipFile]. In contrast to [InputStream.unpackZip] this properly parses the ZIP's central directory, see
- * https://commons.apache.org/proper/commons-compress/zip.html#ZipArchiveInputStream_vs_ZipFile.
- */
-private fun ZipFile.unpack(targetDirectory: File, filter: (ArchiveEntry) -> Boolean) =
-    use { zipFile ->
-        val entries = zipFile.entries
-
-        while (entries.hasMoreElements()) {
-            val entry = entries.nextElement()
-
-            @Suppress("ComplexCondition")
-            if (entry.isDirectory || entry.isUnixSymlink || File(entry.name).isAbsolute || !filter(entry)) {
-                continue
-            }
-
-            val target = targetDirectory.resolve(entry.name)
-
-            // There is no guarantee that directory entries appear before file entries, so ensure that the parent
-            // directory for a file exists.
-            target.parentFile.safeMkdirs()
-
-            target.outputStream().use { output ->
-                zipFile.getInputStream(entry).copyTo(output)
-            }
-
-            copyExecutableModeBit(target, entry.unixMode)
-        }
-    }
