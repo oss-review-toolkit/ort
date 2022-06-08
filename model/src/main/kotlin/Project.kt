b7e8e8a6dc1faaf9871e175a924c97556b57d3af
Sebastian Schuberth <sschuberth@gmail.com>
2022-06-08 14:17:49 +0200

    Project: Fix some docs to say "project" instead of "package"
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- model/src/main/kotlin/Project.kt
+++ model/src/main/kotlin/Project.kt
@@ -57,13 +57,13 @@ data class Project(
     val definitionFilePath: String,
 
     /**
-     * The list of authors declared for this package.
+     * The list of authors declared for this project.
      */
     @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     val authors: SortedSet<String> = sortedSetOf(),
 
     /**
-     * The list of licenses the authors have declared for this package. This does not necessarily correspond to the
+     * The list of licenses the authors have declared for this project. This does not necessarily correspond to the
      * licenses as detected by a scanner. Both need to be taken into account for any conclusions.
      */
     val declaredLicenses: SortedSet<String>,
