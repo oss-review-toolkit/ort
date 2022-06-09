695e2f2c90edc506566e046c27fc0537c31a6335
Frank Viernau <frank_viernau@epam.com>
2022-06-09 22:07:42 +0200

    StaticHtmlReporter: Slightly simplify a call chain
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- reporter/src/main/kotlin/reporters/StaticHtmlReporter.kt
+++ reporter/src/main/kotlin/reporters/StaticHtmlReporter.kt
@@ -377,7 +377,7 @@ class StaticHtmlReporter : Reporter {
 
         val issues = (row.analyzerIssues + row.scanIssues).flatMap { it.value }
 
-        val worstSeverity = issues.filterNot { it.isResolved }.map { it.severity }.minOrNull() ?: Severity.ERROR
+        val worstSeverity = issues.filterNot { it.isResolved }.minOfOrNull { it.severity } ?: Severity.ERROR
 
         val areAllResolved = issues.isNotEmpty() && issues.all { it.isResolved }
 
