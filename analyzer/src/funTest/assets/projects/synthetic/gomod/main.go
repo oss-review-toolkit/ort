cbf9d77dde87f2077e68a0d02d3ccc16f3e82da2
Frank Viernau <frank_viernau@epam.com>
2022-06-14 15:12:50 +0200

    synthetic/gomod: Add further dependencies
    Add a dependency on a module from GitHub and two Go standard library
    dependencies to cover further use cases with the corresponding
    functional test.
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- analyzer/src/funTest/assets/projects/synthetic/gomod/main.go
+++ analyzer/src/funTest/assets/projects/synthetic/gomod/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/fatih/color"
+import (
+    "fmt"
+	"strings"
+
+    "github.com/fatih/color"
+    "github.com/pborman/uuid"
+)
 
 func main() {
 	c := color.New(color.FgCyan).Add(color.Underline)
 	c.Println("Prints cyan text with an underline.")
+
+    uuidWithHyphen := uuid.NewRandom()
+    uuidWithoutHypen := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
+    fmt.Println(uuidWithoutHypen)
 }
