ffb09a32b51cf51045b2ae072a5020a91dbf11b6
Frank Viernau <frank_viernau@epam.com>
2022-07-07 14:58:04 +0200

    ModelTest: Use `shouldEqualJson` for JSON assertions
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- clients/osv/src/test/kotlin/ModelTest.kt
+++ clients/osv/src/test/kotlin/ModelTest.kt
@@ -19,17 +19,15 @@
 
 package org.ossreviewtoolkit.clients.osv
 
+import io.kotest.assertions.json.shouldEqualJson
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.inspectors.forAll
-import io.kotest.matchers.shouldBe
 
 import java.io.File
 
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
 
 class ModelTest : StringSpec({
     "Deserializing and serializing any vulnerability is idempotent for all official examples" {
@@ -38,7 +36,7 @@ class ModelTest : StringSpec({
 
             val serializedVulnerabilityJson = JSON.encodeToString(vulnerability)
 
-            normalizeJson(serializedVulnerabilityJson) shouldBe normalizeJson(vulnerabilityJson)
+            serializedVulnerabilityJson shouldEqualJson vulnerabilityJson
         }
     }
 })
@@ -47,18 +45,3 @@ private val JSON = Json {}
 
 private fun getVulnerabilityExamplesJson(): List<String> =
     (1..7).map { i -> File("src/test/assets/vulnerability/examples/$i.json").readText() }
-
-private fun normalizeJson(value: String): String {
-    val json = Json { prettyPrint = true }
-    val rootNode = json.decodeFromString<JsonElement>(value)
-
-    return json.encodeToString(rootNode.sortProperties())
-}
-
-private fun JsonElement.sortProperties(): JsonElement =
-    when (this) {
-        is JsonObject -> JsonObject(
-            entries.map { it.key to it.value.sortProperties() }.sortedBy { it.first }.toMap()
-        )
-        else -> this
-    }
