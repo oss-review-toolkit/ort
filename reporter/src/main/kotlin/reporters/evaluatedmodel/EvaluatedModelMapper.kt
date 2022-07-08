28c4600f4bb7fee75b6e91324284461740d4ca58
Sebastian Schuberth <sschuberth@gmail.com>
2022-07-08 17:28:34 +0200

    EvaluatedModelMapper: Drop an explicit variable type
    Also reformat the code slightly for readability.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- reporter/src/main/kotlin/reporters/evaluatedmodel/EvaluatedModelMapper.kt
+++ reporter/src/main/kotlin/reporters/evaluatedmodel/EvaluatedModelMapper.kt
@@ -212,15 +212,14 @@ internal class EvaluatedModelMapper(private val input: ReporterInput) {
                 results.flatMap { result ->
                     result.vulnerabilities.map { vulnerability ->
                         val resolutions = addResolutions(vulnerability)
-                        val evaluatedReferences: List<EvaluatedVulnerabilityReference> =
-                            vulnerability.references.map {
-                                EvaluatedVulnerabilityReference(
-                                    it.url,
-                                    it.scoringSystem,
-                                    it.severity,
-                                    VulnerabilityReference.getSeverityString(it.scoringSystem, it.severity)
-                                )
-                            }
+                        val evaluatedReferences = vulnerability.references.map {
+                            EvaluatedVulnerabilityReference(
+                                it.url,
+                                it.scoringSystem,
+                                it.severity,
+                                VulnerabilityReference.getSeverityString(it.scoringSystem, it.severity)
+                            )
+                        }
 
                         vulnerabilities += EvaluatedVulnerability(
                             pkg = pkg,
