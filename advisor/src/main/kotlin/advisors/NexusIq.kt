44275f412b64e622554731435176d404c3d47cd5
Sebastian Schuberth <sschuberth@gmail.com>
2022-07-13 15:46:56 +0200

    NexusIq: Use the official spelling as the provider name
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- advisor/src/main/kotlin/advisors/NexusIq.kt
+++ advisor/src/main/kotlin/advisors/NexusIq.kt
@@ -53,7 +53,7 @@ private const val BULK_REQUEST_SIZE = 128
  * A wrapper for [Nexus IQ Server](https://help.sonatype.com/iqserver) security vulnerability data.
  */
 class NexusIq(name: String, private val nexusIqConfig: NexusIqConfiguration) : AdviceProvider(name) {
-    class Factory : AbstractAdviceProviderFactory<NexusIq>("NexusIQ") {
+    class Factory : AbstractAdviceProviderFactory<NexusIq>("Nexus IQ Server") {
         override fun create(config: AdvisorConfiguration) = NexusIq(providerName, config.forProvider { nexusIq })
     }
 
