oss-review-toolkit/ort

commit 7088e06ae5a69b98a58e88c49b525d1636affbcd

analyzer: Add a `PackageManagerDependencyHandler`
The `PackageManagerDependencyHandler` can be used to create and resolve
dependencies between different package managers.

Package managers can add a dependency on another definition file and
scope name to their dependency tree using the
`createPackageManagerDependency()` function. This dependency is resolved
when all package managers have finished, by searching for projects from
the named definition file and adding a dependency to each of these
projects, with the transitive dependencies defined by the scope name.

This is implemented as an operation on the dependency graph, to provide
better performance than if it was implemented on the dependency tree.

The resolution of dependencies between different package managers takes
place in the `AnalyzerResultBuilder`. With this change dependency graphs
which are not reachable from any project are not contained anymore in
the result. the `AnalyzerResultBuilder` needs to be adapted to account
for that change, as one test added a dependency graph which was not
linked to any project.

Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

analyzer/src/main/kotlin/AnalyzerResultBuilder.kt
@@ -22,16 +22,19 @@ package org.ossreviewtoolkit.analyzer
 
 import kotlin.time.measureTimedValue
 
+import org.ossreviewtoolkit.analyzer.managers.utils.PackageManagerDependencyHandler
 import org.ossreviewtoolkit.model.AnalyzerResult
 import org.ossreviewtoolkit.model.CuratedPackage
 import org.ossreviewtoolkit.model.DependencyGraph
+import org.ossreviewtoolkit.model.DependencyGraphNavigator
 import org.ossreviewtoolkit.model.Identifier
 import org.ossreviewtoolkit.model.OrtIssue
 import org.ossreviewtoolkit.model.Package
 import org.ossreviewtoolkit.model.Project
 import org.ossreviewtoolkit.model.ProjectAnalyzerResult
 import org.ossreviewtoolkit.model.createAndLogIssue
-import org.ossreviewtoolkit.model.utils.DependencyGraphConverter
+import org.ossreviewtoolkit.model.utils.DependencyGraphBuilder
+import org.ossreviewtoolkit.model.utils.convertToDependencyGraph
 import org.ossreviewtoolkit.utils.common.getDuplicates
 import org.ossreviewtoolkit.utils.ort.log
 
@@ -47,7 +50,9 @@ class AnalyzerResultBuilder(private val curationProvider: PackageCurationProvide
             "AnalyzerResult contains packages that are also projects. Duplicates: '$duplicateIds'."
         }
 
-        return DependencyGraphConverter.convert(AnalyzerResult(projects, packages, issues, dependencyGraphs))
+        return AnalyzerResult(projects, packages, issues, dependencyGraphs)
+            .convertToDependencyGraph()
+            .resolvePackageManagerDependencies()
     }
 
     fun addResult(projectAnalyzerResult: ProjectAnalyzerResult): AnalyzerResultBuilder {
@@ -115,4 +120,30 @@ class AnalyzerResultBuilder(private val curationProvider: PackageCurationProvide
         dependencyGraphs[packageManagerName] = graph
         return this
     }
+}
+
+private fun AnalyzerResult.resolvePackageManagerDependencies(): AnalyzerResult {
+    val handler = PackageManagerDependencyHandler(this)
+    val navigator = DependencyGraphNavigator(dependencyGraphs)
+
+    val graphs = projects.groupBy { it.id.type }.entries.associate { (type, projectsForType) ->
+        val builder = DependencyGraphBuilder(handler)
+
+        projectsForType.forEach { project ->
+            project.scopeNames?.forEach { scopeName ->
+                val qualifiedScopeName = DependencyGraph.qualifyScope(project, scopeName)
+                navigator.directDependencies(project, scopeName).forEach { node ->
+                    handler.resolvePackageManagerDependency(node).forEach {
+                        builder.addDependency(qualifiedScopeName, it)
+                    }
+                }
+            }
+        }
+
+        // Package managers that do not use the dependency graph representation, might not have a check implemented to
+        // verify that packages exist for all dependencies, so we need to disable the reference check here.
+        type to builder.build(checkReferences = false)
+    }
+
+    return copy(dependencyGraphs = graphs)
 }

analyzer/src/test/kotlin/AnalyzerResultBuilderTest.kt
@@ -24,18 +24,22 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.collections.containExactlyInAnyOrder
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.maps.containExactly
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.beTheSameInstanceAs
 
+import org.ossreviewtoolkit.analyzer.managers.utils.PackageManagerDependencyHandler
 import org.ossreviewtoolkit.model.AnalyzerResult
 import org.ossreviewtoolkit.model.DependencyGraph
 import org.ossreviewtoolkit.model.DependencyReference
 import org.ossreviewtoolkit.model.Identifier
 import org.ossreviewtoolkit.model.OrtIssue
 import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.PackageLinkage
+import org.ossreviewtoolkit.model.PackageReference
 import org.ossreviewtoolkit.model.Project
 import org.ossreviewtoolkit.model.ProjectAnalyzerResult
 import org.ossreviewtoolkit.model.RootDependencyIndex
@@ -64,7 +68,8 @@ class AnalyzerResultBuilderTest : WordSpec() {
 
     private val project1 = Project.EMPTY.copy(
         id = Identifier("type-1", "namespace-1", "project-1", "version-1"),
-        scopeDependencies = sortedSetOf(scope1)
+        scopeDependencies = sortedSetOf(scope1),
+        definitionFilePath = "project1"
     )
     private val project2 = Project.EMPTY.copy(
         id = Identifier("type-2", "namespace-2", "project-2", "version-2"),
@@ -208,7 +213,7 @@ class AnalyzerResultBuilderTest : WordSpec() {
 
                 analyzerResult.collectIssues() should containExactly(
                     package1.id to setOf(issue1),
-                    package3.id to setOf(issue2, issue5),
+                    package3.id to setOf(issue2),
                     project1.id to setOf(issue3, issue4),
                     project2.id to setOf(issue4)
                 )
@@ -297,6 +302,72 @@ class AnalyzerResultBuilderTest : WordSpec() {
                         .addResult(analyzerResult1)
                         .addPackages(setOf(packageWithProjectId))
                         .build()
+                }
+            }
+
+            "resolve package manager dependencies" {
+                val packageManagerDependency = PackageManagerDependencyHandler.createPackageManagerDependency(
+                    packageManager = project1.id.type,
+                    definitionFile = project1.definitionFilePath,
+                    scope = scope1.name,
+                    linkage = PackageLinkage.PROJECT_DYNAMIC
+                )
+
+                val scope = Scope(
+                    name = "scope",
+                    dependencies = sortedSetOf(
+                        packageManagerDependency,
+                        PackageReference(
+                            id = pkgRef1.id,
+                            dependencies = sortedSetOf(packageManagerDependency)
+                        )
+                    )
+                )
+
+                val project = Project.EMPTY.copy(
+                    id = Identifier("type", "namespace", "project", "version"),
+                    scopeDependencies = sortedSetOf(scope),
+                    definitionFilePath = "project"
+                )
+
+                val projectAnalyzerResult = ProjectAnalyzerResult(
+                    project = project,
+                    packages = sortedSetOf(package1)
+                )
+
+                val analyzerResult = AnalyzerResultBuilder().run {
+                    addResult(projectAnalyzerResult)
+                    addResult(analyzerResult1)
+                    build()
+                }
+
+                analyzerResult.withResolvedScopes().apply {
+                    projects.find { it.id == project.id } shouldNotBeNull {
+                        project.scopes shouldContainExactly sortedSetOf(
+                            Scope(
+                                name = "scope",
+                                dependencies = sortedSetOf(
+                                    PackageReference(
+                                        id = project1.id,
+                                        dependencies = sortedSetOf(
+                                            PackageReference(id = package1.id)
+                                        )
+                                    ),
+                                    PackageReference(
+                                        id = package1.id,
+                                        dependencies = sortedSetOf(
+                                            PackageReference(
+                                                id = project1.id,
+                                                dependencies = sortedSetOf(
+                                                    PackageReference(id = package1.id)
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    }
                 }
             }
         }

analyzer/src/funTest/assets/projects/external/git-repo-expected-output.yml
@@ -392,7 +392,7 @@ analyzer:
         revision: "<REPLACE_REVISION>"
         path: ""
       homepage_url: ""
-      scope_names: []
+      scopes: []
     packages:
     - package:
         id: "Maven:antlr:antlr:2.7.7"

analyzer/src/main/kotlin/managers/utils/PackageManagerDependencyHandler.kt
@@ -1,0 +1,178 @@
+/*
+ * Copyright (C) 2022 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.analyzer.managers.utils
+
+import com.fasterxml.jackson.module.kotlin.readValue
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+import org.ossreviewtoolkit.model.AnalyzerResult
+import org.ossreviewtoolkit.model.DependencyGraphNavigator
+import org.ossreviewtoolkit.model.DependencyNode
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.OrtIssue
+import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.PackageLinkage
+import org.ossreviewtoolkit.model.PackageReference
+import org.ossreviewtoolkit.model.Project
+import org.ossreviewtoolkit.model.jsonMapper
+import org.ossreviewtoolkit.model.utils.DependencyHandler
+import org.ossreviewtoolkit.utils.ort.log
+
+private const val TYPE = "PackageManagerDependency"
+
+class PackageManagerDependencyHandler(
+    private val analyzerResult: AnalyzerResult
+) : DependencyHandler<ResolvableDependencyNode> {
+    companion object {
+        /**
+         * Create a [PackageReference] that points to the result of a [scope] in the [definitionFile] from another
+         * package manager with the provided [linkage]. The analyzer will replace this reference with the dependency
+         * tree of the provided scope after all package managers have finished.
+         */
+        fun createPackageManagerDependency(
+            packageManager: String,
+            definitionFile: String,
+            scope: String,
+            linkage: PackageLinkage
+        ): PackageReference =
+            PackageReference(
+                id = Identifier(
+                    type = TYPE,
+                    namespace = "",
+                    name = jsonMapper.writeValueAsString(
+                        PackageManagerDependency(
+                            packageManager,
+                            definitionFile,
+                            scope,
+                            linkage
+                        )
+                    ).encode(),
+                    version = ""
+                )
+            )
+
+        private fun getPackageManagerDependency(node: DependencyNode): PackageManagerDependency? =
+            when (node.id.type) {
+                TYPE -> jsonMapper.readValue<PackageManagerDependency>(node.id.name.decode())
+                else -> null
+            }
+    }
+
+    private val navigator = DependencyGraphNavigator(analyzerResult.dependencyGraphs)
+
+    override fun createPackage(dependency: ResolvableDependencyNode, issues: MutableList<OrtIssue>): Package? =
+        analyzerResult.packages.find { it.pkg.id == dependency.id }?.pkg
+
+    override fun dependenciesFor(dependency: ResolvableDependencyNode): Collection<ResolvableDependencyNode> =
+        buildList {
+            dependency.visitDependencies { dependencies ->
+                dependencies.forEach { node ->
+                    addAll(resolvePackageManagerDependency(node))
+                }
+            }
+        }
+
+    override fun identifierFor(dependency: ResolvableDependencyNode): Identifier = dependency.id
+
+    override fun issuesForDependency(dependency: ResolvableDependencyNode): Collection<OrtIssue> = dependency.issues
+
+    override fun linkageFor(dependency: ResolvableDependencyNode): PackageLinkage = dependency.linkage
+
+    fun resolvePackageManagerDependency(dependency: DependencyNode): List<ResolvableDependencyNode> =
+        getPackageManagerDependency(dependency)?.let { packageManagerDependency ->
+            packageManagerDependency.findProjects(analyzerResult).map { project ->
+                val dependencies = navigator.directDependencies(project, packageManagerDependency.scope)
+
+                ProjectScopeDependencyNode(
+                    id = project.id,
+                    linkage = packageManagerDependency.linkage,
+                    issues = emptyList(),
+                    dependencies = dependencies.map { it.getStableReference() }.toList().asSequence()
+                )
+            }
+        } ?: listOf(DependencyNodeDelegate(dependency.getStableReference()))
+}
+
+private data class PackageManagerDependency(
+    val packageManager: String,
+    val definitionFile: String,
+    val scope: String,
+    val linkage: PackageLinkage
+) {
+    fun findProjects(analyzerResult: AnalyzerResult): List<Project> =
+        analyzerResult.projects.filter { it.definitionFilePath == definitionFile }.also { projects ->
+            if (projects.isEmpty()) {
+                log.warn { "Could not find any project for definition file '$definitionFile'." }
+            }
+
+            projects.forEach { verify(it) }
+        }
+
+    @ExperimentalContracts
+    fun verify(project: Project?) {
+        contract {
+            returns() implies (project != null)
+        }
+
+        requireNotNull(project) {
+            "Could not find a project for the definition file '$definitionFile'."
+        }
+
+        require(project.id.type == packageManager) {
+            "The project '${project.id.toCoordinates()}' from definition file '$definitionFile' uses the wrong " +
+                    "package manager '${project.id.type}', expected is '$packageManager'."
+        }
+
+        requireNotNull(project.scopeNames) {
+            "The project '${project.id.toCoordinates()}' from definition file '$definitionFile' does not use a " +
+                    "dependency graph."
+        }
+
+        if (scope !in project.scopeNames.orEmpty()) {
+            log.warn {
+                "The project '${project.id.toCoordinates()}' from definition file '$definitionFile' does not contain " +
+                        "the requested scope '$scope'."
+            }
+        }
+    }
+}
+
+sealed class ResolvableDependencyNode : DependencyNode
+
+class ProjectScopeDependencyNode(
+    override val id: Identifier,
+    override val linkage: PackageLinkage,
+    override val issues: List<OrtIssue>,
+    private val dependencies: Sequence<DependencyNode>
+) : ResolvableDependencyNode() {
+    override fun <T> visitDependencies(block: (Sequence<DependencyNode>) -> T): T = block(dependencies)
+}
+
+class DependencyNodeDelegate(private val node: DependencyNode) : ResolvableDependencyNode() {
+    override val id: Identifier = node.id
+    override val linkage = node.linkage
+    override val issues = node.issues
+    override fun <T> visitDependencies(block: (Sequence<DependencyNode>) -> T): T = node.visitDependencies(block)
+}
+
+private fun String.encode() = replace(":", "__")
+private fun String.decode() = replace("__", ":")

model/src/main/kotlin/utils/DependencyGraphConverter.kt
@@ -118,3 +118,5 @@ object DependencyGraphConverter {
             dependency.issues
     }
 }
+
+fun AnalyzerResult.convertToDependencyGraph() = DependencyGraphConverter.convert(this)

commit 794e38572ba1cee901d74f35af444c310686465a

AnalyzerResultBuilder: Fix a problem with results without dependencies
This fixes a problem introduced by 7088e06a to support references
between different package managers. The corner case that a result does
not contain any dependencies caused an exception when navigating over
the dependency graph.

Resolves #5557.

Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

analyzer/src/main/kotlin/AnalyzerResultBuilder.kt
@@ -123,6 +123,8 @@ class AnalyzerResultBuilder(private val curationProvider: PackageCurationProvide
 }
 
 private fun AnalyzerResult.resolvePackageManagerDependencies(): AnalyzerResult {
+    if (dependencyGraphs.isEmpty()) return this
+
     val handler = PackageManagerDependencyHandler(this)
     val navigator = DependencyGraphNavigator(dependencyGraphs)
 

analyzer/src/test/kotlin/AnalyzerResultBuilderTest.kt
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.beEmpty
 import io.kotest.matchers.collections.containExactlyInAnyOrder
 import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.collections.shouldHaveSize
@@ -369,6 +370,13 @@ class AnalyzerResultBuilderTest : WordSpec() {
                         )
                     }
                 }
+            }
+
+            "handle a result without dependencies" {
+                val emptyResult = AnalyzerResultBuilder().build()
+
+                emptyResult.projects should beEmpty()
+                emptyResult.packages should beEmpty()
             }
         }
     }