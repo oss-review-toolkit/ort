132848dbc64b820bcc8621a19a27118ae5c91a15
Frank Viernau <frank_viernau@epam.com>
2022-07-28 17:09:46 +0200

    Evaluator: Add a rule which allows examining the project's source tree
    Allow access to the project's source tree in order to enable doing
    arbitrary checks on files like the ones in repolinter [1]. Adding
    arbitrary file checks do make the rules API more powerful, as it allows
    highly customizable checks which can automate parts of the checks
    typically done prior to open sourcing a project.
    
    Not using a third-party tool makes sense as it is simpler to use,
    because it sticks to a single way (rules.kts) for writing the policy rules.
    
    Note that the implementation intentionally is limited to the project's
    source tree, e.g. it does not work for dependencies, because doing
    such checks throughout the dependency tree does not have obvious needs
    and is not feasible anyway in terms of execution time.
    
    [1] https://github.com/todogroup/repolinter
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

---
+++ evaluator/src/main/kotlin/SourceTreeResolver.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 EPAM Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.evaluator
+
+import java.io.File
+
+import org.apache.logging.log4j.kotlin.Logging
+
+import org.ossreviewtoolkit.downloader.Downloader
+import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.SourceCodeOrigin
+import org.ossreviewtoolkit.model.VcsInfo
+import org.ossreviewtoolkit.model.config.DownloaderConfiguration
+import org.ossreviewtoolkit.utils.ort.createOrtTempDir
+
+class SourceTreeResolver private constructor(getRepositoryRoot: () -> File) {
+    companion object : Logging {
+        fun forRemoteRepository(vcsInfo: VcsInfo) =
+            SourceTreeResolver {
+                val downloadDir = createOrtTempDir()
+                val downloaderConfiguration = DownloaderConfiguration(sourceCodeOrigins = listOf(SourceCodeOrigin.VCS))
+                val downloader = Downloader(downloaderConfiguration)
+                val pkg = Package.EMPTY.copy(vcsProcessed = vcsInfo)
+
+                downloader.download(pkg, downloadDir)
+
+                downloadDir
+            }
+    }
+
+    val rootDir: File by lazy {
+        logger.info {
+            "The evaluator rules attempt to access the project's source code for the first time. Fetching sources..."
+        }
+
+        getRepositoryRoot()
+    }
+}
