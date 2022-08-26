bcf1400dc603acdc44f48c7908397250aa1dba22
Frank Viernau <frank_viernau@epam.com>
2022-08-26 12:04:13 +0200

    MavenLogger: Fix a broken KDoc reference
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- analyzer/src/main/kotlin/managers/utils/MavenLogger.kt
+++ analyzer/src/main/kotlin/managers/utils/MavenLogger.kt
@@ -41,8 +41,8 @@ private fun toPlexusLoggerLevel(level: Level) =
     }
 
 /**
- * Implementation of the Plexus [Logger] that forwards all logs to the [org.slf4j.Logger] [log] using the appropriate
- * log levels.
+ * Implementation of the Plexus [Logger] that forwards all logs to [org.apache.logging.log4j.kotlin.Logging] using the
+ * appropriate log levels.
  */
 class MavenLogger(level: Level) : AbstractLogger(toPlexusLoggerLevel(level), "MavenLogger") {
     companion object : Logging
