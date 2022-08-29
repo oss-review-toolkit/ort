4d450e320ebe2448406f543a76562b81d74ce74e
Sebastian Schuberth <sschuberth@gmail.com>
2022-08-29 18:39:14 +0200

    Reduce nesting in tests for readability
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- evaluator/src/test/kotlin/EvaluatorTest.kt
+++ evaluator/src/test/kotlin/EvaluatorTest.kt
@@ -30,92 +30,90 @@ import org.ossreviewtoolkit.model.LicenseSource
 import org.ossreviewtoolkit.model.Severity
 import org.ossreviewtoolkit.utils.spdx.toSpdx
 
-class EvaluatorTest : WordSpec() {
-    init {
-        "checkSyntax" should {
-            "succeed if the script can be compiled" {
-                val script = javaClass.getResource("/rules/no_gpl.rules.kts").readText()
+class EvaluatorTest : WordSpec({
+    "checkSyntax" should {
+        "succeed if the script can be compiled" {
+            val script = javaClass.getResource("/rules/no_gpl.rules.kts").readText()
 
-                val result = Evaluator(ortResult).checkSyntax(script)
+            val result = Evaluator(ortResult).checkSyntax(script)
 
-                result shouldBe true
-            }
-
-            "fail if the script can not be compiled" {
-                val result = Evaluator(ortResult).checkSyntax(
-                    """
-                    broken script
-                    """.trimIndent()
-                )
-
-                result shouldBe false
-            }
+            result shouldBe true
         }
 
-        "evaluate" should {
-            "return no rule violations for an empty script" {
-                val result = Evaluator(ortResult).run("")
+        "fail if the script can not be compiled" {
+            val result = Evaluator(ortResult).checkSyntax(
+                """
+                broken script
+                """.trimIndent()
+            )
 
-                result.violations should beEmpty()
-            }
+            result shouldBe false
+        }
+    }
 
-            "be able to access the ORT result" {
-                val result = Evaluator(ortResult).run(
-                    """
-                    require(ortResult.labels["label"] == "value") { "Failed to verify the ORT result." }
-                    """.trimIndent()
+    "evaluate" should {
+        "return no rule violations for an empty script" {
+            val result = Evaluator(ortResult).run("")
+
+            result.violations should beEmpty()
+        }
+
+        "be able to access the ORT result" {
+            val result = Evaluator(ortResult).run(
+                """
+                require(ortResult.labels["label"] == "value") { "Failed to verify the ORT result." }
+                """.trimIndent()
+            )
+
+            result.violations should beEmpty()
+        }
+
+        "contain rule violations in the result" {
+            val result = Evaluator(ortResult).run(
+                """
+                ruleViolations += RuleViolation(
+                    rule = "rule 1",
+                    pkg = Identifier("type:namespace:name:1.0"),
+                    license = SpdxLicenseIdExpression("license-1"),
+                    licenseSource = LicenseSource.DETECTED,
+                    severity = Severity.ERROR,
+                    message = "message 1",
+                    howToFix = "how to fix 1"
                 )
 
-                result.violations should beEmpty()
+                ruleViolations += RuleViolation(
+                    rule = "rule 2",
+                    pkg = Identifier("type:namespace:name:2.0"),
+                    license = SpdxLicenseIdExpression("license-2"),
+                    licenseSource = LicenseSource.DECLARED,
+                    severity = Severity.WARNING,
+                    message = "message 2",
+                    howToFix = "how to fix 2"
+                )
+                """.trimIndent()
+            )
+
+            result.violations should haveSize(2)
+
+            with(result.violations[0]) {
+                rule shouldBe "rule 1"
+                pkg shouldBe Identifier("type:namespace:name:1.0")
+                license shouldBe "license-1".toSpdx()
+                licenseSource shouldBe LicenseSource.DETECTED
+                severity shouldBe Severity.ERROR
+                message shouldBe "message 1"
+                howToFix shouldBe "how to fix 1"
             }
 
-            "contain rule violations in the result" {
-                val result = Evaluator(ortResult).run(
-                    """
-                    ruleViolations += RuleViolation(
-                        rule = "rule 1",
-                        pkg = Identifier("type:namespace:name:1.0"),
-                        license = SpdxLicenseIdExpression("license-1"),
-                        licenseSource = LicenseSource.DETECTED,
-                        severity = Severity.ERROR,
-                        message = "message 1",
-                        howToFix = "how to fix 1"
-                    )
-
-                    ruleViolations += RuleViolation(
-                        rule = "rule 2",
-                        pkg = Identifier("type:namespace:name:2.0"),
-                        license = SpdxLicenseIdExpression("license-2"),
-                        licenseSource = LicenseSource.DECLARED,
-                        severity = Severity.WARNING,
-                        message = "message 2",
-                        howToFix = "how to fix 2"
-                    )
-                    """.trimIndent()
-                )
-
-                result.violations should haveSize(2)
-
-                with(result.violations[0]) {
-                    rule shouldBe "rule 1"
-                    pkg shouldBe Identifier("type:namespace:name:1.0")
-                    license shouldBe "license-1".toSpdx()
-                    licenseSource shouldBe LicenseSource.DETECTED
-                    severity shouldBe Severity.ERROR
-                    message shouldBe "message 1"
-                    howToFix shouldBe "how to fix 1"
-                }
-
-                with(result.violations[1]) {
-                    rule shouldBe "rule 2"
-                    pkg shouldBe Identifier("type:namespace:name:2.0")
-                    license shouldBe "license-2".toSpdx()
-                    licenseSource shouldBe LicenseSource.DECLARED
-                    severity shouldBe Severity.WARNING
-                    message shouldBe "message 2"
-                    howToFix shouldBe "how to fix 2"
-                }
+            with(result.violations[1]) {
+                rule shouldBe "rule 2"
+                pkg shouldBe Identifier("type:namespace:name:2.0")
+                license shouldBe "license-2".toSpdx()
+                licenseSource shouldBe LicenseSource.DECLARED
+                severity shouldBe Severity.WARNING
+                message shouldBe "message 2"
+                howToFix shouldBe "how to fix 2"
             }
         }
     }
-}
+})
