7a3643aa5cb10fc21d14ea871fe49b11b52485d3
Sebastian Schuberth <sschuberth@gmail.com>
2022-08-30 21:47:36 +0200

    RuleSetTest: Reduce nesting for readability
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- evaluator/src/test/kotlin/RuleSetTest.kt
+++ evaluator/src/test/kotlin/RuleSetTest.kt
@@ -32,172 +32,170 @@ import org.ossreviewtoolkit.model.licenses.LicenseView
 import org.ossreviewtoolkit.utils.spdx.SpdxExpression
 import org.ossreviewtoolkit.utils.spdx.toSpdx
 
-class RuleSetTest : WordSpec() {
-    private val errorMessage = "error message"
-    private val howToFix = "how to fix"
+class RuleSetTest : WordSpec({
+    val errorMessage = "error message"
+    val howToFix = "how to fix"
 
-    init {
-        "package rule" should {
-            "add errors if it has no requirements" {
-                val ruleSet = ruleSet(ortResult) {
-                    packageRule("test") {
-                        error(errorMessage, howToFix)
-                    }
+    "package rule" should {
+        "add errors if it has no requirements" {
+            val ruleSet = ruleSet(ortResult) {
+                packageRule("test") {
+                    error(errorMessage, howToFix)
                 }
-
-                ruleSet.violations should haveSize(allPackages.size + allProjects.size)
             }
 
-            "add errors only if all requirements are met" {
-                val ruleSet = ruleSet(ortResult) {
-                    packageRule("test") {
-                        require {
-                            +isExcluded()
-                        }
-
-                        error(errorMessage, howToFix)
-                    }
-                }
-
-                ruleSet.violations should haveSize(2)
-            }
-
-            "add license errors only if all requirements are met" {
-                val ruleSet = ruleSet(ortResult) {
-                    packageRule("test") {
-                        require {
-                            -isExcluded()
-                        }
-
-                        licenseRule("test", LicenseView.ALL) {
-                            require {
-                                +isSpdxLicense()
-                            }
-
-                            error(errorMessage, howToFix)
-                        }
-                    }
-                }
-
-                ruleSet.violations should haveSize(6)
-            }
+            ruleSet.violations should haveSize(allPackages.size + allProjects.size)
         }
 
-        "dependency rule" should {
-            "add errors if it has no requirements" {
-                val ruleSet = ruleSet(ortResult) {
-                    dependencyRule("test") {
+        "add errors only if all requirements are met" {
+            val ruleSet = ruleSet(ortResult) {
+                packageRule("test") {
+                    require {
+                        +isExcluded()
+                    }
+
+                    error(errorMessage, howToFix)
+                }
+            }
+
+            ruleSet.violations should haveSize(2)
+        }
+
+        "add license errors only if all requirements are met" {
+            val ruleSet = ruleSet(ortResult) {
+                packageRule("test") {
+                    require {
+                        -isExcluded()
+                    }
+
+                    licenseRule("test", LicenseView.ALL) {
+                        require {
+                            +isSpdxLicense()
+                        }
+
                         error(errorMessage, howToFix)
                     }
                 }
-
-                ruleSet.violations should haveSize(allPackages.size)
             }
 
-            "add errors only if all requirements are met" {
-                val ruleSet = ruleSet(ortResult) {
-                    dependencyRule("test") {
-                        require {
-                            +isStaticallyLinked()
-                        }
-
-                        error(errorMessage, howToFix)
-                    }
-                }
-
-                ruleSet.violations should haveSize(1)
-            }
-
-            "add license errors only if all requirements are met" {
-                val ruleSet = ruleSet(ortResult) {
-                    dependencyRule("test") {
-                        require {
-                            -isStaticallyLinked()
-                        }
-
-                        licenseRule("test", LicenseView.ALL) {
-                            require {
-                                +isSpdxLicense()
-                            }
-
-                            error(errorMessage, howToFix)
-                        }
-                    }
-                }
-
-                ruleSet.violations should haveSize(6)
-            }
-
-            "add no license errors if license is removed by package license choice in the correct order" {
-                val ruleSet = ruleSet(ortResult) {
-                    dependencyRule("test") {
-                        licenseRule("test", LicenseView.ONLY_CONCLUDED) {
-                            require {
-                                +containsLicense("LicenseRef-b".toSpdx())
-                            }
-
-                            error(errorMessage, howToFix)
-                        }
-                    }
-                }
-
-                ruleSet.violations should haveSize(1)
-            }
-
-            "add no license errors if license is removed by repository license choice" {
-                val ruleSet = ruleSet(ortResult) {
-                    dependencyRule("test") {
-                        licenseRule("test", LicenseView.ONLY_CONCLUDED) {
-                            require {
-                                +containsLicense("LicenseRef-c".toSpdx())
-                            }
-
-                            error(errorMessage, howToFix)
-                        }
-                    }
-                }
-
-                ruleSet.violations should beEmpty()
-            }
-
-            "use stable references as ancestor nodes" {
-                val result = spyk(ortResult)
-                val navigator = spyk(ortResult.dependencyNavigator)
-                every { result.dependencyNavigator } returns navigator
-
-                every { navigator.directDependencies(any(), any()) } answers {
-                    ortResult.dependencyNavigator.directDependencies(firstArg(), secondArg()).map { node ->
-                        val spyNode = spyk(node)
-                        every { spyNode.getStableReference() } answers {
-                            val ref = spyk(spyNode)
-                            every { ref.id } answers { node.id.copy(name = node.id.name + "-ref") }
-                            ref
-                        }
-                        spyNode
-                    }
-                }
-
-                val ruleSet = ruleSet(result) {
-                    dependencyRule("test") {
-                        require {
-                            -isStaticallyLinked()
-                        }
-
-                        if (ancestors.any { !it.id.name.endsWith("-ref") }) {
-                            error("Node is not a reference.", howToFix)
-                        }
-                    }
-                }
-
-                ruleSet.violations should beEmpty()
-            }
+            ruleSet.violations should haveSize(6)
         }
     }
 
-    private fun PackageRule.LicenseRule.containsLicense(expression: SpdxExpression) =
-        object : RuleMatcher {
-            override val description = "containsLicense(license)"
+    "dependency rule" should {
+        "add errors if it has no requirements" {
+            val ruleSet = ruleSet(ortResult) {
+                dependencyRule("test") {
+                    error(errorMessage, howToFix)
+                }
+            }
 
-            override fun matches() = license == expression
+            ruleSet.violations should haveSize(allPackages.size)
         }
-}
+
+        "add errors only if all requirements are met" {
+            val ruleSet = ruleSet(ortResult) {
+                dependencyRule("test") {
+                    require {
+                        +isStaticallyLinked()
+                    }
+
+                    error(errorMessage, howToFix)
+                }
+            }
+
+            ruleSet.violations should haveSize(1)
+        }
+
+        "add license errors only if all requirements are met" {
+            val ruleSet = ruleSet(ortResult) {
+                dependencyRule("test") {
+                    require {
+                        -isStaticallyLinked()
+                    }
+
+                    licenseRule("test", LicenseView.ALL) {
+                        require {
+                            +isSpdxLicense()
+                        }
+
+                        error(errorMessage, howToFix)
+                    }
+                }
+            }
+
+            ruleSet.violations should haveSize(6)
+        }
+
+        "add no license errors if license is removed by package license choice in the correct order" {
+            val ruleSet = ruleSet(ortResult) {
+                dependencyRule("test") {
+                    licenseRule("test", LicenseView.ONLY_CONCLUDED) {
+                        require {
+                            +containsLicense("LicenseRef-b".toSpdx())
+                        }
+
+                        error(errorMessage, howToFix)
+                    }
+                }
+            }
+
+            ruleSet.violations should haveSize(1)
+        }
+
+        "add no license errors if license is removed by repository license choice" {
+            val ruleSet = ruleSet(ortResult) {
+                dependencyRule("test") {
+                    licenseRule("test", LicenseView.ONLY_CONCLUDED) {
+                        require {
+                            +containsLicense("LicenseRef-c".toSpdx())
+                        }
+
+                        error(errorMessage, howToFix)
+                    }
+                }
+            }
+
+            ruleSet.violations should beEmpty()
+        }
+
+        "use stable references as ancestor nodes" {
+            val result = spyk(ortResult)
+            val navigator = spyk(ortResult.dependencyNavigator)
+            every { result.dependencyNavigator } returns navigator
+
+            every { navigator.directDependencies(any(), any()) } answers {
+                ortResult.dependencyNavigator.directDependencies(firstArg(), secondArg()).map { node ->
+                    val spyNode = spyk(node)
+                    every { spyNode.getStableReference() } answers {
+                        val ref = spyk(spyNode)
+                        every { ref.id } answers { node.id.copy(name = node.id.name + "-ref") }
+                        ref
+                    }
+                    spyNode
+                }
+            }
+
+            val ruleSet = ruleSet(result) {
+                dependencyRule("test") {
+                    require {
+                        -isStaticallyLinked()
+                    }
+
+                    if (ancestors.any { !it.id.name.endsWith("-ref") }) {
+                        error("Node is not a reference.", howToFix)
+                    }
+                }
+            }
+
+            ruleSet.violations should beEmpty()
+        }
+    }
+})
+
+private fun PackageRule.LicenseRule.containsLicense(expression: SpdxExpression) =
+    object : RuleMatcher {
+        override val description = "containsLicense(license)"
+
+        override fun matches() = license == expression
+    }
