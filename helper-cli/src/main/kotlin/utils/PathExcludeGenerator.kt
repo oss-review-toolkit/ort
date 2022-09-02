d6ffa110548eaff1c3369a0953379b2d07632412
Frank Viernau <frank_viernau@epam.com>
2022-09-02 10:32:16 +0200

    PathExcludeGenerate: Make a function parameter more generic
    This simplifies the use of the function.
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- helper-cli/src/main/kotlin/utils/PathExcludeGenerator.kt
+++ helper-cli/src/main/kotlin/utils/PathExcludeGenerator.kt
@@ -34,7 +34,7 @@ internal object PathExcludeGenerator {
      * Return path excludes which likely but not necessarily apply to a source tree containing all given [filePaths]
      * which must be relative to the root directory of the source tree.
      */
-    fun generatePathExcludes(filePaths: Set<String>): List<PathExclude> {
+    fun generatePathExcludes(filePaths: Collection<String>): List<PathExclude> {
         val files = filePaths.mapTo(mutableSetOf()) { File(it) }
         val dirs = getAllDirs(files)
 
