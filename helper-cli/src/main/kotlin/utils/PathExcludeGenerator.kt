83a5bba8c69765cb9d05ade836e2233d982331cf
Frank Viernau <frank_viernau@epam.com>
2022-09-02 11:56:33 +0200

    PathExcludeGenerator: Add further directory names for exclusion
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- helper-cli/src/main/kotlin/utils/PathExcludeGenerator.kt
+++ helper-cli/src/main/kotlin/utils/PathExcludeGenerator.kt
@@ -82,13 +82,34 @@ private fun File.getAncestorFiles(): List<File> {
 }
 
 private val PATH_EXCLUDES_REASON_FOR_DIR_NAME = mapOf(
+    ".github" to BUILD_TOOL_OF,
+    ".gradle" to BUILD_TOOL_OF,
+    ".idea" to BUILD_TOOL_OF,
+    ".mvn" to BUILD_TOOL_OF,
+    ".travis" to BUILD_TOOL_OF,
+    "CMake" to BUILD_TOOL_OF,
+    "Example" to DOCUMENTATION_OF,
+    "Tests" to TEST_OF,
     "bench" to TEST_OF,
+    "benches" to TEST_OF,
     "benchmark" to TEST_OF,
     "benchmarks" to TEST_OF,
     "build" to BUILD_TOOL_OF,
+    "cmake" to BUILD_TOOL_OF,
+    "demos" to DOCUMENTATION_OF,
+    "doc" to DOCUMENTATION_OF,
+    "doc-files" to DOCUMENTATION_OF,
     "docs" to DOCUMENTATION_OF,
+    "example" to DOCUMENTATION_OF,
+    "examples" to DOCUMENTATION_OF,
+    "funTest" to TEST_OF,
+    "javadoc" to DOCUMENTATION_OF,
     "m4" to BUILD_TOOL_OF,
+    "scripts" to BUILD_TOOL_OF,
     "test" to TEST_OF,
     "tests" to TEST_OF,
-    "tools" to BUILD_TOOL_OF
+    "testutils" to TEST_OF,
+    "tools" to BUILD_TOOL_OF,
+    "tutorial" to DOCUMENTATION_OF,
+    "winbuild" to BUILD_TOOL_OF
 )
