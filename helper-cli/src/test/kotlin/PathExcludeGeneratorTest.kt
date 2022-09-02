dcc020d1af90b26629ad538fe571e68fcea5a5af
Frank Viernau <frank_viernau@epam.com>
2022-09-02 11:06:33 +0200

    PathExcludeGenerator: Add a simple test for excluding directories
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

---
+++ helper-cli/src/test/kotlin/PathExcludeGeneratorTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2022 EPAM Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.containExactlyInAnyOrder
+import io.kotest.matchers.should
+
+import org.ossreviewtoolkit.helper.utils.PathExcludeGenerator.generatePathExcludes
+
+class PathExcludeGeneratorTest : WordSpec({
+    "generatePathExcludes()" should {
+        "return the expected excludes for directories" {
+            val files = listOf(
+                "docs/file.ext",
+                "src/main/file.ext",
+                "src/test/file.ext"
+            )
+
+            generatePathExcludes(files).map { it.pattern } should containExactlyInAnyOrder(
+                "docs/**",
+                "src/test/**"
+            )
+        }
+    }
+})
