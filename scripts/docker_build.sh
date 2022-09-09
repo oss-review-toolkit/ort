55d6b54f2bc54a60ceea6fb482ba5bfe6b81c30d
Helio Chissini de Castro <helio.chissini-de-castro@bmw.de>
2022-09-09 10:09:58 +0200

    docker_build: Enable builds on Apple Silicon
    Use Docker's `buildx` command [1] to enable building multi-architecture
    Docker images. Prior to this commit building the ORT Docker image on Apple
    Silicon failed with an unsupported architecture error.
    
    Resolves #5411.
    
    [1]: https://docs.docker.com/engine/reference/commandline/buildx_build/
    
    Signed-off-by: Helio Chissini de Castro <helio.chissini-de-castro@bmw.de>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # Copyright (C) 2020 Bosch.IO GmbH
+# Copyright (C) 2022 BMW CarIT GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,4 +22,4 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 GIT_REVISION=$(git describe --abbrev=10 --always --tags --dirty)
 
 echo "Setting ORT_VERSION to $GIT_REVISION."
-DOCKER_BUILDKIT=1 docker build -f $GIT_ROOT/Dockerfile -t ort --build-arg ORT_VERSION=$GIT_REVISION $GIT_ROOT
+docker buildx build -f $GIT_ROOT/Dockerfile -t ort --build-arg ORT_VERSION=$GIT_REVISION --platform linux/amd64 $GIT_ROOT
