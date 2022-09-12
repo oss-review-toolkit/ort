c1df0661937de5305786149b3cdc08d0b797d9f1
Sebastian Schuberth <sschuberth@gmail.com>
2022-09-12 09:00:56 +0200

    scanoss: Replace the hard-coded "Blacklist" with configurable suffixes
    For reference, the original hard-coded extensions from ScanOss's
    deprecated Java client [1] are used.
    
    Additionally, support for generic filename suffixes as declared as part of
    the maintained Python client [2] is added, and the Python client's
    extensions and suffixes are used as defaults.
    
    This is a step towards getting rid of the dependency on the deprecated
    ScanOss Java library complelety in favor of only using ORT's own client
    library.
    
    [1]: https://github.com/scanoss/scanner.java/blob/350c211/src/main/java/com/scanoss/scanner/BlacklistRules.java#L32-L40
    [2]: https://github.com/scanoss/scanoss.py/blob/f27ed92/src/scanoss/scanner.py#L54-L75
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- scanner/src/main/kotlin/scanners/scanoss/ScanOssConfig.kt
+++ scanner/src/main/kotlin/scanners/scanoss/ScanOssConfig.kt
@@ -31,7 +31,10 @@ internal data class ScanOssConfig(
     val apiUrl: String,
 
     /** API Key required to authenticate with the ScanOSS server. */
-    val apiKey: String
+    val apiKey: String,
+
+    /** A list of suffixes of files to be ignored during the scan. */
+    val ignoredFileSuffixes: List<String>
 ) {
     companion object {
         /** Name of the configuration property for the API URL. */
@@ -40,6 +43,31 @@ internal data class ScanOssConfig(
         /** Name of the configuration property for the API key. */
         const val API_KEY_PROPERTY = "apiKey"
 
+        /** Name of the configuration property for the list of ignored file suffixes. */
+        const val IGNORED_FILE_SUFFIXES_PROPERTY = "ignoredFileSuffixes"
+
+        /** The default list of ignored file extension. */
+        private val DEFAULT_IGNORED_FILE_EXTENSIONS = listOf(
+            ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", ".ac", ".adoc", ".am", ".asciidoc", ".bmp", ".build",
+            ".c9", ".c9revisions", ".cache", ".cfg", ".chm", ".class", ".cmake", ".cnf", ".conf", ".config",
+            ".contributors", ".copying", ".cover", ".coverage", ".crt", ".csproj", ".css", ".csv", ".dat", ".data",
+            ".doc", ".docx", ".dotcover", ".dtd", ".dts", ".dtsi", ".dump", ".editorconfig", ".egg", ".eot", ".eps",
+            ".gdoc", ".gem", ".geojson", ".gif", ".glif", ".gml", ".gmo", ".gradle", ".guess", ".hex", ".htm", ".html",
+            ".ico", ".iml", ".in", ".inc", ".info", ".ini", ".ipynb", ".iws", ".jpeg", ".jpg", ".json", ".jsonld",
+            ".lcov", ".lock", ".log", ".lst", ".m4", ".manifest", ".map", ".markdown", ".md", ".md5", ".meta", ".mk",
+            ".mxml", ".o", ".otf", ".out", ".pbtxt", ".pdb", ".pdf", ".pem", ".phtml", ".pickle", ".pid", ".plist",
+            ".plt", ".png", ".po", ".pot", ".ppt", ".prefs", ".properties", ".pyc", ".qdoc", ".result", ".rgb", ".rst",
+            ".scss", ".sha", ".sha1", ".sha2", ".sha256", ".sln", ".spec", ".sql", ".sub", ".svg", ".svn-base", ".tab",
+            ".template", ".test", ".tex", ".tiff", ".toml", ".ttf", ".txt", ".utf-8", ".vim", ".wav", ".wfp", ".whl",
+            ".woff", ".xht", ".xhtml", ".xls", ".xlsx", ".xml", ".xpm", ".xsd", ".xul", ".yaml", ".yml"
+        )
+
+        /** The default list of ignored file suffixes. */
+        private val DEFAULT_IGNORED_FILE_SUFFIXES = DEFAULT_IGNORED_FILE_EXTENSIONS + listOf(
+            "-doc", "authors", "changelog", "config", "copying", "ignore", "license", "licenses", "manifest", "news",
+            "notice", "readme", "sqlite", "sqlite3", "swiftdoc", "texidoc", "todo", "version"
+        )
+
         fun create(scannerConfig: ScannerConfiguration): ScanOssConfig {
             val scanOssIdScannerOptions = scannerConfig.options?.get("ScanOss")
 
@@ -47,8 +75,12 @@ internal data class ScanOssConfig(
 
             val apiURL = scanOssIdScannerOptions[API_URL_PROPERTY] ?: "https://osskb.org/api/"
             val apiKey = scanOssIdScannerOptions[API_KEY_PROPERTY].orEmpty()
+            val ignoredFileSuffixes = scanOssIdScannerOptions[IGNORED_FILE_SUFFIXES_PROPERTY]
+                ?.split(',')
+                ?.map { it.trim() }
+                ?: DEFAULT_IGNORED_FILE_SUFFIXES
 
-            return ScanOssConfig(apiURL, apiKey)
+            return ScanOssConfig(apiURL, apiKey, ignoredFileSuffixes)
         }
     }
 }
