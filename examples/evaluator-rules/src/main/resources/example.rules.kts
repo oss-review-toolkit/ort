302198945a02a5b7bb9a80dacbae8d7e8c40818a
Frank Viernau <frank_viernau@epam.com>
2022-09-16 15:32:11 +0200

    evaluator-rules: Examplify asserting detected licenses in certain files
    Examplify the use of the recently added `ProjectSourceRule` function
    `projectSourceGetDetectedLicensesByFilePath()`.
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- examples/evaluator-rules/src/main/resources/example.rules.kts
+++ examples/evaluator-rules/src/main/resources/example.rules.kts
@@ -334,6 +334,28 @@ fun RuleSet.missingReadmeFileLicenseSectionRule() = projectSourceRule("MISSING_R
     )
 }
 
+fun RuleSet.wrongLicenseInLicenseFileRule() = projectSourceRule("WRONG_LICENSE_IN_LICENSE_FILE_RULE") {
+    require {
+        +projectSourceHasFile("LICENSE")
+    }
+
+    val allowedRootLicenses = setOf("Apackage-2.0", "MIT")
+    val detectedRootLicenses = projectSourceGetDetectedLicensesByFilePath("LICENSE").values.flatten().toSet()
+    val wrongLicenses = detectedRootLicenses - allowedRootLicenses
+
+    if (wrongLicenses.isNotEmpty()) {
+        error(
+            message = "The file 'LICENSE' contains the following disallowed licenses ${wrongLicenses.joinToString()}.",
+            howToFix = "Please use only the following allowed licenses: ${allowedRootLicenses.joinToString()}."
+        )
+    } else if (detectedRootLicenses.isEmpty()) {
+        error(
+            message = "The file 'LICENSE' does not contain any license which is not allowed.",
+            howToFix = "Please use one of the following allowed licenses: ${allowedRootLicenses.joinToString()}."
+        )
+    }
+}
+
 /**
  * The set of policy rules.
  */
@@ -359,6 +381,7 @@ val ruleSet = ruleSet(ortResult, licenseInfoResolver, resolutionProvider) {
     missingContributingFileRule()
     missingReadmeFileRule()
     missingReadmeFileLicenseSectionRule()
+    wrongLicenseInLicenseFileRule()
 }
 
 // Populate the list of policy rule violations to return.
