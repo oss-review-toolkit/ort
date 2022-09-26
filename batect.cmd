2caa7d050ef770b8e9ee322bca2baff71f485f41
Sebastian Schuberth <sschuberth@gmail.com>
2022-09-26 11:23:37 +0200

    Upgrade Batect to version 0.82.0
    See [1].
    
    [1]: https://github.com/batect/batect/releases/tag/0.82.0
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- batect.cmd
+++ batect.cmd
@@ -6,7 +6,7 @@ rem For more information, visit https://github.com/batect/batect.
 
 setlocal EnableDelayedExpansion
 
-set "version=0.81.1"
+set "version=0.82.0"
 
 if "%BATECT_CACHE_DIR%" == "" (
     set "BATECT_CACHE_DIR=%USERPROFILE%\.batect\cache"
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'^
 
 ^
 
-$Version='0.81.1'^
+$Version='0.82.0'^
 
 ^
 
@@ -48,7 +48,7 @@ $UrlEncodedVersion = [Uri]::EscapeDataString($Version)^
 
 $DownloadUrl = getValueOrDefault $env:BATECT_DOWNLOAD_URL "$DownloadUrlRoot/$UrlEncodedVersion/batect-$UrlEncodedVersion.jar"^
 
-$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '0e15559080bbbac00ca30ff8099a0b19e7cb61ea5f1dc0d38334cf67e513a556'^
+$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '6d3e7e26e718f705d8a344c85048c2821aedd8ae84fec1db2251fe6f3adec3ea'^
 
 ^
 
