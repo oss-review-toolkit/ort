a96f752197e11e03d34301d82138f7f28f4646cf
Oliver Heger <oliver.heger@bosch.io>
2022-09-28 15:06:33 +0200

    Utils: Add a MaskedString class
    The class is a wrapper for char sequences, but exposes a configurable
    mask string to the outside. It can be used to prevent that sensitive
    information is logged to the console.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ utils/common/src/main/kotlin/MaskedString.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2022 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.utils.common
+
+/**
+ * A class that wraps a [CharSequence] and overrides some functions to return a configurable mask string
+ * instead of the actual content. The main use case for this class is to prevent that sensitive information like
+ * passwords are visible in log output. The class can be used everywhere a [CharSequence] is accepted.
+ */
+data class MaskedString(
+    /** The wrapped string. */
+    val value: CharSequence,
+
+    /** A string that is returned by the toString method instead of the actual value. */
+    val mask: String = DEFAULT_MASK
+) : CharSequence by mask {
+    companion object {
+        const val DEFAULT_MASK = "*****"
+    }
+
+    override fun toString() = mask
+}
+
+/**
+ * Return an array with strings from the given [args]. If one of the input strings is a [MaskedString], include its
+ * actual value. So, this function can be used to access the unmasked strings.
+ */
+fun unmaskedStrings(vararg args: CharSequence): Array<out String> =
+    args.map { s ->
+        when (s) {
+            is MaskedString -> s.value.toString()
+            else -> s.toString()
+        }
+    }.toTypedArray()
+
+/**
+ * Convert this [CharSequence] to a [MaskedString] which uses the provided [mask].
+ */
+fun CharSequence.masked(mask: String = MaskedString.DEFAULT_MASK): MaskedString = MaskedString(this, mask)
---
+++ utils/common/src/test/kotlin/MaskedStringTest.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2022 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.utils.common
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.string.shouldHaveLength
+
+class MaskedStringTest : StringSpec({
+    "CharSequence functions can be invoked" {
+        val masked = MaskedString("This is a test.")
+
+        masked shouldHaveLength MaskedString.DEFAULT_MASK.length
+        masked.substring(1..3) shouldBe MaskedString.DEFAULT_MASK.substring(1..3)
+    }
+
+    "toString returns a default mask" {
+        val masked = MaskedString("mySecretPassword")
+
+        masked.toString() shouldBe MaskedString.DEFAULT_MASK
+    }
+
+    "toString returns the configured mask" {
+        val mask = "???"
+        val masked = MaskedString("anotherSecret", mask)
+
+        masked.toString() shouldBe mask
+    }
+
+    "The string is masked when referenced in a join expression" {
+        val strings = listOf("foo", MaskedString("bar"), "baz")
+
+        val joinResult = strings.joinToString()
+
+        joinResult shouldBe "foo, ${MaskedString.DEFAULT_MASK}, baz"
+    }
+
+    "Two objects can be compared with equals" {
+        val masked1 = MaskedString("value", "mask")
+        val masked2 = MaskedString("value", "mask")
+        val masked3 = MaskedString("value1", "mask")
+        val masked4 = MaskedString("value", "mask1")
+
+        masked1 shouldBe masked2
+        masked1 shouldNotBe masked3
+        masked1 shouldNotBe masked4
+        masked1 shouldNotBe Object()
+    }
+
+    "unmaskedStrings() returns unmasked strings" {
+        val str = unmaskedStrings("foo", MaskedString("bar"), "baz").joinToString()
+
+        str shouldBe "foo, bar, baz"
+    }
+
+    "A CharSequence can be converted to a MaskedString" {
+        val value = "This is some text"
+        val mask = "+++"
+
+        val maskedValue = value.masked(mask)
+
+        maskedValue.value shouldBe value
+        maskedValue.mask shouldBe mask
+    }
+})
