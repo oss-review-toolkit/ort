oss-review-toolkit/ort

commit 3e9d11d83626c7886f2706d72ce05dddd40937cc

ResolvedLicense: Rewrite `toResolvedCopyrights` for clarity and efficiency
Return early in case no processing is wanted to keep following code
simple. Handle the mapping of processed an unprocessed statements to
resolved copyrights separately as they allow for different optimizations.

Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

model/src/main/kotlin/licenses/ResolvedLicense.kt
@@ -121,19 +121,20 @@ data class ResolvedLicense(
 }
 
 private fun Collection<ResolvedCopyrightFinding>.toResolvedCopyrights(process: Boolean): List<ResolvedCopyright> {
-    val allStatements = map { it.statement }
-    val processedStatements = if (process) {
-        CopyrightStatementsProcessor().process(allStatements).toMap()
-    } else {
-        allStatements.associateBy({ it }, { setOf(it) })
+    if (!process) return map { ResolvedCopyright(it.statement, setOf(it)) }
+
+    val statementToFinding = associateBy { it.statement }
+    val result = CopyrightStatementsProcessor().process(statementToFinding.keys)
+
+    val processedCopyrights = result.processedStatements.map { (statement, originalStatements) ->
+        val findings = originalStatements.mapTo(mutableSetOf()) { statementToFinding.getValue(statement) }
+        ResolvedCopyright(statement, findings)
     }
 
-    return processedStatements.mapValues { (_, originalStatements) ->
-        filter { it.statement in originalStatements }
-    }.filterValues { it.isNotEmpty() }.entries.map { (statement, findings) ->
-        ResolvedCopyright(statement, findings.toSet())
+    val unprocessedCopyrights = result.unprocessedStatements.mapNotNull { statement ->
+        val findings = filterTo(mutableSetOf()) { it.statement == statement }
+        findings.takeUnless { it.isEmpty() }?.let { ResolvedCopyright(statement, it) }
     }
+
+    return processedCopyrights + unprocessedCopyrights
 }
-
-private fun CopyrightStatementsProcessor.Result.toMap(): Map<String, Set<String>> =
-    processedStatements + unprocessedStatements.associateWith { setOf(it) }

commit 804669f379069da99691866b9bdfb1a924ec9750

ResolvedLicense: Group by identical statements even without processing
This fixes another regression introduced in 3e9d11d.

Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

model/src/main/kotlin/licenses/ResolvedLicense.kt
@@ -121,9 +121,12 @@ data class ResolvedLicense(
 }
 
 internal fun Collection<ResolvedCopyrightFinding>.toResolvedCopyrights(process: Boolean): List<ResolvedCopyright> {
-    if (!process) return map { ResolvedCopyright(it.statement, setOf(it)) }
-
     val statementToFindings = groupBy { it.statement }
+
+    if (!process) {
+        return statementToFindings.map { (statement, findings) -> ResolvedCopyright(statement, findings.toSet()) }
+    }
+
     val result = CopyrightStatementsProcessor.process(statementToFindings.keys)
 
     val processedCopyrights = result.processedStatements.map { (statement, originalStatements) ->

model/src/test/kotlin/licenses/ResolvedLicenseTest.kt
@@ -47,7 +47,7 @@ class ResolvedLicenseTest : StringSpec({
             ),
         )
 
-        val resolvedCopyrights = originalFindings.toResolvedCopyrights(process = true)
+        val resolvedCopyrights = originalFindings.toResolvedCopyrights(process = false)
 
         resolvedCopyrights shouldHaveSize 1
         with(resolvedCopyrights.first()) {