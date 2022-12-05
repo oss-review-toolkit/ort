915d28dcb063c62bb56d2300687e5ba0bf745cb4
Sebastian Schuberth <sschuberth@gmail.com>
2022-12-05 14:34:37 +0100

    Use the upstream name for the OSADL matrix JSON file
    Even if the name is less telling, use the original name from upstream
    (see [1]) to simplify updates by just copying over the file without any
    renaming.
    
    [1]: https://www.osadl.org/Access-to-raw-data.oss-compliance-raw-data-access.0.html#c17923
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- evaluator/src/main/kotlin/osadl/CompatibilityMatrix.kt
+++ evaluator/src/main/kotlin/osadl/CompatibilityMatrix.kt
@@ -49,7 +49,7 @@ object CompatibilityMatrix : Logging {
     }
 
     private val matrix by lazy {
-        javaClass.getResourceAsStream("/rules/osadl-matrix-with-explanations.json").use {
+        javaClass.getResourceAsStream("/rules/matrixseqexpl.json").use {
             Json.Default.decodeFromStream<MatrixLicenses>(it)
         }
     }
