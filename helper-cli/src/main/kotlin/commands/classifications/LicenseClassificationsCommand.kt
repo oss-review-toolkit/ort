dafc7dec846eaa08a1382d6af43439f698a9121b
Sebastian Schuberth <sschuberth@gmail.com>
2022-12-07 17:36:39 +0100

    helper-cli: Add a commands to import and merge license classifications
    Users who are not willing or able to create their own license
    classifications may want to build on existing classifications from various
    public sources / providers. Support this by introducing a subcommand to
    `import` license classifications, for now from Eclipse or LDBcollector.
    
    Additionally, the different classifications are likely to classify on
    different aspects of a license, like its coarse category ("permissive",
    "strong copyleft" etc.) vs. fine-granular obligations ("must state
    changes", "can use commercially" etc.). To benefit from all of these
    classifications, allow to merge multiple classifications into one via a
    new `merge` subcommand.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

---
+++ helper-cli/src/main/kotlin/commands/classifications/LicenseClassificationsCommand.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.helper.commands.classifications
+
+import com.github.ajalt.clikt.core.NoOpCliktCommand
+import com.github.ajalt.clikt.core.subcommands
+
+import org.ossreviewtoolkit.model.licenses.LicenseClassifications
+
+class LicenseClassificationsCommand : NoOpCliktCommand(
+    help = "Commands for working with license classifications."
+) {
+    init {
+        subcommands(
+            ImportCommand(),
+            MergeCommand()
+        )
+    }
+}
+
+internal fun LicenseClassifications.prefixCategoryNames(prefix: String) =
+    LicenseClassifications(
+        categories = categories.map { it.copy(name = prefix + it.name) },
+        categorizations = categorizations.map { categorization ->
+            categorization.copy(categories = categorization.categories.mapTo(mutableSetOf()) { prefix + it })
+        }
+    )
+
+internal fun LicenseClassifications.sort() =
+    LicenseClassifications(
+        categories = categories.sortedBy { it.name },
+        categorizations = categorizations.sortedBy { it.id.toString() }.map {
+            it.copy(categories = it.categories.toSortedSet())
+        }
+    )
