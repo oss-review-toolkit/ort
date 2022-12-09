8f8e6e78266fcedd10da36807d41388ee19656db
Sebastian Schuberth <sschuberth@gmail.com>
2022-12-09 13:15:56 +0100

    VersionControlSystemTest: Add a test for `getRevisionCandidates()`
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- downloader/src/test/kotlin/VersionControlSystemTest.kt
+++ downloader/src/test/kotlin/VersionControlSystemTest.kt
@@ -22,7 +22,16 @@ package org.ossreviewtoolkit.downloader
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
+import io.mockk.every
+import io.mockk.mockk
+
 import java.io.File
+
+import org.ossreviewtoolkit.downloader.vcs.Git
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.VcsInfo
+import org.ossreviewtoolkit.model.VcsType
 
 class VersionControlSystemTest : WordSpec({
     val vcsRoot = File("..").absoluteFile.normalize()
@@ -58,6 +67,28 @@ class VersionControlSystemTest : WordSpec({
             relVcsDir.getPathToRoot(relProjDir) shouldBe "downloader/src/test"
             relVcsDir.getPathToRoot(File("..")) shouldBe ""
             relVcsDir.getPathToRoot(File("src/test/kotlin")) shouldBe "downloader/src/test/kotlin"
+        }
+    }
+
+    "getRevisionCandidates()" should {
+        "return revision candidates in the expected order" {
+            val pkg = Package.EMPTY.copy(
+                id = Identifier("Gem::google-cloud-core:1.6.0"),
+                vcsProcessed = VcsInfo(
+                    type = VcsType.GIT,
+                    url = "https://github.com/googleapis/google-cloud-ruby.git",
+                    revision = "master"
+                )
+            )
+
+            val workingTree = mockk<WorkingTree>()
+
+            every { workingTree.guessRevisionName(any(), any()) } returns "v1.6.0"
+
+            Git().getRevisionCandidates(workingTree, pkg, allowMovingRevisions = true) shouldBe listOf(
+                "master",
+                "v1.6.0"
+            )
         }
     }
 })
