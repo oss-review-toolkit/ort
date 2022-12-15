oss-review-toolkit/ort

commit 721ee9d5e2dcc63e9fb4770c3351e9571dda4d46

Remove the feature to bootstrap scanners
Align with the semantics of analyzer tools, which is that any external
tools need to be present on the host running ORT. This allows to remove
quite some complexity from the scanner code. Bootstrapping was barely
used anyway, with ScanCode de facto being the only scanner used, and
ScanCode also being installed in the `Dockerfile`.

Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

scanner/src/main/kotlin/scanners/scancode/ScanCode.kt
@@ -31,22 +31,16 @@ import org.ossreviewtoolkit.model.config.DownloaderConfiguration
 import org.ossreviewtoolkit.model.config.ScannerConfiguration
 import org.ossreviewtoolkit.model.readTree
 import org.ossreviewtoolkit.scanner.AbstractScannerWrapperFactory
-import org.ossreviewtoolkit.scanner.BuildConfig
 import org.ossreviewtoolkit.scanner.CommandLinePathScannerWrapper
 import org.ossreviewtoolkit.scanner.ScanContext
 import org.ossreviewtoolkit.scanner.ScanResultsStorage
 import org.ossreviewtoolkit.scanner.ScannerCriteria
 import org.ossreviewtoolkit.utils.common.Os
-import org.ossreviewtoolkit.utils.common.ProcessCapture
 import org.ossreviewtoolkit.utils.common.isTrue
 import org.ossreviewtoolkit.utils.common.safeDeleteRecursively
-import org.ossreviewtoolkit.utils.common.safeMkdirs
 import org.ossreviewtoolkit.utils.common.splitOnWhitespace
-import org.ossreviewtoolkit.utils.common.unpack
 import org.ossreviewtoolkit.utils.common.withoutPrefix
-import org.ossreviewtoolkit.utils.ort.OkHttpClientHelper
 import org.ossreviewtoolkit.utils.ort.createOrtTempDir
-import org.ossreviewtoolkit.utils.ort.ortToolsDirectory
 
 /**
  * A wrapper for [ScanCode](https://github.com/nexB/scancode-toolkit).
@@ -63,9 +57,9 @@ import org.ossreviewtoolkit.utils.ort.ortToolsDirectory
  *   contain an SPDX expression.
  */
 class ScanCode internal constructor(
-    name: String,
+    override val name: String,
     private val scannerConfig: ScannerConfiguration
-) : CommandLinePathScannerWrapper(name) {
+) : CommandLinePathScannerWrapper() {
     companion object : Logging {
         const val SCANNER_NAME = "ScanCode"
 
@@ -103,9 +97,7 @@ class ScanCode internal constructor(
             ScanCode(name, scannerConfig)
     }
 
-    override val name = SCANNER_NAME
     override val criteria by lazy { ScannerCriteria.fromConfig(details, scannerConfig) }
-    override val expectedVersion = BuildConfig.SCANCODE_VERSION
 
     override val configuration by lazy {
         buildList {
@@ -141,43 +133,6 @@ class ScanCode internal constructor(
         }.orEmpty()
     }
 
-    override fun bootstrap(): File {
-        val versionWithoutHyphen = expectedVersion.replace("-", "")
-        val unpackDir = ortToolsDirectory.resolve(name).resolve(expectedVersion)
-        val scannerDir = unpackDir.resolve("scancode-toolkit-$versionWithoutHyphen")
-
-        if (scannerDir.resolve(command()).isFile) {
-            logger.info { "Skipping to bootstrap $name as it was found in $unpackDir." }
-            return scannerDir
-        }
-
-        val archive = when {
-            // Use the .zip file despite it being slightly larger than the .tar.gz file here as the latter for some
-            // reason does not complete to unpack on Windows.
-            Os.isWindows -> "v$versionWithoutHyphen.zip"
-            else -> "v$versionWithoutHyphen.tar.gz"
-        }
-
-        // Use the source code archive instead of the release artifact from S3 to enable OkHttp to cache the download
-        // locally. For details see https://github.com/square/okhttp/issues/4355#issuecomment-435679393.
-        val url = "https://github.com/nexB/scancode-toolkit/archive/$archive"
-
-        // Download ScanCode to a file instead of unpacking directly from the response body as doing so on the > 200 MiB
-        // archive causes issues.
-        logger.info { "Downloading $name from $url... " }
-        unpackDir.safeMkdirs()
-        val scannerArchive = OkHttpClientHelper.downloadFile(url, unpackDir).getOrThrow()
-
-        logger.info { "Unpacking '$scannerArchive' to '$unpackDir'... " }
-        scannerArchive.unpack(unpackDir)
-
-        if (!scannerArchive.delete()) {
-            logger.warn { "Unable to delete temporary file '$scannerArchive'." }
-        }
-
-        return scannerDir
-    }
-
     override fun scanPath(path: File, context: ScanContext): ScanSummary {
         val resultFile = createOrtTempDir().resolve("result.json")
         val process = runScanCode(path, resultFile)
@@ -211,8 +166,7 @@ class ScanCode internal constructor(
     internal fun runScanCode(
         path: File,
         resultFile: File
-    ) = ProcessCapture(
-        scannerPath.absolutePath,
+    ) = run(
         *commandLineOptions.toTypedArray(),
         path.absolutePath,
         OUTPUT_FORMAT_OPTION,

build.gradle.kts
@@ -40,7 +40,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 @Suppress("DSL_SCOPE_VIOLATION") // See https://youtrack.jetbrains.com/issue/KTIJ-19369.
 plugins {
-    alias(libs.plugins.buildConfig)
     alias(libs.plugins.detekt)
     alias(libs.plugins.dokka)
     alias(libs.plugins.ideaExt)
@@ -139,7 +138,6 @@ allprojects {
         mavenCentral()
     }
 
-    apply(plugin = "com.github.gmazzo.buildconfig")
     apply(plugin = "io.gitlab.arturbosch.detekt")
 
     // Note: Kotlin DSL cannot directly access configurations that are created by applying a plugin in the very same

gradle.properties
@@ -27,12 +27,6 @@ kotlin.code.style = official
 # issue, see https://youtrack.jetbrains.com/issue/KT-41642.
 kotlin.stdlib.default.dependency = false
 
-# Declare the path scanner versions to bootstrap if no version is present in the PATH environment.
-askalonoVersion = 0.4.6
-boyterLcVersion = 1.3.1
-licenseeVersion = 9.15.1
-scancodeVersion = 31.2.1
-
 # The version of the SPDX license list which is used to import license texts and generate SPDX enums. Must be a valid
 # tag, see https://github.com/spdx/license-list-data/tags.
 spdxLicenseListVersion = 3.18

gradle/libs.versions.toml
@@ -1,5 +1,4 @@
 [versions]
-buildConfigPlugin = "3.1.0"
 detektPlugin = "1.22.0"
 dokkaPlugin = "1.7.20"
 downloadPlugin = "5.3.0"
@@ -64,7 +63,6 @@ wiremock = "2.35.0"
 xz = "1.9"
 
 [plugins]
-buildConfig = { id = "com.github.gmazzo.buildconfig", version.ref = "buildConfigPlugin" }
 detekt = { id = "io.gitlab.arturbosch.detekt", version.ref = "detektPlugin" }
 dokka = { id = "org.jetbrains.dokka", version.ref = "dokkaPlugin" }
 download = { id = "de.undercouch.download", version.ref = "downloadPlugin" }

scanner/build.gradle.kts
@@ -65,15 +65,6 @@ dependencies {
     testImplementation(libs.wiremock)
 }
 
-buildConfig {
-    packageName("org.ossreviewtoolkit.scanner")
-
-    buildConfigField("String", "ASKALONO_VERSION", "\"$askalonoVersion\"")
-    buildConfigField("String", "BOYTER_LC_VERSION", "\"$boyterLcVersion\"")
-    buildConfigField("String", "LICENSEE_VERSION", "\"$licenseeVersion\"")
-    buildConfigField("String", "SCANCODE_VERSION", "\"$scancodeVersion\"")
-}
-
 tasks.withType<KotlinCompile>().configureEach {
     val customCompilerArgs = listOf(
         "-opt-in=kotlinx.serialization.ExperimentalSerializationApi"

scanner/src/main/kotlin/CommandLinePathScannerWrapper.kt
@@ -20,95 +20,24 @@
 package org.ossreviewtoolkit.scanner
 
 import java.io.File
-import java.io.IOException
-
-import kotlin.time.measureTimedValue
 
 import org.apache.logging.log4j.kotlin.Logging
 
 import org.ossreviewtoolkit.model.ScannerDetails
 import org.ossreviewtoolkit.utils.common.CommandLineTool
-import org.ossreviewtoolkit.utils.common.Os
 
 /**
  * A [PathScannerWrapper] that is executed as a [CommandLineTool] on the local machine.
  */
-abstract class CommandLinePathScannerWrapper(
-    scannerName: String
-) : PathScannerWrapper, CommandLineTool {
+abstract class CommandLinePathScannerWrapper : PathScannerWrapper, CommandLineTool {
     companion object : Logging
-
-    /**
-     * The directory the scanner was bootstrapped to, if so.
-     */
-    private val scannerDir by lazy {
-        val scannerExe = command()
-
-        Os.getPathFromEnvironment(scannerExe)?.parentFile?.also {
-            val actualVersion = getVersion(it)
-            if (actualVersion != expectedVersion) {
-                logger.warn {
-                    "ORT is currently tested with $name version $expectedVersion, but you are using version " +
-                            "$actualVersion. This could lead to problems with parsing the $name output."
-                }
-            }
-        } ?: run {
-            if (scannerExe.isNotEmpty()) {
-                logger.info {
-                    "Bootstrapping scanner '$scannerName' as expected version $expectedVersion was not found in PATH."
-                }
-
-                val (bootstrapDirectory, duration) = measureTimedValue {
-                    bootstrap().also {
-                        val actualVersion = getVersion(it)
-                        if (actualVersion != expectedVersion) {
-                            throw IOException(
-                                "Bootstrapped scanner version $actualVersion does not match expected version " +
-                                        "$expectedVersion."
-                            )
-                        }
-                    }
-                }
-
-                logger.info { "Bootstrapped scanner '$scannerName' version $expectedVersion in $duration." }
-
-                bootstrapDirectory
-            } else {
-                logger.info { "Skipping to bootstrap scanner '$scannerName' as it has no executable." }
-
-                File("")
-            }
-        }
-    }
-
-    /**
-     * The expected version of the scanner. This is also the version that would get bootstrapped.
-     */
-    protected abstract val expectedVersion: String
-
-    /**
-     * The full path to the scanner executable.
-     */
-    protected val scannerPath by lazy { scannerDir.resolve(command()) }
 
     /**
      * The configuration used by the scanner, should contain command line options that influence the scan result.
      */
     abstract val configuration: String
 
-    /**
-     * The actual version of the scanner, or an empty string in case of failure.
-     */
-    val version by lazy { getVersion(scannerDir) }
-
-    override val details by lazy { ScannerDetails(name, version, configuration) }
-
-    /**
-     * Bootstrap the scanner to be ready for use, like downloading and / or configuring it.
-     *
-     * @return The directory the scanner is installed in.
-     */
-    protected open fun bootstrap(): File = throw NotImplementedError()
+    override val details by lazy { ScannerDetails(name, getVersion(), configuration) }
 
     /**
      * Return the invariant relative path of the [scanned file][scannedFilename] with respect to the

scanner/src/main/kotlin/scanners/Askalono.kt
@@ -33,22 +33,17 @@ import org.ossreviewtoolkit.model.config.DownloaderConfiguration
 import org.ossreviewtoolkit.model.config.ScannerConfiguration
 import org.ossreviewtoolkit.model.jsonMapper
 import org.ossreviewtoolkit.scanner.AbstractScannerWrapperFactory
-import org.ossreviewtoolkit.scanner.BuildConfig
 import org.ossreviewtoolkit.scanner.CommandLinePathScannerWrapper
 import org.ossreviewtoolkit.scanner.ScanContext
 import org.ossreviewtoolkit.scanner.ScanException
 import org.ossreviewtoolkit.scanner.ScannerCriteria
 import org.ossreviewtoolkit.utils.common.Os
-import org.ossreviewtoolkit.utils.common.ProcessCapture
-import org.ossreviewtoolkit.utils.common.unpackZip
-import org.ossreviewtoolkit.utils.ort.OkHttpClientHelper
-import org.ossreviewtoolkit.utils.ort.ortToolsDirectory
 import org.ossreviewtoolkit.utils.spdx.calculatePackageVerificationCode
 
 class Askalono internal constructor(
-    name: String,
+    override val name: String,
     private val scannerConfig: ScannerConfiguration
-) : CommandLinePathScannerWrapper(name) {
+) : CommandLinePathScannerWrapper() {
     companion object : Logging
 
     class Factory : AbstractScannerWrapperFactory<Askalono>("Askalono") {
@@ -56,9 +51,7 @@ class Askalono internal constructor(
             Askalono(name, scannerConfig)
     }
 
-    override val name = "Askalono"
     override val criteria by lazy { ScannerCriteria.fromConfig(details, scannerConfig) }
-    override val expectedVersion = BuildConfig.ASKALONO_VERSION
     override val configuration = ""
 
     override fun command(workingDir: File?) =
@@ -69,38 +62,10 @@ class Askalono internal constructor(
         // askalono 0.2.0-beta.1
         output.removePrefix("askalono ")
 
-    override fun bootstrap(): File {
-        val unpackDir = ortToolsDirectory.resolve(name).resolve(expectedVersion)
-
-        if (unpackDir.resolve(command()).isFile) {
-            logger.info { "Skipping to bootstrap $name as it was found in $unpackDir." }
-            return unpackDir
-        }
-
-        val platform = when {
-            Os.isLinux -> "Linux"
-            Os.isMac -> "macOS"
-            Os.isWindows -> "Windows"
-            else -> throw IllegalArgumentException("Unsupported operating system.")
-        }
-
-        val archive = "askalono-$platform.zip"
-        val url = "https://github.com/amzn/askalono/releases/download/$expectedVersion/$archive"
-
-        logger.info { "Downloading $name from $url... " }
-        val (_, body) = OkHttpClientHelper.download(url).getOrThrow()
-
-        logger.info { "Unpacking '$archive' to '$unpackDir'... " }
-        body.bytes().unpackZip(unpackDir)
-
-        return unpackDir
-    }
-
     override fun scanPath(path: File, context: ScanContext): ScanSummary {
         val startTime = Instant.now()
 
-        val process = ProcessCapture(
-            scannerPath.absolutePath,
+        val process = run(
             "--format", "json",
             "crawl", path.absolutePath
         )

scanner/src/main/kotlin/scanners/BoyterLc.kt
@@ -33,24 +33,19 @@ import org.ossreviewtoolkit.model.config.DownloaderConfiguration
 import org.ossreviewtoolkit.model.config.ScannerConfiguration
 import org.ossreviewtoolkit.model.readTree
 import org.ossreviewtoolkit.scanner.AbstractScannerWrapperFactory
-import org.ossreviewtoolkit.scanner.BuildConfig
 import org.ossreviewtoolkit.scanner.CommandLinePathScannerWrapper
 import org.ossreviewtoolkit.scanner.ScanContext
 import org.ossreviewtoolkit.scanner.ScanException
 import org.ossreviewtoolkit.scanner.ScannerCriteria
 import org.ossreviewtoolkit.utils.common.Os
-import org.ossreviewtoolkit.utils.common.ProcessCapture
 import org.ossreviewtoolkit.utils.common.safeDeleteRecursively
-import org.ossreviewtoolkit.utils.common.unpackZip
-import org.ossreviewtoolkit.utils.ort.OkHttpClientHelper
 import org.ossreviewtoolkit.utils.ort.createOrtTempDir
-import org.ossreviewtoolkit.utils.ort.ortToolsDirectory
 import org.ossreviewtoolkit.utils.spdx.calculatePackageVerificationCode
 
 class BoyterLc internal constructor(
-    name: String,
+    override val name: String,
     private val scannerConfig: ScannerConfiguration
-) : CommandLinePathScannerWrapper(name) {
+) : CommandLinePathScannerWrapper() {
     companion object : Logging {
         val CONFIGURATION_OPTIONS = listOf(
             "--confidence", "0.95", // Cut-off value to only get most relevant matches.
@@ -63,9 +58,7 @@ class BoyterLc internal constructor(
             BoyterLc(name, scannerConfig)
     }
 
-    override val name = "BoyterLc"
     override val criteria by lazy { ScannerCriteria.fromConfig(details, scannerConfig) }
-    override val expectedVersion = BuildConfig.BOYTER_LC_VERSION
     override val configuration = CONFIGURATION_OPTIONS.joinToString(" ")
 
     override fun command(workingDir: File?) =
@@ -76,39 +69,11 @@ class BoyterLc internal constructor(
         // licensechecker version 1.1.1
         output.removePrefix("licensechecker version ")
 
-    override fun bootstrap(): File {
-        val unpackDir = ortToolsDirectory.resolve(name).resolve(expectedVersion)
-
-        if (unpackDir.resolve(command()).isFile) {
-            logger.info { "Skipping to bootstrap $name as it was found in $unpackDir." }
-            return unpackDir
-        }
-
-        val platform = when {
-            Os.isLinux -> "x86_64-unknown-linux"
-            Os.isMac -> "x86_64-apple-darwin"
-            Os.isWindows -> "x86_64-pc-windows"
-            else -> throw IllegalArgumentException("Unsupported operating system.")
-        }
-
-        val archive = "lc-$expectedVersion-$platform.zip"
-        val url = "https://github.com/boyter/lc/releases/download/v$expectedVersion/$archive"
-
-        logger.info { "Downloading $name from $url... " }
-        val (_, body) = OkHttpClientHelper.download(url).getOrThrow()
-
-        logger.info { "Unpacking '$archive' to '$unpackDir'... " }
-        body.bytes().unpackZip(unpackDir)
-
-        return unpackDir
-    }
-
     override fun scanPath(path: File, context: ScanContext): ScanSummary {
         val startTime = Instant.now()
 
         val resultFile = createOrtTempDir().resolve("result.json")
-        val process = ProcessCapture(
-            scannerPath.absolutePath,
+        val process = run(
             *CONFIGURATION_OPTIONS.toTypedArray(),
             "--output", resultFile.absolutePath,
             path.absolutePath

commit 653146a88de5d68fc7a4d1a415b1be68b2b6ddb0

ScanCode: Do not require success when running ScanCode
This corrects a regression that was introduced with 721ee9d: When
ScanCode runs into a timeout, it returns with exit code 1 (also see e.g.
[1]). However, throwing an exception in this case prevents ORT from
running `mapTimeoutErrors()` in order to create ORT issues.

[1]: https://github.com/nexB/scancode-toolkit/issues/3174

Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

scanner/src/main/kotlin/scanners/scancode/ScanCode.kt
@@ -36,6 +36,7 @@ import org.ossreviewtoolkit.scanner.ScanContext
 import org.ossreviewtoolkit.scanner.ScanResultsStorage
 import org.ossreviewtoolkit.scanner.ScannerCriteria
 import org.ossreviewtoolkit.utils.common.Os
+import org.ossreviewtoolkit.utils.common.ProcessCapture
 import org.ossreviewtoolkit.utils.common.isTrue
 import org.ossreviewtoolkit.utils.common.safeDeleteRecursively
 import org.ossreviewtoolkit.utils.common.splitOnWhitespace
@@ -166,7 +167,8 @@ class ScanCode internal constructor(
     internal fun runScanCode(
         path: File,
         resultFile: File
-    ) = run(
+    ) = ProcessCapture(
+        command(),
         *commandLineOptions.toTypedArray(),
         path.absolutePath,
         OUTPUT_FORMAT_OPTION,