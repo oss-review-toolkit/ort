2e0c2741d02f66bfcbbb4d8fb6aec80b1a18c9f9
Sebastian Schuberth <sschuberth@gmail.com>
2023-02-17 13:30:53 +0100

    refactor(PackageManagerTest): Simplify the creation of `VcsType`s
    Use the single-parameter constructor.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- analyzer/src/test/kotlin/PackageManagerTest.kt
+++ analyzer/src/test/kotlin/PackageManagerTest.kt
@@ -234,13 +234,13 @@ class PackageManagerTest : WordSpec({
 
         "maintain an unknown VCS type" {
             val vcsFromPackage = VcsInfo(
-                type = VcsType(listOf("darcs")),
+                type = VcsType("darcs"),
                 url = "http://hub.darcs.net/ross/transformers",
                 revision = ""
             )
 
             PackageManager.processPackageVcs(vcsFromPackage) shouldBe VcsInfo(
-                type = VcsType(listOf("darcs")),
+                type = VcsType("darcs"),
                 url = "http://hub.darcs.net/ross/transformers",
                 revision = ""
             )
