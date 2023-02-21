a0491f5e397fae4bb8087c4e1bc8e05425fc0323
Frank Viernau <frank_viernau@epam.com>
2023-02-21 14:58:59 +0100

    feat(GoMod): Obtain the VCS info from the Go tooling
    Previously, ORT's GoMod integration had a rudimentary
    (re-)implementation of the Go toolings' logic for resolving the VCS infos
    for the dependencies, covering only the more common use cases. For
    example, the implementation is VCS host specific and cannot handle mono
    repository setups at all, without an ugly workaround in the downloader,
    see [1]. Furthermore, I expect many not yet known issues in the more
    uncommon use cases.
    
    Avoid all these issues by just relying on the VCS info resolution of the
    Go tooling.
    
    Note that as of Go 1.19, the `.info` files under '$GOPATH/pkg/mod' are
    guaranteed to contain the VCS info of the modules in case no Go proxy
    is used [2]. For now that information is only accessible by parsing the
    files directly, but there are plans to make this information available
    via the command line tools like `go list -json` [3].
    
    Fixes: #5555.
    
    [1]: https://github.com/oss-review-toolkit/ort/blob/1dc5c54de3630f0f1249a7ec56ce0a3ba87ac5f1/downloader/src/main/kotlin/VersionControlSystem.kt#L361-L366
    [2]: https://github.com/golang/go/issues/44742#issuecomment-1210986778
    [3]: https://github.com/golang/go/issues/18387
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- analyzer/src/test/kotlin/managers/GoModTest.kt
+++ analyzer/src/test/kotlin/managers/GoModTest.kt
@@ -23,88 +23,8 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.collections.beEmpty
 import io.kotest.matchers.collections.containExactlyInAnyOrder
 import io.kotest.matchers.should
-import io.kotest.matchers.shouldBe
-
-import org.ossreviewtoolkit.model.VcsType
 
 class GoModTest : WordSpec({
-    "toVcsInfo" should {
-        "return the VCS type 'Git'" {
-            val moduleInfo = GoMod.ModuleInfo(
-                path = "github.com/chai2010/gettext-go",
-                version = "v1.0.0"
-            )
-
-            moduleInfo.toVcsInfo().type shouldBe VcsType.GIT
-        }
-
-        "return the revision" {
-            val moduleInfo = GoMod.ModuleInfo(
-                path = "github.com/chai2010/gettext-go",
-                version = "v1.0.0"
-            )
-
-            moduleInfo.toVcsInfo().revision shouldBe "v1.0.0"
-        }
-
-        "return the VCS URL and path for a package from a single module repository" {
-            val moduleInfo = GoMod.ModuleInfo(path = "github.com/chai2010/gettext-go", version = "v1.0.0")
-
-            with(moduleInfo.toVcsInfo()) {
-                path shouldBe ""
-                url shouldBe "https://github.com/chai2010/gettext-go.git"
-            }
-        }
-
-        "return the VCS URL and path for a package from a mono repository" {
-            val moduleInfo = GoMod.ModuleInfo(path = "github.com/Azure/go-autorest/autorest/date", version = "v0.1.0")
-
-            with(moduleInfo.toVcsInfo()) {
-                path shouldBe "autorest/date"
-                url shouldBe "https://github.com/Azure/go-autorest.git"
-            }
-        }
-
-        "return the SHA1 from a 'pseudo version', when there is no known base version" {
-            // See https://golang.org/ref/mod#pseudo-versions.
-            val moduleInfo = GoMod.ModuleInfo(
-                path = "github.com/example/project",
-                version = "v0.0.0-20191109021931-daa7c04131f5"
-            )
-
-            moduleInfo.toVcsInfo().revision shouldBe "daa7c04131f5"
-        }
-
-        "return the SHA1 from a 'pseudo version', when base version is a release version" {
-            // See https://golang.org/ref/mod#pseudo-versions.
-            val moduleInfo = GoMod.ModuleInfo(
-                path = "github.com/example/project",
-                version = "v0.8.1-0.20171018195549-f15c970de5b7"
-            )
-
-            moduleInfo.toVcsInfo().revision shouldBe "f15c970de5b7"
-        }
-
-        "return the SHA1 from a 'pseudo version', when base version is a pre-release version" {
-            // See https://golang.org/ref/mod#pseudo-versions.
-            val moduleInfo = GoMod.ModuleInfo(
-                path = "github.com/example/project",
-                version = "v0.8.1-pre.0.20171018195549-f15c970de5b7"
-            )
-
-            moduleInfo.toVcsInfo().revision shouldBe "f15c970de5b7"
-        }
-
-        "return the SHA1 for a version with a '+incompatible' suffix" {
-            val moduleInfo = GoMod.ModuleInfo(
-                path = "github.com/example/project",
-                version = "v43.3.0+incompatible"
-            )
-
-            moduleInfo.toVcsInfo().revision shouldBe "v43.3.0"
-        }
-    }
-
     "parseWhyOutput" should {
         "detect packages that are used" {
             val output = """
