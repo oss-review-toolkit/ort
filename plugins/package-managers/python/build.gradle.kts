f13eff8b903306f5530bc4aab461d547bfd4180f
Sebastian Schuberth <sschuberth@gmail.com>
2023-03-29 08:53:14 +0200

    build(bower,python): Explain why some dependencies are necessary
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- plugins/package-managers/python/build.gradle.kts
+++ plugins/package-managers/python/build.gradle.kts
@@ -31,10 +31,14 @@ plugins {
 dependencies {
     api(project(":analyzer"))
     api(project(":model"))
-    api(project(":utils:common-utils"))
+    api(project(":utils:common-utils")) {
+        because("This is a CommandLineTool.")
+    }
 
     api(libs.log4jApiKotlin)
-    api(libs.semver4j)
+    api(libs.semver4j) {
+        because("This is a CommandLineTool.")
+    }
 
     implementation(project(":downloader"))
     implementation(project(":utils:ort-utils"))
