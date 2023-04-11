d4e272113ba21458d6afe42addcf0ad1c03b3824
Sebastian Schuberth <sschuberth@gmail.com>
2023-04-11 12:11:48 +0200

    build(plugins): Start a platform project for reporter plugins
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -145,6 +145,7 @@ dependencies {
 
     implementation(platform(project(":plugins:package-curation-providers")))
     implementation(platform(project(":plugins:package-managers")))
+    implementation(platform(project(":plugins:reporters")))
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation(libs.bundles.exposed)
