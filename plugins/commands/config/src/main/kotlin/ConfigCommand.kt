7ef0192a68f63e51994adb726d86085d39f14d0f
Sebastian Schuberth <sschuberth@gmail.com>
2023-05-02 12:41:47 +0200

    build(cli): Move the config command to its own Gradle project
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- plugins/commands/config/src/main/kotlin/ConfigCommand.kt
+++ plugins/commands/config/src/main/kotlin/ConfigCommand.kt
@@ -17,7 +17,7 @@
  * License-Filename: LICENSE
  */
 
-package org.ossreviewtoolkit.cli.commands
+package org.ossreviewtoolkit.plugins.commands.config
 
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
