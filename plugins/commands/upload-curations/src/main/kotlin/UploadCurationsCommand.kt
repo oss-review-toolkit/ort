32a7f5fb9c3adcc3526d1280ddf9b1c7b478f678
Sebastian Schuberth <sschuberth@gmail.com>
2023-05-02 14:47:07 +0200

    build(cli): Move the upload-curations command to its own Gradle project
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- plugins/commands/upload-curations/src/main/kotlin/UploadCurationsCommand.kt
+++ plugins/commands/upload-curations/src/main/kotlin/UploadCurationsCommand.kt
@@ -17,7 +17,7 @@
  * License-Filename: LICENSE
  */
 
-package org.ossreviewtoolkit.cli.commands
+package org.ossreviewtoolkit.plugins.commands.uploadcurations
 
 import com.github.ajalt.clikt.core.ProgramResult
 import com.github.ajalt.clikt.parameters.options.convert
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -47,6 +47,7 @@ include(":plugins:commands:notifier")
 include(":plugins:commands:reporter")
 include(":plugins:commands:requirements")
 include(":plugins:commands:scanner")
+include(":plugins:commands:upload-curations")
 include(":plugins:package-curation-providers")
 include(":plugins:package-curation-providers:api")
 include(":plugins:package-curation-providers:clearly-defined")
@@ -112,6 +113,7 @@ project(":plugins:commands:notifier").name = "notifier-command"
 project(":plugins:commands:reporter").name = "reporter-command"
 project(":plugins:commands:requirements").name = "requirements-command"
 project(":plugins:commands:scanner").name = "scanner-command"
+project(":plugins:commands:upload-curations").name = "upload-curations-command"
 
 project(":plugins:package-curation-providers:api").name = "package-curation-provider-api"
 project(":plugins:package-curation-providers:clearly-defined").name = "clearly-defined-package-curation-provider"
