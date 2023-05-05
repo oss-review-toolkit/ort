07869f31ac1780fd336c88c0291b4944b26ea85b
Frank Viernau <frank_viernau@epam.com>
2023-05-05 11:34:07 +0200

    feat(scanner): Add a model for file listings
    This is supposed to be used for storing the list of file paths along
    with SHA1 sums for each resolved provenance in a
    `ProvenanceFileStorage`. The first use case this targets is producing
    SBOMs which include file paths and SHA1 sums for the file findings and
    later potentially for all files.
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

---
+++ scanner/src/main/kotlin/FileListing.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.scanner
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.util.StdConverter
+
+import java.util.SortedSet
+
+import org.ossreviewtoolkit.scanner.FileListing.FileEntry
+import org.ossreviewtoolkit.utils.common.StringSortedSetConverter
+import org.ossreviewtoolkit.utils.common.getDuplicates
+
+/**
+ * The model to store a file listing for a resolved provenance.
+ */
+internal data class FileListing(
+    /**
+     * The set of glob expressions which have been used to match directories to be excluded from the file listing.
+     */
+    @JsonSerialize(converter = StringSortedSetConverter::class)
+    val ignorePatterns: Set<String>,
+
+    /**
+     * The set of files contained in the resolved provenance, excluding files which are within a directory ignored by
+     * [ignorePatterns].
+     */
+    @JsonSerialize(converter = FileEntrySortedSetConverter::class)
+    val files: Set<FileEntry>
+) {
+    data class FileEntry constructor(
+        val path: String,
+        val sha1: String
+    )
+
+    init {
+        val duplicates = files.getDuplicates { it.path }.keys
+
+        require(duplicates.isEmpty()) {
+            "The file listing contains duplicate paths which is not allowed: ${duplicates.joinToString()}."
+        }
+    }
+}
+
+private class FileEntrySortedSetConverter : StdConverter<Set<FileEntry>, SortedSet<FileEntry>>() {
+    override fun convert(value: Set<FileEntry>) = value.toSortedSet(compareBy { it.path })
+}
