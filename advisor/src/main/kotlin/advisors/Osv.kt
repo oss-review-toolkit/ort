6fdb7e31489e72503dba1d718b8d4c53b86cf3a0
Sebastian Schuberth <sschuberth@gmail.com>
2023-05-11 13:21:55 +0200

    feat(Osv): Also pass the PURL if known
    The optional PURL is recommended to be specified if known, see [1].
    
    [1]: https://ossf.github.io/osv-schema/#affectedpackage-field
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- advisor/src/main/kotlin/advisors/Osv.kt
+++ advisor/src/main/kotlin/advisors/Osv.kt
@@ -158,7 +158,8 @@ private fun createRequest(pkg: Package): VulnerabilitiesForPackageRequest? {
         return VulnerabilitiesForPackageRequest(
             pkg = org.ossreviewtoolkit.clients.osv.Package(
                 name = name,
-                ecosystem = ecosystem
+                ecosystem = ecosystem,
+                purl = pkg.purl.takeUnless { it.isEmpty() }
             ),
             version = pkg.id.version
         )
