3fe4c5b4447e33e4ed39020c92b433627850cff0
Sebastian Schuberth <sschuberth@gmail.com>
2023-05-30 12:18:30 +0200

    build(Gradle): Apply a work-around for precompiled plugin scripts
    Allow precompiled plugin scripts to access the version catalog, see [1].
    
    [1]: https://github.com/gradle/gradle/issues/15383
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -17,21 +17,10 @@
  * License-Filename: LICENSE
  */
 
-import org.gradle.accessors.dm.LibrariesForLibs
-
-private val Project.libs: LibrariesForLibs
-    get() = extensions.getByType()
-
-plugins {
-    // Use Kotlin DSL to write precompiled script plugins.
-    `kotlin-dsl`
-}
-
-repositories {
-    // Allow to resolve external plugins from precompiled script plugins.
-    gradlePluginPortal()
-}
-
-dependencies {
-    implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
 }
