23f21dfc5330d028ef3430ba929bc7e3fbfa36ca
Sebastian Schuberth <sschuberth@gmail.com>
2023-05-30 23:28:13 +0200

    docs(spdx-reporter): Document the extension functions
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- plugins/reporters/spdx/src/main/kotlin/Extensions.kt
+++ plugins/reporters/spdx/src/main/kotlin/Extensions.kt
@@ -46,12 +46,19 @@ import org.ossreviewtoolkit.utils.spdx.toSpdxId
  */
 internal fun Identifier.toSpdxId(infix: String) = "${SpdxConstants.REF_PREFIX}$infix-${toCoordinates()}".toSpdxId()
 
+/**
+ * Get the text with all Copyright statements associated with the package of the given [id], or return `NONE` if there
+ * are no associated Copyright statements.
+ */
 private fun LicenseInfoResolver.getSpdxCopyrightText(id: Identifier): String {
     val copyrightStatements = resolveLicenseInfo(id).flatMapTo(sortedSetOf()) { it.getCopyrights() }
     if (copyrightStatements.isEmpty()) return SpdxConstants.NONE
     return copyrightStatements.joinToString("\n")
 }
 
+/**
+ * Return all SPDX external references contained in the metadata of the ORT package.
+ */
 private fun Package.toSpdxExternalReferences(): List<SpdxExternalReference> {
     val externalRefs = mutableListOf<SpdxExternalReference>()
 
@@ -77,6 +84,10 @@ private fun Package.toSpdxExternalReferences(): List<SpdxExternalReference> {
     return externalRefs
 }
 
+/**
+ * Convert this ORT package to an SPDX package using the information from [licenseInfoResolver]. If [isProject] is
+ * `true` then the package is treated as a project.
+ */
 internal fun Package.toSpdxPackage(licenseInfoResolver: LicenseInfoResolver, isProject: Boolean = false) =
     SpdxPackage(
         spdxId = id.toSpdxId(if (isProject) "Project" else "Package"),
@@ -101,6 +112,9 @@ internal fun Package.toSpdxPackage(licenseInfoResolver: LicenseInfoResolver, isP
         versionInfo = id.version
     )
 
+/**
+ * Convert processed declared licenses to SPDX. Unmapped licenses are represented as `NOASSERTION`.
+ */
 private fun ProcessedDeclaredLicense.toSpdxDeclaredLicense(): String =
     when {
         // If there are unmapped licenses, represent this by adding NOASSERTION.
@@ -117,12 +131,18 @@ private fun ProcessedDeclaredLicense.toSpdxDeclaredLicense(): String =
         else -> spdxExpression.nullOrBlankToSpdxNoassertionOrNone()
     }
 
+/**
+ * Wrap a scan summary's package verification code in a [SpdxPackageVerificationCode].
+ */
 internal fun ScanResult.toSpdxPackageVerificationCode(): SpdxPackageVerificationCode =
     SpdxPackageVerificationCode(
         packageVerificationCodeExcludedFiles = emptyList(),
         packageVerificationCodeValue = summary.packageVerificationCode
     )
 
+/**
+ * Use [licenseTextProvider] to add the license texts for all packages to the [SpdxDocument].
+ */
 internal fun SpdxDocument.addExtractedLicenseInfo(licenseTextProvider: LicenseTextProvider): SpdxDocument {
     val nonSpdxLicenses = packages.flatMapTo(mutableSetOf()) {
         // TODO: Also add detected non-SPDX licenses here.
@@ -143,6 +163,9 @@ internal fun SpdxDocument.addExtractedLicenseInfo(licenseTextProvider: LicenseTe
     return copy(hasExtractedLicensingInfos = extractedLicenseInfo)
 }
 
+/**
+ * Convert an [SpdxExpression] to `NOASSERTION` if null, to `NONE` if blank, or to its string representation otherwise.
+ */
 private fun SpdxExpression?.nullOrBlankToSpdxNoassertionOrNone(): String =
     when {
         this == null -> SpdxConstants.NOASSERTION
@@ -150,8 +173,14 @@ private fun SpdxExpression?.nullOrBlankToSpdxNoassertionOrNone(): String =
         else -> toString()
     }
 
+/**
+ * Convert a null or blank [String] to `NONE`.
+ */
 internal fun String?.nullOrBlankToSpdxNone(): String = if (isNullOrBlank()) SpdxConstants.NONE else this
 
+/**
+ * Create an SPDX download location string from [VcsInfo] and an optional [resolvedRevision].
+ */
 internal fun VcsInfo.toSpdxDownloadLocation(resolvedRevision: String?): String {
     val vcsTool = when (type) {
         VcsType.GIT -> "git"
