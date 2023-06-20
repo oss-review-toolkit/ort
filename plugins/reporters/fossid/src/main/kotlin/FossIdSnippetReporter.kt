9559abfc2bfc71b597e5f77cddd3b0a80bf311b9
Nicolas Nobelis <nicolas.nobelis@bosch.io>
2023-06-20 09:29:58 +0200

    feat(reporter): Implement the FossID snippet reporter
    This is an HTML report to present the snippet findings.
    HTML format was chosen to be able to use AsciiDoc collapsible blocks [1]
    for how-to-fix hints. Such blocks will be added in a future commit.
    
    [1]: https://docs.asciidoctor.org/asciidoc/latest/blocks/collapsible/
    
    Signed-off-by: Nicolas Nobelis <nicolas.nobelis@bosch.io>

---
+++ plugins/reporters/fossid/src/main/kotlin/FossIdSnippetReporter.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.plugins.reporters.fossid
+
+import java.io.File
+
+import org.apache.logging.log4j.kotlin.Logging
+
+import org.ossreviewtoolkit.plugins.reporters.asciidoc.HtmlTemplateReporter
+import org.ossreviewtoolkit.plugins.reporters.freemarker.FreemarkerTemplateProcessor
+import org.ossreviewtoolkit.reporter.Reporter
+import org.ossreviewtoolkit.reporter.ReporterInput
+
+class FossIdSnippetReporter : Reporter by delegateReporter {
+    companion object : Logging {
+        private const val TEMPLATE_NAME = "fossid_snippet"
+
+        val delegateReporter = HtmlTemplateReporter()
+    }
+
+    override val type = "FossIdSnippet"
+
+    override fun generateReport(input: ReporterInput, outputDir: File, options: Map<String, String>): List<File> {
+        val extendedOptions = options + (FreemarkerTemplateProcessor.OPTION_TEMPLATE_ID to TEMPLATE_NAME)
+        return delegateReporter.generateReport(input, outputDir, extendedOptions)
+    }
+}
