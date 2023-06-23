96ba67ffef249e56627f5ba82663dd5cc379e057
Frank Viernau <frank_viernau@epam.com>
2023-06-23 10:56:14 +0200

    test(scanner): Switch to a test asset which is fully under our control
    Previously, the `ScannerIntegrationTest` scanned 3rd party Open Source
    projects like `commons-lang3` and `junit`. Switch to a new analyzer
    result as input which solely contains packages with [1] as repository
    provenance, which has been setup solely for use from
    `ScannerIntegrationTest`. Main benefits are:
    
    1. It allows to hand-craft all needed corner cases. For example,
       provenances with sub-repositories are currently not covered.
    2. The scanned data is minimized, so the test and `git clone` executes
       faster and the tests itself are less redundant.
    
    While at it, create the new test asset programatically in order to
    remain flexible in the test asset construction, as further tests will be
    added in upcoming changes.
    
    [1] https://github.com/oss-review-toolkit/ort-test-data-scanner.git
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- scanner/src/funTest/kotlin/scanners/ScannerIntegrationFunTest.kt
+++ scanner/src/funTest/kotlin/scanners/ScannerIntegrationFunTest.kt
@@ -24,15 +24,23 @@ import io.kotest.matchers.should
 
 import java.io.File
 
+import org.ossreviewtoolkit.model.AnalyzerResult
+import org.ossreviewtoolkit.model.AnalyzerRun
+import org.ossreviewtoolkit.model.Identifier
 import org.ossreviewtoolkit.model.LicenseFinding
 import org.ossreviewtoolkit.model.OrtResult
+import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.PackageReference
 import org.ossreviewtoolkit.model.PackageType
+import org.ossreviewtoolkit.model.Project
 import org.ossreviewtoolkit.model.ScanSummary
 import org.ossreviewtoolkit.model.ScannerDetails
+import org.ossreviewtoolkit.model.Scope
 import org.ossreviewtoolkit.model.TextLocation
+import org.ossreviewtoolkit.model.VcsInfo
+import org.ossreviewtoolkit.model.VcsType
 import org.ossreviewtoolkit.model.config.DownloaderConfiguration
 import org.ossreviewtoolkit.model.config.ScannerConfiguration
-import org.ossreviewtoolkit.model.readValue
 import org.ossreviewtoolkit.model.toYaml
 import org.ossreviewtoolkit.scanner.PathScannerWrapper
 import org.ossreviewtoolkit.scanner.ScanContext
@@ -44,6 +52,7 @@ import org.ossreviewtoolkit.scanner.provenance.DefaultProvenanceDownloader
 import org.ossreviewtoolkit.scanner.provenance.DummyNestedProvenanceStorage
 import org.ossreviewtoolkit.scanner.provenance.DummyProvenanceStorage
 import org.ossreviewtoolkit.scanner.utils.DefaultWorkingTreeCache
+import org.ossreviewtoolkit.utils.common.VCS_DIRECTORIES
 import org.ossreviewtoolkit.utils.spdx.SpdxConstants
 import org.ossreviewtoolkit.utils.test.getAssetFile
 import org.ossreviewtoolkit.utils.test.matchExpectedResult
@@ -99,15 +108,78 @@ private fun createScanner(): Scanner {
     )
 }
 
-private fun createAnalyzerResult(): OrtResult =
-    getAssetFile("analyzer-result.yml").readValue()
+private fun createAnalyzerResult(): OrtResult {
+    val packages = setOf(pkg1, pkg2, pkg3)
+
+    val scope = Scope(
+        name = "deps",
+        dependencies = packages.mapTo(mutableSetOf()) { PackageReference(it.id) }
+    )
+
+    val project = Project.EMPTY.copy(
+        id = createId("project"),
+        scopeDependencies = setOf(scope)
+    )
+
+    val analyzerRun = AnalyzerRun.EMPTY.copy(
+        result = AnalyzerResult.EMPTY.copy(
+            projects = setOf(project),
+            packages = packages.toSet()
+        )
+    )
+
+    return OrtResult.EMPTY.copy(analyzer = analyzerRun)
+}
+
+private fun createId(name: String): Identifier =
+    Identifier("Dummy::$name:1.0.0")
+private fun createPackage(name: String, vcs: VcsInfo): Package =
+    Package.EMPTY.copy(
+        id = createId(name),
+        vcs = vcs,
+        vcsProcessed = vcs.normalize()
+    )
+
+private val pkg1 = createPackage(
+    name = "pkg1",
+    vcs = VcsInfo(
+        type = VcsType.GIT,
+        url = "https://github.com/oss-review-toolkit/ort-test-data-scanner.git",
+        revision = "f6d8b9b73d8dedbe5cf1212b797e09e4f8ea290c",
+        path = "pkg1"
+    )
+)
+
+private val pkg2 = createPackage(
+    name = "pkg2",
+    vcs = VcsInfo(
+        type = VcsType.GIT,
+        url = "https://github.com/oss-review-toolkit/ort-test-data-scanner.git",
+        revision = "f6d8b9b73d8dedbe5cf1212b797e09e4f8ea290c",
+        path = "pkg2"
+    )
+)
+
+private val pkg3 = createPackage(
+    name = "pkg3",
+    vcs = VcsInfo(
+        type = VcsType.GIT,
+        url = "https://github.com/oss-review-toolkit/ort-test-data-scanner.git",
+        revision = "f6d8b9b73d8dedbe5cf1212b797e09e4f8ea290c",
+        path = "pkg3"
+    )
+)
 
 private class DummyScanner : PathScannerWrapper {
     override val details = ScannerDetails(name = "Dummy", version = "1.0.0", configuration = "")
     override val criteria = ScannerCriteria.forDetails(details)
 
     override fun scanPath(path: File, context: ScanContext): ScanSummary {
-        val licenseFindings = path.listFiles().orEmpty().mapTo(mutableSetOf()) { file ->
+        val relevantFiles = path.walk()
+            .onEnter { it.name !in VCS_DIRECTORIES }
+            .filterTo(mutableSetOf()) { it.isFile }
+
+        val licenseFindings = relevantFiles.mapTo(mutableSetOf()) { file ->
             LicenseFinding(
                 license = SpdxConstants.NONE,
                 location = TextLocation(file.relativeTo(path).invariantSeparatorsPath, TextLocation.UNKNOWN_LINE)
