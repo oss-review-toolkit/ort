297a158c127a307a26dd687fa1f97ccda2ab416b
Marcel Bochtler <marcel.bochtler@bosch.com>
2023-07-17 13:03:50 +0200

    feat(Plugins): Add package-configuration provider for ort-config
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.com>

---
+++ plugins/package-configuration-providers/ort-config/src/test/kotlin/OrtConfigPackageConfigurationProviderFunTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.beEmpty
+import io.kotest.matchers.collections.haveSize
+import io.kotest.matchers.should
+
+import org.ossreviewtoolkit.model.ArtifactProvenance
+import org.ossreviewtoolkit.model.Hash
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.RemoteArtifact
+import org.ossreviewtoolkit.plugins.packageconfigurationproviders.ortconfig.OrtConfigPackageConfigurationProvider
+
+class OrtConfigPackageConfigurationProviderFunTest : StringSpec({
+    "provider can load package-configurations from the ort-config repository" {
+        val id = Identifier("Maven:net.java.dev.jna:jna-platform:5.8.0")
+        val provenance = ArtifactProvenance(
+            RemoteArtifact(
+                url = "https://repo.maven.apache.org/maven2/net/java/dev/jna/jna-platform/5.8.0/" +
+                        "jna-platform-5.8.0-sources.jar",
+                hash = Hash.NONE
+            )
+        )
+
+        val packageConfigurations = OrtConfigPackageConfigurationProvider().getPackageConfigurations(id, provenance)
+
+        packageConfigurations should haveSize(1)
+        packageConfigurations.filter { it.id == id } should haveSize(1)
+    }
+
+    "provider does not fail for packages which have no package-configurations" {
+        val id = Identifier("Maven:net.java.dev.jna:jna-platform:0.0.0")
+        val provenance = ArtifactProvenance(
+            RemoteArtifact(
+                url = "https://example.com/invalid/group/package/0.0.0/package-0.0.0-sources.jar",
+                hash = Hash.NONE
+            )
+        )
+
+        val packageConfigurations = OrtConfigPackageConfigurationProvider().getPackageConfigurations(id, provenance)
+
+        packageConfigurations should beEmpty()
+    }
+})
