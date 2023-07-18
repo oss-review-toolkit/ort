10bf84c2a51bbcfec60d666cbf0a177428758939
Sebastian Schuberth <sschuberth@gmail.com>
2023-07-18 10:36:07 +0200

    refactor(scanner)!: Always parse ScanCode license expressions
    Remove the feature to toggle parsing of ScanCode license expressions.
    Starting with ScanCode output format version 3, license expressions are
    first class citizens in ScanCode results, making this feature cumbersome
    to maintain.
    
    Note that the web-app report still would not show license expressions
    anyway [1].
    
    [1]: https://github.com/oss-review-toolkit/ort/issues/5017
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- plugins/scanners/scancode/src/main/kotlin/ScanCode.kt
+++ plugins/scanners/scancode/src/main/kotlin/ScanCode.kt
@@ -37,7 +37,6 @@ import org.ossreviewtoolkit.scanner.ScanResultsStorage
 import org.ossreviewtoolkit.scanner.ScannerCriteria
 import org.ossreviewtoolkit.utils.common.Os
 import org.ossreviewtoolkit.utils.common.ProcessCapture
-import org.ossreviewtoolkit.utils.common.isTrue
 import org.ossreviewtoolkit.utils.common.safeDeleteRecursively
 import org.ossreviewtoolkit.utils.common.splitOnWhitespace
 import org.ossreviewtoolkit.utils.common.withoutPrefix
@@ -56,9 +55,6 @@ import org.semver4j.RangesListFactory
  *   looking up results from the [ScanResultsStorage]. Defaults to [DEFAULT_CONFIGURATION_OPTIONS].
  * * **"commandLineNonConfig":** Command line options that do not modify the result and should therefore not be
  *   considered in [configuration], like "--processes". Defaults to [DEFAULT_NON_CONFIGURATION_OPTIONS].
- * * **"parseLicenseExpressions":** By default the license `key`, which can contain a single license id, is used for the
- *   detected licenses. If this option is set to "true", the detected `license_expression` is used instead, which can
- *   contain an SPDX expression.
  */
 class ScanCode internal constructor(
     name: String,
@@ -166,8 +162,7 @@ class ScanCode internal constructor(
     }
 
     override fun createSummary(result: String, startTime: Instant, endTime: Instant): ScanSummary {
-        val parseLicenseExpressions = scanCodeConfiguration["parseLicenseExpressions"].isTrue()
-        val summary = parseResult(result).toScanSummary(parseLicenseExpressions)
+        val summary = parseResult(result).toScanSummary()
 
         val issues = summary.issues.toMutableList()
 
