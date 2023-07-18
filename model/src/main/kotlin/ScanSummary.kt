b6ab06ee34ea5ae62f3d09eb79f1771c31e25098
Frank Viernau <frank_viernau@epam.com>
2023-07-18 09:58:07 +0200

    docs(model): Fix several similar mentions of `hasIssues`
    The `hasIssuees` property has been removed by [1], [2] and [3]. So,
    fix-up these commits to eliminate the broken KDoc references.
    
    [1] 1336fd747a3a5108ed3990230787abc8cd5632b4
    [2] f3e490a5b1a62f39aa13cf49793e7b01a1641dee
    [3] ee56e751e7dfee687e73b444da4f8b31c9fab8f6
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- model/src/main/kotlin/ScanSummary.kt
+++ model/src/main/kotlin/ScanSummary.kt
@@ -76,8 +76,7 @@ data class ScanSummary(
 
     /**
      * The list of issues that occurred during the scan. This property is not serialized if the list is empty to reduce
-     * the size of the result file. If there are no issues at all, [ScannerRun.hasIssues] already contains that
-     * information.
+     * the size of the result file.
      */
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     val issues: List<Issue> = emptyList()
