4daf7e9ba16e1b5fd4eb3d52b4dad0652941ffd2
Frank Viernau <frank_viernau@epam.com>
2023-07-18 10:30:16 +0200

    refactor(gradle): Eliminate the `utils` package
    The Gradle plugin has two packages with one class each. This seems a bit
    too fine grained. So, merge the packages and thereby fix a broken KDoc
    reference from `GradleDependencyHandlerTest` to `Gradle`.
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- plugins/package-managers/gradle/src/main/kotlin/Gradle.kt
+++ plugins/package-managers/gradle/src/main/kotlin/Gradle.kt
@@ -55,7 +55,6 @@ import org.ossreviewtoolkit.model.config.PackageManagerConfiguration
 import org.ossreviewtoolkit.model.config.RepositoryConfiguration
 import org.ossreviewtoolkit.model.createAndLogIssue
 import org.ossreviewtoolkit.model.utils.DependencyGraphBuilder
-import org.ossreviewtoolkit.plugins.packagemanagers.gradle.utils.GradleDependencyHandler
 import org.ossreviewtoolkit.utils.common.Os
 import org.ossreviewtoolkit.utils.common.splitOnWhitespace
 import org.ossreviewtoolkit.utils.common.temporaryProperties
