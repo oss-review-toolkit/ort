acf6ae85c2ccdbeab9a00c43f6cda7849fa46932
Etsija <jyrki.keisala@gmail.com>
2023-08-09 12:35:18 +0300

    scanner-dos: Receive and log scan issues returned from DOS API
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- plugins/scanners/dos/src/main/kotlin/DOSResultParser.kt
+++ plugins/scanners/dos/src/main/kotlin/DOSResultParser.kt
@@ -12,38 +12,37 @@ package org.ossreviewtoolkit.plugins.scanners.dos
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.ossreviewtoolkit.model.*
 
 import java.time.Instant
-
-import org.ossreviewtoolkit.model.CopyrightFinding
-import org.ossreviewtoolkit.model.LicenseFinding
-import org.ossreviewtoolkit.model.ScanSummary
-import org.ossreviewtoolkit.model.TextLocation
 
 internal fun generateSummary(startTime: Instant, endTime: Instant, jsonString: String): ScanSummary {
     val mapper = ObjectMapper()
     val result: JsonNode = mapper.readTree(jsonString)
     val licenseFindings = getLicenseFindings(result)
     val copyrightFindings = getCopyrightFindings(result)
+    val issues = getIssues(result)
 
     return ScanSummary(
         startTime,
         endTime,
         licenseFindings,
-        copyrightFindings
+        copyrightFindings,
+        emptySet(),
+        issues
     )
 }
 private fun getLicenseFindings(result: JsonNode): Set<LicenseFinding> {
     val licenseFindings = mutableListOf<LicenseFinding>()
     val licenses = result["licenses"] ?: return emptySet()
 
-    licenses.forEach {
-        val license = it["license"].asText()
-        val location = it["location"]
+    licenses.forEach { licenseNode ->
+        val license = licenseNode["license"].asText()
+        val location = licenseNode["location"]
         val path = location["path"].asText()
         val startLine = location["start_line"].asInt()
         val endLine = location["end_line"].asInt()
-        val score = it["score"].asDouble()
+        val score = licenseNode["score"].asDouble()
         licenseFindings.add(
             LicenseFinding(
                 license,
@@ -64,9 +63,9 @@ private fun getCopyrightFindings(result: JsonNode): Set<CopyrightFinding> {
     val copyrightFindings = mutableListOf<CopyrightFinding>()
     val copyrights = result["copyrights"] ?: return emptySet()
 
-    copyrights.forEach {
-        val statement = it["statement"].asText()
-        val location = it["location"]
+    copyrights.forEach { copyrightNode ->
+        val statement = copyrightNode["statement"].asText()
+        val location = copyrightNode["location"]
         val path = location["path"].asText()
         val startLine = location["start_line"].asInt()
         val endLine = location["end_line"].asInt()
@@ -83,4 +82,27 @@ private fun getCopyrightFindings(result: JsonNode): Set<CopyrightFinding> {
     }
 
     return copyrightFindings.toSet()
+}
+
+private fun getIssues(result: JsonNode): List<Issue> {
+    val issueFindings = mutableListOf<Issue>()
+    val issues = result["issues"] ?: return emptyList()
+
+    issues.forEach { issueNode ->
+        val timestamp = Instant.parse(issueNode["timestamp"].asText())
+        val source = issueNode["source"].asText()
+        val message = issueNode["message"].asText()
+        val severity = Severity.valueOf(issueNode["severity"].asText().uppercase())
+
+        issueFindings.add(
+            Issue(
+                timestamp,
+                source,
+                message,
+                severity
+            )
+        )
+    }
+
+    return issueFindings
 }
