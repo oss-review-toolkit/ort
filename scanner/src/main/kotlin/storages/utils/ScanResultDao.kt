6262ea94229500606d211f566a7a1154ee033ae0
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2023-08-17 18:24:39 +0200

    refactor(scanner): Remove explicit property types
    Remove explicit property types in `ScanResultDao` to simplify the code.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

--- scanner/src/main/kotlin/storages/utils/ScanResultDao.kt
+++ scanner/src/main/kotlin/storages/utils/ScanResultDao.kt
@@ -23,14 +23,13 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Column
 
 import org.ossreviewtoolkit.model.Identifier
 import org.ossreviewtoolkit.model.ScanResult
 
 object ScanResults : IntIdTable("scan_results") {
-    val identifier: Column<String> = text("identifier").index("identifier")
-    val scanResult: Column<ScanResult> = jsonb("scan_result")
+    val identifier = text("identifier").index("identifier")
+    val scanResult = jsonb<ScanResult>("scan_result")
 }
 
 class ScanResultDao(id: EntityID<Int>) : IntEntity(id) {
