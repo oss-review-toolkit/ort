c64c24139aa06d85dcc7c884c57aaba5445a01e8
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2023-09-15 17:42:13 +0200

    docs: Add an introduction button
    Add a button for the introduction to the website and let the getting
    started button actually link to the getting started docs.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -35,8 +35,13 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--secondary button--lg margin-right--lg"
             to="/docs/intro">
+            Introduction
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/getting-started/installation">
             Getting Started
           </Link>
         </div>
