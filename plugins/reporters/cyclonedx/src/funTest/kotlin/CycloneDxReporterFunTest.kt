012f09956b8773e5f603b28a352f9bea305fa03b
Sebastian Schuberth <sschuberth@gmail.com>
2023-10-17 08:10:07 +0200

    chore(CycloneDxReporterFunTest): Simplify patching code
    The limitation to the header is not actually needed when also limiting
    replacement to the first occurrence, so remove that code to simplify
    things.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- plugins/reporters/cyclonedx/src/funTest/kotlin/CycloneDxReporterFunTest.kt
+++ plugins/reporters/cyclonedx/src/funTest/kotlin/CycloneDxReporterFunTest.kt
@@ -168,19 +168,14 @@ class CycloneDxReporterFunTest : WordSpec({
     }
 })
 
-private fun String.patchCycloneDxResult(): String {
-    val headerEnd = indexOf("components").takeUnless { it < 0 } ?: length
-    return substring(0, headerEnd)
-        .replaceFirst(
-            """urn:uuid:[a-f0-9]{8}(?:-[a-f0-9]{4}){4}[a-f0-9]{8}""".toRegex(),
-            "urn:uuid:01234567-0123-0123-0123-01234567"
-        )
-        .replaceFirst(
-            """(timestamp[>"](\s*:\s*")?)\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z""".toRegex(),
-            "$11970-01-01T00:00:00Z"
-        )
-        .replaceFirst(
-            """(version[>"](\s*:\s*")?)[\w.+-]+""".toRegex(),
-            "$1deadbeef"
-        ) + substring(headerEnd)
-}
+private fun String.patchCycloneDxResult(): String =
+    replaceFirst(
+        """urn:uuid:[a-f0-9]{8}(?:-[a-f0-9]{4}){4}[a-f0-9]{8}""".toRegex(),
+        "urn:uuid:01234567-0123-0123-0123-01234567"
+    ).replaceFirst(
+        """(timestamp[>"](\s*:\s*")?)\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z""".toRegex(),
+        "$11970-01-01T00:00:00Z"
+    ).replaceFirst(
+        """(version[>"](\s*:\s*")?)[\w.+-]+""".toRegex(),
+        "$1deadbeef"
+    )
