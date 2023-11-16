20001812c7adc402f7da8538ae8d947a8a205cae
Sebastian Schuberth <sebastian@doubleopen.org>
2023-11-16 17:41:10 +0100

    fix: Remove any YAML front matter from ScanCode license files
    ScanCode 32.0.0 started to prepend its `*.LICENSE` files with
    YAML-encoded metadata, see [1]. This is a hot fix to remove this header,
    if present, from the license files. A better solution will be
    implemented later as part of a larger refactoring of license providers.
    
    Different ScanCode versions also differ in whether license files come
    with a final newline or not. Align on not having a final newline to make
    tests pass either way.
    
    [1]: https://github.com/nexB/scancode-toolkit/pull/3100
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- utils/spdx/src/test/kotlin/UtilsTest.kt
+++ utils/spdx/src/test/kotlin/UtilsTest.kt
@@ -184,7 +184,7 @@ class UtilsTest : WordSpec() {
         "getLicenseText provided a custom dir" should {
             "return the custom license text for a license ID not known by ort but in custom dir" {
                 val id = "LicenseRef-ort-abc"
-                val text = "a\nb\nc\n"
+                val text = "a\nb\nc"
 
                 setupTempFile(id, text)
 
@@ -195,6 +195,58 @@ class UtilsTest : WordSpec() {
                 setupTempFile("LicenseRef-ort-abc", "abc")
 
                 getLicenseText("LicenseRef-not-present", handleExceptions = true, listOf(tempDir)) should beNull()
+            }
+        }
+
+        "removeYamlFrontMatter" should {
+            "remove a YAML front matter" {
+                val text = """
+                    ---
+                    key: alasir
+                    short_name: Alasir Licence
+                    name: The Alasir Licence
+                    category: Proprietary Free
+                    owner: Alasir
+                    homepage_url: http://alasir.com/licence/TAL.txt
+                    spdx_license_key: LicenseRef-scancode-alasir
+                    ---
+
+                    The Alasir Licence
+
+                        This is a free software. It's provided as-is and carries absolutely no
+                    warranty or responsibility by the author and the contributors, neither in
+                    general nor in particular. No matter if this software is able or unable to
+                    cause any damage to your or third party's computer hardware, software, or any
+                    other asset available, neither the author nor a separate contributor may be
+                    found liable for any harm or its consequences resulting from either proper or
+                    improper use of the software, even if advised of the possibility of certain
+                    injury as such and so forth.
+                """.trimIndent()
+
+                text.removeYamlFrontMatter() shouldBe """
+                    The Alasir Licence
+
+                        This is a free software. It's provided as-is and carries absolutely no
+                    warranty or responsibility by the author and the contributors, neither in
+                    general nor in particular. No matter if this software is able or unable to
+                    cause any damage to your or third party's computer hardware, software, or any
+                    other asset available, neither the author nor a separate contributor may be
+                    found liable for any harm or its consequences resulting from either proper or
+                    improper use of the software, even if advised of the possibility of certain
+                    injury as such and so forth.
+                """.trimIndent()
+            }
+
+            "remove trailing whitespace" {
+                "last sentence\n".removeYamlFrontMatter() shouldBe "last sentence"
+            }
+
+            "remove leading empty lines" {
+                "\nfirst sentence".removeYamlFrontMatter() shouldBe "first sentence"
+            }
+
+            "keep leading whitespace" {
+                "    indented title".removeYamlFrontMatter() shouldBe "    indented title"
             }
         }
     }
