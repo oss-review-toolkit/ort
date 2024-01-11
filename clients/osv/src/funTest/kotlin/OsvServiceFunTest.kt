6ae49d8d9a84d677f86baba715a58c5202d32663
Sebastian Schuberth <sebastian@doubleopen.org>
2024-01-11 23:08:54 +0100

    test(osv): Update expected results
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- clients/osv/src/funTest/kotlin/OsvServiceFunTest.kt
+++ clients/osv/src/funTest/kotlin/OsvServiceFunTest.kt
@@ -103,6 +103,7 @@ class OsvServiceFunTest : StringSpec({
                     "GHSA-8r7q-cvjq-x353",
                     "GHSA-fqh9-2qgg-h84h",
                     "GHSA-g3rq-g295-4j3m",
+                    "GHSA-h5c8-rqwp-cp95",
                     "GHSA-hj2j-77xm-mc5v",
                     "PYSEC-2014-8",
                     "PYSEC-2014-82",
