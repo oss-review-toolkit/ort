a0ea68220b985d0e2d1c2f69429ca0966af2ed7d
Frank Viernau <frank_viernau@epam.com>
2024-01-22 15:11:24 +0100

    test(swiftpm): Add a lockfile for the synthetic `spm-lib` project
    Add a lockfile for the project in order to avoid the need for constantly
    updating the expected results. In order to keep on testing the "no
    lockfile" case, commit a copy of `spm-lib` without the lockfile to the
    `spm-lib-no-lockfile` directory.
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

---
+++ plugins/package-managers/swiftpm/src/funTest/assets/projects/synthetic/spm-lib-no-lockfile/Package.swift
@@ -0,0 +1,118 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "vapor",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "Vapor", targets: ["Vapor"]),
+        .library(name: "XCTVapor", targets: ["XCTVapor"]),
+    ],
+    dependencies: [
+        // HTTP client library built on SwiftNIO
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.0"),
+
+        // Sugary extensions for the SwiftNIO library
+        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0"),
+
+        // 💻 APIs for creating interactive CLI tools.
+        .package(url: "https://github.com/vapor/console-kit.git", from: "4.0.0"),
+
+        // 🔑 Hashing (BCrypt, SHA2, HMAC), encryption (AES), public-key (RSA), and random data generation.
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
+
+        // 🚍 High-performance trie-node router.
+        .package(url: "https://github.com/vapor/routing-kit.git", from: "4.0.0"),
+
+        // 💥 Backtraces for Swift on Linux
+        .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.1.1"),
+
+        // Event-driven network application framework for high performance protocol servers & clients, non-blocking.
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
+
+        // Bindings to OpenSSL-compatible libraries for TLS support in SwiftNIO
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.8.0"),
+
+        // HTTP/2 support for SwiftNIO
+        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.19.2"),
+
+        // Useful code around SwiftNIO.
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
+
+        // Swift logging API
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+
+        // Swift metrics API
+        .package(url: "https://github.com/apple/swift-metrics.git", from: "2.0.0"),
+
+        // WebSocket client library built on SwiftNIO
+        .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.0.0"),
+
+        // MultipartKit, Multipart encoding and decoding
+        .package(url: "https://github.com/vapor/multipart-kit.git", from: "4.2.1"),
+    ],
+    targets: [
+        // C helpers
+        .target(name: "CBase32"),
+        .target(name: "CBcrypt"),
+        .target(name: "COperatingSystem"),
+        .target(name: "CURLParser"),
+
+        // Vapor
+        .target(name: "Vapor", dependencies: [
+            .product(name: "AsyncHTTPClient", package: "async-http-client"),
+            .product(name: "AsyncKit", package: "async-kit"),
+            .product(name: "Backtrace", package: "swift-backtrace"),
+            .target(name: "CBase32"),
+            .target(name: "CBcrypt"),
+            .target(name: "COperatingSystem"),
+            .target(name: "CURLParser"),
+            .product(name: "ConsoleKit", package: "console-kit"),
+            .product(name: "Logging", package: "swift-log"),
+            .product(name: "Metrics", package: "swift-metrics"),
+            .product(name: "NIO", package: "swift-nio"),
+            .product(name: "NIOCore", package: "swift-nio"),
+            .product(name: "NIOExtras", package: "swift-nio-extras"),
+            .product(name: "NIOFoundationCompat", package: "swift-nio"),
+            .product(name: "NIOHTTPCompression", package: "swift-nio-extras"),
+            .product(name: "NIOHTTP1", package: "swift-nio"),
+            .product(name: "NIOHTTP2", package: "swift-nio-http2"),
+            .product(name: "NIOSSL", package: "swift-nio-ssl"),
+            .product(name: "NIOWebSocket", package: "swift-nio"),
+            .product(name: "Crypto", package: "swift-crypto"),
+            .product(name: "RoutingKit", package: "routing-kit"),
+            .product(name: "WebSocketKit", package: "websocket-kit"),
+            .product(name: "MultipartKit", package: "multipart-kit"),
+        ]),
+
+        // Development
+        .target(name: "Development", dependencies: [
+            .target(name: "Vapor"),
+        ], swiftSettings: [
+            // Enable better optimizations when building in Release configuration. Despite the use of
+            // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+            // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
+            .unsafeFlags([
+                            "-cross-module-optimization"
+            ], .when(configuration: .release)),
+        ]),
+
+        // Testing
+        .target(name: "XCTVapor", dependencies: [
+            .target(name: "Vapor"),
+        ]),
+        .testTarget(name: "VaporTests", dependencies: [
+            .product(name: "NIOTestUtils", package: "swift-nio"),
+            .target(name: "XCTVapor"),
+        ]),
+        .testTarget(name: "AsyncTests", dependencies: [
+            .product(name: "NIOTestUtils", package: "swift-nio"),
+            .target(name: "XCTVapor"),
+        ]),
+    ]
+)
