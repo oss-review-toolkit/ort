3634603e823bf4c8226da47cb887273a73e28b4a
Sebastian Schuberth <sebastian@doubleopen.org>
2024-02-19 12:21:29 +0100

    test(HashAlgorithm): Use a `WordSpec` to group tests
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- model/src/test/kotlin/HashAlgorithmTest.kt
+++ model/src/test/kotlin/HashAlgorithmTest.kt
@@ -19,41 +19,47 @@
 
 package org.ossreviewtoolkit.model
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.WordSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldStartWith
 
 import java.io.File
 
-class HashAlgorithmTest : StringSpec({
+class HashAlgorithmTest : WordSpec({
     // Use a file that is always checked out with Unix line endings in the working tree to get the same file contents on
     // all platforms and in the Git object store.
     val file = File("../LICENSE")
 
-    "SHA string representations need to contain a dash for MessageDigest compatibility" {
-        HashAlgorithm.entries.filter { it.toString().startsWith("SHA") }.forAll {
-            it.toString() shouldStartWith "SHA-"
+    "String representations" should {
+        "contain a dash after 'SHA' for MessageDigest compatibility" {
+            HashAlgorithm.entries.filter { it.toString().startsWith("SHA") }.forAll {
+                it.toString() shouldStartWith "SHA-"
+            }
         }
     }
 
-    "Calculating the SHA1 on a file should yield the correct result" {
-        // The expected hash was calculated with "sha1sum".
-        HashAlgorithm.SHA1.calculate(file) shouldBe "c00ef43045659b53da5d71d49b8cd7e528c9d55b"
+    "Calculating the SHA1" should {
+        "yield the correct result for a file" {
+            // The expected hash was calculated with "sha1sum".
+            HashAlgorithm.SHA1.calculate(file) shouldBe "c00ef43045659b53da5d71d49b8cd7e528c9d55b"
+        }
     }
 
-    "Calculating the SHA1GIT on a file should yield the correct result" {
-        // The expected hash was calculated with "git hash-object".
-        HashAlgorithm.SHA1GIT.calculate(file) shouldBe "3f4d322ebd76de0f1bbb9c867e1f818f5202efd3"
-    }
+    "Calculating the SHA1GIT" should {
+        "yield the correct result for a file" {
+            // The expected hash was calculated with "git hash-object".
+            HashAlgorithm.SHA1GIT.calculate(file) shouldBe "3f4d322ebd76de0f1bbb9c867e1f818f5202efd3"
+        }
 
-    "Calculating the SHA1GIT on a resource should yield the correct result" {
-        // The expected hash was calculated with "git hash-object".
-        HashAlgorithm.SHA1GIT.calculate("/licenses/Apache-2.0") shouldBe
-            "261eeb9e9f8b2b4b0d119366dda99c6fd7d35c64"
-    }
+        "yield the correct result for a resource should " {
+            // The expected hash was calculated with "git hash-object".
+            HashAlgorithm.SHA1GIT.calculate("/licenses/Apache-2.0") shouldBe
+                "261eeb9e9f8b2b4b0d119366dda99c6fd7d35c64"
+        }
 
-    "Calculating the SHA1GIT on non-existent field should return null" {
-        HashAlgorithm.SHA1GIT.calculate("/license/DOESNOTEXIST") shouldBe null
+        "return null for a non-existent file" {
+            HashAlgorithm.SHA1GIT.calculate("/license/DOESNOTEXIST") shouldBe null
+        }
     }
 })
