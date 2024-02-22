5713ab0665ee318f4903e8c8e788eaf38a079bf0
Sebastian Schuberth <sebastian@doubleopen.org>
2024-02-22 09:30:18 +0100

    test(fixtures): Fix analyzer formatting issues
    These were not found by Detekt in CI as it is not configured to check test
    fixtures yet.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- analyzer/src/testFixtures/kotlin/Extensions.kt
+++ analyzer/src/testFixtures/kotlin/Extensions.kt
@@ -109,21 +109,23 @@ private fun Project.filterReferencedPackages(allPackages: Set<Package>): Set<Pac
     return allPackages.filterTo(mutableSetOf()) { it.id in projectDependencies }
 }
 
-fun Collection<PackageReference>.withInvariantIssues(): Set<PackageReference> = mapTo(mutableSetOf()) { ref ->
-    ref.copy(
-        dependencies = ref.dependencies.withInvariantIssues(),
-        issues = ref.issues.map { it.copy(timestamp = Instant.EPOCH) }
-    )
-}
+fun Collection<PackageReference>.withInvariantIssues(): Set<PackageReference> =
+    mapTo(mutableSetOf()) { ref ->
+        ref.copy(
+            dependencies = ref.dependencies.withInvariantIssues(),
+            issues = ref.issues.map { it.copy(timestamp = Instant.EPOCH) }
+        )
+    }
 
-fun ProjectAnalyzerResult.withInvariantIssues() = copy(
-    project = project.copy(
-        scopeDependencies = project.scopeDependencies?.mapTo(mutableSetOf()) { scope ->
-            scope.copy(dependencies = scope.dependencies.withInvariantIssues())
-        }
-    ),
-    issues = issues.sortedBy { it.message }.map { it.copy(timestamp = Instant.EPOCH) }
-)
+fun ProjectAnalyzerResult.withInvariantIssues() =
+    copy(
+        project = project.copy(
+            scopeDependencies = project.scopeDependencies?.mapTo(mutableSetOf()) { scope ->
+                scope.copy(dependencies = scope.dependencies.withInvariantIssues())
+            }
+        ),
+        issues = issues.sortedBy { it.message }.map { it.copy(timestamp = Instant.EPOCH) }
+    )
 
 fun Spec.analyze(
     projectDir: File,
