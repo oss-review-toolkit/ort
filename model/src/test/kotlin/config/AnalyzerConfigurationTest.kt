8ce948350221b4ae053512d4b271cdf0e6c49ea5
Sebastian Schuberth <sebastian@doubleopen.org>
2024-03-14 22:02:19 +0100

    feat(gradle-inspector): Allow to customize the Java home for analysis
    Note that `setJavaHome()` [1] sets the Java home "to use for the Gradle
    process". However, as by default (unless toolchains are used [2]) "Gradle
    will compile Java code to the language level of the JVM running Gradle"
    [3], this will impact the Java target of the build.
    
    See #8249 for context.
    
    [1]: https://docs.gradle.org/current/javadoc/org/gradle/tooling/ConfigurableLauncher.html#setJavaHome(java.io.File)
    [2]: https://docs.gradle.org/8.10/userguide/building_java_projects.html#sec:compiling_with_toolchain
    [3]: https://docs.gradle.org/8.10/userguide/building_java_projects.html#sec:java_cross_compilation
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- model/src/test/kotlin/config/AnalyzerConfigurationTest.kt
+++ model/src/test/kotlin/config/AnalyzerConfigurationTest.kt
@@ -136,9 +136,9 @@ class AnalyzerConfigurationTest : WordSpec({
         "override an existing entry" {
             val original = AnalyzerConfiguration(
                 packageManagers = mapOf(
-                    "Gradle" to PackageManagerConfiguration(
+                    "GradleInspector" to PackageManagerConfiguration(
                         mustRunAfter = listOf("Npm"),
-                        options = mapOf("gradleVersion" to "7.6.1")
+                        options = mapOf("gradleVersion" to "7.6.1", "javaHome" to "/path/to/java/home")
                     ),
                     "Npm" to PackageManagerConfiguration(
                         mustRunAfter = listOf("Yarn")
@@ -148,9 +148,9 @@ class AnalyzerConfigurationTest : WordSpec({
 
             val patched = AnalyzerConfiguration(
                 packageManagers = mapOf(
-                    "Gradle" to PackageManagerConfiguration(
+                    "GradleInspector" to PackageManagerConfiguration(
                         mustRunAfter = listOf("Npm"),
-                        options = mapOf("gradleVersion" to "8.0.2")
+                        options = mapOf("gradleVersion" to "8.0.2", "javaHome" to "/path/to/java/home")
                     ),
                     "Npm" to PackageManagerConfiguration(
                         mustRunAfter = listOf("Yarn")
@@ -158,7 +158,7 @@ class AnalyzerConfigurationTest : WordSpec({
                 )
             )
 
-            original.withPackageManagerOption("Gradle", "gradleVersion", "8.0.2") shouldBe patched
+            original.withPackageManagerOption("GradleInspector", "gradleVersion", "8.0.2") shouldBe patched
         }
 
         "add a non-existing entry" {
