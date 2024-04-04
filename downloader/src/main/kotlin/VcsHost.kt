e407d11ea350c55c6fd7701f3b8d0404085cf8b0
Sebastian Schuberth <sebastian@doubleopen.org>
2024-04-04 13:39:18 +0200

    chore(downloader): Remove the redundant `protected` enum qualifier
    Enums cannot be inherited from.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- downloader/src/main/kotlin/VcsHost.kt
+++ downloader/src/main/kotlin/VcsHost.kt
@@ -39,7 +39,7 @@ enum class VcsHost(
     /**
      * The hostname of VCS host.
      */
-    protected val hostname: String,
+    val hostname: String,
 
     /**
      * The VCS types the host supports.
