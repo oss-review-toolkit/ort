ca1798541f03a59dfa90c783586fa83369b2a89e
Frank Viernau <frank_viernau@epam.com>
2024-04-22 15:48:38 +0200

    test(static-html): Make a replace operation less invasive
    The replacement has been introduced for replacing the report creation
    timestamp only. However, `replace()` replaces all matches and the
    pattern would all so match the timestamp value of issues. So, replace
    only the first match, so that issues (with timestamps) can be added in a
    following change.
    
    [1]: https://github.com/oss-review-toolkit/ort/commit/ba0e4cb637037275a91763ecde885327c6b0d6fd
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- plugins/reporters/static-html/src/funTest/kotlin/StaticHtmlReporterFunTest.kt
+++ plugins/reporters/static-html/src/funTest/kotlin/StaticHtmlReporterFunTest.kt
@@ -54,7 +54,7 @@ class StaticHtmlReporterFunTest : WordSpec({
         "successfully export to a static HTML page" {
             val timeStampPattern = Regex("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d+)?Z")
             val ortResult = readOrtResult("src/funTest/assets/reporter-test-input.yml")
-            val actualReport = generateReport(ortResult).replace(timeStampPattern, "<REPLACE_TIMESTAMP>")
+            val actualReport = generateReport(ortResult).replaceFirst(timeStampPattern, "<REPLACE_TIMESTAMP>")
 
             val expectedReport = matchExpectedResult(
                 getAssetFile("static-html-reporter-test-expected-output.html"),
