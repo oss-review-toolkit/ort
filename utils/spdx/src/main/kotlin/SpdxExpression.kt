d42863c689009806b3a6ee458ba5b753d404cfbe
Sebastian Schuberth <sebastian@doubleopen.org>
2024-05-17 19:12:15 +0200

    fix(spdx-utils): List correct choices in an exception message
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- utils/spdx/src/main/kotlin/SpdxExpression.kt
+++ utils/spdx/src/main/kotlin/SpdxExpression.kt
@@ -302,7 +302,7 @@ class SpdxCompoundExpression(
     override fun applyChoice(choice: SpdxExpression, subExpression: SpdxExpression): SpdxExpression {
         if (!subExpression.isSubExpression(choice)) {
             throw InvalidLicenseChoiceException(
-                "$choice is not a valid choice for $subExpression. Valid choices are: ${validChoices()}."
+                "$choice is not a valid choice for $subExpression. Valid choices are: ${subExpression.validChoices()}."
             )
         }
 
