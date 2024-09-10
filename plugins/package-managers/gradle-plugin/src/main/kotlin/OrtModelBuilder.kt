ce8fa65c004f01fce79e5b8b26f769b6da2509e9
Sebastian Schuberth <sebastian@doubleopen.org>
2024-09-10 17:35:07 +0200

    fix(gradle-inspector): Handle dependency cycles properly
    Do not cut the dependency tree on any already visited dependency in a
    configuration, but only for those that appear on the path from the root to
    a leaf. This fixes the discrepancy compared to the `Gradle` package
    manager that was identified at [1] but justified wrongly.
    
    [1]: https://github.com/oss-review-toolkit/ort/pull/6882#discussion_r1171238229
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- plugins/package-managers/gradle-plugin/src/main/kotlin/OrtModelBuilder.kt
+++ plugins/package-managers/gradle-plugin/src/main/kotlin/OrtModelBuilder.kt
@@ -27,7 +27,6 @@ import org.apache.maven.model.building.ModelBuildingResult
 
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.artifacts.ModuleVersionIdentifier
 import org.gradle.api.artifacts.component.ComponentIdentifier
 import org.gradle.api.artifacts.component.ModuleComponentIdentifier
 import org.gradle.api.artifacts.component.ProjectComponentIdentifier
@@ -51,9 +50,6 @@ internal class OrtModelBuilder : ToolingModelBuilder {
     private val repositories = mutableMapOf<String, String?>()
 
     private val platformCategories = setOf("platform", "enforced-platform")
-
-    private val visitedDependencies = mutableSetOf<ModuleComponentIdentifier>()
-    private val visitedProjects = mutableSetOf<ModuleVersionIdentifier>()
 
     private val logger = Logging.getLogger(OrtModelBuilder::class.java)
     private val errors = mutableListOf<String>()
@@ -85,11 +81,7 @@ internal class OrtModelBuilder : ToolingModelBuilder {
 
             // Omit configurations without dependencies.
             root.dependencies.takeUnless { it.isEmpty() }?.let { dep ->
-                // Reset visited dependencies and projects per configuration.
-                visitedDependencies.clear()
-                visitedProjects.clear()
-
-                OrtConfigurationImpl(name = config.name, dependencies = dep.toOrtDependencies(poms))
+                OrtConfigurationImpl(name = config.name, dependencies = dep.toOrtDependencies(poms, emptySet()))
             }
         }
 
@@ -146,7 +138,8 @@ internal class OrtModelBuilder : ToolingModelBuilder {
     }
 
     private fun Collection<DependencyResult>.toOrtDependencies(
-        poms: Map<String, ModelBuildingResult>
+        poms: Map<String, ModelBuildingResult>,
+        visited: Set<ComponentIdentifier>
     ): List<OrtDependency> =
         if (GradleVersion.current() < GradleVersion.version("5.1")) {
             this
@@ -163,8 +156,12 @@ internal class OrtModelBuilder : ToolingModelBuilder {
                     if (isBom) return@mapNotNull null
 
                     val selectedComponent = dep.selected
+                    val id = selectedComponent.id
 
-                    when (val id = selectedComponent.id) {
+                    // Cut the graph on cyclic dependencies.
+                    if (id in visited) return@mapNotNull null
+
+                    when (id) {
                         is ModuleComponentIdentifier -> {
                             val pomFile = if (selectedComponent is ResolvedComponentResultInternal) {
                                 val repositoryId = runCatching {
@@ -199,19 +196,13 @@ internal class OrtModelBuilder : ToolingModelBuilder {
                             }
 
                             val modelBuildingResult = poms[id.toString()]
-                            if (modelBuildingResult == null && id !in visitedDependencies) {
+                            if (modelBuildingResult == null) {
                                 val message = "No POM found for $id."
                                 logger.warn(message)
                                 warnings += message
                             }
 
-                            val dependencies = if (id in visitedDependencies) {
-                                // Cut the graph on cyclic dependencies.
-                                emptyList()
-                            } else {
-                                visitedDependencies += id
-                                selectedComponent.dependencies.toOrtDependencies(poms)
-                            }
+                            val dependencies = selectedComponent.dependencies.toOrtDependencies(poms, visited + id)
 
                             OrtDependencyImpl(
                                 groupId = id.group,
@@ -238,13 +229,7 @@ internal class OrtModelBuilder : ToolingModelBuilder {
 
                         is ProjectComponentIdentifier -> {
                             val moduleId = selectedComponent.moduleVersion ?: return@mapNotNull null
-                            val dependencies = if (moduleId in visitedProjects) {
-                                // Cut the graph on cyclic dependencies.
-                                emptyList()
-                            } else {
-                                visitedProjects += moduleId
-                                selectedComponent.dependencies.toOrtDependencies(poms)
-                            }
+                            val dependencies = selectedComponent.dependencies.toOrtDependencies(poms, visited + id)
 
                             OrtDependencyImpl(
                                 groupId = moduleId.group,
