563ce35fba318a01290d21efcd27c46afee0b7b1
Frank Viernau <frank_viernau@epam.com>
2024-09-23 09:49:26 +0200

    test(node): Move Yarn2 test projects into a dedicated `yarn2` directory
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- plugins/package-managers/node/src/funTest/kotlin/Yarn2FunTest.kt
+++ plugins/package-managers/node/src/funTest/kotlin/Yarn2FunTest.kt
@@ -32,7 +32,7 @@ import org.ossreviewtoolkit.utils.test.matchExpectedResult
 class Yarn2FunTest : WordSpec({
     "Yarn 2" should {
         "resolve dependencies for a project with lockfile correctly" {
-            val definitionFile = getAssetFile("projects/synthetic/yarn2-project-with-lockfile/package.json")
+            val definitionFile = getAssetFile("projects/synthetic/yarn2/project-with-lockfile/package.json")
             val expectedResultFile = getAssetFile("projects/synthetic/yarn2-project-with-lockfile-expected-output.yml")
 
             val result = create("Yarn2").resolveSingleProject(definitionFile, resolveScopes = true)
@@ -41,7 +41,7 @@ class Yarn2FunTest : WordSpec({
         }
 
         "resolved dependencies for a project with lockfile correctly and skip excluded scopes" {
-            val definitionFile = getAssetFile("projects/synthetic/yarn2-project-with-lockfile/package.json")
+            val definitionFile = getAssetFile("projects/synthetic/yarn2/project-with-lockfile/package.json")
             val expectedResultFile = getAssetFile(
                 "projects/synthetic/yarn2-project-with-lockfile-skip-excluded-scopes-expected-output.yml"
             )
@@ -53,7 +53,7 @@ class Yarn2FunTest : WordSpec({
         }
 
         "resolve dependencies for a workspaces project correctly" {
-            val definitionFile = getAssetFile("projects/synthetic/yarn2-workspaces/package.json")
+            val definitionFile = getAssetFile("projects/synthetic/yarn2/workspaces/package.json")
             val expectedResultFile = getAssetFile("projects/synthetic/yarn2-workspaces-expected-output.yml")
 
             val result = create("Yarn2").collateMultipleProjects(definitionFile).withResolvedScopes()
