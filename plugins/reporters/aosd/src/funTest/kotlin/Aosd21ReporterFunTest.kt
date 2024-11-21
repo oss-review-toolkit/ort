30178a55b8ab45f77ada71c3de315d0a3f930d16
Sebastian Schuberth <sebastian@doubleopen.org>
2024-11-21 16:48:02 +0100

    feat(aosd): Add an AOSD 2.1 reporter
    Note that despite the small version number increment, AOSD 2.1 is a
    completely different format compared to AOSD 2.0.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

---
+++ plugins/reporters/aosd/src/funTest/kotlin/Aosd21ReporterFunTest.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2024 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.plugins.reporters.aosd
+
+import com.networknt.schema.InputFormat
+import com.networknt.schema.JsonSchemaFactory
+import com.networknt.schema.SpecVersion
+
+import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.json.shouldEqualJson
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.engine.spec.tempdir
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.collections.beEmpty
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.result.shouldBeSuccess
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+
+import org.ossreviewtoolkit.reporter.ORT_RESULT
+import org.ossreviewtoolkit.reporter.ReporterInput
+import org.ossreviewtoolkit.utils.test.getAssetFile
+
+class Aosd21ReporterFunTest : WordSpec({
+    "The example JSON report" should {
+        "be valid according to the schema" {
+            val schemaFile = getAssetFile("aosd21/AOSD2.1_Importscheme_V2.1.0.json")
+            val schema = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7).getSchema(schemaFile.toURI())
+
+            val exampleFile = getAssetFile("aosd21/AOSD2.1_Example_Json_Import_File_V2.1.0.json")
+            val errors = schema.validate(exampleFile.readText(), InputFormat.JSON)
+
+            errors should beEmpty()
+        }
+
+        "deserialize correctly" {
+            val aosdFile = getAssetFile("aosd21/AOSD2.1_Example_Json_Import_File_V2.1.0.json")
+            val aosd = aosdFile.readAosd21Report()
+
+            with(aosd) {
+                schemaVersion shouldBe "2.1.0"
+                externalId shouldBe "myownId"
+                scanned shouldBe true
+                directDependencies shouldHaveSize 10
+                components shouldHaveSize 13
+            }
+        }
+    }
+
+    "The generated report" should {
+        val outputDir = tempdir()
+        val reportFiles = Aosd21Reporter().generateReport(ReporterInput(ORT_RESULT), outputDir)
+
+        "be valid according to the schema" {
+            val schemaFile = getAssetFile("aosd21/AOSD2.1_Importscheme_V2.1.0.json")
+            val schema = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7).getSchema(schemaFile.toURI())
+
+            reportFiles.forAll {
+                it.shouldBeSuccess { reportFile ->
+                    val errors = schema.validate(reportFile.readText(), InputFormat.JSON)
+                    errors should beEmpty()
+                }
+            }
+        }
+
+        "match the expected result" {
+            reportFiles shouldHaveSize 2
+
+            assertSoftly {
+                with(reportFiles[0]) {
+                    this shouldBeSuccess { actualFile ->
+                        val expectedFile = getAssetFile("aosd21/aosd.NPM-%40ort-project-with-findings-1.0.json")
+                        actualFile.readText() shouldEqualJson expectedFile.readText()
+                    }
+                }
+
+                with(reportFiles[1]) {
+                    this shouldBeSuccess { actualFile ->
+                        val expectedFile = getAssetFile("aosd21/aosd.NPM-%40ort-project-without-findings-1.0.json")
+                        actualFile.readText() shouldEqualJson expectedFile.readText()
+                    }
+                }
+            }
+        }
+    }
+})
---
+++ plugins/reporters/aosd/src/main/kotlin/Aosd21Reporter.kt
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2024 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.plugins.reporters.aosd
+
+import java.io.File
+
+import org.ossreviewtoolkit.model.CuratedPackage
+import org.ossreviewtoolkit.model.DependencyNavigator
+import org.ossreviewtoolkit.model.DependencyNode
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.PackageLinkage
+import org.ossreviewtoolkit.model.Project
+import org.ossreviewtoolkit.model.licenses.LicenseView
+import org.ossreviewtoolkit.model.licenses.ResolvedLicenseInfo
+import org.ossreviewtoolkit.plugins.api.OrtPlugin
+import org.ossreviewtoolkit.plugins.api.PluginDescriptor
+import org.ossreviewtoolkit.plugins.reporters.aosd.AOSD21.Component
+import org.ossreviewtoolkit.reporter.Reporter
+import org.ossreviewtoolkit.reporter.ReporterFactory
+import org.ossreviewtoolkit.reporter.ReporterInput
+import org.ossreviewtoolkit.utils.spdx.SpdxExpression
+import org.ossreviewtoolkit.utils.spdx.nullOrBlankToSpdxNoassertionOrNone
+
+@OrtPlugin(
+    id = "AOSD2.1",
+    displayName = "Audi Open Source Diagnostics 2.1 Reporter",
+    description = "A reporter for the Audi Open Source Diagnostics (AOSD) 2.1 format.",
+    factory = ReporterFactory::class
+)
+class Aosd21Reporter(override val descriptor: PluginDescriptor = Aosd20ReporterFactory.descriptor) : Reporter {
+    override fun generateReport(input: ReporterInput, outputDir: File): List<Result<File>> {
+        val reportFiles = input.ortResult.getProjects(omitExcluded = true).map { project ->
+            val indexedPackages = input.ortResult.getPackages(omitExcluded = true).withIndex().associateBy {
+                it.value.metadata.id
+            }
+
+            val directDependencies = input.ortResult.getDependencies(project.id, maxLevel = 1, omitExcluded = true)
+
+            runCatching {
+                // Do not validate the AOSD21 object here as it has the impracticable requirement to contain at least
+                // one direct dependency / component.
+                val model = AOSD21(
+                    externalId = project.id.toCoordinates(),
+                    directDependencies = directDependencies.mapNotNullTo(mutableSetOf()) {
+                        indexedPackages[it]?.index?.toLong()
+                    },
+                    components = indexedPackages.toComponents(input, project)
+                )
+
+                val projectName = project.id.toPath("-")
+
+                outputDir.resolve("aosd21.$projectName.json").writeReport(model)
+            }
+        }
+
+        return reportFiles
+    }
+}
+
+private fun Map<Identifier, IndexedValue<CuratedPackage>>.toComponents(
+    input: ReporterInput,
+    project: Project
+): Set<Component> =
+    values.mapTo(mutableSetOf()) { (index, pkg) ->
+        val dependencies = input.ortResult.getDependencies(pkg.metadata.id, maxLevel = 1, omitExcluded = true)
+        val node = input.ortResult.dependencyNavigator.findNode(project, pkg.metadata.id)
+
+        val nonExcludedLicenseInfo = input.licenseInfoResolver.resolveLicenseInfo(pkg.metadata.id).filterExcluded()
+        val relevantLicenseInfo = nonExcludedLicenseInfo.filter(LicenseView.CONCLUDED_OR_DECLARED_AND_DETECTED)
+
+        val licenseExpressions = relevantLicenseInfo.toSimplifiedCompoundExpression()
+
+        val selectedLicenseInfo = relevantLicenseInfo
+            .applyChoices(input.ortResult.getPackageLicenseChoices(pkg.metadata.id))
+            .applyChoices(input.ortResult.getRepositoryLicenseChoices())
+
+        val selectedExpressions = selectedLicenseInfo.toSimplifiedCompoundExpression()
+
+        val licenseTexts = licenseExpressions.licenses().mapNotNullTo(mutableSetOf()) { license ->
+            input.licenseTextProvider.getLicenseText(license)
+        }.joinToString("\n--\n")
+
+        with(pkg.metadata) {
+            Component(
+                id = index.toLong(),
+                componentName = id.name,
+                componentVersion = id.version,
+                scmUrl = vcsProcessed.url.takeUnless { it.isEmpty() } ?: homepageUrl,
+                modified = isModified,
+                // For simplicity, only use the first linkage found in the graph for the package.
+                linking = node?.linkage?.toLinking(),
+                transitiveDependencies = dependencies.mapNotNullTo(mutableSetOf()) { get(it)?.index?.toLong() },
+                subcomponents = listOf(
+                    AOSD21.Subcomponent(
+                        subcomponentName = FIRST_SUBCOMPONENT_NAME,
+                        spdxId = licenseExpressions.nullOrBlankToSpdxNoassertionOrNone(),
+                        copyrights = relevantLicenseInfo.getCopyrights().sorted(),
+                        authors = pkg.metadata.authors.sorted(),
+                        licenseText = licenseTexts,
+                        // Can be empty as the license information is the result of a file level scan.
+                        licenseTextUrl = "",
+                        selectedLicense = selectedExpressions.nullOrBlankToSpdxNoassertionOrNone()
+                    ).validate()
+                )
+            ).validate()
+        }
+    }
+
+private fun DependencyNavigator.findNode(project: Project, id: Identifier): DependencyNode? {
+    fun traverse(node: DependencyNode): DependencyNode? {
+        if (node.id == id) return node
+
+        node.visitDependencies { dependencies ->
+            dependencies.forEach(::traverse)
+        }
+
+        return null
+    }
+
+    scopeNames(project).forEach { scopeName ->
+        directDependencies(project, scopeName).forEach { node ->
+            traverse(node)?.also { return it }
+        }
+    }
+
+    return null
+}
+
+private fun ResolvedLicenseInfo.toSimplifiedCompoundExpression(): SpdxExpression =
+    licenses
+        .flatMap { it.originalExpressions }
+        .map { it.expression }
+        .reduce(SpdxExpression::and)
+        .simplify()
+        .sorted()
+
+private fun PackageLinkage.toLinking(): String? =
+    when (this) {
+        PackageLinkage.DYNAMIC -> "dynamic_linking"
+        PackageLinkage.STATIC -> "static_linking"
+        else -> null
+    }
