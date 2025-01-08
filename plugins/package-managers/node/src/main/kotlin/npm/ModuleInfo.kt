bb610d0f02a6caf7bac445929ebdb495c05fbf57
Sebastian Schuberth <sebastian@doubleopen.org>
2025-01-08 18:36:39 +0100

    docs(npm): Document the `ModuleInfo` class
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- plugins/package-managers/node/src/main/kotlin/npm/ModuleInfo.kt
+++ plugins/package-managers/node/src/main/kotlin/npm/ModuleInfo.kt
@@ -27,17 +27,37 @@ private val JSON = Json { ignoreUnknownKeys = true }
 
 internal fun parseNpmList(json: String): ModuleInfo = JSON.decodeFromString(json)
 
+/**
+ * Module information for installed NPM packages.
+ */
 @Serializable
 internal data class ModuleInfo(
+    /** The name of the package. */
     val name: String? = null,
+
+    /** The resolved version of the package. */
     val version: String? = null,
+
+    /** The path to the directory where the package is installed. */
     val path: String? = null,
+
+    /** The ID of the NPM package. */
     @SerialName("_id")
     val id: String? = null,
+
+    /** The dependencies of the package with unresolved versions. */
     @SerialName("_dependencies")
     val dependencyConstraints: Map<String, String> = emptyMap(),
+
+    /** The dependencies of the package. */
     val dependencies: Map<String, ModuleInfo> = emptyMap(),
+
+    /** A flag to indicate whether this is an optional dependency. */
     val optional: Boolean = false,
+
+    /** A flag to indicate whether this is a development dependency. */
     val dev: Boolean = false,
+
+    /** The URI from where the package was resolved. Starts with "file:" for local packages. */
     val resolved: String? = null
 )
