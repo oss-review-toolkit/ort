3eb2b4a552e93a1978725887fbc0b0ae4ad890d1
Sebastian Schuberth <sebastian@doubleopen.org>
2025-01-24 16:37:14 +0100

    style(node): Remove one level of nesting from tests
    Use `StringSpec`s instead of `WordSpec`s as the latter were used to nest
    all tests as a whole only.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- plugins/package-managers/node/src/funTest/kotlin/npm/NpmFunTest.kt
+++ plugins/package-managers/node/src/funTest/kotlin/npm/NpmFunTest.kt
@@ -19,7 +19,7 @@
 
 package org.ossreviewtoolkit.plugins.packagemanagers.node.npm
 
-import io.kotest.core.spec.style.WordSpec
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.engine.spec.tempdir
 import io.kotest.inspectors.forAll
 import io.kotest.inspectors.forAtLeastOne
@@ -43,116 +43,114 @@ import org.ossreviewtoolkit.utils.test.matchExpectedResult
 import org.ossreviewtoolkit.utils.test.patchActualResult
 import org.ossreviewtoolkit.utils.test.patchExpectedResult
 
-class NpmFunTest : WordSpec({
-    "NPM" should {
-        "resolve dependencies for a project with a 'shrinkwrap.json' correctly" {
-            val definitionFile = getAssetFile("projects/synthetic/npm/shrinkwrap/package.json")
-            val expectedResultFile = getAssetFile("projects/synthetic/npm/shrinkwrap-expected-output.yml")
+class NpmFunTest : StringSpec({
+    "Resolve dependencies for a project with a 'shrinkwrap.json' correctly" {
+        val definitionFile = getAssetFile("projects/synthetic/npm/shrinkwrap/package.json")
+        val expectedResultFile = getAssetFile("projects/synthetic/npm/shrinkwrap-expected-output.yml")
 
-            val result = create("NPM").resolveSingleProject(definitionFile, resolveScopes = true)
+        val result = create("NPM").resolveSingleProject(definitionFile, resolveScopes = true)
 
-            patchActualResult(result.toYaml()) should matchExpectedResult(
-                expectedResultFile,
-                definitionFile,
-                custom = mapOf(
-                    "<REPLACE_PROJECT_NAME>" to "npm-${definitionFile.parentFile.name}",
-                    "<REPLACE_LOCKFILE_NAME>" to "npm-shrinkwrap.json"
-                )
+        patchActualResult(result.toYaml()) should matchExpectedResult(
+            expectedResultFile,
+            definitionFile,
+            custom = mapOf(
+                "<REPLACE_PROJECT_NAME>" to "npm-${definitionFile.parentFile.name}",
+                "<REPLACE_LOCKFILE_NAME>" to "npm-shrinkwrap.json"
             )
+        )
+    }
+
+    "Exclude scopes if configured" {
+        val definitionFile = getAssetFile("projects/synthetic/npm/shrinkwrap/package.json")
+        val expectedResultFile = getAssetFile(
+            "projects/synthetic/npm/shrinkwrap-skip-excluded-scopes-expected-output.yml"
+        )
+
+        val result = create("NPM", excludedScopes = setOf("devDependencies"))
+            .resolveSingleProject(definitionFile, resolveScopes = true)
+
+        patchActualResult(result.toYaml()) should matchExpectedResult(expectedResultFile, definitionFile)
+    }
+
+    "Resolve dependencies for a project with lockfile correctly" {
+        val definitionFile = getAssetFile("projects/synthetic/npm/project-with-lockfile/package.json")
+        val expectedResultFile = getAssetFile("projects/synthetic/npm/project-with-lockfile-expected-output.yml")
+
+        val result = create("NPM").resolveSingleProject(definitionFile, resolveScopes = true)
+
+        patchActualResult(result.toYaml()) should matchExpectedResult(expectedResultFile, definitionFile)
+    }
+
+    "Show an error if no lockfile is present" {
+        val definitionFile = getAssetFile("projects/synthetic/npm/no-lockfile/package.json")
+        val expectedResultFile = getAssetFile("projects/synthetic/npm/no-lockfile-expected-output.yml")
+
+        val result = create("NPM").resolveSingleProject(definitionFile)
+
+        patchActualResult(result.toYaml()) should matchExpectedResult(expectedResultFile, definitionFile)
+    }
+
+    "Show an error if the 'package.json' file is invalid" {
+        val workingDir = tempdir()
+        val definitionFile = workingDir.resolve("package.json").apply { writeText("<>") }
+
+        val result = create("NPM", allowDynamicVersions = true).resolveSingleProject(definitionFile)
+
+        result.issues.forAtLeastOne {
+            it.source shouldBe "NPM"
+            it.severity shouldBe Severity.ERROR
+            it.message shouldContain "Unexpected token"
         }
+    }
 
-        "exclude scopes if configured" {
-            val definitionFile = getAssetFile("projects/synthetic/npm/shrinkwrap/package.json")
-            val expectedResultFile = getAssetFile(
-                "projects/synthetic/npm/shrinkwrap-skip-excluded-scopes-expected-output.yml"
-            )
+    "Create issues for list errors" {
+        val definitionFile = getAssetFile("projects/synthetic/npm/list-issues/package.json")
 
-            val result = create("NPM", excludedScopes = setOf("devDependencies"))
-                .resolveSingleProject(definitionFile, resolveScopes = true)
+        val result = create("NPM", "legacyPeerDeps" to "true").resolveSingleProject(definitionFile)
 
-            patchActualResult(result.toYaml()) should matchExpectedResult(expectedResultFile, definitionFile)
-        }
+        result.issues shouldNot beEmpty()
+        val elsproblems = result.issues.filter { it.message.startsWith("invalid: ") }
 
-        "resolve dependencies for a project with lockfile correctly" {
-            val definitionFile = getAssetFile("projects/synthetic/npm/project-with-lockfile/package.json")
-            val expectedResultFile = getAssetFile("projects/synthetic/npm/project-with-lockfile-expected-output.yml")
+        elsproblems shouldHaveSize 2
+        elsproblems.forAll { it.severity shouldBe Severity.ERROR }
 
-            val result = create("NPM").resolveSingleProject(definitionFile, resolveScopes = true)
+        elsproblems shouldHaveSingleElement { it.message.startsWith("invalid: react@18.2.0") }
+        elsproblems shouldHaveSingleElement { it.message.startsWith("invalid: react-dom@18.2.0") }
+    }
 
-            patchActualResult(result.toYaml()) should matchExpectedResult(expectedResultFile, definitionFile)
-        }
+    "Resolve dependencies even if the 'node_modules' directory already exists" {
+        val definitionFile = getAssetFile("projects/synthetic/npm/node-modules/package.json")
+        val expectedResultFile = getAssetFile("projects/synthetic/npm/node-modules-expected-output.yml")
 
-        "show an error if no lockfile is present" {
-            val definitionFile = getAssetFile("projects/synthetic/npm/no-lockfile/package.json")
-            val expectedResultFile = getAssetFile("projects/synthetic/npm/no-lockfile-expected-output.yml")
+        val result = create("NPM").resolveSingleProject(definitionFile, resolveScopes = true)
 
-            val result = create("NPM").resolveSingleProject(definitionFile)
+        patchActualResult(result.toYaml()) should matchExpectedResult(
+            expectedResultFile,
+            definitionFile,
+            custom = mapOf("<REPLACE_LOCKFILE_NAME>" to "package-lock.json")
+        )
+    }
 
-            patchActualResult(result.toYaml()) should matchExpectedResult(expectedResultFile, definitionFile)
-        }
+    "Resolve Babel dependencies correctly" {
+        val definitionFile = getAssetFile("projects/synthetic/npm/babel/package.json")
+        val expectedResultFile = getAssetFile("projects/synthetic/npm/babel-expected-output.yml")
+        val expectedResult = patchExpectedResult(expectedResultFile, definitionFile)
+            .fromYaml<ProjectAnalyzerResult>()
 
-        "show an error if the 'package.json' file is invalid" {
-            val workingDir = tempdir()
-            val definitionFile = workingDir.resolve("package.json").apply { writeText("<>") }
+        val result = create("NPM").resolveSingleProject(definitionFile, resolveScopes = true)
 
-            val result = create("NPM", allowDynamicVersions = true).resolveSingleProject(definitionFile)
+        result.withInvariantIssues().toYaml() shouldBe expectedResult.withInvariantIssues().toYaml()
+    }
 
-            result.issues.forAtLeastOne {
-                it.source shouldBe "NPM"
-                it.severity shouldBe Severity.ERROR
-                it.message shouldContain "Unexpected token"
-            }
-        }
+    "Resolve dependencies with URLs as versions correctly" {
+        val definitionFile = getAssetFile("projects/synthetic/npm/version-urls/package.json")
+        val expectedResultFile = getAssetFile("projects/synthetic/npm/version-urls-expected-output.yml")
+        val expectedResult = patchExpectedResult(expectedResultFile, definitionFile)
+            .fromYaml<ProjectAnalyzerResult>()
 
-        "create issues for list errors" {
-            val definitionFile = getAssetFile("projects/synthetic/npm/list-issues/package.json")
+        val result = create("NPM", allowDynamicVersions = true)
+            .resolveSingleProject(definitionFile, resolveScopes = true)
 
-            val result = create("NPM", "legacyPeerDeps" to "true").resolveSingleProject(definitionFile)
-
-            result.issues shouldNot beEmpty()
-            val elsproblems = result.issues.filter { it.message.startsWith("invalid: ") }
-
-            elsproblems shouldHaveSize 2
-            elsproblems.forAll { it.severity shouldBe Severity.ERROR }
-
-            elsproblems shouldHaveSingleElement { it.message.startsWith("invalid: react@18.2.0") }
-            elsproblems shouldHaveSingleElement { it.message.startsWith("invalid: react-dom@18.2.0") }
-        }
-
-        "resolve dependencies even if the 'node_modules' directory already exists" {
-            val definitionFile = getAssetFile("projects/synthetic/npm/node-modules/package.json")
-            val expectedResultFile = getAssetFile("projects/synthetic/npm/node-modules-expected-output.yml")
-
-            val result = create("NPM").resolveSingleProject(definitionFile, resolveScopes = true)
-
-            patchActualResult(result.toYaml()) should matchExpectedResult(
-                expectedResultFile,
-                definitionFile,
-                custom = mapOf("<REPLACE_LOCKFILE_NAME>" to "package-lock.json")
-            )
-        }
-
-        "resolve Babel dependencies correctly" {
-            val definitionFile = getAssetFile("projects/synthetic/npm/babel/package.json")
-            val expectedResultFile = getAssetFile("projects/synthetic/npm/babel-expected-output.yml")
-            val expectedResult = patchExpectedResult(expectedResultFile, definitionFile)
-                .fromYaml<ProjectAnalyzerResult>()
-
-            val result = create("NPM").resolveSingleProject(definitionFile, resolveScopes = true)
-
-            result.withInvariantIssues().toYaml() shouldBe expectedResult.withInvariantIssues().toYaml()
-        }
-
-        "resolve dependencies with URLs as versions correctly" {
-            val definitionFile = getAssetFile("projects/synthetic/npm/version-urls/package.json")
-            val expectedResultFile = getAssetFile("projects/synthetic/npm/version-urls-expected-output.yml")
-            val expectedResult = patchExpectedResult(expectedResultFile, definitionFile)
-                .fromYaml<ProjectAnalyzerResult>()
-
-            val result = create("NPM", allowDynamicVersions = true)
-                .resolveSingleProject(definitionFile, resolveScopes = true)
-
-            result.withInvariantIssues().toYaml() shouldBe expectedResult.withInvariantIssues().toYaml()
-        }
+        result.withInvariantIssues().toYaml() shouldBe expectedResult.withInvariantIssues().toYaml()
     }
 })
