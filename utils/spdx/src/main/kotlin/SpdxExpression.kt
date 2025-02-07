oss-review-toolkit/ort

commit 31b9be83c5793954a7d052a0f1e7e3ca4d9384bd

feat(spdx-utils): Simplify `and` / `or` operators for equal operands
Do not even create a copy if operands are equal. Resolves #8714.

Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

utils/spdx/src/main/kotlin/SpdxExpression.kt
@@ -200,12 +200,14 @@ sealed class SpdxExpression {
     /**
      * Concatenate [this][SpdxExpression] and [other] using [SpdxOperator.AND].
      */
-    infix fun and(other: SpdxExpression) = SpdxCompoundExpression(SpdxOperator.AND, listOf(this, other))
+    infix fun and(other: SpdxExpression) =
+        takeIf { this == other } ?: SpdxCompoundExpression(SpdxOperator.AND, listOf(this, other))
 
     /**
      * Concatenate [this][SpdxExpression] and [other] using [SpdxOperator.OR].
      */
-    infix fun or(other: SpdxExpression) = SpdxCompoundExpression(SpdxOperator.OR, listOf(this, other))
+    infix fun or(other: SpdxExpression) =
+        takeIf { this == other } ?: SpdxCompoundExpression(SpdxOperator.OR, listOf(this, other))
 }
 
 /**

utils/spdx/src/test/kotlin/SpdxExpressionChoiceTest.kt
@@ -336,9 +336,9 @@ class SpdxExpressionChoiceTest : WordSpec({
             val choices = "(a OR b) AND (a OR b)".toSpdx().validChoices()
 
             choices.map { it.toString() } should containExactlyInAnyOrder(
-                "a AND a",
+                "a",
                 "b AND a",
-                "b AND b"
+                "b"
             )
         }
     }

plugins/reporters/spdx/src/funTest/assets/disclosure-cli-expected-output.spdx.yml
@@ -273,7 +273,7 @@ packages:
   filesAnalyzed: false
   homepage: "NONE"
   licenseConcluded: "NOASSERTION"
-  licenseDeclared: "Apache-2.0 AND Apache-2.0 AND Apache-2.0 AND MIT AND MIT AND MIT"
+  licenseDeclared: "Apache-2.0 AND MIT AND MIT"
   name: "gopkg.in/yaml.v3"
   summary: "NONE"
   versionInfo: "3.0.1"
@@ -288,7 +288,7 @@ packages:
   filesAnalyzed: false
   homepage: "NONE"
   licenseConcluded: "NOASSERTION"
-  licenseDeclared: "Apache-2.0 AND Apache-2.0 AND Apache-2.0 AND MIT AND MIT AND MIT"
+  licenseDeclared: "Apache-2.0 AND MIT AND MIT"
   name: "gopkg.in/yaml.v3"
   summary: "NONE"
   versionInfo: "3.0.1"

commit a212b72b3473a02d1549e61d51c64ada7af7e73a

fix(spdx-utils): Fix a performance issue in `SpdxExpression::and()`
31b9be8 introduced an `equals()` into `SpdxCompoundExpression::and`. For
packages with large amounts of detected licenses including ones with
`OR` operators, the performance of the `and` function becomes so poor
that the evaluator runs for 3 days (with the open source `ort-config`
rules, without terminating. Also the reporter performance degrades from
less than 30 seconds to 3 hours.

Unfortunately the call tree involved is so complicated that there is no
simple fix for this. `equals()` involves recursive `validChoices()`
calls which in turn call `equals()` again when computing distinctness or
inserting further `SpdxExpressions` into sets.

Revert the change to fix the problem in a timely manner. Note that it
makes sense to bring back the now reverted functionality, but the code
should be refactored first to produce more managable call trees.

Fixes: #9902.

This reverts commit 31b9be83c5793954a7d052a0f1e7e3ca4d9384bd.

Signed-off-by: Frank Viernau <x9fviern@zeiss.com>

utils/spdx/src/main/kotlin/SpdxExpression.kt
@@ -205,14 +205,12 @@ sealed class SpdxExpression {
     /**
      * Concatenate [this][SpdxExpression] and [other] using [SpdxOperator.AND].
      */
-    infix fun and(other: SpdxExpression) =
-        takeIf { this == other } ?: SpdxCompoundExpression(SpdxOperator.AND, listOf(this, other))
+    infix fun and(other: SpdxExpression) = SpdxCompoundExpression(SpdxOperator.AND, listOf(this, other))
 
     /**
      * Concatenate [this][SpdxExpression] and [other] using [SpdxOperator.OR].
      */
-    infix fun or(other: SpdxExpression) =
-        takeIf { this == other } ?: SpdxCompoundExpression(SpdxOperator.OR, listOf(this, other))
+    infix fun or(other: SpdxExpression) = SpdxCompoundExpression(SpdxOperator.OR, listOf(this, other))
 }
 
 /**

utils/spdx/src/test/kotlin/SpdxExpressionChoiceTest.kt
@@ -342,9 +342,9 @@ class SpdxExpressionChoiceTest : WordSpec({
             val choices = "(a OR b) AND (a OR b)".toSpdx().validChoices()
 
             choices.map { it.toString() } should containExactlyInAnyOrder(
-                "a",
+                "a AND a",
                 "b AND a",
-                "b"
+                "b AND b"
             )
         }
 