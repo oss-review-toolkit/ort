3bf082177355a8c65bc13cc2c68475b9c967b93f
Sebastian Schuberth <sebastian@doubleopen.org>
2025-02-17 16:08:49 +0100

    chore(spdx-utils): Say "simple mapping" instead of "custom mapping"
    Match the name of the file containing the mapping by calling variables
    after "simple mapping", which also is a bit more specific than "custom
    mapping".
    
    While at it, also distinguish between mapping to licenses or
    expressions. For consistency this is also done for the mapping of
    deprecated expressions.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- utils/spdx/src/main/kotlin/SpdxSimpleLicenseMapping.kt
+++ utils/spdx/src/main/kotlin/SpdxSimpleLicenseMapping.kt
@@ -28,23 +28,24 @@ import com.fasterxml.jackson.module.kotlin.readValue
  */
 object SpdxSimpleLicenseMapping {
     /**
-     * The raw map which associates custom license IDs with their corresponding SPDX license ID.
+     * The map of simple license names associated with their corresponding [SPDX license][SpdxLicense].
      */
-    internal val customLicenseIdsMap by lazy {
+    internal val simpleLicenseMapping by lazy {
         val resource = checkNotNull(javaClass.getResource("/simple-license-mapping.yml"))
         yamlMapper.readValue<Map<String, SpdxLicense>>(resource)
     }
 
     /**
-     * The map of custom license ids associated with their corresponding SPDX expression.
+     * The map of simple license names associated with their corresponding [SPDX expression][SpdxLicenseIdExpression].
      */
-    internal val customLicenseIds = customLicenseIdsMap.mapValues { (_, v) -> v.toExpression() }
+    internal val simpleExpressionMapping = simpleLicenseMapping.mapValues { (_, v) -> v.toExpression() }
         .toSortedMap(String.CASE_INSENSITIVE_ORDER)
 
     /**
-     * The map of deprecated SPDX license ids associated with their current SPDX expression.
+     * The map of deprecated SPDX license IDs associated with their current [SPDX expression]
+     * [SpdxSingleLicenseExpression].
      */
-    private val deprecatedLicenseIds by lazy {
+    private val deprecatedExpressionMapping by lazy {
         val resource = checkNotNull(javaClass.getResource("/deprecated-license-mapping.yml"))
         yamlMapper.readValue<Map<String, SpdxSingleLicenseExpression>>(resource)
     }
@@ -52,7 +53,7 @@ object SpdxSimpleLicenseMapping {
     /**
      * The map of varied SPDX license ids associated with their corresponding SPDX expression.
      */
-    val mapping = (customLicenseIds + deprecatedLicenseIds).toSortedMap(String.CASE_INSENSITIVE_ORDER)
+    val mapping = (simpleExpressionMapping + deprecatedExpressionMapping).toSortedMap(String.CASE_INSENSITIVE_ORDER)
 
     /**
      * Return the [SpdxExpression] the [license] id maps to, or null if there is no corresponding expression. If
@@ -60,5 +61,5 @@ object SpdxSimpleLicenseMapping {
      * corresponding current expression, otherwise they are mapped to their corresponding deprecated expression.
      */
     fun map(license: String, mapDeprecated: Boolean = true) =
-        (if (mapDeprecated) mapping else customLicenseIds)[license] ?: SpdxLicense.forId(license)?.toExpression()
+        (if (mapDeprecated) mapping else simpleExpressionMapping)[license] ?: SpdxLicense.forId(license)?.toExpression()
 }
