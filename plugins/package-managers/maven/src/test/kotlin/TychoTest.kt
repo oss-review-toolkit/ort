961003cf45e20c5c1d337b0bf06ebb899e40f28e
Oliver Heger <oliver.heger@bosch.io>
2025-02-20 12:11:32 +0100

    feat(Maven): Generate hints about auto-generated POMs for Tycho
    To make the required functionality available to the Tycho
    implementation, extract it as an extension function.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

--- plugins/package-managers/maven/src/test/kotlin/TychoTest.kt
+++ plugins/package-managers/maven/src/test/kotlin/TychoTest.kt
@@ -19,11 +19,35 @@
 
 package org.ossreviewtoolkit.plugins.packagemanagers.maven
 
+import io.kotest.core.TestConfiguration
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.engine.spec.tempdir
+import io.kotest.engine.spec.tempfile
+import io.kotest.matchers.collections.beEmpty
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.spyk
+
+import java.io.File
+import java.io.PrintStream
+
+import org.apache.maven.cli.MavenCli
+import org.apache.maven.execution.MavenSession
+import org.apache.maven.project.MavenProject
+
+import org.eclipse.aether.graph.DependencyNode
+
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.Severity
+import org.ossreviewtoolkit.model.utils.DependencyGraphBuilder
+import org.ossreviewtoolkit.plugins.packagemanagers.maven.utils.DependencyTreeMojoNode
+import org.ossreviewtoolkit.plugins.packagemanagers.maven.utils.JSON
 
 class TychoTest : WordSpec({
     "mapDefinitionFiles()" should {
@@ -56,4 +80,98 @@ class TychoTest : WordSpec({
             mappedDefinitionFiles shouldContainExactlyInAnyOrder listOf(tychoDefinitionFile1, tychoDefinitionFile2)
         }
     }
+
+    "resolveDependencies()" should {
+        "generate warnings about auto-generated pom files" {
+            val definitionFile = tempfile()
+            val rootProject = createMavenProject("root", definitionFile)
+            val subProject = createMavenProject("sub")
+            val projectsList = listOf(rootProject, subProject)
+
+            val tycho = spyk(Tycho("Tycho", tempdir(), mockk(relaxed = true), mockk(relaxed = true)))
+            injectCliMock(tycho, projectsList.toJson(), projectsList)
+
+            val pkg1 = mockk<Package>(relaxed = true)
+            val pkg2 = mockk<Package> {
+                every { id } returns Identifier("Tycho:org.ossreviewtoolkit:sub-dependency:1.0.0")
+                every { description } returns "POM was created by Sonatype Nexus"
+            }
+
+            val graphBuilder = mockk<DependencyGraphBuilder<DependencyNode>>(relaxed = true) {
+                every { packages() } returns setOf(pkg1, pkg2)
+            }
+
+            every { tycho.createGraphBuilder(any(), any()) } returns graphBuilder
+
+            val results = tycho.resolveDependencies(definitionFile, emptyMap())
+
+            val (rootResults, subResults) = results.partition { it.project.id.name == "root" }
+
+            with(rootResults.single().issues.single()) {
+                severity shouldBe Severity.HINT
+                message shouldContain pkg2.id.toCoordinates()
+                message shouldContain "auto-generated POM"
+                source shouldBe "Tycho"
+            }
+
+            subResults.single().issues should beEmpty()
+        }
+    }
 })
+
+/**
+ * Create a mock [MavenProject] with default coordinates and the given [name] and optional [definitionFile].
+ */
+private fun TestConfiguration.createMavenProject(name: String, definitionFile: File = tempfile()): MavenProject =
+    mockk(relaxed = true) {
+        every { groupId } returns "org.ossreviewtoolkit"
+        every { artifactId } returns name
+        every { version } returns "1.0.0"
+        every { file } returns definitionFile
+        every { id } returns "org.ossreviewtoolkit:$name:jar:1.0.0"
+        every { parent } returns null
+    }
+
+/**
+ * Return a [DependencyTreeMojoNode] with the properties of this [MavenProject].
+ */
+private fun MavenProject.toDependencyTreeMojoNode(): DependencyTreeMojoNode =
+    DependencyTreeMojoNode(groupId, artifactId, version, "jar", "compile", "")
+
+/**
+ * Generate JSON output analogously to the Maven Dependency Plugin for this list of [MavenProject]s. Here only the
+ * root project nodes are relevant; dependencies are not included.
+ */
+private fun Collection<MavenProject>.toJson(): String =
+    joinToString("\n") { JSON.encodeToString(it.toDependencyTreeMojoNode()) }
+
+/**
+ * Create a mock for a [MavenCli] object and configure the given [tycho] spi to use it. The mock CLI simulates a
+ * Maven build that produces the given [buildOutput] and detects the given [projectsList]. It returns the given
+ * [exitCode].
+ */
+private fun injectCliMock(
+    tycho: Tycho,
+    buildOutput: String,
+    projectsList: List<MavenProject>,
+    exitCode: Int = 0
+): MavenCli {
+    val cli = mockk<MavenCli> {
+        every { doMain(any(), any(), any(), any()) } returns exitCode
+    }
+
+    val session = mockk<MavenSession> {
+        every { projects } returns projectsList
+    }
+
+    every { tycho.createMavenCli(any(), any()) } answers {
+        val collector = firstArg<TychoProjectsCollector>()
+        collector.afterSessionEnd(session)
+
+        val out = secondArg<PrintStream>()
+        out.println(buildOutput)
+        cli
+    }
+
+    return cli
+}
