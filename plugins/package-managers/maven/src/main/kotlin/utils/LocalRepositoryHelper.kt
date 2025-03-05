c6eb3a2f4f499dbf2ecbcd6b63c79764b9ca6563
Oliver Heger <oliver.heger@bosch.io>
2025-03-05 08:52:21 +0100

    feat(Maven): Add functionality to parse artifacts files
    In the local Maven repository, Tycho creates files ending with
    `-p2artifacts.xml` that contain properties about artifacts. This
    information is needed to construct proper `RemoteArtifact`s for
    packages downloaded from a P2 repository. Therefore, extend
    `LocalRepositoryHelper` to support reading such files.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

--- plugins/package-managers/maven/src/main/kotlin/utils/LocalRepositoryHelper.kt
+++ plugins/package-managers/maven/src/main/kotlin/utils/LocalRepositoryHelper.kt
@@ -23,12 +23,17 @@ import java.io.File
 import java.util.jar.JarInputStream
 import java.util.jar.Manifest
 
+import javax.xml.parsers.SAXParserFactory
+
 import kotlin.io.resolve
 
 import org.apache.logging.log4j.kotlin.logger
 import org.apache.maven.repository.RepositorySystem
 
 import org.eclipse.aether.artifact.Artifact
+
+import org.xml.sax.Attributes
+import org.xml.sax.helpers.DefaultHandler
 
 /**
  * A class providing some helper functionality for accessing artifacts and metadata in the local Maven repository.
@@ -43,6 +48,49 @@ class LocalRepositoryHelper(
 
         /** The name of the folder that stores OSGi bundles. */
         private const val BUNDLE_ROOT = "osgi/bundle"
+
+        /**
+         * The suffix of the file with properties for P2 artifacts. When downloading artifacts from a P2 repository,
+         * Tycho creates an additional file with the same base name as the artifact JAR file, but with this suffix that
+         * contains further metadata.
+         */
+        private const val ARTIFACTS_FILE_SUFFIX = "p2artifacts.xml"
+
+        /**
+         * Parse the given [file] with information about artifacts and extract the properties for the given [artifact].
+         */
+        private fun parseArtifactsFile(file: File, artifact: Artifact): Map<String, String> {
+            val properties = mutableMapOf<String, MutableMap<String, String>>()
+
+            val handler = object : DefaultHandler() {
+                private var currentArtifactId = ""
+
+                override fun startElement(uri: String?, localName: String, qName: String, attributes: Attributes) {
+                    when (qName) {
+                        "artifact" -> {
+                            currentArtifactId = "${artifact.groupId}:${attributes.getValue("id")}:" +
+                                attributes.getValue("version")
+                        }
+
+                        "property" -> {
+                            val key = attributes.getValue("name")
+                            val value = attributes.getValue("value")
+
+                            val artifactProperties = properties.getOrPut(currentArtifactId) { mutableMapOf() }
+                            artifactProperties[key] = value
+                        }
+                    }
+                }
+            }
+
+            val factory = SAXParserFactory.newInstance()
+            val parser = factory.newSAXParser()
+
+            logger.info { "Parsing P2 artifacts file '$file'." }
+
+            parser.parse(file, handler)
+            return properties[artifact.identifier()].orEmpty()
+        }
     }
 
     /**
@@ -76,5 +124,21 @@ class LocalRepositoryHelper(
             JarInputStream(artifactFile.inputStream()).use { jar ->
                 jar.manifest ?: Manifest()
             }
+        }
+
+    /**
+     * Read the XML file with properties for the given P2 [artifact] and return the properties as a [Map]. If the
+     * artifact cannot be resolved, return *null*. If the XML file does not exist or cannot be parsed, return an empty
+     * [Map].
+     */
+    fun p2Properties(artifact: Artifact): Map<String, String>? =
+        folderForOsgiArtifact(artifact)?.let { artifactsFolder ->
+            val artifactsFile =
+                artifactsFolder.resolve("${artifact.artifactId}-${artifact.version}-$ARTIFACTS_FILE_SUFFIX")
+            runCatching {
+                parseArtifactsFile(artifactsFile, artifact)
+            }.onFailure {
+                logger.error(it) { "Could not parse P2 artifacts file '$artifactsFile'." }
+            }.getOrDefault(emptyMap())
         }
 }
