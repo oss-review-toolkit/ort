332467ece679fd5b307f381964637f906fe1d996
Oliver Heger <oliver.heger@bosch.io>
2025-03-05 12:20:12 +0100

    feat(Maven): Add a class to create artifacts for OSGi dependencies
    So far, the Tycho implementation creates only dummy `RemoteArtifact`
    objects for packages representing OSGi bundles. This is because no
    information about the repositories from which bundles were downloaded
    is available.
    
    To improve this, introduce a new `P2ArtifactTracker` class that is
    able to detect downloads during the running Tycho build.
    Unfortunately, no way could be found to obtain this information via
    some Maven API, such as event listeners or the repository session.
    Therefore, it is extracted from log output generated during the build.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ plugins/package-managers/maven/src/main/kotlin/utils/P2ArtifactTracker.kt
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2025 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.plugins.packagemanagers.maven.utils
+
+import java.util.concurrent.ConcurrentHashMap
+
+import org.apache.logging.log4j.kotlin.logger
+
+import org.codehaus.plexus.DefaultPlexusContainer
+import org.codehaus.plexus.PlexusContainer
+import org.codehaus.plexus.logging.BaseLoggerManager
+import org.codehaus.plexus.logging.Logger
+
+import org.eclipse.aether.artifact.Artifact
+
+import org.ossreviewtoolkit.model.Hash
+import org.ossreviewtoolkit.model.HashAlgorithm
+import org.ossreviewtoolkit.model.RemoteArtifact
+
+/**
+ * A helper class for Tycho that tracks the artifacts downloaded from P2 repositories.
+ *
+ * The information collected by this class is used by the Tycho package manager implementation to generate correct
+ * remote artifacts for package binaries and source bundles. Unfortunately, it is rather hard to gather this
+ * information. The Tycho extension circumvents Maven's standard artifact resolution, so that access via the Maven
+ * session, the repository system, or event listener APIs is not possible. What this class does instead is to monitor
+ * the build log output for messages about downloaded artifacts. It is then possible to associate artifacts with the
+ * source URLs from where they have been loaded. This might be a bit fragile, but it has the advantage that no further
+ * steps are required to locate artifacts in repositories; the work done by Tycho during the build is basically reused.
+ */
+class P2ArtifactTracker(
+    /** The helper object for accessing the local Maven repository. */
+    private val repositoryHelper: LocalRepositoryHelper
+) {
+    companion object {
+        /** The name of the logger used by Tycho for log messages about downloaded artifacts. */
+        internal const val TYCHO_TRANSPORT_LOGGER = "org.eclipse.tycho.p2maven.transport.TychoRepositoryTransport"
+
+        /**
+         * A regular expression to detect log messages about downloaded artifacts. Via the groups, the ID and the
+         * version of the affected artifact can be extracted.
+         */
+        private val regexDownload = Regex("Downloaded from p2: ((.+)/([^/]*)_(.+)\\.jar).*")
+
+        /** The prefix for properties in the local Maven repository that store checksums for downloaded artifacts. */
+        private const val CHECKSUM_PROPERTY_PREFIX = "download.checksum.sha-"
+
+        /** An ordered list with different hash algorithms to try when looking for checksums. */
+        private val hashAlgorithms = listOf(
+            "512" to HashAlgorithm.SHA512,
+            "256" to HashAlgorithm.SHA256,
+            "1" to HashAlgorithm.SHA1
+        )
+
+        /**
+         * Perform the necessary setup in the given [container], so that the provided [tracker] receives log messages
+         * about downloaded artifacts.
+         */
+        fun install(tracker: P2ArtifactTracker, container: PlexusContainer) {
+            (container as? DefaultPlexusContainer)?.loggerManager = object : BaseLoggerManager() {
+                override fun createLogger(name: String): Logger =
+                    if (TYCHO_TRANSPORT_LOGGER == name) {
+                        createDownloadTrackingLogger(tracker.downloadedArtifacts)
+                    } else {
+                        MavenLogger(logger.delegate.level)
+                    }
+            }.also {
+                logger.info { "Installed logger for tracking P2 downloads." }
+            }
+        }
+
+        /**
+         * Create a special [Logger] that tracks log messages about downloaded artifacts and stores the extracted
+         * information in the given [trackingMap].
+         */
+        private fun createDownloadTrackingLogger(trackingMap: MutableMap<String, String>): Logger {
+            val mavenLogger = MavenLogger(logger.delegate.level)
+
+            return object : Logger by mavenLogger {
+                override fun info(message: String) {
+                    mavenLogger.info(message)
+
+                    regexDownload.matchEntire(message)?.also { result ->
+                        val sourceUrl = result.groupValues[1]
+                        val artifactId = result.groupValues[3]
+                        val version = result.groupValues[4]
+                        trackingMap[downloadKey(artifactId, version)] = sourceUrl
+
+                        logger.info { "Recorded source URL '$sourceUrl' for '$artifactId:$version'." }
+                    }
+                }
+            }
+        }
+
+        /**
+         * Generate a key into the map with download information for the given [artifactId] and [version].
+         */
+        private fun downloadKey(artifactId: String, version: String) = "$artifactId:$version"
+
+        /**
+         * Return an [Artifact] to represent the source code bundle of the given [artifact].
+         */
+        private fun mapToSourceArtifact(artifact: Artifact): Artifact =
+            object : Artifact by artifact {
+                override fun getArtifactId(): String = "${artifact.artifactId}.source"
+            }
+    }
+
+    /** The [Map] with information about downloaded artifacts. */
+    private val downloadedArtifacts = ConcurrentHashMap<String, String>()
+
+    /**
+     * Construct a [RemoteArtifact] object for the binary of the given [artifact] that contains all information
+     * available. If this artifact has been downloaded from a P2 repository, set the correct source URL. Also try to
+     * obtain a checksum from the properties stored in the local Maven repository.
+     */
+    fun getBinaryArtifactFor(artifact: Artifact): RemoteArtifact =
+        downloadedArtifacts[downloadKey(artifact.artifactId, artifact.version)]?.let { sourceUrl ->
+            RemoteArtifact(sourceUrl, findHash(artifact))
+        } ?: RemoteArtifact.EMPTY
+
+    /**
+     * Construct a [RemoteArtifact] object for the source code bundle of the given [artifact] that contains all
+     * information available. This function works analogously to [getSourceArtifactFor], but looks for an artifact
+     * with the extension `.source`. This is the way how Tycho handles source code bundles.
+     */
+    fun getSourceArtifactFor(artifact: Artifact): RemoteArtifact = getBinaryArtifactFor(mapToSourceArtifact(artifact))
+
+    /**
+     * Try to find a hash value for the given [artifact] in the local Maven repository. Use the strongest hash value
+     * available. Return [Hash.NONE] if no hash value can be found.
+     */
+    private fun findHash(artifact: Artifact): Hash =
+        repositoryHelper.p2Properties(artifact)?.let { properties ->
+            hashAlgorithms.firstOrNull { "$CHECKSUM_PROPERTY_PREFIX${it.first}" in properties }
+                ?.let { (size, algorithm) ->
+                    Hash(properties.getValue("${CHECKSUM_PROPERTY_PREFIX}$size"), algorithm)
+                }
+        } ?: Hash.NONE
+}
---
+++ plugins/package-managers/maven/src/test/kotlin/utils/P2ArtifactTrackerTest.kt
@@ -0,0 +1,234 @@
+/*
+ * Copyright (C) 2025 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.plugins.packagemanagers.maven.utils
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.beInstanceOf
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+
+import org.codehaus.plexus.DefaultPlexusContainer
+import org.codehaus.plexus.logging.Logger
+import org.codehaus.plexus.logging.LoggerManager
+
+import org.eclipse.aether.artifact.Artifact
+import org.eclipse.aether.artifact.DefaultArtifact
+
+import org.ossreviewtoolkit.model.Hash
+import org.ossreviewtoolkit.model.HashAlgorithm
+import org.ossreviewtoolkit.model.RemoteArtifact
+
+class P2ArtifactTrackerTest : WordSpec({
+    "install()" should {
+        "install a log manager that handles other components correctly" {
+            val (_, logManager) = getInstanceWithLoggerManager()
+
+            val logger = logManager.getLoggerForComponent("org.eclipse.tycho.p2maven.transport.OtherComponent")
+
+            logger should beInstanceOf<MavenLogger>()
+        }
+    }
+
+    "getBinaryArtifactFor()" should {
+        "return an empty artifact if no information about the artifact is available" {
+            val (tracker, _) = getInstanceWithLogger()
+
+            tracker.getBinaryArtifactFor(testArtifact) shouldBe RemoteArtifact.EMPTY
+        }
+
+        "return an artifact with the correct source URL" {
+            val (tracker, logger) = getInstanceWithLogger()
+            logger.logDownload(ARTIFACT_URL)
+
+            val artifact = tracker.getBinaryArtifactFor(testArtifact)
+
+            artifact.url shouldBe ARTIFACT_URL
+        }
+
+        "return an artifact with a correct SHA-512 hash" {
+            val hashValue = "ada37fcc95884a4d2cbc64495f5f67556c847e7724e26ccfbb15cc42a476436fa54b5d4fd4d9ed340241" +
+                "d848999d415e1cff07045d9e97d451c16aeed4911045"
+            val properties = mapOf(
+                "download.checksum.sha-512" to hashValue,
+                "download.checksum.sha-256" to "SHA-256",
+                "download.checksum.sha-1" to "SHA-1"
+            )
+            val repositoryHelper = mockk<LocalRepositoryHelper> {
+                every { p2Properties(testArtifact) } returns properties
+            }
+
+            val (tracker, logger) = getInstanceWithLogger(repositoryHelper)
+            logger.logDownload(ARTIFACT_URL)
+
+            val artifact = tracker.getBinaryArtifactFor(testArtifact)
+
+            artifact.hash shouldBe Hash(hashValue, HashAlgorithm.SHA512)
+        }
+
+        "return an artifact with a correct SHA-256 hash if no stronger hash is available" {
+            val hashValue = "adf46d5e34940bdf148ecdd26a9ee8eea94496a72034ff7141066b3eea5c4e9d"
+            val properties = mapOf(
+                "download.checksum.sha-256" to hashValue,
+                "download.checksum.sha-1" to "SHA-1"
+            )
+            val repositoryHelper = mockk<LocalRepositoryHelper> {
+                every { p2Properties(testArtifact) } returns properties
+            }
+
+            val (tracker, logger) = getInstanceWithLogger(repositoryHelper)
+            logger.logDownload(ARTIFACT_URL)
+
+            val artifact = tracker.getBinaryArtifactFor(testArtifact)
+
+            artifact.hash shouldBe Hash(hashValue, HashAlgorithm.SHA256)
+        }
+
+        "return an artifact with a correct SHA-1 hash if no stronger hash is available" {
+            val hashValue = "073d7b3086e14beb604ced229c302feff6449723"
+            val properties = mapOf("download.checksum.sha-1" to hashValue)
+            val repositoryHelper = mockk<LocalRepositoryHelper> {
+                every { p2Properties(testArtifact) } returns properties
+            }
+
+            val (tracker, logger) = getInstanceWithLogger(repositoryHelper)
+            logger.logDownload(ARTIFACT_URL)
+
+            val artifact = tracker.getBinaryArtifactFor(testArtifact)
+
+            artifact.hash shouldBe Hash(hashValue, HashAlgorithm.SHA1)
+        }
+
+        "return an artifact with an empty hash if no hash property is available" {
+            val repositoryHelper = mockk<LocalRepositoryHelper> {
+                every { p2Properties(testArtifact) } returns emptyMap()
+            }
+
+            val (tracker, logger) = getInstanceWithLogger(repositoryHelper)
+            logger.logDownload(ARTIFACT_URL)
+
+            val artifact = tracker.getBinaryArtifactFor(testArtifact)
+
+            artifact.hash shouldBe Hash.NONE
+        }
+
+        "return an artifact with an empty hash if the file with properties is not available" {
+            val repositoryHelper = mockk<LocalRepositoryHelper> {
+                every { p2Properties(testArtifact) } returns null
+            }
+
+            val (tracker, logger) = getInstanceWithLogger(repositoryHelper)
+            logger.logDownload(ARTIFACT_URL)
+
+            val artifact = tracker.getBinaryArtifactFor(testArtifact)
+
+            artifact.hash shouldBe Hash.NONE
+        }
+    }
+
+    "getSourceArtifactFor()" should {
+        "return an empty artifact if no information about the artifact is available" {
+            val (tracker, _) = getInstanceWithLogger()
+
+            tracker.getSourceArtifactFor(testArtifact) shouldBe RemoteArtifact.EMPTY
+        }
+
+        "return an artifact with the information available" {
+            val sourceArtifactId = "org.some.component.source"
+            val sourceArtifactUrl = "$REPOSITORY_URL/plugins/${sourceArtifactId}_${testArtifact.version}.jar"
+            val hashValue = "073d7b3086e14beb604ced229c302feff6449723"
+            val properties = mapOf("download.checksum.sha-1" to hashValue)
+            val repositoryHelper = mockk<LocalRepositoryHelper> {
+                every { p2Properties(any()) } returns properties
+            }
+
+            val (tracker, logger) = getInstanceWithLogger(repositoryHelper)
+            logger.logDownload(sourceArtifactUrl)
+
+            val artifact = tracker.getSourceArtifactFor(testArtifact)
+
+            artifact.url shouldBe sourceArtifactUrl
+            artifact.hash shouldBe Hash(hashValue, HashAlgorithm.SHA1)
+
+            val slotArtifact = slot<Artifact>()
+            verify {
+                repositoryHelper.p2Properties(capture(slotArtifact))
+            }
+
+            slotArtifact.captured.artifactId shouldBe sourceArtifactId
+        }
+    }
+})
+
+/** A test artifact to be looked up. */
+private val testArtifact = DefaultArtifact("test-group", "org.some.component", "jar", "0.0.1")
+
+/** URL of a test repository. */
+private const val REPOSITORY_URL = "https://p2.example.com/repo"
+
+/** The URL of the test artifact in the test repository. */
+private val ARTIFACT_URL = "$REPOSITORY_URL/plugins/${testArtifact.artifactId}_${testArtifact.version}.jar"
+
+/**
+ * Create a test [P2ArtifactTracker] instance that uses the given optional [repositoryHelper] and install it into a
+ * container. Return this instance and the special [LoggerManager] that is used to gather logs about artifact
+ * downloads.
+ */
+private fun getInstanceWithLoggerManager(
+    repositoryHelper: LocalRepositoryHelper = mockk()
+): Pair<P2ArtifactTracker, LoggerManager> {
+    val container = mockk<DefaultPlexusContainer> {
+        every { loggerManager = any() } returns Unit
+    }
+
+    val tracker = P2ArtifactTracker(repositoryHelper)
+    P2ArtifactTracker.install(tracker, container)
+
+    val slotLoggerManager = slot<LoggerManager>()
+    verify {
+        container.loggerManager = capture(slotLoggerManager)
+    }
+
+    return tracker to slotLoggerManager.captured
+}
+
+/**
+ * Create a test [P2ArtifactTracker] instance that uses the given optional [repositoryHelper] and install it into a
+ * container. Return this instance and the special [Logger] that is used to gather logs about artifact downloads.
+ * By writing messages to this logger, the tracker instance is populated.
+ */
+private fun getInstanceWithLogger(
+    repositoryHelper: LocalRepositoryHelper = mockk(relaxed = true)
+): Pair<P2ArtifactTracker, Logger> {
+    val (tracker, loggerManager) = getInstanceWithLoggerManager(repositoryHelper)
+
+    return tracker to loggerManager.getLoggerForComponent(P2ArtifactTracker.TYCHO_TRANSPORT_LOGGER)
+}
+
+/**
+ * Log a message about an artifact being downloaded from the given [artifactUrl].
+ */
+private fun Logger.logDownload(artifactUrl: String) {
+    info("Downloaded from p2: $artifactUrl (42 KB at 123 KB/s)")
+}
