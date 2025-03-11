89701d13a495d074ee1d01b1da39b3e738f0f8e6
Oliver Heger <oliver.heger@bosch.io>
2025-03-11 16:14:53 +0100

    chore(Maven): Remove obsolete code from `LocalRepositoryHelper`
    The parsing of `p2artifacts.xml` files from the local Maven repository
    is no longer needed, since this information is now obtained from the
    P2 repository that hosts the artifact.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

--- plugins/package-managers/maven/src/main/kotlin/tycho/LocalRepositoryHelper.kt
+++ plugins/package-managers/maven/src/main/kotlin/tycho/LocalRepositoryHelper.kt
@@ -23,7 +23,6 @@ import java.io.File
 import java.util.jar.JarInputStream
 import java.util.jar.Manifest
 
-import kotlin.collections.getOrPut
 import kotlin.io.resolve
 
 import org.apache.logging.log4j.kotlin.logger
@@ -46,32 +45,6 @@ internal class LocalRepositoryHelper(
 
         /** The name of the folder that stores OSGi bundles. */
         private const val BUNDLE_ROOT = "osgi/bundle"
-
-        /**
-         * The suffix of the file with properties for P2 artifacts. When downloading artifacts from a P2 repository,
-         * Tycho creates an additional file with the same base name as the artifact JAR file, but with this suffix that
-         * contains further metadata.
-         */
-        private const val ARTIFACTS_FILE_SUFFIX = "p2artifacts.xml"
-
-        /**
-         * Parse the given [file] with information about artifacts and extract the properties for the given [artifact].
-         */
-        private fun parseArtifactsFile(file: File, artifact: Artifact): Map<String, String> {
-            val handler = ElementHandler(ParsePropertiesState())
-                .handleElement("artifact") { state, attributes ->
-                    state.withCurrentArtifactId(
-                        bundleIdentifier(attributes.getValue("id"), attributes.getValue("version"))
-                    )
-                }.handleElement("property") { state, attributes ->
-                    state.withProperty(attributes.getValue("name"), attributes.getValue("value"))
-                }
-
-            logger.info { "Parsing P2 artifacts file '$file'." }
-
-            val result = parseXml(file, handler)
-            return result.properties[bundleIdentifier(artifact.artifactId, artifact.version)].orEmpty()
-        }
     }
 
     /**
@@ -106,52 +79,4 @@ internal class LocalRepositoryHelper(
                 jar.manifest ?: Manifest()
             }
         }
-
-    /**
-     * Read the XML file with properties for the given P2 [artifact] and return the properties as a [Map]. If the
-     * artifact cannot be resolved, return *null*. If the XML file does not exist or cannot be parsed, return an empty
-     * [Map].
-     */
-    fun p2Properties(artifact: Artifact): Map<String, String>? =
-        folderForOsgiArtifact(artifact)?.let { artifactsFolder ->
-            val artifactsFile =
-                artifactsFolder.resolve("${artifact.artifactId}-${artifact.version}-$ARTIFACTS_FILE_SUFFIX")
-            runCatching {
-                parseArtifactsFile(artifactsFile, artifact)
-            }.onFailure {
-                logger.error(it) { "Could not parse P2 artifacts file '$artifactsFile'." }
-            }.getOrDefault(emptyMap())
-        }
-}
-
-/**
- * Generate an identifier for an OSGi bundle artifact based on the given [artifactId] and [version].
- */
-private fun bundleIdentifier(artifactId: String, version: String) = "$artifactId:$version"
-
-/**
- * A data class to hold the state while parsing the properties of a P2 artifact.
- */
-private data class ParsePropertiesState(
-    /** The ID of the artifact whose properties are currently processed. */
-    val currentArtifactId: String = "",
-
-    /** A [Map] with the aggregated properties of all encountered artifacts. */
-    val properties: MutableMap<String, MutableMap<String, String>> = mutableMapOf<String, MutableMap<String, String>>()
-) {
-    /**
-     * Return an update [ParsePropertiesState] instance that has the given [artifactId] set as current artifact.
-     */
-    fun withCurrentArtifactId(artifactId: String) = copy(currentArtifactId = artifactId)
-
-    /**
-     * Return an update [ParsePropertiesState] instance that stores the property defined by the given [key] and [value]
-     * for the current artifact.
-     */
-    fun withProperty(key: String, value: String): ParsePropertiesState {
-        val artifactProperties = properties.getOrPut(currentArtifactId) { mutableMapOf() }
-        artifactProperties[key] = value
-
-        return this
-    }
 }
