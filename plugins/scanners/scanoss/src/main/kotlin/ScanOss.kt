cd5cb5d14dfee559c1eeff4fc37074f2b425d146
Agustin Isasmendi <isasmendi.agus@gmail.com>
2025-03-12 14:53:33 +0100

    refactor(scanoss): Replace direct API calls with SCANOSS SDK
    Replace custom direct API calls to SCANOSS with the official Java SDK.
    This change improves maintainability by leveraging the SDK's functionality
    instead of maintaining custom implementation for API interactions.
    
    Signed-off-by: Agustin Isasmendi <agustin.isasmendi@scanoss.com>

--- plugins/scanners/scanoss/src/main/kotlin/ScanOss.kt
+++ plugins/scanners/scanoss/src/main/kotlin/ScanOss.kt
@@ -19,17 +19,12 @@
 
 package org.ossreviewtoolkit.plugins.scanners.scanoss
 
-import com.scanoss.Winnowing
-import com.scanoss.dto.ScanFileResult
-import com.scanoss.rest.ScanApi
+import com.scanoss.Scanner
 import com.scanoss.utils.JsonUtils
 import com.scanoss.utils.PackageDetails
 
 import java.io.File
 import java.time.Instant
-import java.util.UUID
-
-import org.apache.logging.log4j.kotlin.logger
 
 import org.ossreviewtoolkit.model.ScanSummary
 import org.ossreviewtoolkit.plugins.api.OrtPlugin
@@ -37,9 +32,7 @@ import org.ossreviewtoolkit.plugins.api.PluginDescriptor
 import org.ossreviewtoolkit.scanner.PathScannerWrapper
 import org.ossreviewtoolkit.scanner.ScanContext
 import org.ossreviewtoolkit.scanner.ScannerMatcher
-import org.ossreviewtoolkit.scanner.ScannerMatcherConfig
 import org.ossreviewtoolkit.scanner.ScannerWrapperFactory
-import org.ossreviewtoolkit.utils.common.VCS_DIRECTORIES
 
 @OrtPlugin(
     id = "SCANOSS",
@@ -49,13 +42,8 @@ import org.ossreviewtoolkit.utils.common.VCS_DIRECTORIES
 )
 class ScanOss(
     override val descriptor: PluginDescriptor = ScanOssFactory.descriptor,
-    config: ScanOssConfig
+    private var config: ScanOssConfig
 ) : PathScannerWrapper {
-    private val service = ScanApi.builder()
-        // As there is only a single endpoint, the SCANOSS API client expects the path to be part of the API URL.
-        .url(config.apiUrl.removeSuffix("/") + "/scan/direct")
-        .apiKey(config.apiKey.value)
-        .build()
 
     override val version: String by lazy {
         // TODO: Find out the best / cheapest way to query the SCANOSS server for its version.
@@ -64,73 +52,27 @@ class ScanOss(
 
     override val configuration = ""
 
-    override val matcher by lazy {
-        ScannerMatcher.create(
-            details,
-            ScannerMatcherConfig(
-                config.regScannerName,
-                config.minVersion,
-                config.maxVersion,
-                configuration
-            )
-        )
-    }
+    override val matcher: ScannerMatcher? = null
 
     override val readFromStorage = config.readFromStorage
 
     override val writeToStorage = config.writeToStorage
 
-    /**
-     * The name of the file corresponding to the fingerprints can be sent to SCANOSS for more precise matches.
-     * However, for anonymity, a unique identifier should be generated and used instead. This property holds the
-     * mapping between the file paths and the unique identifiers. When receiving the response, the UUID will be
-     * replaced by the actual file path.
-     *
-     * TODO: This behavior should be driven by a configuration parameter enabled by default.
-     */
-    private val fileNamesAnonymizationMapping = mutableMapOf<UUID, String>()
-
     override fun scanPath(path: File, context: ScanContext): ScanSummary {
         val startTime = Instant.now()
 
-        val wfpString = buildString {
-            path.walk()
-                .onEnter { it.name !in VCS_DIRECTORIES }
-                .filterNot { it.isDirectory }
-                .forEach {
-                    logger.info { "Computing fingerprint for file ${it.absolutePath}..." }
-                    append(createWfpForFile(it))
-                }
+        val scanoss = Scanner.builder()
+            .url(config.apiUrl.removeSuffix("/") + "/scan/direct")
+            .apiKey(config.apiKey.value)
+            .build()
+
+        val rawResults: List<String> = when {
+            path.isFile -> listOf(scanoss.scanFile(path.absolutePath))
+            else -> scanoss.scanFolder(path.absolutePath)
         }
 
-        val result = service.scan(
-            wfpString,
-            context.labels["scanOssContext"],
-            context.labels["scanOssId"]?.toIntOrNull() ?: Thread.currentThread().threadId().toInt()
-        )
-
-        // Replace the anonymized UUIDs by their file paths.
-        val results = JsonUtils.toScanFileResultsFromObject(JsonUtils.toJsonObject(result)).map {
-            val uuid = UUID.fromString(it.filePath)
-
-            val fileName = fileNamesAnonymizationMapping[uuid] ?: throw IllegalArgumentException(
-                "The ${descriptor.id} server returned UUID '$uuid' which is not present in the mapping."
-            )
-
-            ScanFileResult(fileName, it.fileDetails)
-        }
-
+        val results = JsonUtils.toScanFileResults(rawResults)
         val endTime = Instant.now()
         return generateSummary(startTime, endTime, results)
-    }
-
-    internal fun generateRandomUUID() = UUID.randomUUID()
-
-    internal fun createWfpForFile(file: File): String {
-        generateRandomUUID().let { uuid ->
-            // TODO: Let's keep the original file extension to give SCANOSS some hint about the mime type.
-            fileNamesAnonymizationMapping[uuid] = file.path
-            return Winnowing.builder().build().wfpForFile(file.path, uuid.toString())
-        }
     }
 }
