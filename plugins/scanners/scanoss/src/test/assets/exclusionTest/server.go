c546cdded681836170a673a5958b20a5a99fc303
Agustin Isasmendi <agustin.isasmendi@scanoss.com>
2025-03-14 17:19:27 +0100

    chore(scanoss): Replace test data with random content
    Generate test files with random content using /dev/urandom to avoid
    any potential matches when scanning ORT source code.
    
    Signed-off-by: Agustin Isasmendi <agustin.isasmendi@scanoss.com>

--- plugins/scanners/scanoss/src/test/assets/exclusionTest/server.go
+++ plugins/scanners/scanoss/src/test/assets/exclusionTest/server.go
@@ -1,122 +1,31 @@
-package cmd
+/*
+ * This file contains random data generated using the following command:
+ * head -c 1k < /dev/urandom | base64
+ *
+ * The command takes 1 kilobyte of random bytes from the /dev/urandom device
+ * and then encodes it as base64 text.
+ *
+ * Generated on: Fri Mar 14 05:05:40 PM CET 2025
+ * Purpose: To create test data with completely random content that cannot
+ *          match any existing code in repositories, thereby avoiding false
+ *          positives when scanning ORT source code.
+ */
 
-import (
-	_ "embed"
-	"flag"
-	"fmt"
-	"os"
-	"strings"
-
-	"github.com/golobby/config/v3"
-	"github.com/golobby/config/v3/pkg/feeder"
-	zlog "github.com/scanoss/zap-logging-helper/pkg/logger"
-	myconfig "scanoss.com/go-api/pkg/config"
-	"scanoss.com/go-api/pkg/protocol/rest"
-)
-
-//go:generate bash ../../get_version.sh
-//go:embed version.txt
-var version string
-
-// getConfig checks command line args for option to feed into the config parser.
-func getConfig() (*myconfig.ServerConfig, error) {
-	var jsonConfig, envConfig, loggingConfig string
-	flag.StringVar(&jsonConfig, "json-config", "", "Application JSON config")
-	flag.StringVar(&envConfig, "env-config", "", "Application dot-ENV config")
-	flag.StringVar(&loggingConfig, "logging-config", "", "Logging config file")
-	debug := flag.Bool("debug", false, "Enable debug")
-	ver := flag.Bool("version", false, "Display current version")
-	flag.Parse()
-	if *ver {
-		fmt.Printf("Version: %v", version)
-		os.Exit(1)
-	}
-	var feeders []config.Feeder
-	if len(jsonConfig) > 0 {
-		feeders = append(feeders, feeder.Json{Path: jsonConfig})
-	}
-	if len(envConfig) > 0 {
-		feeders = append(feeders, feeder.DotEnv{Path: envConfig})
-	}
-	if *debug {
-		err := os.Setenv("APP_DEBUG", "1")
-		if err != nil {
-			fmt.Printf("Warning: Failed to set env APP_DEBUG to 1: %v", err)
-			return nil, err
-		}
-	}
-	myConfig, err := myconfig.NewServerConfig(feeders)
-	if len(loggingConfig) > 0 {
-		myConfig.Logging.ConfigFile = loggingConfig // Override any logging config file with this one.
-	}
-	return myConfig, err
-}
-
-// setupEnvVars configures a custom env vars for the scanoss engine.
-func setupEnvVars(cfg *myconfig.ServerConfig) {
-	if len(cfg.Scanning.ScanningURL) > 0 {
-		err := os.Setenv("SCANOSS_API_URL", cfg.Scanning.ScanningURL)
-		if err != nil {
-			zlog.S.Infof("Failed to set alternative SCANOSS_API_URL value to %s: %v", cfg.Scanning.ScanningURL, err)
-		}
-	}
-	if customURL := os.Getenv("SCANOSS_API_URL"); len(customURL) > 0 {
-		zlog.S.Infof("Using custom API URL: %s", customURL)
-	}
-	err := os.Setenv("SCANOSS_FILE_CONTENTS", fmt.Sprintf("%v", cfg.Scanning.FileContents))
-	if err != nil {
-		zlog.S.Infof("Failed to set SCANOSS_FILE_CONTENTS SCANOSS_API_URL value to %v: %v", cfg.Scanning.FileContents, err)
-	}
-	if customContents := os.Getenv("SCANOSS_FILE_CONTENTS"); len(customContents) > 0 && customContents == "false" {
-		zlog.S.Infof("Skipping file_url datafield.")
-	}
-}
-
-// RunServer runs the gRPC Dependency Server.
-func RunServer() error {
-	// Load command line options and config
-	cfg, err := getConfig()
-	if err != nil {
-		return fmt.Errorf("failed to load config: %v", err)
-	}
-	// Check mode to determine which logger to load
-	{
-		var err error
-		switch strings.ToLower(cfg.App.Mode) {
-		case "prod":
-			if len(cfg.Logging.ConfigFile) > 0 {
-				err = zlog.NewSugaredLoggerFromFile(cfg.Logging.ConfigFile)
-			} else {
-				err = zlog.NewSugaredProdLogger(cfg.Logging.OutputPaths...)
-			}
-		default:
-			if len(cfg.Logging.ConfigFile) > 0 {
-				err = zlog.NewSugaredLoggerFromFile(cfg.Logging.ConfigFile)
-			} else {
-				err = zlog.NewSugaredDevLogger()
-			}
-		}
-		if err != nil {
-			return fmt.Errorf("failed to load logger: %v", err)
-		}
-		if cfg.App.Debug {
-			zlog.SetLevel("debug")
-		}
-		zlog.L.Debug("Running with debug enabled")
-		defer zlog.SyncZap()
-	}
-	zlog.S.Infof("Starting SCANOSS Dependency Service: %v", strings.TrimSpace(version))
-	// Setup database connection pool
-	if cfg.Logging.DynamicLogging && len(cfg.Logging.DynamicPort) > 0 {
-		zlog.S.Infof("Setting up dynamic logging level on %v.", cfg.Logging.DynamicPort)
-		zlog.SetupDynamicLogging(cfg.Logging.DynamicPort)
-		zlog.S.Infof("Use the following to get the current status: curl -X GET %v/log/level", cfg.Logging.DynamicPort)
-		zlog.S.Infof("Use the following to set the current status: curl -X PUT %v/log/level -d level=debug", cfg.Logging.DynamicPort)
-	}
-	zlog.S.Infof("Running with %v worker(s) per scan request", cfg.Scanning.Workers)
-	zlog.S.Infof("Running with config: %+v", *cfg)
-	// Setup custom env variables if requested
-	setupEnvVars(cfg)
-	return rest.RunServer(cfg, version)
-}
-
+O8uBEiQRmSvrKRn33rErjHcbg7fTCq2ZFFNDs9WiTlEArfr7g2anf0cRFmQkq3/LqViXDBLaVw8y
+NQU3wNIbCuqFNzF2Yo0/i2v54MZWIvsnqaXj1hZBHUlXXJvfT+HLQzJAJRS8QJ066Ruui0njNsE6
+Xk2UM05WlS75ZwhO41G6tZtp1qfCvUYDzc5gCkWhce0IIftGjz2a8c6cFv6t0Cudknwqj5p59UPJ
+bV445vLh9aGE5SZqwEZKReSyjtzXctz0t33JrbEBsYEBaVLBH2FGbWzMj5iOBTZ6/YEdY4sVKmbk
+xPlsS2pW9kURX3BhPmfdfLRXgO2tFC2s+5tmwhtOQzisplaHv+feZVytxv2jRMy+qLWYrDDTA7z7
+PoxrfXckupavs4NMXlbar/wlODwbu3VbFIatY5vJ0bWj63QbzP23J1iE+26+4IH8uhW8HST02LRP
+z72f9f8a0JdEyDPxM3mz+WAkVjc8gw3sdmOw/6WCOmFrU08U2jdAISE47ft+SI7CM4YkQxcCRdND
+WpxGl/ARXd9JJLdemmLrXjRa9RltSAvcJAkGCYb/YHOKW9MVubYPLC+cFDX8pLrNr3XGRdoYuH2q
+Uoz3A/LBYK2nI6UkDCfcigFvi3gaeTmf5muMEa4PBmruLM5aIpd8NyqoS+sbsSsJIRaJQHR+COf6
+ZQuOvBn5Z6siSelZUn/PDruCIlutHNnV7eF2SjVj7mU6WsawpE73oLIpeQ37NHGZoXbxg0qP3Cf7
+Q0ukwki+tDKb82JNJ02eu2Ht3//iGevGJWr0Qz02Jt9iknIjIBHBLkypOOEqMZQWKPXKDwtcB950
+BNr5SFh0Ml4nPDm+vmfkOylHYOWCvH9gcQDOoYWZWPXhUQUBHhpXvPVB3bLqUunByKzRufB0JuGV
+K9whoy78dNLBrDC/Zk+CU3B0cB8FSdBIcWZqtKSjISHrXB3eflWjsErRZxvZWutRINd79oGYkbBB
+6rWRu6a7XJzbNogYiuF5eAh3cIeGWVISwmhF77AH+c6ojatciC+6JpwJTBI8d6hcahDGxs9k9sWQ
+opAWyMWvBMJHKZ15V9UbNBl/WSNGjL37UIlQLAfTy64WCFzlnIbKcBFaV8E969DTjbxjKfCPto3u
+/u/ABCrkbJAVKxBKgZ7XTX0Ww3TJh8q7Qp8G+ZTmlAVDvvXa+Jx94dyEiRoihecDcA+5OjCiod+X
+qaoGgTuQ3TKAO2sKWNy5cUuYzPKOfCtUim4cj1M7FbR3p8QHs8fqyYp4Wm+n1ub1TerFiS2v34Cx
+bdQCmdoUSDOmatm50Gw/CiCFotHtJeGG0EyEZ50hP1C2v0zYoTSuwi/lY6RfhMZjkBN3WUCHDg==
