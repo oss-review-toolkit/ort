866648d46074f6e3b7feb96ee16f1581a8ed4583
Agustin Isasmendi <agustin.isasmendi@scanoss.com>
2025-03-14 17:19:27 +0100

    chore(scanoss): Replace test data with random content
    Generate test files with random content using /dev/urandom to avoid
    any potential matches when scanning ORT source code.
    
    Signed-off-by: Agustin Isasmendi <agustin.isasmendi@scanoss.com>

--- plugins/scanners/scanoss/src/test/assets/exclusionTest/ArchiveUtils.kt
+++ plugins/scanners/scanoss/src/test/assets/exclusionTest/ArchiveUtils.kt
@@ -1,245 +1,31 @@
 /*
- * Copyright (C) 2017 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ * This file contains random data generated using the following command:
+ * head -c 1k < /dev/urandom | base64
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The command takes 1 kilobyte of random bytes from the /dev/urandom device
+ * and then encodes it as base64 text.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- * License-Filename: LICENSE
+ * Generated on: Fri Mar 14 05:04:43 PM CET 2025
+ * Purpose: To create test data with completely random content that cannot
+ *          match any existing code in repositories, thereby avoiding false
+ *          positives when scanning ORT source code.
  */
 
-@file:Suppress("MatchingDeclarationName")
-
-package org.ossreviewtoolkit.utils
-
-import java.io.File
-import java.io.IOException
-import java.io.InputStream
-import java.nio.file.FileVisitResult
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.SimpleFileVisitor
-import java.nio.file.attribute.BasicFileAttributes
-import java.util.zip.Deflater
-
-import org.apache.commons.compress.archivers.ArchiveEntry
-import org.apache.commons.compress.archivers.ArchiveInputStream
-import org.apache.commons.compress.archivers.sevenz.SevenZFile
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
-import org.apache.commons.compress.archivers.zip.ZipArchiveEntry
-import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
-import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream
-import org.apache.commons.compress.archivers.zip.ZipFile
-import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
-import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
-import org.apache.commons.compress.compressors.xz.XZCompressorInputStream
-import org.apache.commons.compress.utils.SeekableInMemoryByteChannel
-
-enum class ArchiveType(vararg val extensions: String) {
-    TAR(".gem", ".tar"),
-    TAR_BZIP2(".tar.bz2", ".tbz2"),
-    TAR_GZIP(".crate", ".tar.gz", ".tgz"),
-    TAR_XZ(".tar.xz", ".txz"),
-    ZIP(".aar", ".egg", ".jar", ".war", ".whl", ".zip"),
-    SEVENZIP(".7z"),
-    NONE("");
-
-    companion object {
-        fun getType(filename: String): ArchiveType {
-            val lowerName = filename.toLowerCase()
-            return (ArchiveType.entries - NONE).find { type ->
-                type.extensions.any { lowerName.endsWith(it) }
-            } ?: NONE
-        }
-    }
-}
-
-/**
- * Unpack the [File] to [targetDirectory].
- */
-fun File.unpack(targetDirectory: File) =
-    when (ArchiveType.getType(name)) {
-        ArchiveType.SEVENZIP -> unpack7Zip(targetDirectory)
-        ArchiveType.ZIP -> unpackZip(targetDirectory)
-
-        ArchiveType.TAR -> inputStream().unpackTar(targetDirectory)
-        ArchiveType.TAR_BZIP2 -> BZip2CompressorInputStream(inputStream()).unpackTar(targetDirectory)
-        ArchiveType.TAR_GZIP -> GzipCompressorInputStream(inputStream()).unpackTar(targetDirectory)
-        ArchiveType.TAR_XZ -> XZCompressorInputStream(inputStream()).unpackTar(targetDirectory)
-
-        ArchiveType.NONE -> {
-            throw IOException("Unable to guess compression scheme from file name '$name'.")
-        }
-    }
-
-/**
- * Unpack the [File] assuming it is a 7-Zip archive. This implementation ignores empty directories and symbolic links.
- */
-fun File.unpack7Zip(targetDirectory: File) {
-    SevenZFile(this).use { zipFile ->
-        while (true) {
-            val entry = zipFile.nextEntry ?: break
-
-            if (entry.isDirectory || entry.isAntiItem) {
-                continue
-            }
-
-            val target = targetDirectory.resolve(entry.name)
-
-            // There is no guarantee that directory entries appear before file entries, so ensure that the parent
-            // directory for a file exists.
-            target.parentFile.safeMkdirs()
-
-            target.outputStream().use { output ->
-                zipFile.getInputStream(entry).copyTo(output)
-            }
-        }
-    }
-}
-
-/**
- * Unpack the [File] assuming it is a Zip archive.
- */
-fun File.unpackZip(targetDirectory: File) = ZipFile(this).unpack(targetDirectory)
-
-/**
- * Unpack the [ByteArray] assuming it is a Zip archive.
- */
-fun ByteArray.unpackZip(targetDirectory: File) = ZipFile(SeekableInMemoryByteChannel(this)).unpack(targetDirectory)
-
-/**
- * Pack the file into a ZIP [targetFile] using [Deflater.BEST_COMPRESSION]. If the file is a directory its content is
- * recursively added to the archive. Only regular files are added, e.g. symbolic links or directories are skipped. If
- * a [prefix] is specified, it is added to the file names in the ZIP file.
- * If not all files shall be added to the archive a [filter] can be provided.
- */
-fun File.packZip(
-    targetFile: File,
-    prefix: String = "",
-    overwrite: Boolean = false,
-    filter: (Path) -> Boolean = { true }
-) {
-    require(overwrite || !targetFile.exists()) {
-        "The target ZIP file '${targetFile.absolutePath}' must not exist."
-    }
-
-    ZipArchiveOutputStream(targetFile).use { output ->
-        output.setLevel(Deflater.BEST_COMPRESSION)
-        Files.walkFileTree(toPath(), object : SimpleFileVisitor<Path>() {
-            override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
-                if (attrs.isRegularFile && filter(file)) {
-                    val entry = ZipArchiveEntry(file.toFile(), "$prefix${this@packZip.toPath().relativize(file)}")
-                    output.putArchiveEntry(entry)
-                    file.toFile().inputStream().use { input -> input.copyTo(output) }
-                    output.closeArchiveEntry()
-                }
-
-                return FileVisitResult.CONTINUE
-            }
-        })
-    }
-}
-
-/**
- * Unpack the [InputStream] to [targetDirectory] assuming that it is a tape archive (TAR). This implementation ignores
- * empty directories and symbolic links.
- */
-fun InputStream.unpackTar(targetDirectory: File) =
-    TarArchiveInputStream(this).unpack(
-        targetDirectory,
-        { entry -> !(entry as TarArchiveEntry).isFile },
-        { entry -> (entry as TarArchiveEntry).mode }
-    )
-
-/**
- * Unpack the [InputStream] to [targetDirectory] assuming that it is a ZIP archive. This implementation ignores empty
- * directories and symbolic links.
- */
-fun InputStream.unpackZip(targetDirectory: File) =
-    ZipArchiveInputStream(this).unpack(
-        targetDirectory,
-        { entry -> (entry as ZipArchiveEntry).let { it.isDirectory || it.isUnixSymlink } },
-        { entry -> (entry as ZipArchiveEntry).unixMode }
-    )
-
-/**
- * Copy the executable bit contained in [mode] to the [target] file's mode bits.
- */
-private fun copyExecutableModeBit(target: File, mode: Int) {
-    if (Os.isWindows) return
-
-    // Note: In contrast to Java, Kotlin does not support octal literals, see
-    // https://kotlinlang.org/docs/reference/basic-types.html#literal-constants.
-    // The bit-triplets from left to right stand for user, groups, other, respectively.
-    if (mode and 0b001_000_001 != 0) {
-        target.setExecutable(true, (mode and 0b000_000_001) == 0)
-    }
-}
-
-/**
- * Unpack this [ArchiveInputStream] to the [targetDirectory], skipping all entries for which [shouldSkip] returns true,
- * and using what [mode] returns as the file mode bits.
- */
-private fun ArchiveInputStream.unpack(
-    targetDirectory: File,
-    shouldSkip: (ArchiveEntry) -> Boolean,
-    mode: (ArchiveEntry) -> Int
-) =
-    use { input ->
-        while (true) {
-            val entry = input.nextEntry ?: break
-
-            if (shouldSkip(entry)) continue
-
-            val target = targetDirectory.resolve(entry.name)
-
-            // There is no guarantee that directory entries appear before file entries, so ensure that the parent
-            // directory for a file exists.
-            target.parentFile.safeMkdirs()
-
-            target.outputStream().use { output ->
-                input.copyTo(output)
-            }
-
-            copyExecutableModeBit(target, mode(entry))
-        }
-    }
-
-/**
- * Unpack the [ZipFile]. In contrast to [InputStream.unpackZip] this properly parses the ZIP's central directory, see
- * https://commons.apache.org/proper/commons-compress/zip.html#ZipArchiveInputStream_vs_ZipFile.
- */
-private fun ZipFile.unpack(targetDirectory: File) =
-    use { zipFile ->
-        val entries = zipFile.entries
-
-        while (entries.hasMoreElements()) {
-            val entry = entries.nextElement()
-
-            if (entry.isDirectory || entry.isUnixSymlink) {
-                continue
-            }
-
-            val target = targetDirectory.resolve(entry.name)
-
-            // There is no guarantee that directory entries appear before file entries, so ensure that the parent
-            // directory for a file exists.
-            target.parentFile.safeMkdirs()
-
-            target.outputStream().use { output ->
-                zipFile.getInputStream(entry).copyTo(output)
-            }
-
-            copyExecutableModeBit(target, entry.unixMode)
-        }
-    }
+7m8Y06QhHzmQ4ePs0UUUasqsc8SP1ayNTFdQb6wffQwMu605hXOGHbOoy5pUv7ksgf6sw5ET2qXp
+T23LF2yA1cdNeDt8DBDd3IDmLX/wGgXcQjcaCtfSsMWB7oqHBMGkzwC5fMcDKPLK6ec2MwX6WPkw
+E18ImifWtAmGPEFGxWuqIinhE1yGSN+ImqJPVmpYfMOaDIAaS3JpiHZDmJW5uyQ5DB6W7lpm0q+f
+ZbtGPBeimy1jWF0H6kEW/TIve8RzUjdHU/t//O9r0b2AP08shSrSDWGlbQzxTniLOp2VZxNUEcVM
+c9/Lx4OXEaM/3NDCdr4qQS/1kZpGKFrv06zzC8tlncGaxBfdZSCsh1i+LbZtvUmTSv/wz7g+mld5
+WB2lSzF1Ervzqnm2+3iY+9TvVxDWzZ27LWsd1kvFrJCM03jI1q0c7uJrnnovAOoZkH2QiMPNBQmB
+wShT36h3su/aiOXEquXi+DoTSYDNgXeHVGI2joLVWYLfeTcTTfdvZiwp0K+XQp6fKtWX8tpUibNq
+ngp2dOlzl5yiT+WAD2ETGuyEML/wM3oz+wB93me2YYLJqz/1gtlnnRvGnAukbFLpxxXGK7Vnz+FF
+KfcPWF+O/FNV3nJD+m2nlMVj1n4lRM/mUEdVDhDDtxhywvi6DdNQMcUoeXZRT3dLk27+efNLvMDk
+7TsW/asvMoPrioAkDiTHqWvy+OUImWqqzNpzxIMuTWZrApSklw2UeyXknvHBORUN95AM6Oe9iKb0
+7B2g8U9dIFo7v/AhaDqoQMw+Dz1KfH6+fPaqZEy2H1U7/9RSorKz0fycz7n7BtqWxjenqw11LLxy
+lO36udPuvtr2b/WB/4ch0LuoI2eA11iTeIG4DuTxvizU3lExBXP+e8EAjkWx6F2ymDrI21PYPp++
+uidSk3g/RmaRZGk8akcXbs3pDO/twfjaH3YWYZzBf8aP1TRYDp4NF5v2OhWDa5d2dqdQGDRGg/wy
+Gf0W8txn/fQ3QN7SS9qPftgD6OYIpxKjIWBq/zb5+SAzhBZVjFYw+KVi+zu/P7he7xLRko6APCum
+Ugk7wqohWVdbl2IG2RIuPUOH2zQdzVJvLisKhfq3q6ydGmjD/WRNOxbebpmSKcmZWVg0Ko7/e0ys
+ymV2Ud0tIZwfIH/7476SZAh0ym1U7mgyzm/jlxKm5gUIF1+NWQiqa80GmsAJfquf1Yj4i0ftF+eO
+6OPJqbkZERpu24u2HIfL6CvlUkx08mS+eqLzyRiRuidDcQGFOK+0xPUk01jOZnGiY1ptG4W+Fo5K
+OhcT2H14wQqiHsthzMhpSLXwMG2ddM7P69rHEAXB3iXyWopgdWopVekxHEuar0mv3D6uO/4HKQ==
--- plugins/scanners/scanoss/src/test/assets/exclusionTest/ScannerFactory.kt
+++ plugins/scanners/scanoss/src/test/assets/exclusionTest/ScannerFactory.kt
@@ -1,54 +1,31 @@
 /*
- * Copyright (C) 2017 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ * This file contains random data generated using the following command:
+ * head -c 1k < /dev/urandom | base64
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The command takes 1 kilobyte of random bytes from the /dev/urandom device
+ * and then encodes it as base64 text.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- * License-Filename: LICENSE
+ * Generated on: Fri Mar 14 05:05:29 PM CET 2025
+ * Purpose: To create test data with completely random content that cannot
+ *          match any existing code in repositories, thereby avoiding false
+ *          positives when scanning ORT source code.
  */
 
-package org.ossreviewtoolkit.scanner
-
-import java.util.ServiceLoader
-
-import org.ossreviewtoolkit.model.config.ScannerConfiguration
-
-/**
- * A common interface for use with [ServiceLoader] that all [AbstractScannerFactory] classes need to implement.
- */
-interface ScannerFactory {
-    /**
-     * The name to use to refer to the scanner.
-     */
-    val scannerName: String
-
-    /**
-     * Create a [Scanner] using the specified [config].
-     */
-    fun create(config: ScannerConfiguration): Scanner
-}
-
-/**
- * A generic factory class for a [Scanner].
- */
-abstract class AbstractScannerFactory<out T : Scanner>(
-    override val scannerName: String
-) : ScannerFactory {
-    abstract override fun create(config: ScannerConfiguration): T
-
-    /**
-     * Return the scanner's name here to allow Clikt to display something meaningful when listing the scanners
-     * which are enabled by default via their factories.
-     */
-    override fun toString() = scannerName
-}
+IyaayUoMK28Ib11Z55hC2OShY3p3HzWQyGy199hx20oqZrypl9AuDhKtBdl+qozcZBNajzvkU3H/
+jh3vV/P9I2VLQNVqMCpjelQoXyVq/nmbwxdQBXGbLgcC4J05ujQ2hoXuF4jdtEttDxca8P/EpUub
+nmSO3zmz86LqyyYgFj3imketFw0GvnCYU/8VDjmLxnigspEVI7ZDOacKOshObwH+Br/XgFHr5tyc
+ulGqACTjGY3EEdAjC2+tcTqoI+4mXVxx4CcBD4lRn90khfFOcAM8Iu2pGaERHnAtUrf9EX3rLsOW
+wV+wYllChP71rI/4ueEch9X8ph1dA0nQQN1tLUi58pQlkCHY6K0QNFiD6K+RxaBl3yBt1IZqjfZi
+UVjTb7xJDrYnLPIASlPd0AduDik8pKn+GTqIFWgkkRr5mY6c9jTqHxY7rASDNi7LGKUE9gPFd1LD
+xPJmsl+8L+lcVJjJNU7Tkps/ZZJuo/EqlwbUd/Wq45S++YBBfYlFaOXn/bVMhxXi1SH3xMHSAjH1
+aYj0YHEdBHnEF1ouahyS4607cundZcSR29kITrUnFSi/ZP3zKREa3MGm/qrJS7qFSxlHVsYHBIjy
+VRx+teV4nQWKJyA6x/T9Sx63lM7duwhVRdh9JxhxnrKAyUBH5HwhpFXHreMjudNdY9nMaWaKP9Ge
+oD4Rr4iA3kvaHjtqSfhB55PgQO7Od/KLNTRfMMPl7IjbouQNCai++hV+p7BRAjtGUwTOXp9FHbv7
+YFGBFl3a0e1+YEoQA+0Psf1x2lENCJwH87DuZxuKI3kbcY6XA5kebt43m/eztRa17z/vmwyiQ/up
++RpMU9Xp1bv39h84QbyvZYN40xzHc8togJmPtSKCyEPcmHdt9t0LF6TCsb4k+kIBRUXMfnYpEDqv
+E6dldgWHjVy/4llWqyj3SsToERP1VhaloWyq8QRNke6lKzxMXOhmupKX195V2cA+6EGY3sK/ykhl
+fYOofbKcHwevHKgOJyj7Tj6+9qUgda/EI01lcJicTO8Nqb0LW+FfwIiws7WlsZWuxQGUZ0SOMBU4
+MnR9NPbS6rUSx2rMfSPn18Jd82D5eoM32ogRQb7C2pgXQbQoAegl98vtOjkze4wsa6CmW0rmbQrJ
+bpgPoWiZ1t/BlAUvxjRuzQSNNhnyvaC5nib6NYZAcr9BCm3yJ0sR/uSOUG8cCoJptMYhH9XxHqKl
+ACwfHgq7/mHBTxhQCmw5hkDWvY7FqzDPME3igab1Mda4lxOyUjJ3PeVzZbWZY2s/oUaSbntsSqRM
+z+zutj83Nm76iOSS0MXxCfi5VKYThzGdfXkYB2tZP8yPhh+sw0CpqeV5KB810C76abZbVZ+EDw==
