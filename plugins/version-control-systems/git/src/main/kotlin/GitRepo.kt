8171dfadb624a69f36691dae2e4e503cadef574f
Sebastian Schuberth <sebastian@doubleopen.org>
2025-04-06 13:43:34 +0200

    deps(git-repo): Upgrade to the latest `repo` version
    At some point this should probably be made configurable via plugin
    options. But this requires a bit of refactoring as `GitRepo` currently
    takes a `GitConfig`.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- plugins/version-control-systems/git/src/main/kotlin/GitRepo.kt
+++ plugins/version-control-systems/git/src/main/kotlin/GitRepo.kt
@@ -54,7 +54,7 @@ import org.ossreviewtoolkit.utils.ort.showStackTrace
 /**
  * The branch or tag of git-repo to use. This allows to override git-repo's default of using the "stable" branch.
  */
-private const val GIT_REPO_REV = "v2.39"
+private const val GIT_REPO_REV = "v2.53"
 
 /**
  * The minimal manifest structure as used by the wrapping "manifest.xml" file as of repo version 2.4. For the full
