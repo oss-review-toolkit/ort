2526a1b29d57b7006295b5e56603017e36595ee1
Matthias Grandl <matthias.grandl@croit.io>
2025-04-24 08:10:27 +0200

    chore(scanner): Fix S3 config comment
    Signed-off-by: Matthias Grandl <matthias.grandl@croit.io>

--- model/src/main/kotlin/config/S3FileStorageConfiguration.kt
+++ model/src/main/kotlin/config/S3FileStorageConfiguration.kt
@@ -32,7 +32,7 @@ data class S3FileStorageConfiguration(
     /** The name of the S3 bucket used to store files in. */
     val bucketName: String,
 
-    /** Whether to use compression for storing files or not. Defaults to true. */
+    /** Whether to use compression for storing files or not. Defaults to false. */
     val compression: Boolean = false,
 
     /** Custom endpoint to perform AWS API Requests */
