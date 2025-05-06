071d324fbec19f8c6136fd5f369d021ec74d7f81
Agustin Isasmendi <agustin.isasmendi@scanoss.com>
2025-05-06 15:49:14 +0200

    refactor(scanoss): Replace test assets with randomly generated data
    This commit replaces test files containing OSS source code with randomly
    generated data to prevent false positives during code scanning.
    
    Signed-off-by: Agustin Isasmendi <agustin.isasmendi@scanoss.com>

--- plugins/scanners/scanoss/src/test/assets/filesToScan/ScannerFactory.kt
+++ plugins/scanners/scanoss/src/test/assets/filesToScan/ScannerFactory.kt
@@ -1,54 +1,31 @@
 /*
- * Copyright (C) 2017 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
+ * This file contains random data generated using the following command:
+ * head -c 1k < /dev/urandom | base64
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The command takes 1 kilobyte of random bytes from the /dev/urandom device
+ * and then encodes it as base64 text.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- * License-Filename: LICENSE
+ * Generated on: Fri Mar 14 05:07:04 PM CET 2025
+ * Purpose: To create test data with completely random content that cannot
+ *          match any existing code in repositories, thereby avoiding false
+ *          positives when scanning ORT source code.
  */
 
-package org.ossreviewtoolkit.scanner
-
-import java.util.ServiceLoader
-
-import org.ossreviewtoolkit.model.config.ScannerConfiguration
-
-/**
- * A common interface for use with [ServiceLoader] that all [AbstractScannerFactory] classes need to implement.
- */
-interface ScannerFactory {
-    /**
-     * The name to use to refer to the scanner.
-     */
-    val scannerName: String
-
-    /**
-     * Create a [Scanner] using the specified [config].
-     */
-    fun create(config: ScannerConfiguration): Scanner
-}
-
-/**
- * A generic factory class for a [Scanner].
- */
-abstract class AbstractScannerFactory<out T : Scanner>(
-    override val scannerName: String
-) : ScannerFactory {
-    abstract override fun create(config: ScannerConfiguration): T
-
-    /**
-     * Return the scanner's name here to allow Clikt to display something meaningful when listing the scanners
-     * which are enabled by default via their factories.
-     */
-    override fun toString() = scannerName
-}
+mneGKU/gTsBek3i9p6pHaccif0G7AhDAF/w3j6QSXhUJACJY9h+yN+gBf0Duf1DONiZrOlt96w77
+yQH9J0inoH0rJr0EKXWVkjyHTBwhmMWDcnwrAEmQRn/kCtFNfMRFAyJsxBE2suK754qfhUbS4hjt
+qB9JHimTQCqQ2bgLJvgMgZ9Bfs21lIhkpTlUsdEyEBjy7VB5DVS8Usal+lIJENJ/RaJmxULcPj1C
+LEUSaEf+GRr8wDoysFcIGav2uJpVaZfAvbfWxwnMScjpaKeY+lzScmnnYNXhRBYYh+mI1qJY/ycW
+b5jREjm3iF1RQ5x9/PeLpx5fRPL11G9aaHWCrrHT9xrV8Lb5WIf5aGaZG+p7c3CDdHGhT0E7ltY9
+tFKs9vstrgqhL/l0hFxvoZtjBE2tdt6+lhGt2puZ6XoWAZf4Baas2cTUr5i6WvdDKbPnytcCFMuN
+UgBJS60JpNaWPV45QQS970G2BhCoeI5fYi2//0KFz2axIuVfRhEaojFS+CNVwpd9hXrNQSXFaS38
+oYZF/p09SiWJHHoaaKSTEGkSZZRHDhEnfRhOnTmSdmqftMywk5178mcJWhOfemK/IcILhNeB+5am
+E+AKJy+rsYffep5KXPobFn3BntAdJilwe6R0CHf5d3wIwUIDxMiqSgnDJZthiVFYBBhYTrLZza+t
+eXTfmFvdjcsUqNSUuglSjFBVUFRM4rKk7Y8zOQJ6PWge7LOqVxsrLY9KsRt1iI6JZ9Pq3G7yL9aD
+qsivx4mnGmUvVPSbSGK4soWKYOKpMkYkyALyHJhmQMY6BxYmbHbqIaxWS67IEj00XjcIi2tsV8qN
+juG7Rgyc33peN/Xu6EDEN+Uv4+EMB+VbwEsSgVhIdIAjiMMQT/r9u7hjXcDyZZNpHK3wBd3bUfn0
+MMPbWI4hIIj6/c5YM5SiNi8zPGWAbpc76FAg7cskvYwMOFfoaUxtkMD8IW5Y7X/m3XSE/O9cy7Wa
+2+cq+YkIs+YmyixoiQV/eJimhmH6P2VYgDYUoRzfcKsE1QpjiWdLq2PEjD5iTsOgy/HdWKLAvdnW
+j3jgzvZNBmfX/MYyFJn/pBALbAVFvQtQTLBmAJlyVjPfLCEhA6RhMmATvOhm3OLsacdFjrrz/Mhb
+hkOCDPSyqoinHTcHKvpKORhCnlYPBGshboJ28h1oQIcRhGYGCxdZfIVClbUG3os0AzjHRFtI8f0Z
+WQmL0LrqrdxwmdAJauLjglYARUkAB/UHRBvI3S8DMLo/E8dpfhvyzb/Jgu0lfXXPmdSDCRXDL6/0
+vpbI40itpGnQ52u8wOHM3tz11v2UqcYi/HSiLQEt1CKOLpNnJuJ7KcwybUqJoR3Sbn7cBmvVxg==
