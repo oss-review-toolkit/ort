oss-review-toolkit/ort

commit e6d9f491b6acdfd520604fd657aadd83225ddb70

chore(node): Remove the `workingDir` parameter for getting package info
Getting remote package info is independent of the working directory, so
remove the parameter to simplify code.

Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

plugins/package-managers/node/src/main/kotlin/npm/Npm.kt
@@ -152,15 +152,15 @@ class Npm(
         return parseNpmList(listProcess.stdout)
     }
 
-    internal fun getRemotePackageDetails(workingDir: File, packageName: String): PackageJson? {
+    internal fun getRemotePackageDetails(packageName: String): PackageJson? {
         npmViewCache[packageName]?.let { return it }
 
         return runCatching {
-            val process = NpmCommand.run(workingDir, "info", "--json", packageName).requireSuccess()
+            val process = NpmCommand.run("info", "--json", packageName).requireSuccess()
 
             parsePackageJson(process.stdout)
         }.onFailure { e ->
-            logger.warn { "Error getting details for $packageName in directory $workingDir: ${e.message.orEmpty()}" }
+            logger.warn { "Error getting details for $packageName: ${e.message.orEmpty()}" }
         }.onSuccess {
             npmViewCache[packageName] = it
         }.getOrNull()

plugins/package-managers/node/src/main/kotlin/pnpm/Pnpm.kt
@@ -145,15 +145,15 @@ class Pnpm(
         // fixed major version to be sure to get consistent results.
         PnpmCommand.checkVersion()
 
-    internal fun getRemotePackageDetails(workingDir: File, packageName: String): PackageJson? {
+    internal fun getRemotePackageDetails(packageName: String): PackageJson? {
         packageDetailsCache[packageName]?.let { return it }
 
         return runCatching {
-            val process = PnpmCommand.run(workingDir, "info", "--json", packageName).requireSuccess()
+            val process = PnpmCommand.run("info", "--json", packageName).requireSuccess()
 
             parsePackageJson(process.stdout)
         }.onFailure { e ->
-            logger.warn { "Error getting details for $packageName in directory $workingDir: ${e.message.orEmpty()}" }
+            logger.warn { "Error getting details for $packageName: ${e.message.orEmpty()}" }
         }.onSuccess {
             packageDetailsCache[packageName] = it
         }.getOrNull()

plugins/package-managers/node/src/main/kotlin/yarn/Yarn.kt
@@ -349,10 +349,10 @@ open class Yarn(
         YarnCommand.run(workingDir, "install", "--ignore-scripts", "--ignore-engines", "--immutable").requireSuccess()
     }
 
-    internal fun getRemotePackageDetails(workingDir: File, packageName: String): PackageJson? {
+    internal fun getRemotePackageDetails(packageName: String): PackageJson? {
         yarnInfoCache.read(packageName)?.let { return parsePackageJson(it) }
 
-        val process = YarnCommand.run(workingDir, "info", "--json", packageName).requireSuccess()
+        val process = YarnCommand.run("info", "--json", packageName).requireSuccess()
 
         return parseYarnInfo(process.stdout, process.stderr)?.also {
             yarnInfoCache.write(packageName, Json.encodeToString(it))

plugins/package-managers/node/src/main/kotlin/NpmSupport.kt
@@ -142,9 +142,8 @@ internal fun parseVcsInfo(packageJson: PackageJson): VcsInfo {
  * content via the `npm view` command. The result is a [Pair] with the raw identifier and the new package.
  */
 internal fun parsePackage(
-    workingDir: File,
     packageJsonFile: File,
-    getRemotePackageDetails: (workingDir: File, packageName: String) -> PackageJson?
+    getRemotePackageDetails: (packageName: String) -> PackageJson?
 ): Package {
     val packageJson = parsePackageJson(packageJsonFile)
 
@@ -180,7 +179,7 @@ internal fun parsePackage(
         || hash == Hash.NONE || vcsFromPackage == VcsInfo.EMPTY
 
     if (hasIncompleteData) {
-        getRemotePackageDetails(workingDir, "$rawName@$version")?.let { details ->
+        getRemotePackageDetails("$rawName@$version")?.let { details ->
             if (description.isEmpty()) description = details.description.orEmpty()
             if (homepageUrl.isEmpty()) homepageUrl = details.homepage.orEmpty()
 

plugins/package-managers/node/src/main/kotlin/npm/NpmDependencyHandler.kt
@@ -56,7 +56,6 @@ internal class NpmDependencyHandler(private val npm: Npm) : DependencyHandler<Mo
     override fun createPackage(dependency: ModuleInfo, issues: MutableCollection<Issue>): Package? =
         dependency.takeUnless { it.isProject || !it.isInstalled }?.let {
             parsePackage(
-                workingDir = it.workingDir,
                 packageJsonFile = it.packageJsonFile,
                 getRemotePackageDetails = npm::getRemotePackageDetails
             )
@@ -65,8 +64,6 @@ internal class NpmDependencyHandler(private val npm: Npm) : DependencyHandler<Mo
     private fun readPackageJson(packageJsonFile: File): PackageJson =
         packageJsonCache.getOrPut(packageJsonFile.realFile()) { parsePackageJson(packageJsonFile) }
 }
-
-private val ModuleInfo.workingDir: File get() = File(path)
 
 private val ModuleInfo.isInstalled: Boolean get() = path != null
 

plugins/package-managers/node/src/main/kotlin/pnpm/PnpmDependencyHandler.kt
@@ -67,7 +67,6 @@ internal class PnpmDependencyHandler(private val pnpm: Pnpm) : DependencyHandler
     override fun createPackage(dependency: Dependency, issues: MutableCollection<Issue>): Package? =
         dependency.takeUnless { it.isProject() || !it.isInstalled }?.let {
             parsePackage(
-                workingDir = it.workingDir,
                 packageJsonFile = it.packageJsonFile,
                 getRemotePackageDetails = pnpm::getRemotePackageDetails
             )

plugins/package-managers/node/src/main/kotlin/yarn/YarnDependencyHandler.kt
@@ -36,6 +36,6 @@ internal class YarnDependencyHandler(private val yarn: Yarn) : DependencyHandler
 
     override fun createPackage(dependency: NpmModuleInfo, issues: MutableCollection<Issue>): Package? =
         yarn.takeUnless { dependency.isProject }?.let {
-            parsePackage(dependency.workingDir, dependency.packageFile, it::getRemotePackageDetails)
+            parsePackage(dependency.packageFile, it::getRemotePackageDetails)
         }
 }

commit 34816e7c7d78e5608cde9e8965078486449d341e

fix(node): Add back the `workingDir` parameter for getting module infos
Whether configuration via `.npmrc` is applied depends on the working
used for the execution of the command. The removal of the `workingDir`
parameter by e6d9f49 has caused the `.npmrc` to not take any effect.
So, add back that parameter to fix the issue.

Fixes: #10272.

Signed-off-by: Frank Viernau <frank_viernau@epam.com>

plugins/package-managers/node/src/main/kotlin/npm/Npm.kt
@@ -84,9 +84,9 @@ class Npm(override val descriptor: PluginDescriptor = NpmFactory.descriptor, pri
 
     private lateinit var stash: DirectoryStash
 
-    private val moduleInfoResolver = ModuleInfoResolver.create { _, moduleId ->
+    private val moduleInfoResolver = ModuleInfoResolver.create { workingDir, moduleId ->
         runCatching {
-            val process = NpmCommand.run("info", "--json", moduleId).requireSuccess()
+            val process = NpmCommand.run(workingDir, "info", "--json", moduleId).requireSuccess()
             parsePackageJson(process.stdout)
         }.onFailure { e ->
             logger.warn { "Error getting details for $moduleId: ${e.message.orEmpty()}" }

plugins/package-managers/node/src/main/kotlin/pnpm/Pnpm.kt
@@ -65,11 +65,11 @@ class Pnpm(override val descriptor: PluginDescriptor = PnpmFactory.descriptor) :
 
     private lateinit var stash: DirectoryStash
 
-    private val moduleInfoResolver = ModuleInfoResolver.create { _, moduleId ->
+    private val moduleInfoResolver = ModuleInfoResolver.create { workingDir, moduleId ->
         runCatching {
             // Note that pnpm does not actually implement the "info" subcommand itself, but just forwards to npm, see
             // https://github.com/pnpm/pnpm/issues/5935.
-            val process = PnpmCommand.run("info", "--json", moduleId).requireSuccess()
+            val process = PnpmCommand.run(workingDir, "info", "--json", moduleId).requireSuccess()
             parsePackageJson(process.stdout)
         }.onFailure { e ->
             logger.warn { "Error getting details for $moduleId: ${e.message.orEmpty()}" }

plugins/package-managers/node/src/main/kotlin/yarn/Yarn.kt
@@ -76,8 +76,8 @@ class Yarn(override val descriptor: PluginDescriptor = YarnFactory.descriptor) :
 
     private lateinit var stash: DirectoryStash
 
-    private val moduleInfoResolver = ModuleInfoResolver.create { _, moduleId ->
-        val process = YarnCommand.run("info", "--json", moduleId)
+    private val moduleInfoResolver = ModuleInfoResolver.create { workingDir, moduleId ->
+        val process = YarnCommand.run(workingDir, "info", "--json", moduleId)
 
         parseYarnInfo(process.stdout, process.stderr)
     }