2450f2f830335f71bddb0df0bd6cd18d1bf5fcd9
Marcel Bochtler <marcel.bochtler@bosch.com>
2025-05-16 11:20:42 +0200

    build: Add `printVersionName` task
    Simplify retrieving the current ORT version by creating a separate task.
    This is helpful for automated tasks where the exact version is required.
    
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,7 @@
  *
  *     https://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -83,6 +82,17 @@ tasks.register("allDependencies") {
     // Ensure deterministic output by requiring to run tasks after each other in always the same order.
     dependenciesTasks.sorted().zipWithNext().forEach { (a, b) ->
         b.mustRunAfter(a)
+    }
+}
+
+tasks.register("printVersionName") {
+    group = "Versioning"
+    description = "Prints the current project version name"
+
+    val versionName = project.version
+
+    doLast {
+        println(versionName)
     }
 }
 
