1fad42aae778830232ddc42c8cded47238ef5e42
Sebastian Schuberth <sebastian@doubleopen.org>
2025-06-05 11:30:20 +0200

    chore(model): Remove `IVY_VERSION_RANGE_INDICATORS`
    These are by now only used for an early return from `getVersionRange()`.
    As checking for a valid range is done by the `RangesListFactory`, the
    only remaining case to check for is the empty string, which would
    otherwise create a ">= 0.0.0" range.
    
    Note that as of 8142cdc the variable name was misleading anyway, as it
    does not only contain Ivy version range indicators.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- model/src/main/kotlin/utils/VersionUtils.kt
+++ model/src/main/kotlin/utils/VersionUtils.kt
@@ -28,11 +28,6 @@ import org.semver4j.RangesListFactory
 import org.semver4j.Semver
 
 /**
- * A list of Strings that are used by Ivy-style version ranges.
- */
-private val IVY_VERSION_RANGE_INDICATORS = listOf(",", "~", "*", "+", ">", "<", "=", " - ", "^", ".x", "||")
-
-/**
  * Return true if the version of this [Identifier] interpreted as an Ivy version matcher is applicable to the
  * package with the given [identifier][pkgId].
  */
@@ -66,7 +61,7 @@ internal fun Identifier.isVersionRange(): Boolean {
 }
 
 private fun Identifier.getVersionRanges(): RangesList? {
-    if (IVY_VERSION_RANGE_INDICATORS.none { version.contains(it, ignoreCase = true) }) return null
+    if (version.isEmpty()) return null
 
     return runCatching {
         RangesListFactory.create(version).takeUnless { it.get().isEmpty() }
