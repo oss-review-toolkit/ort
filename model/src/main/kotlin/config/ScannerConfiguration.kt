78c9abd440e7d48fb3c97e8c3c572d21b0f92d01
Sebastian Schuberth <sebastian@doubleopen.org>
2025-06-06 12:30:34 +0200

    chore(model): Remove an old `JsonAlias` annotation
    It was added 1.5 years ago, so just remove it.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- model/src/main/kotlin/config/ScannerConfiguration.kt
+++ model/src/main/kotlin/config/ScannerConfiguration.kt
@@ -19,7 +19,6 @@
 
 package org.ossreviewtoolkit.model.config
 
-import com.fasterxml.jackson.annotation.JsonAlias
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
 
@@ -94,7 +93,6 @@ data class ScannerConfiguration(
      * Scanner-specific configuration options. The key needs to match the name of the scanner class, e.g. "ScanCode"
      * for the ScanCode wrapper. See the documentation of the scanner for available options.
      */
-    @JsonAlias("options")
     val config: Map<String, PluginConfig>? = null,
 
     /**
