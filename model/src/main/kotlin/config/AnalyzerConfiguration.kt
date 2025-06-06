dfa10e38f54910fd4f6c64e06f23217d28d858dd
Sebastian Schuberth <sebastian@doubleopen.org>
2025-06-06 11:51:51 +0200

    docs(model): Explain where and how tool configuration is (de-)serialized
    Note that these docs delibertely omit the pathological case in the
    `ConfigCommand`, which uses Jackson to serialize `OrtConfiguration` to a
    YAML string via Jackson as Hoplite does not support serialization at all.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- model/src/main/kotlin/config/AnalyzerConfiguration.kt
+++ model/src/main/kotlin/config/AnalyzerConfiguration.kt
@@ -23,6 +23,11 @@ import com.fasterxml.jackson.annotation.JsonInclude
 
 import org.ossreviewtoolkit.utils.common.zip
 
+/**
+ * The configuration model of the analyzer. This class is (de-)serialized in the following places:
+ * - Deserialized from "config.yml" as part of [OrtConfiguration] (via Hoplite).
+ * - (De-)Serialized as part of [org.ossreviewtoolkit.model.OrtResult] (via Jackson).
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class AnalyzerConfiguration(
     /**
