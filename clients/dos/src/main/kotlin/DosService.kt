5b8e899d72e6be047dc75524e966d12cd9d33d70
Sebastian Schuberth <sebastian@doubleopen.org>
2025-07-14 22:39:48 +0200

    refactor(dos-client): Directly return response bodies to simplify code
    See [1].
    
    [1]: https://square.github.io/retrofit/declarations/#kotlin-support
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- clients/dos/src/main/kotlin/DosService.kt
+++ clients/dos/src/main/kotlin/DosService.kt
@@ -26,7 +26,6 @@ import okhttp3.OkHttpClient
 import okhttp3.RequestBody
 import okhttp3.logging.HttpLoggingInterceptor
 
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import retrofit2.http.Body
@@ -83,40 +82,38 @@ interface DosService {
      * Get the package configuration for specified [purl][PackageConfigurationRequestBody.purl].
      */
     @POST("package-configuration")
-    suspend fun getPackageConfiguration(
-        @Body body: PackageConfigurationRequestBody
-    ): Response<PackageConfigurationResponseBody>
+    suspend fun getPackageConfiguration(@Body body: PackageConfigurationRequestBody): PackageConfigurationResponseBody
 
     /**
      * Get the pre-signed upload URL for S3 object storage with the specified object [key][UploadUrlRequestBody.key].
      */
     @POST("upload-url")
-    suspend fun getUploadUrl(@Body body: UploadUrlRequestBody): Response<UploadUrlResponseBody>
+    suspend fun getUploadUrl(@Body body: UploadUrlRequestBody): UploadUrlResponseBody
 
     /**
      * Put the [file] into the S3 object storage at [url].
      */
     @SkipAuthorization
     @PUT
-    suspend fun uploadFile(@Url url: String, @Body file: RequestBody): Response<Unit>
+    suspend fun uploadFile(@Url url: String, @Body file: RequestBody)
 
     /**
      * Add new scanner job for specified [packages][JobRequestBody.packages]. In case multiple packages are provided, it
      * is assumed that they all refer to the same provenance (like a monorepo).
      */
     @POST("job")
-    suspend fun addScanJob(@Body body: JobRequestBody): Response<JobResponseBody>
+    suspend fun addScanJob(@Body body: JobRequestBody): JobResponseBody
 
     /**
      * Get scan results for specified [packages][ScanResultsRequestBody.packages]. In case multiple packages are
      * provided, it is assumed that they all refer to the same provenance (like a monorepo).
      */
     @POST("scan-results")
-    suspend fun getScanResults(@Body body: ScanResultsRequestBody): Response<ScanResultsResponseBody>
+    suspend fun getScanResults(@Body body: ScanResultsRequestBody): ScanResultsResponseBody
 
     /**
      * Get the state for scan job with given [id].
      */
     @GET("job-state/{id}")
-    suspend fun getScanJobState(@Path("id") id: String): Response<JobStateResponseBody>
+    suspend fun getScanJobState(@Path("id") id: String): JobStateResponseBody
 }
