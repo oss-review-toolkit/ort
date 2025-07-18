934122b0c2ddfaec446e836e11e3c65d9ac6dce5
Frank Viernau <frank_viernau@epam.com>
2025-07-18 12:01:42 +0200

    fix(node): Ignore dangling linked dependencies
    `Yarn.kt` lacks support for linked dependencies. A special case for
    linked dependencies is when a node module generates another bundled
    package during install times and refers to it as "linked" dependency.
    As that generation might not happen in some cases, the path to the
    directory where the linked dependency should reside is a dangling
    reference. Note that references to "linked" dependencies in the
    output of `yarn list` have a relative path to the module directory
    instead of the version and/or alias, for example [1].
    
    Simply ignore such dangling reference to avoid that the entire analysis
    fails running into a `NoSuchElementException` in the
    `Map.getValue()` call within `resolveVersions()`. While this adds some
    fault tolerance, it does not implement general support for linked
    dependencies.
    
    Note: In the case of the function test, the package to be generated are
          Rust bindings.
    
    Fixes #10618.
    
    [1]: @vector-im/matrix-wysiwyg-wasm@link:../.cache/yarn/v6/npm-@vector-im-matrix-wysiwyg-2.38.5-fac9e56abdea0ce9d9ce0f3bc67f1de02ca637a7-integrity/node_modules/bindings/wysiwyg-wasm
    
    Signed-off-by: Frank Viernau <frank_viernau@epam.com>

--- plugins/package-managers/node/src/test/kotlin/NodePackageManagerDetectionTest.kt
+++ plugins/package-managers/node/src/test/kotlin/NodePackageManagerDetectionTest.kt
@@ -243,6 +243,7 @@ class NodePackageManagerDetectionTest : WordSpec({
             filteredFiles.map { it.relativeTo(projectDir).invariantSeparatorsPath } should containExactlyInAnyOrder(
                 "yarn/alias-use-for-transitive-deps/package.json",
                 "yarn/babel/package.json",
+                "yarn/dangling-linked-references/package.json",
                 "yarn/invalid-package-json/package.json",
                 "yarn/project-with-lockfile/package.json",
                 "yarn/workspaces/package.json"
