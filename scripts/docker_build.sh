6423c50344f39ddb229826357c1e5ce614fa8300
Sebastian Schuberth <sebastian@doubleopen.org>
2025-08-29 18:53:14 +0200

    refactor(docker): Externalize versions to an `.env` file
    Dotenv [1] makes it easier to share / handle environment variable
    key-value pairs across tools.
    
    [1]: https://www.dotenv.org/
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -26,7 +26,7 @@ DOCKER_IMAGE_ROOT="${DOCKER_IMAGE_ROOT:-ghcr.io/oss-review-toolkit}"
 echo "Setting ORT_VERSION to $ORT_VERSION."
 
 # shellcheck disable=SC2046
-export $(sed "s/^ARG //" "$GIT_ROOT/docker/versions.dockerfile" | xargs)
+export $(cat "$GIT_ROOT/.env.versions" | xargs)
 
 # ---------------------------
 # image_build function
