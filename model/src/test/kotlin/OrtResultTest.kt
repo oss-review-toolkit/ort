9e4490aa635cb0867f7079f3a6ab459b6b52b033
Jens Keim <jens.keim@forvia.com>
2025-09-16 12:45:20 +0200

    fix(OrtResultTest): No more blank revisions
    Signed-off-by: Jens Keim <jens.keim@forvia.com>

--- model/src/test/kotlin/OrtResultTest.kt
+++ model/src/test/kotlin/OrtResultTest.kt
@@ -163,7 +163,7 @@ class OrtResultTest : WordSpec({
     "getDefinitionFilePathRelativeToAnalyzerRoot()" should {
         "use the correct vcs" {
             val vcs = VcsInfo(type = VcsType.GIT, url = "https://example.com/git", revision = "")
-            val provenance = RepositoryProvenance(vcsInfo = vcs, resolvedRevision = vcs.revision)
+            val provenance = RepositoryProvenance(vcsInfo = vcs, resolvedRevision = HashAlgorithm.SHA1.emptyValue)
             val nestedVcs1 = VcsInfo(type = VcsType.GIT, url = "https://example.com/git1", revision = "")
             val nestedVcs2 = VcsInfo(type = VcsType.GIT, url = "https://example.com/git2", revision = "")
             val project1 = Project.EMPTY.copy(
@@ -204,7 +204,7 @@ class OrtResultTest : WordSpec({
 
         "fail if no vcs matches" {
             val vcs = VcsInfo(type = VcsType.GIT, url = "https://example.com/git", revision = "")
-            val provenance = RepositoryProvenance(vcsInfo = vcs, resolvedRevision = vcs.revision)
+            val provenance = RepositoryProvenance(vcsInfo = vcs, resolvedRevision = HashAlgorithm.SHA1.emptyValue)
             val nestedVcs1 = VcsInfo(type = VcsType.GIT, url = "https://example.com/git1", revision = "")
             val nestedVcs2 = VcsInfo(type = VcsType.GIT, url = "https://example.com/git2", revision = "")
             val project = Project.EMPTY.copy(
