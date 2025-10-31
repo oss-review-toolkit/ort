a19e868097f5d12695513c32fb3718e304f525b6
klw1imb <wolfgang.klenk2@bosch.com>
2025-10-31 09:17:23 +0100

    fix(fossid): Ignore SocketTimeoutException on deletion of archive
    If configured, the FossID plugin will delete uploaded content
    after scan completion. Under some circumstances, this might take
    longer than one minute, and there actually is no need to wait
    until the deletion is finished. If the deletion takes more
    than one minute, then just ignore the SocketTimeoutException
    and continue the execution of the plugin.
    
    Signed-off-by: klw1imb <wolfgang.klenk2@bosch.com>

--- plugins/scanners/fossid/src/main/kotlin/events/UploadArchiveHandler.kt
+++ plugins/scanners/fossid/src/main/kotlin/events/UploadArchiveHandler.kt
@@ -20,6 +20,7 @@
 package org.ossreviewtoolkit.plugins.scanners.fossid.events
 
 import java.io.File
+import java.net.SocketTimeoutException
 
 import kotlin.io.path.createTempFile
 
@@ -131,8 +132,13 @@ class UploadArchiveHandler(
     override suspend fun afterCheckScan(scanCode: String) {
         if (config.deleteUploadedArchiveAfterScan) {
             logger.info { "Deleting uploaded archive for scan '$scanCode'." }
-            service.removeUploadedContent(config.user.value, config.apiKey.value, scanCode)
-                .checkResponse("remove previously uploaded content 2", false)
+
+            try {
+                service.removeUploadedContent(config.user.value, config.apiKey.value, scanCode)
+                    .checkResponse("remove previously uploaded content 2", false)
+            } catch (e: SocketTimeoutException) {
+                logger.info { "Ignoring timeout while deleting uploaded archive: ${e.message}" }
+            }
         }
     }
 
