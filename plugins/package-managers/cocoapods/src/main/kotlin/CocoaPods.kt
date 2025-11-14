e09429d9debaed89556c8f66a2a568410396e6fd
Frank Viernau <frank.viernau@gmail.com>
2025-11-14 10:55:37 +0100

    refactor(cocoapods): Clear the podspec cache upfront
    Simplify an upcoming change.
    
    Signed-off-by: Frank Viernau <frank.viernau@gmail.com>

--- plugins/package-managers/cocoapods/src/main/kotlin/CocoaPods.kt
+++ plugins/package-managers/cocoapods/src/main/kotlin/CocoaPods.kt
@@ -97,15 +97,7 @@ class CocoaPods(override val descriptor: PluginDescriptor = CocoaPodsFactory.des
             CocoaPodsCommand.run("repo", "add-cdn", "trunk", "https://cdn.cocoapods.org", "--allow-root")
                 .requireSuccess()
 
-            try {
-                resolveDependenciesInternal(analysisRoot, definitionFile)
-            } finally {
-                // The cache entries are not re-usable across definition files because the keys do not contain the
-                // dependency version. If non-default Specs repositories were supported, then these would also need to
-                // be part of the key. As that's more complicated and not giving much performance prefer the more memory
-                // consumption friendly option of clearing the cache.
-                dependencyHandler.clearPodspecCache()
-            }
+            resolveDependenciesInternal(analysisRoot, definitionFile)
         }
 
     private fun resolveDependenciesInternal(analysisRoot: File, definitionFile: File): List<ProjectAnalyzerResult> {
@@ -136,6 +128,12 @@ class CocoaPods(override val descriptor: PluginDescriptor = CocoaPodsFactory.des
 
             // Resolve paths of external sources relative to the lockfile.
             val lockfileWithResolvedPaths = lockfileData.withResolvedPaths(lockfile)
+
+            // The cache entries are not re-usable across definition files because the keys do not contain the
+            // dependency version. If non-default Specs repositories were supported, then these would also need to
+            // be part of the key. As that's more complicated and not giving much performance prefer the more memory
+            // consumption friendly option of clearing the cache.
+            dependencyHandler.clearPodspecCache()
 
             // Convert direct dependencies with version constraints to pods with resolved versions.
             val dependencies = lockfileWithResolvedPaths.dependencies.mapNotNull {
