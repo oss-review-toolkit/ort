ba7ac4207f3b7862dfa06268d9697f1a8be77189
Kamil Bielecki <kamil.bielecki@pl.bosch.com>
2025-11-20 15:25:17 +0100

    feat(analyzer): Create simple package list file format analyzer plugin
    This commit creates new analyzer plugin that consumes ort specific
    list of packages in form of definition file(s).
    
    It helps in cases like:
    * Package manager is not supported by ORT yet.
    * Project is using a custom or in-house package manager.
    * Project has no package manager at all.
    * Project contains packages that are not detected by the main package
      manager.
    
    Implements: https://github.com/oss-review-toolkit/ort/issues/10182
    
    Signed-off-by: Kamil Bielecki <kamil.bielecki@pl.bosch.com>

--- model/src/test/kotlin/IdentifierTest.kt
+++ model/src/test/kotlin/IdentifierTest.kt
@@ -20,7 +20,6 @@
 package org.ossreviewtoolkit.model
 
 import io.kotest.assertions.assertSoftly
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.maps.containExactly
