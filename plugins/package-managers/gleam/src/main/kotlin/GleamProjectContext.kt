15cb7470a0854730eb230fe3e3912df0f24d726d
Sebastian Schuberth <sebastian@doubleopen.org>
2025-12-15 19:15:34 +0100

    chore(gleam): Move `GleamProjectContext` to its own file
    Prefer to make the first top-level class match the file name for
    readability.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

---
+++ plugins/package-managers/gleam/src/main/kotlin/GleamProjectContext.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2025 The ORT Project Copyright Holders <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.plugins.packagemanagers.gleam
+
+import java.io.File
+
+import org.ossreviewtoolkit.model.Project
+
+/**
+ * Context containing all project-level information needed for dependency resolution.
+ */
+internal data class GleamProjectContext(
+    val hexClient: HexApiClient,
+    val project: Project,
+    val analysisRoot: File,
+    val workingDir: File,
+    val gleamToml: GleamToml,
+    val manifest: GleamManifest,
+    val projectDirs: Set<File>
+)
