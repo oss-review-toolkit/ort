8527f7b90274615e0c9f6df855f8a7f311324b16
Thomas Steenbergen <opensource@steenbe.nl>
2026-01-18 14:19:59 +0100

    feat(web-app-template): Show metadata in AboutModal
    Display start time, duration, and environment details (e.g., ORT,
    Java/JDK versions, operating system) for each ORT tool in the
    About modal to improve debugging and traceability.
    
    Closes #8701.
    
    Signed-off-by: Thomas Steenbergen <opensource@steenbe.nl>

--- plugins/reporters/web-app-template/src/models/Metadata.js
+++ plugins/reporters/web-app-template/src/models/Metadata.js
@@ -17,19 +17,139 @@
  * License-Filename: LICENSE
  */
 
+import Environment from './Environment';
+
 class Metadata {
+    #advisorEndTime;
+
+    #advisorEnvironment;
+
+    #advisorStartTime;
+
+    #analyzerEndTime;
+
+    #analyzerEnvironment;
+
     #analyzerStartTime;
+
+    #evaluatorEndTime;
+
+    #evaluatorEnvironment;
+
+    #evaluatorStartTime;
+
+    #scannerEndTime;
+
+    #scannerEnvironment;
+
+    #scannerStartTime;
 
     constructor(obj) {
         if (obj instanceof Object) {
+            if (obj.advisor_end_time || obj.advisorEndTime) {
+                this.#advisorEndTime = obj.advisor_end_time || obj.advisorEndTime;
+            }
+
+            if (obj.advisor_environment || obj.advisorEnvironment) {
+                this.#advisorEnvironment = new Environment(
+                    obj.advisor_environment || obj.advisorEnvironment
+                );
+            }
+
+            if (obj.advisor_start_time || obj.advisorStartTime) {
+                this.#advisorStartTime = obj.advisor_start_time || obj.advisorStartTime;
+            }
+
+            if (obj.analyzer_end_time || obj.analyzerEndTime) {
+                this.#analyzerEndTime = obj.analyzer_end_time || obj.analyzerEndTime;
+            }
+
+            if (obj.analyzer_environment || obj.analyzerEnvironment) {
+                this.#analyzerEnvironment = new Environment(
+                    obj.analyzer_environment || obj.analyzerEnvironment
+                );
+            }
+
             if (obj.analyzer_start_time || obj.analyzerStartTime) {
                 this.#analyzerStartTime = obj.analyzer_start_time || obj.analyzerStartTime;
+            }
+
+            if (obj.evaluator_end_time || obj.evaluatorEndTime) {
+                this.#evaluatorEndTime = obj.evaluator_end_time || obj.evaluatorEndTime;
+            }
+
+            if (obj.evaluator_environment || obj.evaluatorEnvironment) {
+                this.#evaluatorEnvironment = new Environment(
+                    obj.evaluator_environment || obj.evaluatorEnvironment
+                );
+            }
+
+            if (obj.evaluator_start_time || obj.evaluatorStartTime) {
+                this.#evaluatorStartTime = obj.evaluator_start_time || obj.evaluatorStartTime;
+            }
+
+            if (obj.scanner_end_time || obj.scannerEndTime) {
+                this.#scannerEndTime = obj.scanner_end_time || obj.scannerEndTime;
+            }
+
+            if (obj.scanner_environment || obj.scannerEnvironment) {
+                this.#scannerEnvironment = new Environment(
+                    obj.scanner_environment || obj.scannerEnvironment
+                );
+            }
+
+            if (obj.scanner_start_time || obj.scannerStartTime) {
+                this.#scannerStartTime = obj.scanner_start_time || obj.scannerStartTime;
             }
         }
     }
 
+    get advisorEndTime() {
+        return this.#advisorEndTime;
+    }
+
+    get advisorStartTime() {
+        return this.#advisorStartTime;
+    }
+
+    get advisorEnvironment() {
+        return this.#advisorEnvironment;
+    }
+
+    get analyzerEndTime() {
+        return this.#analyzerEndTime;
+    }
+
+    get analyzerEnvironment() {
+        return this.#analyzerEnvironment;
+    }
+
     get analyzerStartTime() {
         return this.#analyzerStartTime;
+    }
+
+    get scannerEndTime() {
+        return this.#scannerEndTime;
+    }
+
+    get scannerEnvironment() {
+        return this.#scannerEnvironment;
+    }
+
+    get scannerStartTime() {
+        return this.#scannerStartTime;
+    }
+
+    get evaluatorEndTime() {
+        return this.#evaluatorEndTime;
+    }
+
+    get evaluatorEnvironment() {
+        return this.#evaluatorEnvironment;
+    }
+
+    get evaluatorStartTime() {
+        return this.#evaluatorStartTime;
     }
 }
 
