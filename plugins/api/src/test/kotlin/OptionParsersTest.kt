53466519fdbbd7753e80070149b9703c8a6b0f65
Martin Nonnenmacher <martin.nonnenmacher@doubleopen.org>
2026-01-19 13:48:54 +0100

    test(plugins): Add tests for all plugin option parsing functions
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@doubleopen.org>

--- plugins/api/src/test/kotlin/OptionParsersTest.kt
+++ plugins/api/src/test/kotlin/OptionParsersTest.kt
@@ -19,59 +19,409 @@
 
 package org.ossreviewtoolkit.plugins.api
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.collections.beEmpty
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 
-private val testDescriptor = PluginDescriptor(
-    id = "test",
-    displayName = "Test",
-    description = "Test plugin",
-    options = listOf(
-        PluginOption(
-            name = "stringList",
-            description = "A string list option",
-            type = PluginOptionType.STRING_LIST,
-            defaultValue = "",
-            aliases = emptyList(),
-            isNullable = false,
-            isRequired = false
-        )
-    )
-)
-
-private val testFactory = object : PluginFactory<Plugin> {
-    override val descriptor = testDescriptor
-
-    override fun create(config: PluginConfig) =
-        object : Plugin {
-            override val descriptor = testDescriptor
-        }
-}
-
 class OptionParsersTest : WordSpec({
+    "parseBooleanOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "booleanOption",
+                description = "A boolean option.",
+                type = PluginOptionType.BOOLEAN,
+                defaultValue = "false",
+                aliases = emptyList(),
+                isNullable = false,
+                isRequired = false
+            )
+        )
+
+        "return the correct boolean value from the options" {
+            factory.parseBooleanOption(
+                "booleanOption",
+                PluginConfig(options = mapOf("booleanOption" to "true"))
+            ) shouldBe true
+        }
+
+        "return the default value if the option is not set" {
+            factory.parseBooleanOption("booleanOption", PluginConfig.EMPTY) shouldBe false
+        }
+
+        "throw an exception for an invalid boolean value" {
+            shouldThrow<IllegalArgumentException> {
+                factory.parseBooleanOption(
+                    "booleanOption",
+                    PluginConfig(options = mapOf("booleanOption" to "notABoolean"))
+                )
+            }
+        }
+    }
+
+    "parseNullableBooleanOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "nullableBooleanOption",
+                description = "A nullable boolean option.",
+                type = PluginOptionType.BOOLEAN,
+                defaultValue = null,
+                aliases = emptyList(),
+                isNullable = true,
+                isRequired = false
+            )
+        )
+
+        "return the correct boolean value from the options" {
+            factory.parseNullableBooleanOption(
+                "nullableBooleanOption",
+                PluginConfig(options = mapOf("nullableBooleanOption" to "true"))
+            ) shouldBe true
+        }
+
+        "return null if the option is not set" {
+            factory.parseNullableBooleanOption("nullableBooleanOption", PluginConfig.EMPTY) shouldBe null
+        }
+
+        "throw an exception for an invalid boolean value" {
+            shouldThrow<IllegalArgumentException> {
+                factory.parseNullableBooleanOption(
+                    "nullableBooleanOption",
+                    PluginConfig(options = mapOf("nullableBooleanOption" to "notABoolean"))
+                )
+            }
+        }
+    }
+
+    "parseIntegerOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "integerOption",
+                description = "An integer option.",
+                type = PluginOptionType.INTEGER,
+                defaultValue = "42",
+                aliases = emptyList(),
+                isNullable = false,
+                isRequired = false
+            )
+        )
+
+        "return the correct integer value from the options" {
+            factory.parseIntegerOption(
+                "integerOption",
+                PluginConfig(options = mapOf("integerOption" to "100"))
+            ) shouldBe 100
+        }
+
+        "return the default value if the option is not set" {
+            factory.parseIntegerOption("integerOption", PluginConfig.EMPTY) shouldBe 42
+        }
+
+        "throw an exception for an invalid integer value" {
+            shouldThrow<NumberFormatException> {
+                factory.parseIntegerOption(
+                    "integerOption",
+                    PluginConfig(options = mapOf("integerOption" to "notAnInteger"))
+                )
+            }
+        }
+    }
+
+    "parseNullableIntegerOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "nullableIntegerOption",
+                description = "A nullable integer option.",
+                type = PluginOptionType.INTEGER,
+                defaultValue = null,
+                aliases = emptyList(),
+                isNullable = true,
+                isRequired = false
+            )
+        )
+
+        "return the correct integer value from the options" {
+            factory.parseNullableIntegerOption(
+                "nullableIntegerOption",
+                PluginConfig(options = mapOf("nullableIntegerOption" to "100"))
+            ) shouldBe 100
+        }
+
+        "return null if the option is not set" {
+            factory.parseNullableIntegerOption("nullableIntegerOption", PluginConfig.EMPTY) shouldBe null
+        }
+
+        "throw an exception for an invalid integer value" {
+            shouldThrow<NumberFormatException> {
+                factory.parseNullableIntegerOption(
+                    "nullableIntegerOption",
+                    PluginConfig(options = mapOf("nullableIntegerOption" to "notAnInteger"))
+                )
+            }
+        }
+    }
+
+    "parseLongOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "longOption",
+                description = "A long option.",
+                type = PluginOptionType.LONG,
+                defaultValue = "42000000000",
+                aliases = emptyList(),
+                isNullable = false,
+                isRequired = false
+            )
+        )
+
+        "return the correct long value from the options" {
+            factory.parseLongOption(
+                "longOption",
+                PluginConfig(options = mapOf("longOption" to "100000000000"))
+            ) shouldBe 100000000000L
+        }
+
+        "return the default value if the option is not set" {
+            factory.parseLongOption("longOption", PluginConfig.EMPTY) shouldBe 42000000000L
+        }
+
+        "throw an exception for an invalid long value" {
+            shouldThrow<NumberFormatException> {
+                factory.parseLongOption(
+                    "longOption",
+                    PluginConfig(options = mapOf("longOption" to "notALong"))
+                )
+            }
+        }
+    }
+
+    "parseNullableLongOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "nullableLongOption",
+                description = "A nullable long option.",
+                type = PluginOptionType.LONG,
+                defaultValue = null,
+                aliases = emptyList(),
+                isNullable = true,
+                isRequired = false
+            )
+        )
+
+        "return the correct long value from the options" {
+            factory.parseNullableLongOption(
+                "nullableLongOption",
+                PluginConfig(options = mapOf("nullableLongOption" to "100000000000"))
+            ) shouldBe 100000000000L
+        }
+
+        "return null if the option is not set" {
+            factory.parseNullableLongOption("nullableLongOption", PluginConfig.EMPTY) shouldBe null
+        }
+
+        "throw an exception for an invalid long value" {
+            shouldThrow<NumberFormatException> {
+                factory.parseNullableLongOption(
+                    "nullableLongOption",
+                    PluginConfig(options = mapOf("nullableLongOption" to "notALong"))
+                )
+            }
+        }
+    }
+
+    "parseSecretOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "secretOption",
+                description = "A secret option.",
+                type = PluginOptionType.SECRET,
+                defaultValue = "defaultSecret",
+                aliases = emptyList(),
+                isNullable = false,
+                isRequired = false
+            )
+        )
+
+        "return the correct secret value from the options" {
+            factory.parseSecretOption(
+                "secretOption",
+                PluginConfig(secrets = mapOf("secretOption" to "mySecretValue"))
+            ) shouldBe Secret("mySecretValue")
+        }
+
+        "return the default value if the option is not set" {
+            factory.parseSecretOption("secretOption", PluginConfig.EMPTY) shouldBe Secret("defaultSecret")
+        }
+    }
+
+    "parseNullableSecretOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "nullableSecretOption",
+                description = "A nullable secret option.",
+                type = PluginOptionType.SECRET,
+                defaultValue = null,
+                aliases = emptyList(),
+                isNullable = true,
+                isRequired = false
+            )
+        )
+
+        "return the correct secret value from the options" {
+            factory.parseNullableSecretOption(
+                "nullableSecretOption",
+                PluginConfig(secrets = mapOf("nullableSecretOption" to "mySecretValue"))
+            ) shouldBe Secret("mySecretValue")
+        }
+
+        "return null if the option is not set" {
+            factory.parseNullableSecretOption("nullableSecretOption", PluginConfig.EMPTY) shouldBe null
+        }
+    }
+
+    "parseStringOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "stringOption",
+                description = "A string option.",
+                type = PluginOptionType.STRING,
+                defaultValue = "defaultString",
+                aliases = emptyList(),
+                isNullable = false,
+                isRequired = false
+            )
+        )
+
+        "return the correct string value from the options" {
+            factory.parseStringOption(
+                "stringOption",
+                PluginConfig(options = mapOf("stringOption" to "customString"))
+            ) shouldBe "customString"
+        }
+
+        "return the default value if the option is not set" {
+            factory.parseStringOption("stringOption", PluginConfig.EMPTY) shouldBe "defaultString"
+        }
+    }
+
+    "parseNullableStringOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "nullableStringOption",
+                description = "A nullable string option.",
+                type = PluginOptionType.STRING,
+                defaultValue = null,
+                aliases = emptyList(),
+                isNullable = true,
+                isRequired = false
+            )
+        )
+
+        "return the correct string value from the options" {
+            factory.parseNullableStringOption(
+                "nullableStringOption",
+                PluginConfig(options = mapOf("nullableStringOption" to "customString"))
+            ) shouldBe "customString"
+        }
+
+        "return null if the option is not set" {
+            factory.parseNullableStringOption("nullableStringOption", PluginConfig.EMPTY) shouldBe null
+        }
+    }
+
     "parseStringListOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "stringListOption",
+                description = "A string list option.",
+                type = PluginOptionType.STRING_LIST,
+                defaultValue = "a,b,c",
+                aliases = emptyList(),
+                isNullable = true,
+                isRequired = false
+            )
+        )
+
+        "return the correct string list from the options" {
+            factory.parseStringListOption(
+                "stringListOption",
+                PluginConfig(options = mapOf("stringListOption" to "x,y,z"))
+            ) shouldBe listOf("x", "y", "z")
+        }
+
+        "return the default value if the option is not set" {
+            factory.parseStringListOption("stringListOption", PluginConfig.EMPTY) shouldBe listOf("a", "b", "c")
+        }
+
+        "trim values and filter out empty strings" {
+            factory.parseStringListOption(
+                "stringListOption",
+                PluginConfig(options = mapOf("stringListOption" to " a , , b , c , "))
+            ) shouldBe listOf("a", "b", "c")
+        }
+
         "return an empty list for an empty default value" {
-            val result = testFactory.parseStringListOption("stringList", PluginConfig.EMPTY)
+            createFactory(
+                PluginOption(
+                    name = "stringListOption",
+                    description = "A string list option.",
+                    type = PluginOptionType.STRING_LIST,
+                    defaultValue = "",
+                    aliases = emptyList(),
+                    isNullable = false,
+                    isRequired = true
+                )
+            ).parseStringListOption("stringListOption", PluginConfig.EMPTY) should beEmpty()
+        }
+    }
 
-            result should beEmpty()
+    "parseNullableStringListOption()" should {
+        val factory = createFactory(
+            PluginOption(
+                name = "nullableStringListOption",
+                description = "A nullable string list option.",
+                type = PluginOptionType.STRING_LIST,
+                defaultValue = null,
+                aliases = emptyList(),
+                isNullable = true,
+                isRequired = false
+            )
+        )
+
+        "return the correct string list from the options" {
+            factory.parseNullableStringListOption(
+                "nullableStringListOption",
+                PluginConfig(options = mapOf("nullableStringListOption" to "x,y,z"))
+            ) shouldBe listOf("x", "y", "z")
         }
 
-        "return a list with trimmed values" {
-            val config = PluginConfig(options = mapOf("stringList" to " a , b , c "))
-
-            val result = testFactory.parseStringListOption("stringList", config)
-
-            result shouldBe listOf("a", "b", "c")
+        "return null if the option is not set" {
+            factory.parseNullableStringListOption("nullableStringListOption", PluginConfig.EMPTY) shouldBe null
         }
 
-        "filter out empty strings from the result" {
-            val config = PluginConfig(options = mapOf("stringList" to "a,,b, ,c"))
-
-            val result = testFactory.parseStringListOption("stringList", config)
-
-            result shouldBe listOf("a", "b", "c")
+        "trim values and filter out empty strings" {
+            factory.parseNullableStringListOption(
+                "nullableStringListOption",
+                PluginConfig(options = mapOf("nullableStringListOption" to " a , , b , c , "))
+            ) shouldBe listOf("a", "b", "c")
         }
     }
 })
+
+private fun createFactory(option: PluginOption) =
+    object : PluginFactory<Plugin> {
+        private val pluginDescriptor = PluginDescriptor(
+            id = "test",
+            displayName = "Test",
+            description = "Test plugin",
+            options = listOf(option)
+        )
+
+        override val descriptor = pluginDescriptor
+
+        override fun create(config: PluginConfig) =
+            object : Plugin {
+                override val descriptor = pluginDescriptor
+            }
+    }
