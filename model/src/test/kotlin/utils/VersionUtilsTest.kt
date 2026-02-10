afa9bc727b73e53910cb33aef30afcde5fcba254
Sebastian Schuberth <sebastian@doubleopen.org>
2026-02-10 11:18:56 +0100

    refactor(model): Simplify the code to get version ranges
    The check against `IVY_VERSION_RANGE_INDICATORS` (which did not actually
    contain only *Ivy* indicators) was only needed because the Semver4j
    library by default parses single versions into ranges (like "2" becomes
    ">= 2.0.0, < 3.0.0"), which made it unusable for checking for version
    ranges by simply testing whether range parsing fails.
    
    However, that behavior came from the `XRangeProcessor` being applied by
    default, which is a syntax that ORT never officially supported for
    package curations / configurations. Instead, ORT only ever supported Ivy
    version ranges officially.
    
    So simplify the code by explicitly only using `IvyProcessor` and
    working on `String`s internally.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

---
+++ model/src/test/kotlin/utils/VersionUtilsTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2026 The ORT Project Copyright Holders <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.model.utils
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.beEmpty
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+
+import org.semver4j.range.Range
+
+class VersionUtilsTest : WordSpec({
+    "getIvyVersionRanges()" should {
+        "return an empty range list for pathological cases" {
+            "".getIvyVersionRanges().get() should beEmpty()
+            " ".getIvyVersionRanges().get() should beEmpty()
+            "foo".getIvyVersionRanges().get() should beEmpty()
+            "null".getIvyVersionRanges().get() should beEmpty()
+        }
+
+        "return an empty range list for invalid ranges" {
+            "[a,b]".getIvyVersionRanges().get() should beEmpty()
+            "[1,2))".getIvyVersionRanges().get() should beEmpty()
+            "[1.2.3,4.5.6".getIvyVersionRanges().get() should beEmpty()
+            "[1.2.3.4,5.6.7.8]".getIvyVersionRanges().get() should beEmpty()
+        }
+
+        "return an empty list for single versions" {
+            "2".getIvyVersionRanges().get() shouldBe listOf()
+            "2.0".getIvyVersionRanges().get() shouldBe listOf()
+            "2.0.0".getIvyVersionRanges().get() shouldBe listOf()
+        }
+
+        "return the correct range list for Ivy version ranges" {
+            "[1.2.3,4.5.6]".getIvyVersionRanges().get() shouldBe listOf(
+                listOf(Range("1.2.3", Range.RangeOperator.GTE), Range("4.5.6", Range.RangeOperator.LTE))
+            )
+
+            "[3.3.0,)".getIvyVersionRanges().get() shouldBe listOf(
+                listOf(Range("3.3.0", Range.RangeOperator.GTE))
+            )
+        }
+    }
+})
