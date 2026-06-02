33f979538856cef6c4bec20d8e60987cf37a6e29
Sebastian Schuberth <sebastian@doubleopen.io>
2026-06-02 15:31:56 +0200

    fix(ort-utils): Set the body size to -1 when not sending any body in tests
    This is part of a `SocketTimeoutException` fix when running with Java 25.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.io>

--- utils/ort/src/funTest/kotlin/storage/S3FileStorageFunTest.kt
+++ utils/ort/src/funTest/kotlin/storage/S3FileStorageFunTest.kt
@@ -60,7 +60,7 @@ class S3FileStorageFunTest : WordSpec() {
                 "PUT" -> {
                     val key = exchange.requestURI.toString().removePrefix("/$bucket/")
                     requests[key] = exchange.requestBody.reader().use { it.readText() }.split('\n')[1].trimEnd()
-                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0)
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, -1)
                 }
 
                 "GET" -> {
