f3266f384eb388830e8be61b48e7c4af4c9a6a95
Sebastian Schuberth <sebastian@doubleopen.io>
2026-06-03 21:53:26 +0200

    docs(gradle): Add links to configuration roles for reference
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.io>

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -24,6 +24,7 @@ plugins {
     id("ort-application-conventions")
 }
 
+// See https://docs.gradle.org/current/userguide/declaring_configurations.html#sec:configuration-flags-roles.
 configurations.dependencyScope("pluginClasspath")
 configurations["runtimeOnly"].extendsFrom(configurations["pluginClasspath"])
 
