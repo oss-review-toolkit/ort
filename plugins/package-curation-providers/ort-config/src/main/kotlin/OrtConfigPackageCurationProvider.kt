4632b4cba6c4f789f5e65ffaa9ae62b6ee4c6417
Martin Nonnenmacher <martin.nonnenmacher@doubleopen.io>
2026-06-03 16:31:08 +0200

    feat(ort-config): Extract a generic `GitPackageCurationProvider`
    Extract a generic `GitPackageCurationProvider` from the
    `OrtConfigPackageCurationProvider` and make the latter inherit from the
    new one. The new provider allows to use any Git repository as a source
    for package curation files.
    
    For now, the provider requires the same file layout as the
    `OrtConfigPackageCurationProvider`, this could be made configurable if
    required later on.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@doubleopen.io>

--- plugins/package-curation-providers/ort-config/src/main/kotlin/OrtConfigPackageCurationProvider.kt
+++ plugins/package-curation-providers/ort-config/src/main/kotlin/OrtConfigPackageCurationProvider.kt
@@ -19,29 +19,14 @@
 
 package org.ossreviewtoolkit.plugins.packagecurationproviders.ortconfig
 
-import java.io.File
-import java.io.IOException
-
-import org.apache.logging.log4j.kotlin.logger
-
-import org.ossreviewtoolkit.downloader.VersionControlSystem
-import org.ossreviewtoolkit.model.Identifier
-import org.ossreviewtoolkit.model.Package
 import org.ossreviewtoolkit.model.PackageCuration
-import org.ossreviewtoolkit.model.VcsInfo
-import org.ossreviewtoolkit.model.VcsType
-import org.ossreviewtoolkit.model.readValue
 import org.ossreviewtoolkit.plugins.api.OrtPlugin
 import org.ossreviewtoolkit.plugins.api.PluginDescriptor
 import org.ossreviewtoolkit.plugins.packagecurationproviders.api.PackageCurationProvider
 import org.ossreviewtoolkit.plugins.packagecurationproviders.api.PackageCurationProviderFactory
-import org.ossreviewtoolkit.utils.common.div
-import org.ossreviewtoolkit.utils.common.encodeOr
-import org.ossreviewtoolkit.utils.common.safeMkdirs
-import org.ossreviewtoolkit.utils.ort.ortDataDirectory
 
 private const val ORT_CONFIG_REPOSITORY_BRANCH = "main"
-private const val ORT_CONFIG_REPOSITORY_URL = "https://github.com/oss-review-toolkit/ort-config.git"
+internal const val ORT_CONFIG_REPOSITORY_URL = "https://github.com/oss-review-toolkit/ort-config.git"
 
 /**
  * A [PackageCurationProvider] that provides [PackageCuration]s loaded from the
@@ -53,55 +38,13 @@ private const val ORT_CONFIG_REPOSITORY_URL = "https://github.com/oss-review-too
     summary = "A package curation provider that loads package curations from the ort-config repository.",
     factory = PackageCurationProviderFactory::class
 )
-open class OrtConfigPackageCurationProvider(
-    override val descriptor: PluginDescriptor = OrtConfigPackageCurationProviderFactory.descriptor
-) : PackageCurationProvider {
-    private val curationsDir by lazy {
-        ortDataDirectory.resolve("ort-config").also {
-            updateOrtConfig(it)
-        }
-    }
-
-    override fun getCurationsFor(packages: Collection<Package>): Set<PackageCuration> =
-        packages.flatMapTo(mutableSetOf()) { pkg -> getCurationsFor(pkg.id) }
-
-    private fun getCurationsFor(pkgId: Identifier): List<PackageCuration> {
-        // The ORT config repository follows path layout conventions, so curations can be looked up directly.
-        val packageCurationsFile = curationsDir / "curations" / pkgId.toCurationPath()
-
-        // Also consider curations for all packages in a namespace.
-        val namespaceCurationsFile = packageCurationsFile.resolveSibling("_.yml")
-
-        // Return namespace-level curations before package-level curations to allow overriding the former.
-        return listOf(namespaceCurationsFile, packageCurationsFile).filter { it.isFile }.flatMap { file ->
-            runCatching {
-                file.readValue<List<PackageCuration>>().filter { it.isApplicable(pkgId) }
-            }.getOrElse {
-                throw IOException("Failed parsing package curation from '${file.absolutePath}'.", it)
-            }
-        }
-    }
-}
-
-/**
- * The path must be aligned with the
- * [conventions for the ort-config repository](https://github.com/oss-review-toolkit/ort-config#curations).
- */
-fun Identifier.toCurationPath() = "${type.encodeOr("_")}/${namespace.encodeOr("_")}/${name.encodeOr("_")}.yml"
-
-private fun updateOrtConfig(dir: File) {
-    val vcsInfo = VcsInfo.EMPTY.copy(type = VcsType.GIT, url = ORT_CONFIG_REPOSITORY_URL)
-    val vcs = checkNotNull(VersionControlSystem.forType(vcsInfo.type)) {
-        "No applicable VersionControlSystem implementation found for ${vcsInfo.type}."
-    }
-
-    dir.safeMkdirs()
-
-    vcs.apply {
-        val workingTree = initWorkingTree(dir, vcsInfo)
-        val revision = updateWorkingTree(workingTree, ORT_CONFIG_REPOSITORY_BRANCH).getOrThrow()
-        logger.info {
-            "Successfully cloned $revision from $ORT_CONFIG_REPOSITORY_URL."
-        }
-    }
-}
+class OrtConfigPackageCurationProvider(
+    descriptor: PluginDescriptor = OrtConfigPackageCurationProviderFactory.descriptor
+) : GitPackageCurationProvider(
+    descriptor = descriptor,
+    config = GitPackageCurationProviderConfig(
+        repositoryUrl = ORT_CONFIG_REPOSITORY_URL,
+        revision = ORT_CONFIG_REPOSITORY_BRANCH,
+        path = "curations"
+    )
+)
