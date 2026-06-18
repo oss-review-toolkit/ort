f8fa3705b1b6deea1fe8852704b082e786bfd565
Sebastian Schuberth <sebastian@doubleopen.io>
2026-06-18 13:32:31 +0200

    test(scanner): Assert copyrights for ScanCode and Provenant
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.io>

--- plugins/scanners/scancode/src/funTest/kotlin/ScanCodeFunTest.kt
+++ plugins/scanners/scancode/src/funTest/kotlin/ScanCodeFunTest.kt
@@ -21,6 +21,7 @@ package org.ossreviewtoolkit.plugins.scanners.scancode
 
 import io.kotest.core.annotation.Tags
 
+import org.ossreviewtoolkit.model.CopyrightFinding
 import org.ossreviewtoolkit.model.LicenseFinding
 import org.ossreviewtoolkit.model.TextLocation
 import org.ossreviewtoolkit.scanner.AbstractPathScannerWrapperFunTest
@@ -37,5 +38,13 @@ class ScanCodeFunTest : AbstractPathScannerWrapperFunTest() {
         LicenseFinding("Apache-2.0", TextLocation("COPYING", 1, 201), 99.37f),
         LicenseFinding("Apache-2.0", TextLocation("LICENCE", 1, 201), 99.94f),
         LicenseFinding("Apache-2.0", TextLocation("LICENSE", 1, 201), 99.94f)
+    )
+
+    // TODO: This demonstrates https://github.com/aboutcode-org/scancode-toolkit/issues/5125.
+    override val expectedFileCopyrights = listOf(
+        CopyrightFinding(
+            "Copyright (c) 2007",
+            TextLocation("LICENSE", 189)
+        )
     )
 }
