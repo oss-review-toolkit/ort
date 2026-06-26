9e3d33e186c598afd7a3e3915e33270ff4d1070b
Frank Viernau <frank.viernau@gmail.com>
2026-06-26 13:24:37 +0200

    feat(model)!: Support multiple license fact providers of the same type
    This flexibility can be used to organize the upcoming
    identifier-specific license text curations in two directories, a global
    e.g in an 'ort-config' repository, and a local one e.g. in the source
    code of the project in order to curate license texts for project IDs.
    Apart from this makes the way providers in `OrtConfiguration` are
    specified consistent.
    
    While at it, turn a `let` into an `also` to emphasize that the return
    value does not matter here.
    
    Breaking change: This change is not backwards compatible with regards to
                     deserializing ORT's `config.yml`. The changes in
                     `reference.yml` illustrate the needed alignments.
    
    Signed-off-by: Frank Viernau <frank.viernau@gmail.com>

--- plugins/license-fact-providers/api/src/main/kotlin/LicenseFactProviderFactory.kt
+++ plugins/license-fact-providers/api/src/main/kotlin/LicenseFactProviderFactory.kt
@@ -19,7 +19,12 @@
 
 package org.ossreviewtoolkit.plugins.licensefactproviders.api
 
+import org.apache.logging.log4j.kotlin.logger
+
+import org.ossreviewtoolkit.model.config.ProviderPluginConfiguration
+import org.ossreviewtoolkit.plugins.api.PluginConfig
 import org.ossreviewtoolkit.plugins.api.PluginFactory
+import org.ossreviewtoolkit.utils.common.getDuplicates
 
 /** A factory for [LicenseFactProvider]s. */
 interface LicenseFactProviderFactory : PluginFactory<LicenseFactProvider> {
@@ -29,5 +34,34 @@ interface LicenseFactProviderFactory : PluginFactory<LicenseFactProvider> {
          * their IDs.
          */
         val ALL by lazy { PluginFactory.getAll<LicenseFactProviderFactory, LicenseFactProvider>() }
+
+        /**
+         * Create a new (identifier, provider instance) tuple for each
+         * [enabled][ProviderPluginConfiguration.enabled] provider configuration in [configurations] ordered
+         * highest priority first. The given [configurations] must be ordered highest-priority first as well.
+         */
+        fun create(configurations: List<ProviderPluginConfiguration>): List<Pair<String, LicenseFactProvider>> =
+            configurations.filter {
+                it.enabled
+            }.mapNotNull {
+                ALL[it.type]?.let { factory ->
+                    it.id to factory.create(PluginConfig(it.options))
+                }.also { factory ->
+                    factory ?: logger.error {
+                        "License fact provider of type '${it.type}' is enabled in configuration but not available " +
+                            "in the classpath."
+                    }
+                }
+            }.apply {
+                require(none { (id, _) -> id.isBlank() }) {
+                    "The configuration contains a license fact provider with a blank ID which is not allowed."
+                }
+
+                val duplicateIds = getDuplicates { (id, _) -> id }.keys
+                require(duplicateIds.isEmpty()) {
+                    "Found multiple license fact providers for the IDs ${duplicateIds.joinToString()}, " +
+                        "which is not allowed. Please configure a unique ID for each license fact provider."
+                }
+            }
     }
 }
