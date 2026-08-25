a07972bc1650db14d3801db89914a09ffeb7034e
Frank Viernau <frank.viernau@gmail.com>
2026-08-25 13:36:55 +0200

    fix(npm): Re-write `ModuleInfo.isProject`
    In some cases the property returns `true` where it should not. In that
    case `Npm.kt` adds references to that module using `PROJECT_DYNAMIC` as
    the linkage type, but it neither adds a package nor a project
    corresponding to that module, so the reference is dangling.
    
    When running [1], like `Npm.kt` does it, all modules which are
    dependencies of the `package.json` in the current working directory get
    placed inside the corresponding `node_modules` directory. In case a
    dependency is in the project's source tree, the corresponding directory
    in that `node_modules` directory is a symlink which points to the
    respective real directory in the project's source tree.
    
    Re-write `ModuleInfo.isProject` to be based on that observation. This
    properly adds the previously missing packages, and the corresponding
    references.
    
    [1]: `npm install --legacy-peer-deps`
    
    Signed-off-by: Frank Viernau <frank.viernau@gmail.com>

--- plugins/package-managers/node/src/main/kotlin/npm/NpmDependencyHandler.kt
+++ plugins/package-managers/node/src/main/kotlin/npm/NpmDependencyHandler.kt
@@ -27,6 +27,7 @@ import org.ossreviewtoolkit.model.Package
 import org.ossreviewtoolkit.model.PackageLinkage
 import org.ossreviewtoolkit.model.utils.DependencyHandler
 import org.ossreviewtoolkit.plugins.packagemanagers.node.ModuleInfoResolver
+import org.ossreviewtoolkit.plugins.packagemanagers.node.NODE_MODULES_DIRNAME
 import org.ossreviewtoolkit.plugins.packagemanagers.node.NodePackageManagerType
 import org.ossreviewtoolkit.plugins.packagemanagers.node.PackageJson
 import org.ossreviewtoolkit.plugins.packagemanagers.node.parsePackage
@@ -73,7 +74,16 @@ internal val ModuleInfo.isInstalled: Boolean
     get() = path != null && File(path).isDirectory
 
 internal val ModuleInfo.isProject: Boolean
-    get() = resolved == null || resolved.startsWith("file:")
+    get(): Boolean {
+        val moduleDir = path?.let { File(it).realFile } ?: return false
+        val baseDir = if (name != null && "/" in name) {
+            moduleDir.parentFile.parentFile
+        } else {
+            moduleDir.parentFile
+        }
+
+        return baseDir.name != NODE_MODULES_DIRNAME
+    }
 
 private val ModuleInfo.packageJsonFile: File get() {
     check(isInstalled) { "The module directory '$path' is null or does not exist." }
