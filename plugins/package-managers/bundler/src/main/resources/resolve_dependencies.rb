03b3d30f1ca4d8c44cda3bb0d8fc187a50f6aaae
Sebastian Schuberth <sebastian@doubleopen.io>
2026-09-02 13:53:42 +0200

    docs(bundler): Improve code comments
    Remove obsolete parts, move comments to the right place, and shorten
    them a bit.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.io>

--- plugins/package-managers/bundler/src/main/resources/resolve_dependencies.rb
+++ plugins/package-managers/bundler/src/main/resources/resolve_dependencies.rb
@@ -29,19 +29,17 @@
 require 'bundler'
 require 'yaml'
 
-# Resolve dependencies independently of the Ruby interpreter.
+# Get dependencies independently of the Ruby interpreter.
 Bundler.settings.set_global(:force_ruby_platform, true)
 
 # Resolve all groups including optional ones.
 all_groups = Bundler.definition.dependencies.flat_map(&:groups)
 Bundler.settings.set_global(:with, all_groups)
 
-# This command tries to resolve dependencies that are specified in the Gemfile of the current working directory.
-# Explicitly enable resolution of remote `gem` or `git` dependencies. `path` dependencies are still resolved locally.
 Bundler.ui.silence {
+    # This command resolves dependencies that are specified in the Gemfile of the current working directory.
     Bundler.definition.resolve_remotely!
 
-    # Resolving is triggered lazily, so the below "to_yaml" call might be the place where progress output needs to be
-    # silenced in addition to the above "resolve_remotely" call.
+    # Resolving is triggered lazily, so the below "to_yaml" might create progress output.
     Bundler.definition.specs.map(&:to_yaml).join("\0")
 }
