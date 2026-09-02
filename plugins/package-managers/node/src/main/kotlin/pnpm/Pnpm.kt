f36a89cf048b0f1696d3bbf850550d6433a23e0b
Sebastian Schuberth <sebastian@doubleopen.io>
2026-09-02 18:34:27 +0200

    chore(pnpm): Replace the undocumented `--ignore-pnpmfile` option
    Use the respective config option [1] instead, which is safer with PNPM
    12 as it by default rejects unknown arguments.
    
    [1]: https://pnpm.io/pnpmfile#ignorepnpmfile
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.io>

--- plugins/package-managers/node/src/main/kotlin/pnpm/Pnpm.kt
+++ plugins/package-managers/node/src/main/kotlin/pnpm/Pnpm.kt
@@ -193,7 +193,7 @@ class Pnpm(override val descriptor: PluginDescriptor = PnpmFactory.descriptor) :
     private fun installDependencies(workingDir: File, scopes: Collection<Scope>) {
         val args = listOfNotNull(
             "install",
-            "--ignore-pnpmfile",
+            "--config.ignorePnpmfile=true",
             "--ignore-scripts",
             "--frozen-lockfile", // Use the existing lockfile instead of updating an outdated one.
             "--prod".takeUnless { Scope.DEV_DEPENDENCIES in scopes }
